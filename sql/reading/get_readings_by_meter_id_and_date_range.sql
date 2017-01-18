5a0840f03caa275af91fc9ee94779a3179fae224
Simon Tomlinson <simonbtomlinson@gmail.com>
2017-01-18 13:38:16 -0600

    Clarify a comment about COALESCE

--- sql/reading/get_readings_by_meter_id_and_date_range.sql
+++ sql/reading/get_readings_by_meter_id_and_date_range.sql
@@ -1,8 +1,9 @@
--- Coalesce returns the first non-null value
 SELECT
   meter_id, reading, read_timestamp
 FROM readings
 WHERE meter_id = ${meterID}
   AND read_timestamp BETWEEN
+  -- Coalesce returns the first non-null value, so this defaults the start date to 1970 and end to 50000,
+  -- effectively not restricting them
     COALESCE(${startDate}, DATE '1970-01-01')
     AND COALESCE(${endDate}, DATE '50000-01-01');
