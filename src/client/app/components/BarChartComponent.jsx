a856772346c526348983563589de5f9b47c233af
Simon Tomlinson <simonbtomlinson@gmail.com>
2017-02-07 19:13:53 -0600

    Update LineChartComponent & BarChartComponent to lodash

--- src/client/app/components/BarChartComponent.jsx
+++ src/client/app/components/BarChartComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactHighcharts from 'react-highcharts';
 import axios from 'axios';
+import _ from 'lodash';
 
 export default class BarChartComponent extends React.Component {
 	constructor(props) {
@@ -63,13 +64,7 @@ export default class BarChartComponent extends React.Component {
 	componentWillMount() {
 		axios.get('/api/meters/readings/6')
 			.then(response => {
-				this.setState(prevState => {
-					const seriesCopy = Object.assign({}, prevState.config.series[0]);
-					seriesCopy.data = response.data;
-					return {
-						config: Object.assign({}, prevState.config, { series: [seriesCopy].concat(prevState.config.series.slice(1)) })
-					};
-				});
+				this.setState(prevState => ({ config: _.merge(prevState.config, { series: [{ data: response.data }] }) }));
 			})
 			.catch(error => {
 				console.log(error);
