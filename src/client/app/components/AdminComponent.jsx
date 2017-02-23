ae317feb3cc60f10f466be02ff53acb413011181
robachramowicz <achramowiczrm@beloit.edu>
2017-02-23 15:15:46 -0600

    Added a duumy dropzone to the admin component.
    Eduardo and I wrote a basic dropzone whose only funcionality is to recognize the file being uploaded, next steps are to ensure the file is of the appropriate format and then read in the information.

--- src/client/app/components/AdminComponent.jsx
+++ src/client/app/components/AdminComponent.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 
-export default function AdminComponent() {
-	return (
-		<div>
-			<p>Admin panel</p>
-		</div>
-	);
+
+export default class AdminComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleOnDrop = this.handleOnDrop.bind(this);
+	}
+
+	handleOnDrop(file) {
+		console.log(file);
+	}
+
+	render() {
+		return (
+			<div>
+				<p>Admin panel</p>
+				<button>AddMeter</button>
+				<Dropzone onDrop={this.handleOnDrop}>
+					<div> Add in a CSV file here: </div>
+				</Dropzone>
+			</div>
+		);
+	}
+
+
 }
