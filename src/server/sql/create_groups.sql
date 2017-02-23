b099a85851b9515dc57799aea0c138cbd38b44bc
John DiIorio <johnzdiiorio@gmail.com>
2017-02-23 16:39:22 -0600

    Added MPL v2.0 header to every file in project.

--- src/server/sql/create_groups.sql
+++ src/server/sql/create_groups.sql
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /*
   The groups table contains a row for each group in the database.
   Groups have child groups, which can have child groups, and so on. This forms a Directed Acyclic Graph (DAG).
--- src/server/sql/reading/insert_or_update_reading.sql
+++ src/server/sql/reading/insert_or_update_reading.sql
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 INSERT INTO readings (meter_id, reading, start_timestamp, end_timestamp)
 VALUES (${meterID}, ${reading}, ${startTimestamp}, ${endTimestamp})
 		-- TODO: Deal with conflicts due to overlapping date ranges here.
