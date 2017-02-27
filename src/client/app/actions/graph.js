c961cae06675a7d53deef2fe94aeb83ece938c67
Simon Tomlinson <simonbtomlinson@gmail.com>
2017-02-26 18:00:20 -0600

    Adds license files

--- src/client/app/actions/graph.js
+++ src/client/app/actions/graph.js
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 export const SELECT_METER = 'SELECT_METER';
 export const UNSELECT_METER = 'UNSELECT_METER';
 export const CHANGE_SELECTED_METERS = 'CHANGE_SELECTED_METERS';
