38074abb41f7fbde564c32a0aa10a7e6d64d509d
James McFeeters <jameeters@gmail.com>
2017-02-27 00:32:16 -0600

    added function to `routes/groups.js` to get all groups.

--- src/server/routes/groups.js
+++ src/server/routes/groups.js
@@ -2,3 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const express = require('express');
+const Group = require('../models/Group');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+	try {
+		const rows = await Group.getAll();
+		res.json(rows);
+	} catch (err) {
+		console.error(`Error while preforming GET all groups query: ${err}`); // eslint-disable-line no-console
+	}
+});
