7e972462a05ba9f4eb529cc54ccf462b34b76c56
sandeepacharya464 <acharyas@beloit.edu>
2017-03-30 16:45:19 -0500

    Changed the way it reads time

--- src/server/services/readMetasysData.js
+++ src/server/services/readMetasysData.js
@@ -1,7 +1,7 @@
 const Reading = require('./../models/Reading');
 const readCsv = require('./readCSV');
 const Meter = require('./../models/Meter');
-
+const moment = require('moment');
 /**
  * Reads CSV file passed to input all the Metasys readings into database.
  * @param filePath the filePath to read
@@ -16,31 +16,39 @@ async function readMetasysData(filePath) {
 	const rows = await readCsv(filePath);
 	//meterInformation
 	const meter	= await Meter.getByName(fileName.replace('.csv', ''));
-
+	console.log(meter);
+	let q = 0;
 	for (const row of rows) {
-
+		q = q + 1;
+		if (q % 1000 === 0) {
+			console.log(q);
+		}
+		if (row[0] === '') {
+			continue;
+		}
 		//start timestamp and end timestamp. End timestamp is start timestamp + 1 hour.
-		const start_timestamp = new Date(row[0]);
-		let end_timestamp = new Date(start_timestamp);
-		start_timestamp.setHours(end_timestamp.getHours()-1);
-
+		// 9/25/16 11:30
+		const end_timestamp = moment(row[0], 'MM/DD/YY HH:mm');
+		let start_timestamp = moment(end_timestamp).subtract(30, 'minutes');
 		//meterReading
 		let meterReading = row[3];
-		meterReading = meterReading.replace('kWh', '');
-		meterReading = parseInt(meterReading);
+		meterReading = meterReading.replace(' kW', '');
+		meterReading = Math.round(parseFloat(meterReading));
+
 
 		/* We have hourly trend and monthly trend for Metasys data. We just read hourly trend.
 		 * So, we skip one line as we go through the data
 		 */
-		if ((i % 2 != 0)) {
-			const reading = new Reading(meter.id, meterReading, start_timestamp, end_timestamp);
+//		if ((i % 2 != 0)) {
+			const reading = new Reading(meter.id, meterReading, start_timestamp.toDate(), end_timestamp.toDate());
 			readingArr.push(reading);
-		}
-		i++;
+//		}
+//		i++;
 	}
 	try {
+		console.log("About to insert");
 		//inserting all the data from an array into database and catching error when it occurs.
-		Reading.insertAll(readingArr);
+		Reading.insertAll(readingArr).then(() => console.log("Done"));
 	} catch (err) {
 		console.error(err);
 	}
