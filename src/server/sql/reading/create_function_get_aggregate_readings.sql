da840d98edf91671cae6699f4ba4c12c5f00c8f9
John DiIorio <johnzdiiorio@gmail.com>
2017-04-12 16:18:10 -0500

    Fix overlapping duration and time interval bug in aggregate_readings.
    Add more unit tests for aggregate_readings. Fix Webpack not correctly bundling in production mode.

--- src/server/sql/reading/create_function_get_aggregate_readings.sql
+++ src/server/sql/reading/create_function_get_aggregate_readings.sql
@@ -23,25 +23,23 @@ CREATE OR REPLACE FUNCTION aggregate_readings(
 		INTO real_start_timestamp, real_end_timestamp
 		FROM readings
 		INNER JOIN unnest(meter_ids) specific_meter(id) ON readings.meter_id = specific_meter.id
-		WHERE readings.end_timestamp >= from_timestamp AND readings.start_timestamp <= to_timestamp;
-
+		WHERE (readings.start_timestamp, readings.end_timestamp) OVERLAPS (from_timestamp, to_timestamp);
 		real_duration := least(duration, real_end_timestamp - real_start_timestamp);
 
 		RETURN QUERY
 		SELECT
 			r.meter_id,
 			CAST(
-					SUM(
+					ROUND(SUM(
 							 r.reading / (extract(EPOCH FROM (r.end_timestamp - r.start_timestamp)))
 							 * EXTRACT(epoch FROM (least(r.end_timestamp, (agg.start + real_duration)) - greatest(r.start_timestamp, agg.start)))
-					) AS INTEGER
+					)) AS INTEGER
 			),
 			agg.start,
 			agg.start + real_duration
 		FROM readings r
 		INNER JOIN unnest(meter_ids) specific_meter(id) ON r.meter_id = specific_meter.id
-			INNER JOIN generate_series(real_start_timestamp, real_end_timestamp - real_duration, real_duration) agg(start)
-			--ON r.start_timestamp <= agg.start + real_duration AND r.end_timestamp >= agg.start
+			INNER JOIN generate_series(real_start_timestamp, real_end_timestamp - INTERVAL '1 second', real_duration) agg(start)
 			ON (r.start_timestamp, r.end_timestamp) OVERLAPS (agg.start, agg.start + real_duration)
 		GROUP BY(r.meter_id, agg.start)
 		ORDER BY r.meter_id, agg.start;
--- src/server/test/db/aggregateReadingsTests.js
+++ src/server/test/db/aggregateReadingsTests.js
@@ -40,10 +40,10 @@ mocha.describe('Aggregate Readings', () => {
 
 		const aggregateReadings = await Reading.getAggregateReadings([meter.id], moment.duration(2, 'h'));
 		expect(aggregateReadings[meter.id]).to.have.lengthOf(2);
-		const expectedFirstCompressedRate = reading1.reading + reading2.reading;
-		expect(aggregateReadings[meter.id][0].reading_sum).to.equal(expectedFirstCompressedRate);
-		const expectedSecondCompressedRate = reading3.reading + reading4.reading;
-		expect(aggregateReadings[meter.id][1].reading_sum).to.equal(expectedSecondCompressedRate);
+		const expectedFirstAggregation = Math.floor(reading1.reading + reading2.reading);
+		expect(aggregateReadings[meter.id][0].reading_sum).to.equal(expectedFirstAggregation);
+		const expectedSecondAggregation = Math.floor(reading3.reading + reading4.reading);
+		expect(aggregateReadings[meter.id][1].reading_sum).to.equal(expectedSecondAggregation);
 	}));
 
 	mocha.it('aggregate readings with time interval', () => db.task(async t => {
@@ -57,7 +57,83 @@ mocha.describe('Aggregate Readings', () => {
 		const endTimestamp = startTimestamp.clone().add(2, 'hours');
 		const aggregateReadings = await Reading.getAggregateReadings([meter.id], moment.duration(2, 'h'), startTimestamp, endTimestamp);
 		expect(aggregateReadings[meter.id]).to.have.lengthOf(1);
-		const expectedFirstCompressedRate = (reading1.reading / 2) + reading2.reading + (reading3.reading / 2);
-		expect(aggregateReadings[meter.id][0].reading_sum).to.equal(expectedFirstCompressedRate);
+		const expectedFirstAggregation = Math.floor((reading1.reading * 0.5) + reading2.reading + (reading3.reading * 0.5));
+		expect(aggregateReadings[meter.id][0].reading_sum).to.equal(expectedFirstAggregation);
+	}));
+
+	mocha.it('aggregate readings with overlapping duration and time interval', () => db.task(async t => {
+		const reading1 = new Reading(meter.id, 10, timestamp1, timestamp2);
+		const reading2 = new Reading(meter.id, 100, timestamp2, timestamp3);
+		const reading3 = new Reading(meter.id, 1000, timestamp3, timestamp4);
+		const reading4 = new Reading(meter.id, 400, timestamp4, timestamp5);
+		await Reading.insertAll([reading1, reading2, reading3, reading4], t);
+
+		const startTimestamp = timestamp1.clone().add(15, 'minutes');
+		const endTimestamp = startTimestamp.clone().add(50, 'days');
+		const aggregateReadings = await Reading.getAggregateReadings([meter.id], moment.duration(1, 'h'), startTimestamp, endTimestamp);
+		expect(aggregateReadings[meter.id]).to.have.lengthOf(4);
+		const expectedFirstAggregation = Math.floor((reading1.reading * 0.75) + (reading2.reading * 0.25));
+		expect(aggregateReadings[meter.id][0].reading_sum).to.equal(expectedFirstAggregation);
+		const expectedSecondAggregation = Math.floor((reading2.reading * 0.75) + (reading3.reading * 0.25));
+		expect(aggregateReadings[meter.id][1].reading_sum).to.equal(expectedSecondAggregation);
+		const expectedThirdAggregation = Math.floor((reading3.reading * 0.75) + (reading4.reading * 0.25));
+		expect(aggregateReadings[meter.id][2].reading_sum).to.equal(expectedThirdAggregation);
+		const expectedFourthAggregation = Math.floor(reading4.reading * 0.75);
+		expect(aggregateReadings[meter.id][3].reading_sum).to.equal(expectedFourthAggregation);
+	}));
+
+	mocha.it('aggregate readings with a missing reading', () => db.task(async t => {
+		const reading1 = new Reading(meter.id, 10, timestamp1, timestamp2);
+		const reading2 = new Reading(meter.id, 100, timestamp2, timestamp3);
+		const reading4 = new Reading(meter.id, 400, timestamp4, timestamp5);
+		await Reading.insertAll([reading1, reading2, reading4], t);
+
+		const startTimestamp = timestamp1.clone().add(30, 'minutes');
+		const endTimestamp = startTimestamp.clone().add(5, 'hours');
+		const aggregateReadings = await Reading.getAggregateReadings([meter.id], moment.duration(1, 'h'), startTimestamp, endTimestamp);
+		expect(aggregateReadings[meter.id]).to.have.lengthOf(4);
+		const expectedFirstAggregation = Math.floor((reading1.reading * 0.5) + (reading2.reading * 0.5));
+		expect(aggregateReadings[meter.id][0].reading_sum).to.equal(expectedFirstAggregation);
+		const expectedSecondAggregation = Math.floor(reading2.reading * 0.5);
+		expect(aggregateReadings[meter.id][1].reading_sum).to.equal(expectedSecondAggregation);
+		const expectedThirdAggregation = Math.floor(reading4.reading * 0.5);
+		expect(aggregateReadings[meter.id][2].reading_sum).to.equal(expectedThirdAggregation);
+		const expectedFourthAggregation = Math.floor(reading4.reading * 0.5);
+		expect(aggregateReadings[meter.id][3].reading_sum).to.equal(expectedFourthAggregation);
+	}));
+
+	mocha.it('aggregate readings with a smaller time interval than duration', () => db.task(async t => {
+		const reading1 = new Reading(meter.id, 100, timestamp1, timestamp2);
+		const reading2 = new Reading(meter.id, 1000, timestamp2, timestamp3);
+		await Reading.insertAll([reading1, reading2], t);
+
+		const startTimestamp = timestamp1.clone().add(30, 'minutes');
+		const endTimestamp = startTimestamp.clone().add(15, 'minutes');
+		const aggregateReadings = await Reading.getAggregateReadings([meter.id], moment.duration(1, 'h'), startTimestamp, endTimestamp);
+		expect(aggregateReadings[meter.id]).to.have.lengthOf(1);
+		const expectedFirstAggregation = Math.floor(reading1.reading * 0.25);
+		expect(aggregateReadings[meter.id][0].reading_sum).to.equal(expectedFirstAggregation);
+	}));
+
+	mocha.it('aggregate readings with multiple meters', () => db.task(function* runTest(t) {
+		yield new Meter(undefined, 'Meter2', null, false, Meter.type.MAMAC).insert(t);
+		yield new Meter(undefined, 'Meter3', null, false, Meter.type.MAMAC).insert(t);
+		const meter2 = yield Meter.getByName('Meter2', t);
+		const meter3 = yield Meter.getByName('Meter3', t);
+		const readingMeter1 = new Reading(meter.id, 100, timestamp1, timestamp2);
+		const readingMeter2 = new Reading(meter2.id, 200, timestamp1, timestamp2);
+		const readingMeter3 = new Reading(meter3.id, 300, timestamp1, timestamp2);
+		yield Reading.insertAll([readingMeter1, readingMeter2, readingMeter3], t);
+
+		const startTimestamp = timestamp1.clone();
+		const endTimestamp = startTimestamp.clone().add(2, 'hours');
+		const aggregateReadings = yield Reading.getAggregateReadings([meter.id, meter2.id, meter3.id], moment.duration(1, 'hour'), startTimestamp, endTimestamp);
+		expect(Object.keys(aggregateReadings)).to.have.lengthOf(3);
+		expect(aggregateReadings[meter.id]).to.have.lengthOf(1);
+		expect(aggregateReadings[meter2.id]).to.have.lengthOf(1);
+		expect(aggregateReadings[meter3.id]).to.have.lengthOf(1);
+		expect(aggregateReadings[meter.id][0].reading_sum).to.equal(readingMeter1.reading);
+		expect(aggregateReadings[meter2.id][0].reading_sum).to.equal(readingMeter2.reading);
+		expect(aggregateReadings[meter3.id][0].reading_sum).to.equal(readingMeter3.reading);
 	}));
 });
