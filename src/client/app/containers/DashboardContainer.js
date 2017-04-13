c3fc89f7a3ad8c708bc69a97370b458ba8b470b2
Brandon Brown <monkeyseemonkeykill42@gmail.com>
2017-04-12 20:30:23 -0500

    fixed a comment I accidentally changed in dashboardContainer

--- src/client/app/containers/DashboardContainer.js
+++ src/client/app/containers/DashboardContainer.js
@@ -6,6 +6,6 @@ import { connect } from 'react-redux';
 import DashboardComponent from '../components/DashboardComponent';
 
 /**
- * Connects and passes down the Redux dispatch function to ExportComponent
+ * Connects and passes down the Redux dispatch function to DashboardComponent
  */
 export default connect()(DashboardComponent);
