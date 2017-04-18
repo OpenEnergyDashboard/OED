f373608b6b4d747ab283e865ba4251b64baa7a3b
sandeepacharya464 <acharyas@beloit.edu>
2017-04-18 00:02:46 -0500

    Changes made so that it can read various kinds of data

--- src/server/services/readMetasysData.js
+++ src/server/services/readMetasysData.js
@@ -6,49 +6,83 @@ const moment = require('moment');
  * Reads CSV file passed to input all the Metasys readings into database.
  * @param filePath the filePath to read
  */
-async function readMetasysData(filePath) {
-	const readingArr = [];
-	let i = 1;
+async function readMetasysData(filePath, readingInterval, readingRepetition, cumulativeIndicator) {
+	//arrays to store readings and rows
+	const readingArray = [];
+	const rowArray = [];
+
 	//getting filename
 	const fileNameArray = filePath.split("/");
 	const fileName = fileNameArray.pop();
+
 	//list of readings
 	const rows = await readCsv(filePath);
+
 	//meterInformation
-	const meter	= await Meter.getByName(fileName.replace('.csv', ''));
-	console.log(meter);
-	let q = 0;
+	const meter = await Meter.getByName(fileName.replace('.csv', ''));
+
+	//Initialize timestamps and other variables
+	let start_timestamp = moment(0);
+	let end_timestamp = moment(0);
+	let index = 0;
+	let meterReading, meterReading1, meterReading2 = 0;
+
 	for (const row of rows) {
-		q = q + 1;
-		if (q % 1000 === 0) {
-			console.log(q);
-		}
-		if (row[0] === '') {
-			continue;
-		}
-		//start timestamp and end timestamp. End timestamp is start timestamp + 1 hour.
-		// 9/25/16 11:30
-		const end_timestamp = moment(row[0], 'MM/DD/YY HH:mm');
-		let start_timestamp = moment(end_timestamp).subtract(30, 'minutes');
-		//meterReading
-		let meterReading = row[3];
-		meterReading = meterReading.replace(' kW', '');
-		meterReading = Math.round(parseFloat(meterReading));
+		rowArray.push(row);
+		// To read data where same reading is repeated. Like E-mon D-mon meters
+		if (index != 0 && Math.abs((index - readingRepetition) % readingRepetition) == 0) {
+			//set start_timestamp and end_timestamp
+			start_timestamp = moment(rowArray[index][0], 'MM/DD/YY HH:mm');
+			end_timestamp = moment(rowArray[index - readingRepetition][0], 'MM/DD/YY HH:mm');
+			//meterReading for cumulative readings
+			if (cumulativeIndicator == true) {
+				//meterReading1
+				meterReading1 = rowArray[index - readingRepetition][3];
+				meterReading1 = meterReading1.replace(' kW', '');
+				meterReading1 = Math.round(parseFloat(meterReading1));
 
-
-		/* We have hourly trend and monthly trend for Metasys data. We just read hourly trend.
-		 * So, we skip one line as we go through the data
-		 */
-//		if ((i % 2 != 0)) {
+				//meterReading2
+				meterReading2 = rowArray[index][3];
+				meterReading2 = meterReading2.replace(' kW', '');
+				meterReading2 = Math.round(parseFloat(meterReading2));
+				meterReading = meterReading1 - meterReading2;
+			}
+			//for data points
+			else {
+				//meterReading
+				meterReading = rowArray[index - readingRepetition][3];
+				meterReading = meterReading.replace(' kW', '');
+				meterReading = Math.round(parseFloat(meterReading));
+				console.log(meterReading);
+			}
+			//To handle cumulative readings that resets at midnight
+			if(meterReading < 0){
+				meterReading = meterReading1;
+			}
+			//push into reading Array
 			const reading = new Reading(meter.id, meterReading, start_timestamp.toDate(), end_timestamp.toDate());
-			readingArr.push(reading);
-//		}
-//		i++;
+			readingArray.push(reading);
+		}
+		index = index + 1;
 	}
+
+	/*Deal with the last reading*/
+	// Timestamp for last reading
+	const lastRow = await rowArray.pop();
+	end_timestamp = moment(lastRow[0], 'MM/DD/YY HH:mm');
+	start_timestamp = moment(end_timestamp).subtract(readingInterval, 'minutes');
+	//meterReadingForLastReading
+	let meterReadingEnd = lastRow[3].replace(' kW', '');
+	meterReadingEnd = Math.round(parseFloat(meterReadingEnd));
+	//pushing last reading into array
+	const reading = new Reading(meter.id,meterReadingEnd,start_timestamp.toDate(),end_timestamp.toDate());
+	readingArray.push(reading);
+
+	// //Insert into database
 	try {
 		console.log("About to insert");
 		//inserting all the data from an array into database and catching error when it occurs.
-		Reading.insertAll(readingArr).then(() => console.log("Done"));
+		Reading.insertAll(readingArray).then(() => console.log("Done"));
 	} catch (err) {
 		console.error(err);
 	}
