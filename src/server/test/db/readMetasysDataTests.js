c66af73dbaf8aa887acca3497d5d2b9822ceb63c
sandeepacharya464 <acharyas@beloit.edu>
2017-04-26 13:43:39 -0500

    test for metasys - no. of rows check

---
+++ src/server/test/db/readMetasysDataTests.js
@@ -0,0 +1,39 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const moment = require('moment');
+const path = require('path');
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const recreateDB = require('./common').recreateDB;
+const db = require('../../models/database').db;
+const Meter = require('../../models/Meter');
+const readMetasysData = require('../../services/readMetasysData');
+
+const mocha = require('mocha');
+
+mocha.describe('Insert Metasys readings from a file', () => {
+	mocha.beforeEach(recreateDB);
+	let meter;
+	mocha.beforeEach(() => db.task(async t => {
+		await new Meter(undefined, 'Meter', null, false, Meter.type.METASYS).insert(t);
+		meter = await Meter.getByName('Meter', t);
+	}));
+
+	mocha.it('loads the correct number of rows from a file', () => db.task(async t => {
+		const testFilePath = path.join(__dirname, 'Meter.csv');
+		// readMetasysData(filepath, interval, repetition)
+		await readMetasysData(testFilePath, 30, 1, false,t);
+		const count = await db.one('SELECT COUNT(*) as count FROM readings');
+		expect(parseInt(count)).to.equal(225);
+			// .then(() => db.one('SELECT COUNT(*) as count FROM readings'))
+			// .then(({ count }) => expect(parseInt(count)).to.equal(225));
+	}));
+});
