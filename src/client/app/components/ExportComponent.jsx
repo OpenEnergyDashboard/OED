394c15526d83ab2338d84d87898ba5ff651925d2
Brandon Brown <monkeyseemonkeykill42@gmail.com>
2017-05-03 18:00:22 -0500

    ExportComponent is now a functional component instead of a class

--- src/client/app/components/ExportComponent.jsx
+++ src/client/app/components/ExportComponent.jsx
@@ -7,34 +7,23 @@ import { Button } from 'react-bootstrap';
 import moment from 'moment';
 import graphExport from '../services/exportData';
 
-export default class ExportComponent extends React.Component {
-	/**
-	 * Initializes the component's state, binds all functions to 'this' ExportComponent
-	 * @param props The props passed down through the ExportContainer
-	 */
-	constructor(props) {
-		super(props);
-		this.exportReading = this.exportReading.bind(this);
-	}
-
+const ExportComponent = props => {
 	/**
 	 * Called when Export button is clicked.
 	 * Passes an object containing the selected meter data to a function for export.
 	 */
-	exportReading() {
-		const compressedData = this.props.exportVals.datasets;
+	const exportReading = () => {
+		const compressedData = props.exportVals.datasets;
 		let time = compressedData[0].exportVals[0].x;
 		const chart = compressedData[0].currentChart;
 		time = moment(time).format('ddddMMMDDYYYY');
 		const name = `oedExport${time}${chart}.csv`;
 		graphExport(compressedData,	name);
-	}
-
-	render() {
-		return (
-			<div>
-				<Button bsStyle="default" onClick={this.exportReading}>Export!</Button>
-			</div>
-		);
-	}
-}
+	};
+	return (
+		<div>
+			<Button bsStyle="default" onClick={exportReading}>Export!</Button>
+		</div>
+	);
+};
+export default ExportComponent;
