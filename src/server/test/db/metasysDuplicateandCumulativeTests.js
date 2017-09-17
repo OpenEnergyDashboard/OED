1fa177e171fb7696ee914e765bfee8360b446b93
sandeepacharya464 <acharyas@beloit.edu>
2017-09-17 15:08:29 -0500

    added test files to test how metasysData import code  would respond to various types of data

---
+++ src/server/test/db/metasysDuplicateandCumulativeTests.js
@@ -0,0 +1,42 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const moment = require('moment');
+const path = require('path');
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const recreateDB = require('./common').recreateDB;
+const db = require('../../models/database').db;
+const Meter = require('../../models/Meter');
+const readMetasysData = require('../../services/readMetasysData');
+
+const mocha = require('mocha');
+	mocha.describe('Insert Metasys readings from a file', () => {
+		mocha.beforeEach(recreateDB);
+		let meter;
+		mocha.beforeEach(() => db.task(function* setupTests(t) {
+			yield new Meter(undefined, 'MeterDuplicate', null, false, Meter.type.METASYS).insert(t);
+			meter = yield Meter.getByName('MeterDuplicate', t);
+		}));
+
+		mocha.it('handles duplicate readings', () => {
+			const testFilePath = path.join(__dirname, 'MeterDuplicate.csv');
+			return readMetasysData(testFilePath, 60, 2, false)
+			//what is this doing?
+				.then(() => db.one('SELECT COUNT(*) as count FROM readings'))
+				.then(({count}) => expect(parseInt(count)).to.equal(37));
+		})
+	});
+
+
+	//reads the cumulativeReading properly
+	//reads the gap properly
+
+
---
+++ src/server/test/db/metasysInvalidTests.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Sandeep Acharya on 9/14/2017.
+ */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const moment = require('moment');
+const path = require('path');
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const recreateDB = require('./common').recreateDB;
+const db = require('../../models/database').db;
+const Meter = require('../../models/Meter');
+const readMetasysData = require('../../services/readMetasysData');
+
+const mocha = require('mocha');
+
+
+mocha.describe('Insert Metasys readings from a file', () => {
+	mocha.beforeEach(recreateDB);
+	let meter;
+	mocha.beforeEach(() => db.task(function* setupTests(t) {
+		yield new Meter(undefined, 'MeterInvalid', null, false, Meter.type.METASYS).insert(t);
+		meter = yield Meter.getByName('MeterInvalid', t);
+	}));
+	// shows error but on console.
+	mocha.it('errors correctly on an invalid file', () => {
+		const testFilePath = path.join(__dirname, 'MeterInvalid.csv');
+		return expect(readMetasysData(testFilePath, 30, 1, false)).to.eventually.be.rejected;
+	});
+	//problem with reading invalid dates. test passes even when we are expecting 5 in the case of error.
+	mocha.it('rolls back correctly when it rejects', async () => {
+		const testFilePath = path.join(__dirname, 'MeterInvalid.csv');
+		try {
+			await readMetasysData(testFilePath, 30, 1, false);
+		} catch (e) {
+			const  {count}  = await db.one('SELECT COUNT(*) as count FROM readings');
+			expect(parseInt(count)).to.equal(5);
+		}
+	});
+});
