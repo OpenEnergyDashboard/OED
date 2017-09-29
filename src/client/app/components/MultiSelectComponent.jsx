3d1f5b5ae409631eb9e8d1d7a578e17ecd4b4645
Leo Tindall <lfstindall@gmail.com>
2017-09-28 19:15:51 -0500

    Add searchable data source selector (#125)
    * Transition to selectize MultiSelect in UIOptionsComponent
    
    * Implement typing for data source choices in UIOptionsComponent
    
    * Visual fix for UIOptionsComponent getting squished
    
    * Use inline padding element for alignment instead of nonbreaking spaces.
    
    * Move generally usable datasource types and functions to utils/
    
    * Fix silly mistake in groupsFilterReduce
    
    * Add scrollbar when selector gets too long.
    
    * Encapsulate click-to-remove functionality in MultiSelectComponent
    
    * Testing commit for data fetch bug
    
    * Revert "Fix silly mistake in groupsFilterReduce"
    
    This reverts commit e735d9493de0a978af0d3c9ceea503b797f14a51.
    
    * Move search selector from Selectize to ReactSelect.
    
    * Redo styling/fix rebase errors for search selectors.

---
+++ src/client/app/components/MultiSelectComponent.jsx
@@ -0,0 +1,50 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import '../styles/react-select-css.css';
+
+export default class MultiSelectComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.onValuesChangeInternal = this.onValuesChangeInternal.bind(this);
+		// selectedOptions holds a list of the options that have been selected
+		this.state = { selectedOptions: [] };
+	}
+
+	onValuesChangeInternal(items) {
+		// Defer to the underlying MultiSelect when it has a state change
+		// Note that the MSC state selectedOptions is in fact the canonical source of truth
+		this.setState({ selectedOptions: items });
+		this.props.onValuesChange(items);
+	}
+
+	render() {
+		return (
+			<Select
+				multi
+				options={this.props.options}
+				value={this.state.selectedOptions}
+				placeholder={this.props.placeholder}
+				style={this.props.style}
+				onChange={this.onValuesChangeInternal}
+				clearable={false}
+				closeOnSelect={false}
+			/>
+		);
+	}
+}
+
+MultiSelectComponent.propTypes = {
+	placeholder: PropTypes.string,
+	options: PropTypes.arrayOf(PropTypes.shape({
+		label: PropTypes.string.isRequired,
+		type: PropTypes.string.isRequired,
+		value: PropTypes.number,
+	})),
+	onValuesChange: PropTypes.func.isRequired,
+};
