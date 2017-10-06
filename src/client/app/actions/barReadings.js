83afab0c4f548ed7e464185930ea7fb678e00f99
LeoTindall <lfstindall@gmail.com>
2017-10-06 14:19:26 -0500

    Dummy impl for group data in bar charts.
    Only needs the API to be switched out.

--- src/client/app/actions/barReadings.js
+++ src/client/app/actions/barReadings.js
@@ -5,6 +5,7 @@
  */
 
 import axios from 'axios';
+import { DATA_TYPE_METER, DATA_TYPE_GROUP } from '../utils/Datasources';
 
 export const REQUEST_BAR_READINGS = 'REQUEST_BAR_READINGS';
 export const RECEIVE_BAR_READINGS = 'RECEIVE_BAR_READINGS';
@@ -15,37 +16,50 @@ export const RECEIVE_BAR_READINGS = 'RECEIVE_BAR_READINGS';
  * @param {TimeInterval} timeInterval
  * @param {Moment.Duration} barDuration
  */
-function shouldFetchBarReadings(state, meterID, timeInterval, barDuration) {
-	const readingsForMeterID = state.readings.bar.byMeterID[meterID];
-	if (readingsForMeterID === undefined) {
+function shouldFetchBarReadings(state, dsID, timeInterval, barDuration, dstype) {
+	let readingsForID;
+	if (dstype === DATA_TYPE_METER) {
+		readingsForID = state.readings.bar.byMeterID[dsID];
+	} else if (dstype === DATA_TYPE_GROUP) {
+		readingsForID = state.readings.bar.byGroupID[dsID];
+	}
+	if (readingsForID === undefined) {
 		return true;
 	}
-	if (readingsForMeterID[timeInterval] === undefined) {
+	if (readingsForID[timeInterval] === undefined) {
 		return true;
 	}
-	if (readingsForMeterID[timeInterval][barDuration] === undefined) {
+	if (readingsForID[timeInterval][barDuration] === undefined) {
 		return true;
 	}
-	const readingsForTimeIntervalAndDuration = state.readings.bar.byMeterID[meterID][timeInterval][barDuration];
+	const readingsForTimeIntervalAndDuration = readingsForID[timeInterval][barDuration];
 	return readingsForTimeIntervalAndDuration === undefined && !readingsForTimeIntervalAndDuration.isFetching;
 }
 
-function requestBarReadings(meterIDs, timeInterval, barDuration) {
-	return { type: REQUEST_BAR_READINGS, meterIDs, timeInterval, barDuration };
+function requestBarReadings(dsIDs, timeInterval, barDuration, dstype) {
+	return { type: REQUEST_BAR_READINGS, dsIDs, timeInterval, barDuration, dstype };
 }
 
-function receiveBarReadings(meterIDs, timeInterval, barDuration, readings) {
-	return { type: RECEIVE_BAR_READINGS, meterIDs, timeInterval, barDuration, readings };
+function receiveBarReadings(dsIDs, timeInterval, barDuration, readings, dstype) {
+	return { type: RECEIVE_BAR_READINGS, dsIDs, timeInterval, barDuration, readings, dstype };
 }
 
-function fetchBarReadings(meterIDs, timeInterval) {
+function fetchBarReadings(dsIDs, timeInterval, dstype) {
 	return (dispatch, getState) => {
 		const barDuration = getState().graph.barDuration;
-		dispatch(requestBarReadings(meterIDs, timeInterval, barDuration));
-		const stringifiedMeterIDs = meterIDs.join(',');
-		return axios.get(`/api/readings/bar/${stringifiedMeterIDs}`, {
+		dispatch(requestBarReadings(dsIDs, timeInterval, barDuration, dstype));
+		const stringifiedIDs = dsIDs.join(',');
+
+		let endpoint;
+		if (dstype === DATA_TYPE_METER) {
+			endpoint = '/api/readings/bar';
+		} else if (dstype === DATA_TYPE_GROUP) {
+			endpoint = '/api/readings/bar';
+		}
+
+		return axios.get(`${endpoint}/${stringifiedIDs}`, {
 			params: { timeInterval: timeInterval.toString(), barDuration: barDuration.toISOString() }
-		}).then(response => dispatch(receiveBarReadings(meterIDs, timeInterval, barDuration, response.data)));
+		}).then(response => dispatch(receiveBarReadings(dsIDs, timeInterval, barDuration, response.data, dstype)));
 	};
 }
 
@@ -57,9 +71,13 @@ function fetchBarReadings(meterIDs, timeInterval) {
 export function fetchNeededBarReadings(timeInterval) {
 	return (dispatch, getState) => {
 		const state = getState();
-		const meterIDsToFetchForBar = state.graph.selectedMeters.filter(id => shouldFetchBarReadings(state, id, timeInterval, state.graph.barDuration));
+		const meterIDsToFetchForBar = state.graph.selectedMeters.filter(id => shouldFetchBarReadings(state, id, timeInterval, state.graph.barDuration, DATA_TYPE_METER));
 		if (meterIDsToFetchForBar.length > 0) {
-			return dispatch(fetchBarReadings(meterIDsToFetchForBar, timeInterval));
+			return dispatch(fetchBarReadings(meterIDsToFetchForBar, timeInterval, DATA_TYPE_METER));
+		}
+		const groupIDsToFetchForBar = state.graph.selectedMeters.filter(id => shouldFetchBarReadings(state, id, timeInterval, state.graph.barDuration, DATA_TYPE_GROUP));
+		if (groupIDsToFetchForBar.length > 0) {
+			return dispatch(fetchBarReadings(groupIDsToFetchForBar, timeInterval, DATA_TYPE_GROUP));
 		}
 		return Promise.resolve();
 	};
