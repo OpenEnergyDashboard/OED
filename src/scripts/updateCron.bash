3d1b201a0eee4444f4ae9818a151d86915f0b4de
LeoTindall <lfstindall@gmail.com>
2017-10-23 17:13:26 -0500

    Add missing headers

--- src/scripts/updateCron.bash
+++ src/scripts/updateCron.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# *
+# * This Source Code Form is subject to the terms of the Mozilla Public
+# * License, v. 2.0. If a copy of the MPL was not distributed with this
+# * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# *
 # This should be copied to /etc/cron.hourly/ and the copy renamed so that its function will be clear to admins.
 # The absolute path the project root directory (OED) No trailing /
 project='/example/path/to/project/OED'
