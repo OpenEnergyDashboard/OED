a6b8b78cd7631f935f9481ae0bb513aa0c933854
LeoTindall <lfstindall@gmail.com>
2017-10-23 17:13:11 -0500

    Use regex filtering instead of a massive find command

--- src/scripts/checkheader.sh
+++ src/scripts/checkheader.sh
@@ -10,21 +10,41 @@ echo "[MPL] Checking source files for MPL headers..."
 # Checks for the existance of the MPL header
 MPL_REGEX=".*?This\W+Source\W+Code\W+Form\W+is\W+subject\W+to\W+the\W+terms\W+of\W+the\W+Mozilla\W+Public\W+License,\W+v\.\W+2\.0\.\W+If\W+a\W+copy\W+of\W+the\W+MPL\W+was\W+not\W+distributed\W+with\W+this\W+file,\W+You\W+can\W+obtain\W+one\W+at\W+http:\/\/mozilla\.org\/MPL\/2\.0\/\."
 
-# This find commands lists all our source code files, excluding bad directories
-# It prunes gitignore and gitattributes, and everything in the public dir, .git, node_modules, and postgres-data
-FILES=$(find . -type f \( -iname '*.jsx' -o -iname '*.js' -o -iname '*.sh' -o -iname '*.sql' -o -iname "*.y*ml" \
-	-o -iname "Dockerfile" -o -iname "*.html" -o -iname "*.css" \) -print \
-	\( -iname "*.gitignore" -o -iname "*.gitattributes" \) -prune \
-	-o \( -path '*node_modules*' -o -path '*postgres-data*' -o -path '*.git*' -o -path "*client/public*" \) -prune);
+# This find commands lists all our source code files, excluding node_modules and postgres_data from even being searched
+FILES=$(find . -type f -print -o \( -path '*node_modules*' -o -path '*postgres-data*' \) -prune);
+# Filter out text files
+FILES=$(echo "$FILES" | grep -v "README\.md")
+FILES=$(echo "$FILES" | grep -v "\.editorconfig")
+FILES=$(echo "$FILES" | grep -v "\.gitattributes")
+# Filter out generated files
+FILES=$(echo "$FILES" | grep -v "package-lock\.json")
+FILES=$(echo "$FILES" | grep -v "npm-debug\.log")
+# Filter out all the .git directory
+FILES=$(echo "$FILES" | grep -v "\.\/\.git\/.*")
+# Filter out the IDE files
+FILES=$(echo "$FILES" | grep -v "\.\/\.idea\/.*")
+FILES=$(echo "$FILES" | grep -v "\.\/\.vscode\/.*")
+# Filter out build artifacts
+FILES=$(echo "$FILES" | grep -v "\.\/src\/client\/public\/.*")
+# Filter out images
+FILES=$(echo "$FILES" | grep -v "\.\/src\/client\/app\/images\/.*")
+FILES=$(echo "$FILES" | grep -v "\.\/src\/client\/favicon\.ico")
+# Filter out test data
+FILES=$(echo "$FILES" | grep -v "\.\/src\/server\/test\/db\/data\/.*")
+
+# Counts the files listed in FILES
+NFILES=$(echo $FILES | wc -w)
+echo "[MPL] Checking $NFILES files for header."
 
 # This searches for the above regex. 
 # P enables Perl and z enables multi-line support
 # s turns off warnings, lv lists files that don't match
 MISSING_HEADER=$(grep -Pz -slv "$MPL_REGEX" $FILES)
+NMISSING=$(echo $MISSING_HEADER | wc -w)
 
 # Check if there's 
 if [ ! -z "$MISSING_HEADER" ]; then
-	echo "[MPL] The following files are missing headers:"
+	echo "[MPL] The following $NMISSING files are missing headers:"
 	echo "$MISSING_HEADER"
 	echo ""
 	exit 1
