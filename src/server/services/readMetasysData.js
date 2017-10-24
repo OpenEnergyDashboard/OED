2984967cad63f18e991a9d7739b50041d3747d4c
LeoTindall <lfstindall@gmail.com>
2017-10-24 12:30:37 -0500

    Make readMetasysData lint compliant

--- src/server/services/readMetasysData.js
+++ src/server/services/readMetasysData.js
@@ -18,72 +18,72 @@ const path = require('path');
  * @param  cumulativeIndicator false if readings are not cumulative and vice-versa.
  */
 async function readMetasysData(filePath, readingInterval, readingRepetition, cumulativeIndicator) {
-	//arrays to store readings and rows
+	// arrays to store readings and rows
 	const readingArray = [];
 	const rowArray = [];
 
-	//getting filename
+	// getting filename
 	const fileName = path.basename(filePath);
-	//list of readings
+	// list of readings
 	const rows = await readCsv(filePath);
 
-	//meterInformation
+	// meterInformation
 	const meter = await Meter.getByName(fileName.replace('.csv', ''));
 
-	//Initialize timestamps and other variables
-	let start_timestamp = moment(0);
-	let end_timestamp = moment(0);
+	// Initialize timestamps and other variables
+	let startTimestamp = moment(0);
+	let endTimestamp = moment(0);
 	let index = 0;
-	let meterReading, meterReading1, meterReading2 = 0;
+	let meterReading = 0;
+	let meterReading1 = 0;
+	let meterReading2 = 0;
 
 	for (const row of rows) {
 		rowArray.push(row);
 		// To read data where same reading is repeated. Like E-mon D-mon meters
-		if (index != 0 && Math.abs((index - readingRepetition) % readingRepetition) == 0) {
-			//set start_timestamp and end_timestamp
-			start_timestamp = moment(rowArray[index][0], 'MM/DD/YY HH:mm');
-			end_timestamp = moment(rowArray[index - readingRepetition][0], 'MM/DD/YY HH:mm');
-			//meterReading for cumulative readings
-			if (cumulativeIndicator == true) {
-				//meterReading1
+		if (index !== 0 && Math.abs((index - readingRepetition) % readingRepetition) === 0) {
+			// set start_timestamp and end_timestamp
+			startTimestamp = moment(rowArray[index][0], 'MM/DD/YY HH:mm');
+			endTimestamp = moment(rowArray[index - readingRepetition][0], 'MM/DD/YY HH:mm');
+			// meterReading for cumulative readings
+			if (cumulativeIndicator === true) {
+				// meterReading1
 				meterReading1 = rowArray[index - readingRepetition][3];
 				meterReading1 = meterReading1.replace(' kW', '');
 				meterReading1 = Math.round(parseFloat(meterReading1));
 
-				//meterReading2
+				// meterReading2
 				meterReading2 = rowArray[index][3];
 				meterReading2 = meterReading2.replace(' kW', '');
 				meterReading2 = Math.round(parseFloat(meterReading2));
 				meterReading = meterReading1 - meterReading2;
-			}
-			//for data points
-			else {
-				//meterReading
+			} else { // for data points
+				// meterReading
 				meterReading = rowArray[index - readingRepetition][3];
 				meterReading = meterReading.replace(' kW', '');
 				meterReading = Math.round(parseFloat(meterReading));
 			}
-			//To handle cumulative readings that resets at midnight
-			if(meterReading < 0){
+			// To handle cumulative readings that resets at midnight
+			if (meterReading < 0) {
 				meterReading = meterReading1;
 			}
-			//push into reading Array
-			const reading = new Reading(meter.id, meterReading, start_timestamp.toDate(), end_timestamp.toDate());
+			// push into reading Array
+			const reading = new Reading(meter.id, meterReading, startTimestamp.toDate(), endTimestamp.toDate());
 			readingArray.push(reading);
 		}
-		index = index + 1;
+		index += 1;
 	}
 
-	/*Deal with the last reading*/
+	// Deal with the last reading
 	// Timestamp for last reading
 	const lastRow = await rowArray.pop();
-	end_timestamp = moment(lastRow[0], 'MM/DD/YY HH:mm');
-	start_timestamp = moment(end_timestamp).subtract(readingInterval, 'minutes');
-	//meterReadingForLastReading
+	endTimestamp = moment(lastRow[0], 'MM/DD/YY HH:mm');
+	startTimestamp = moment(endTimestamp).subtract(readingInterval, 'minutes');
+	// meterReadingForLastReading
 	let meterReadingEnd = lastRow[3].replace(' kW', '');
 	meterReadingEnd = Math.round(parseFloat(meterReadingEnd));
-	//pushing last reading into array
-	const reading = new Reading(meter.id, meterReadingEnd, start_timestamp.toDate(), end_timestamp.toDate());
+	// pushing last reading into array
+	const reading = new Reading(meter.id, meterReadingEnd, startTimestamp.toDate(), endTimestamp.toDate());
 	readingArray.push(reading);
 	return await Reading.insertAll(readingArray);
 }
