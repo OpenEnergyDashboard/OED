3da5b91a7aa0d0206ce3d5d5ed32ff2df143071b
LucasFares <faresli@beloit.edu>
2017-10-24 09:33:22 -0500

    Made requested changes
    Made requested changes and removed - sign on negative values for clarity.

--- src/client/app/containers/CompareChartContainer.js
+++ src/client/app/containers/CompareChartContainer.js
@@ -26,22 +26,11 @@ function mapStateToProps(state) {
 	// Power used up to this point last week
 	let currentLastWeek = 0;
 	const soFar = moment().diff(moment().startOf('week'), 'days');
-	const delta = function (change) {
-		if (isNaN(change)) {
-			return '';
-		}
-		if (change < 0) {
-			return `You have used ${parseInt(change.toFixed(2).replace('.', ''), 10)}% less energy this week.`;
-		}
-		return `You have used ${parseInt(change.toFixed(2).replace('.', ''), 10)}% more energy this week.`;
+	const delta = change => {
+		if (isNaN(change)) return ''; if (change < 0) return `${state.meters.byMeterID[state.graph.selectedMeters].name} has used ${parseInt(change.toFixed(2).replace('.', '').slice(1), 10)}% less energy this week.`;
+		return `${state.meters.byMeterID[state.graph.selectedMeters].name} has used ${parseInt(change.toFixed(2).replace('.', ''), 10)}% more energy this week.`;
 	};
-
-	const colorize = function (change) {
-		if (change < 0) {
-			return 'green';
-		}
-		return 'red';
-	};
+	const colorize = change => { if (change < 0) return 'green'; return 'red'; };
 	for (const meterID of state.graph.selectedMeters) {
 		const readingsData = state.readings.bar.byMeterID[meterID][timeInterval][barDuration];
 		if (readingsData !== undefined && !readingsData.isFetching) {
