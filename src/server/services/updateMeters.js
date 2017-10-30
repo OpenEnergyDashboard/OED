9feab56dfbd5d0f44e1bbd9d9e0fa305060abf80
James McFeeters <jameeters@gmail.com>
2017-10-30 16:47:19 -0500

    Reorganize `updateMamacMeters` and `updateMeters`
    Alter `updateMamacMeters` to be passed a list of meters and a data
    reader function as parameters.

--- src/server/services/updateMeters.js
+++ src/server/services/updateMeters.js
@@ -3,47 +3,40 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const _ = require('lodash');
-const Meter = require('../models/Meter');
 const Reading = require('../models/Reading');
-const readMamacData = require('./readMamacData');
+
 const log = require('../log');
 
 /**
- * Pulls new data for all the meters in the database.
- * This assumes that every meter is a MAMAC meter with a valid IP address.
+ * Uses the provided dataReader function to poll the provided meters for new readings,
+ * and inserts any new readings into the database.
+ * @param dataReader {function} A function to fetch readings from each meter
+ * @param metersToUpdate [Meter] An array of meters to be updated
+ * @return {Promise.<void>}
  */
-async function updateAllMeters(reader) {
+async function updateAllMeters(dataReader, metersToUpdate) {
 	const time = new Date();
 	log(`Getting meter data ${time.toISOString()}`);
 	try {
-		const allMeters = await Meter.getAll();
-		const metersToUpdate = allMeters.filter(m => m.enabled && m.type === Meter.type.MAMAC);
-		// console.log(reader);
-		console.log(metersToUpdate);
-		console.log(metersToUpdate.map(reader));
-
 		// Do all the network requests in parallel, then throw out any requests that fail after logging the errors.
 		const readingInsertBatches = _.filter(await Promise.all(
 			metersToUpdate
-				.map(reader)
-				.map(p => console.log(p)
-					// p.catch(err => {
-				// 	console.log(p);
-				// 	let uri = '[NO URI AVAILABLE]';
-				// 	if (err.options !== undefined && err.options.uri !== undefined) {
-				// 		uri = err.options.uri;
-				// 	}
-				// 	log(`ERROR ON REQUEST TO ${uri}, ${err.message}`, 'error');
-				// 	return null;
-				// }))
-		), elem => elem !== null));
+				.map(dataReader)
+				.map(p => p.catch(err => {
+					let uri = '[NO URI AVAILABLE]';
+					if (err.options !== undefined && err.options.uri !== undefined) {
+						uri = err.options.uri;
+					}
+					log(`ERROR ON REQUEST TO ${uri}, ${err.message}`, 'error');
+					return null;
+				}))
+		), elem => elem !== null);
 
 		// Flatten the batches (an array of arrays) into a single array.
 		const allReadingsToInsert = [].concat(...readingInsertBatches);
 		await Reading.insertOrUpdateAll(allReadingsToInsert);
 		log('Update finished');
 	} catch (err) {
-		console.log(err);
 		log(`Error updating all meters: ${err}`, 'error');
 	}
 }
