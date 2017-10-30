2c68bb714a1703e8c61495ee7fd3816116e7fc97
James McFeeters <jameeters@gmail.com>
2017-10-30 16:27:43 -0500

    Begin cleanup of test file

--- src/server/test/db/updateMetersTests.js
+++ src/server/test/db/updateMetersTests.js
@@ -5,13 +5,11 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const moment = require('moment');
-const _ = require('lodash');
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 const recreateDB = require('./common').recreateDB;
-const db = require('../../models/database').db;
 const Meter = require('../../models/Meter');
 const Reading = require('../../models/Reading');
 const updateAllMeters = require('../../services/updateMeters');
@@ -28,31 +26,24 @@ async function testUpdateAllMeters() {
 	const badMeter = new Meter(undefined, 'BAD', 2, true, Meter.type.MAMAC);
 	await badMeter.insert();
 
-	// console.log(goodMeter);
-	const readMamacData = sinon.stub();
-	readMamacData.withArgs(sinon.match({
-		id: goodMeter.id,
-		name: goodMeter.name,
-		ipAddress: goodMeter.ipAddress,
-		enabled: goodMeter.enabled,
-		type: goodMeter.type })
-	).resolves(new Reading(
+	const metersToUpdate = [goodMeter, badMeter];
+
+	const dataReader = sinon.stub();
+	dataReader.withArgs(goodMeter).resolves(new Reading(
 		goodMeter.id,
 		0,
 		moment('1970-01-01 00:00:00'),
 		moment('1970-01-01 01:00:00')
 	));
-	readMamacData.withArgs(_.cloneDeep(badMeter)).rejects(new Error("shit's on fire, yo."));
-	readMamacData.returns('hello');
+	dataReader.withArgs(badMeter).rejects(new Error('Bland error message'));
+	dataReader.throws();
 
-
-	console.log([goodMeter, badMeter].map(readMamacData));
-
-	await updateAllMeters(readMamacData);
-	console.log(readMamacData.called);
+	await updateAllMeters(dataReader, metersToUpdate);
 	const goodReadings = await Reading.getAllByMeterID(goodMeter.id);
 	const badReadings = await Reading.getAllByMeterID(badMeter.id);
-	console.log(goodReadings);
+
+	expect(goodReadings.length).to.equal(1);
+	expect(badReadings.length).to.equal(0);
 }
 
 testUpdateAllMeters();
