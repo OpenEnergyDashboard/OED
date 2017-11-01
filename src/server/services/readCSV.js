febefb1ad09911ffd722936ee74d430707372b1a
sandeepacharya464 <acharyas@beloit.edu>
2017-10-31 22:07:25 -0500

    fixed readCSV

--- src/server/services/readCSV.js
+++ src/server/services/readCSV.js
@@ -19,15 +19,4 @@ async function readCSV(fileName) {
 	return await parseCsv(buffer.toString());
 }
 
-/**
- * Returns a promise to read a given CSV file from the server.
- * @param buffer fileName and the file to read
- * @returns {Promise.<array, <array>>}
- */
-async function readCSVFromString(buffer) {
-	const bufferContent = buffer.toString('utf8');
-	return await parseCsv(bufferContent);
-}
-
-exports.readCSV = readCSV;
-exports.readCSVFromString = readCSVFromString;
+module.exports = readCSV;
