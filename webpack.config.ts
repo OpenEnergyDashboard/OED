aeb59c838ddb1e23ddd6532f869db9d37c0aff65
LeoTindall <lfstindall@gmail.com>
2017-11-02 11:14:29 -0500

    Fix remainder of configuration for TypeScript

--- webpack.config.ts
+++ webpack.config.ts
@@ -35,15 +35,6 @@ const config = {
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-	},
 	plugins: [
 		new LodashModuleReplacementPlugin()
 	],
