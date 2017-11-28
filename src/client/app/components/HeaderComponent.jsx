a53fbeb6195fea7446a73677eec876a47e2b66f6
John DiIorio <johnzdiiorio@gmail.com>
2017-11-27 20:01:26 -0600

    Add log out button

--- src/client/app/components/HeaderComponent.jsx
+++ src/client/app/components/HeaderComponent.jsx
@@ -14,77 +14,95 @@ import getToken from '../utils/getToken';
  * @param props The props passed down by the parent component
  * @return JSX to create the header strip
  */
-export default function HeaderComponent(props) {
-	const urlArr = window.location.href.split('/');
-	const page = urlArr[urlArr.length - 1];
-	let renderOptionsButton = false;
-	let renderLoginButton = false;
-	let renderAdminButton = false;
-	let renderGroupsButton = false;
-
-	switch (page) {
-		case '': // home page
-			renderOptionsButton = true;
-			if (getToken()) {
-				renderAdminButton = true;
-				renderGroupsButton = true;
-			} else {
-				renderLoginButton = true;
-			}
-			break;
-		case 'groups':
-			renderAdminButton = true;
-			break;
-		case 'admin':
-			renderGroupsButton = true;
-			break;
-		case 'login':
-			break;
-		default: // Unknown page, routes to 404, show nothing
-			break;
-
+export default class HeaderComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleLogOut = this.handleLogOut.bind(this);
 	}
 
-	const titleStyle = {
-		display: 'inline-block'
-	};
-	const divStyle = {
-		paddingBottom: '5px'
-	};
-	const divRightStyle = {
-		float: 'right',
-		marginTop: '5px',
-		display: 'flex'
-	};
-	const loginLinkStyle = {
-		display: renderLoginButton ? 'inline' : 'none',
-		paddingLeft: '5px'
-	};
-	const adminLinkStyle = {
-		display: renderAdminButton ? 'inline' : 'none',
-		paddingLeft: '5px'
-	};
-	const groupsLinkStyle = {
-		display: renderGroupsButton ? 'inline' : 'none',
-		paddingLeft: '5px'
-	};
+	handleLogOut() {
+		localStorage.removeItem('token');
+		this.forceUpdate();
+	}
 
-	return (
-		<div className="container-fluid" style={divStyle}>
-			<div className="col-xs-4">
-				<Link to="/"><LogoComponent url="./app/images/logo.png" /></Link>
-			</div>
-			<div className="col-xs-4 text-center">
-				<h1 style={titleStyle}>{props.title}</h1>
-			</div>
-			<div style={divRightStyle}>
-				<div className="visible-sm visible-xs">
-					{(renderOptionsButton) ? <UIModalComponent /> : null}
+	render() {
+		const urlArr = window.location.href.split('/');
+		const page = urlArr[urlArr.length - 1];
+		let renderOptionsButton = false;
+		let renderLoginButton = false;
+		let renderAdminButton = false;
+		let renderGroupsButton = false;
+		const renderLogoutButton = !!getToken();
+
+		switch (page) {
+			case '': // home page
+				renderOptionsButton = true;
+				if (getToken()) {
+					renderAdminButton = true;
+					renderGroupsButton = true;
+				} else {
+					renderLoginButton = true;
+				}
+				break;
+			case 'groups':
+				renderAdminButton = true;
+				break;
+			case 'admin':
+				renderGroupsButton = true;
+				break;
+			case 'login':
+				break;
+			default: // Unknown page, routes to 404, show nothing
+				break;
+
+		}
+
+		const titleStyle = {
+			display: 'inline-block'
+		};
+		const divStyle = {
+			paddingBottom: '5px'
+		};
+		const divRightStyle = {
+			float: 'right',
+			marginTop: '5px',
+			display: 'flex'
+		};
+		const loginLinkStyle = {
+			display: renderLoginButton ? 'inline' : 'none',
+			paddingLeft: '5px'
+		};
+		const adminLinkStyle = {
+			display: renderAdminButton ? 'inline' : 'none',
+			paddingLeft: '5px'
+		};
+		const groupsLinkStyle = {
+			display: renderGroupsButton ? 'inline' : 'none',
+			paddingLeft: '5px'
+		};
+		const logoutButtonStyle = {
+			display: renderLogoutButton ? 'inline' : 'none',
+			paddingLeft: '5px'
+		};
+
+		return (
+			<div className="container-fluid" style={divStyle}>
+				<div className="col-xs-4">
+					<Link to="/"><LogoComponent url="./app/images/logo.png" /></Link>
 				</div>
-				<Link style={loginLinkStyle} to="/login"><Button bsStyle="default">Log In</Button></Link>
-				<Link style={adminLinkStyle} to="/admin"><Button bsStyle="default">Admin panel</Button></Link>
-				<Link style={groupsLinkStyle} to="/groups"><Button bsStyle="default">Groups</Button></Link>
+				<div className="col-xs-4 text-center">
+					<h1 style={titleStyle}>{this.props.title}</h1>
+				</div>
+				<div style={divRightStyle}>
+					<div className="visible-sm visible-xs">
+						{(renderOptionsButton) ? <UIModalComponent /> : null}
+					</div>
+					<Link style={loginLinkStyle} to="/login"><Button bsStyle="default">Log In</Button></Link>
+					<Link style={adminLinkStyle} to="/admin"><Button bsStyle="default">Admin panel</Button></Link>
+					<Link style={groupsLinkStyle} to="/groups"><Button bsStyle="default">Groups</Button></Link>
+					<Link style={logoutButtonStyle} to="/"><Button bsStyle="default" onClick={this.handleLogOut}>Log Out</Button></Link>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
