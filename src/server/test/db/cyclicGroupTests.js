9d9f40c9f4fcc2692b455d7c78c41cc5d7cc73d2
Simon Tomlinson <simonbtomlinson@gmail.com>
2018-02-01 13:57:32 -0600

    Add a failing test for deep cycle group insertion

--- src/server/test/db/cyclicGroupTests.js
+++ src/server/test/db/cyclicGroupTests.js
@@ -19,15 +19,24 @@ mocha.describe('Group Cycles', async () => {
 	mocha.beforeEach(recreateDB);
 	let group1;
 	let group2;
+	let group3;
 	mocha.beforeEach(async () => {
 		await new Group(undefined, 'group1').insert();
 		await new Group(undefined, 'group2').insert();
+		await new Group(undefined, 'group3').insert();
 		group1 = await Group.getByName('group1');
 		group2 = await Group.getByName('group2');
+		group3 = await Group.getByName('group3');
 	});
 
-	mocha.it('Cannot be saved', async () => {
+	mocha.it('Cannot save immediate cycles', async () => {
 		await group1.adoptGroup(group2.id);
 		await expect(group2.adoptGroup(group1.id), 'cyclic group insert was not rejected').to.eventually.be.rejected;
+	});
+
+	mocha.it('Cannot save deeply nested cycles', async () => {
+		await group1.adoptGroup(group2.id);
+		await group2.adoptGroup(group3.id);
+		await expect(group3.adoptGroup(group1.id), 'cyclic group insert was not rejected').to.eventually.be.rejected;
 	});
 });
