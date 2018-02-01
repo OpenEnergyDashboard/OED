ade759fcdba9a90ec08224a164362a6fa979ec72
buip <prestonbui97@gmail.com>
2018-01-31 18:15:06 -0600

    Jsonschema for preference route

--- src/server/routes/preferences.js
+++ src/server/routes/preferences.js
@@ -6,6 +6,7 @@ const express = require('express');
 const Preferences = require('../models/Preferences');
 const { log } = require('../log');
 const authentication = require('./authenticator');
+const validate = require('jsonschema').validate;
 
 const router = express.Router();
 
@@ -28,11 +29,34 @@ router.use(authentication);
  * @param user_id
  */
 router.post('/', async (req, res) => {
-	try {
-		const rows = await Preferences.update(req.body.preferences);
-		res.json(rows);
-	} catch (err) {
-		log.error(`Error while performing POST update preferences: ${err}`, err);
+	const validParams = {
+		type: 'object',
+		maxProperties: 4,
+		required: ['token', 'displayTitle', 'defaultChartToRender', 'defaultBarTracking'],
+		properties: {
+			token: {
+				type: 'string',
+			},
+			displayTitle: {
+				type: 'string'
+			},
+			defaultChartToRender: {
+				type: 'string'
+			},
+			defaultBarTracking: {
+				type: 'boolean'
+			}
+		}
+	};
+	if (!validate(req.body, validParams).valid) {
+		res.sendStatus(400);
+	} else {
+		try {
+			const rows = await Preferences.update(req.body.preferences);
+			res.json(rows);
+		} catch (err) {
+			log.error(`Error while performing POST update preferences: ${err}`, err);
+		}
 	}
 });
 
