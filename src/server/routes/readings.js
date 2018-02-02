a19fb1912c318a297c98548efe2e49cc4e3e3d9b
buip <prestonbui97@gmail.com>
2018-02-02 00:50:58 -0600

    Added jsonschema for reading routes

--- src/server/routes/readings.js
+++ src/server/routes/readings.js
@@ -8,6 +8,7 @@ const moment = require('moment');
 const Reading = require('../models/Reading');
 const TimeInterval = require('../../common/TimeInterval');
 const { log } = require('../log');
+const validate = require('jsonschema').validate;
 
 const router = express.Router();
 
@@ -31,15 +32,30 @@ function formatBarReadings(rows) {
  * @param {Date} [endDate]
  */
 router.get('/line/meters/:meter_ids', async (req, res) => {
-	// We can't do .map(parseInt) here because map would give parseInt a radix value of the current array position.
-	const meterIDs = req.params.meter_ids.split(',').map(s => parseInt(s));
-	const timeInterval = TimeInterval.fromString(req.query.timeInterval);
-	try {
-		const rawCompressedReadings = await Reading.getCompressedReadings(meterIDs, timeInterval.startTimestamp, timeInterval.endTimestamp, 100);
-		const formattedCompressedReadings = _.mapValues(rawCompressedReadings, formatLineReadings);
-		res.json(formattedCompressedReadings);
-	} catch (err) {
-		log.error(`Error while performing GET readings for line with meters ${meterIDs} with time interval ${timeInterval}: ${err}`, err);
+	const validParams = {
+		type: 'object',
+		maxProperties: 1,
+		required: ['meter_ids'],
+		properties: {
+			group_id: {
+				type: 'number'
+			}
+		}
+	};
+	if (!validate(validParams, req.params).valid) {
+		res.sendStatus(400);
+	} else {
+		// We can't do .map(parseInt) here because map would give parseInt a radix value of the current array position.
+		const meterIDs = req.params.meter_ids.split(',').map(s => parseInt(s));
+		const timeInterval = TimeInterval.fromString(req.query.timeInterval);
+		try {
+			const rawCompressedReadings = await Reading.getCompressedReadings(meterIDs, timeInterval.startTimestamp, timeInterval.endTimestamp, 100);
+			const formattedCompressedReadings = _.mapValues(rawCompressedReadings, formatLineReadings);
+			res.json(formattedCompressedReadings);
+		} catch (err) {
+			log.error(`Error while performing GET readings for line with meters ${meterIDs} with time interval ${timeInterval}: ${err}`, err);
+			res.sendStatus(500);
+		}
 	}
 });
 
@@ -50,15 +66,30 @@ router.get('/line/meters/:meter_ids', async (req, res) => {
  * @param {TimeInterval} [timeInterval]
  */
 router.get('/line/groups/:group_ids', async (req, res) => {
-	// We can't do .map(parseInt) here because map would give parseInt a radix value of the current array position.
-	const groupIDs = req.params.group_ids.split(',').map(s => parseInt(s));
-	const timeInterval = TimeInterval.fromString(req.query.timeInterval);
-	try {
-		const rawCompressedReadings = await Reading.getCompressedGroupReadings(groupIDs, timeInterval.startTimestamp, timeInterval.endTimestamp, 100);
-		const formattedCompressedReadings = _.mapValues(rawCompressedReadings, formatLineReadings);
-		res.json(formattedCompressedReadings);
-	} catch (err) {
-		log.error(`Error while performing GET readings for line with groups ${groupIDs} with time interval ${timeInterval}: ${err}`, err);
+	const validParams = {
+		type: 'object',
+		maxProperties: 1,
+		required: ['group_ids'],
+		properties: {
+			group_id: {
+				type: 'number'
+			}
+		}
+	};
+	if (!validate(validParams, req.params).valid) {
+		res.sendStatus(400);
+	} else {
+		// We can't do .map(parseInt) here because map would give parseInt a radix value of the current array position.
+		const groupIDs = req.params.group_ids.split(',').map(s => parseInt(s));
+		const timeInterval = TimeInterval.fromString(req.query.timeInterval);
+		try {
+			const rawCompressedReadings = await Reading.getCompressedGroupReadings(groupIDs, timeInterval.startTimestamp, timeInterval.endTimestamp, 100);
+			const formattedCompressedReadings = _.mapValues(rawCompressedReadings, formatLineReadings);
+			res.json(formattedCompressedReadings);
+		} catch (err) {
+			log.error(`Error while performing GET readings for line with groups ${groupIDs} with time interval ${timeInterval}: ${err}`, err);
+			res.sendStatus(500);
+		}
 	}
 });
 
@@ -70,16 +101,31 @@ router.get('/line/groups/:group_ids', async (req, res) => {
  * @param {Date} [endDate]
  */
 router.get('/bar/meters/:meter_ids', async (req, res) => {
-	// We can't do .map(parseInt) here because map would give parseInt a radix value of the current array position.
-	const meterIDs = req.params.meter_ids.split(',').map(s => parseInt(s));
-	const timeInterval = TimeInterval.fromString(req.query.timeInterval);
-	const barDuration = moment.duration(req.query.barDuration);
-	try {
-		const barchartReadings = await Reading.getBarchartReadings(meterIDs, barDuration, timeInterval.startTimestamp, timeInterval.endTimestamp);
-		const formattedBarchartReadings = _.mapValues(barchartReadings, formatBarReadings);
-		res.json(formattedBarchartReadings);
-	} catch (err) {
-		log.error(`Error while performing GET readings for bar with meters ${meterIDs} with time interval ${timeInterval}: ${err}`);
+	const validParams = {
+		type: 'object',
+		maxProperties: 1,
+		required: ['meter_ids'],
+		properties: {
+			group_id: {
+				type: 'number'
+			}
+		}
+	};
+	if (!validate(validParams, req.params).valid) {
+		res.sendStatus(400);
+	} else {
+		// We can't do .map(parseInt) here because map would give parseInt a radix value of the current array position.
+		const meterIDs = req.params.meter_ids.split(',').map(s => parseInt(s));
+		const timeInterval = TimeInterval.fromString(req.query.timeInterval);
+		const barDuration = moment.duration(req.query.barDuration);
+		try {
+			const barchartReadings = await Reading.getBarchartReadings(meterIDs, barDuration, timeInterval.startTimestamp, timeInterval.endTimestamp);
+			const formattedBarchartReadings = _.mapValues(barchartReadings, formatBarReadings);
+			res.json(formattedBarchartReadings);
+		} catch (err) {
+			log.error(`Error while performing GET readings for bar with meters ${meterIDs} with time interval ${timeInterval}: ${err}`);
+			res.sendStatus(500);
+		}
 	}
 });
 
@@ -91,17 +137,32 @@ router.get('/bar/meters/:meter_ids', async (req, res) => {
  * @param {Date} [endDate]
  */
 router.get('/bar/groups/:group_ids', async (req, res) => {
-	// We can't do .map(parseInt) here because map would give parseInt a radix value of the current array position.
-	const groupIDs = req.params.group_ids.split(',').map(s => parseInt(s));
-	const timeInterval = TimeInterval.fromString(req.query.timeInterval);
-	const barDuration = moment.duration(req.query.barDuration);
-	try {
-		const barchartReadings = await Reading.getGroupBarchartReadings(
-			groupIDs, barDuration, timeInterval.startTimestamp, timeInterval.endTimestamp);
-		const formattedBarchartReadings = _.mapValues(barchartReadings, formatBarReadings);
-		res.json(formattedBarchartReadings);
-	} catch (err) {
-		log.error(`Error while performing GET readings for bar with groups ${groupIDs} with time interval ${timeInterval}: ${err}`, err);
+	const validParams = {
+		type: 'object',
+		maxProperties: 1,
+		required: ['group_ids'],
+		properties: {
+			group_id: {
+				type: 'number'
+			}
+		}
+	};
+	if (!validate(validParams, req.params).valid) {
+		res.sendStatus(400);
+	} else {
+		// We can't do .map(parseInt) here because map would give parseInt a radix value of the current array position.
+		const groupIDs = req.params.group_ids.split(',').map(s => parseInt(s));
+		const timeInterval = TimeInterval.fromString(req.query.timeInterval);
+		const barDuration = moment.duration(req.query.barDuration);
+		try {
+			const barchartReadings = await Reading.getGroupBarchartReadings(
+				groupIDs, barDuration, timeInterval.startTimestamp, timeInterval.endTimestamp);
+			const formattedBarchartReadings = _.mapValues(barchartReadings, formatBarReadings);
+			res.json(formattedBarchartReadings);
+		} catch (err) {
+			log.error(`Error while performing GET readings for bar with groups ${groupIDs} with time interval ${timeInterval}: ${err}`, err);
+			res.sendStatus(500);
+		}
 	}
 });
 
