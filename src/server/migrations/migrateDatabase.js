a6546a0f557079ad5fd6c217d7ec60f35f3db436
buip <prestonbui97@gmail.com>
2018-02-20 13:31:32 -0600

    Finished a rough version of bfs algorithm. Did not work fully

--- src/server/migrations/migrateDatabase.js
+++ src/server/migrations/migrateDatabase.js
@@ -1,27 +1,53 @@
 const migrationList = require('./registerMigration');
 
-class Graph {
-	constructor(v) {
-		this.v = v;
+// breath first search to find path to migrate databse
+function findPathToMigrate(from, to, adjListArray) {
+	const queue = [];
+	const path = [];
+	const visited = [];
+
+	for (const vertex in adjListArray) {
+		if (Object.prototype.hasOwnProperty.call(adjListArray, vertex)) {
+			visited[vertex] = false;
+			path[vertex] = -1;
+		}
 	}
 
+	queue.push(from);
+	visited[from] = true;
 
+	while (queue.length > 0) {
+		const currentVertexID = queue.shift();
+		const currentVertex = adjListArray[currentVertexID];
+		const edges = currentVertex.length;
+		for (let i = 0; i < edges; i++) {
+			const target = currentVertex[i];
+			if (!visited[target]) {
+				visited[target] = true;
+				path[target] = currentVertexID;
+				queue.push(target);
+			}
+		}
+	}
+	return path;
 }
 
-
-const arrayLength = migrationList.length;
-
-const adjListArray = [];
-
-for (let i = 0; i < arrayLength; i++) {
-	ad
+function printPathToMigrate(s, f, path) {
+	if (s === f) {
+		console.log(`${f} `);
+	} else {
+		if (path[f] === -1) {
+			console.log('No path');
+		} else {
+			printPathToMigrate(s, path[f], path);
+			console.log(`${f} `);
+		}
+	}
 }
 
-const graph = {
-	0.1: 0.2,
-};
+const path = findPathToMigrate(0.1, 0.3, migrationList);
+console.log(path);
+printPathToMigrate(0.1, 0.3, path);
 
-for (let i = 0; i < arrayLength; i++) {
-	console.log(migrationList[i].fromVersion);
-	console.log(migrationList[i].toVersion);
-}
+// console.log(findPathToMigrate(0.1, 0.2, migrationList));
+// printPathToMigrate(0.1, 0.2, migrationList);
