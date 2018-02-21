9541b1c96edd049ce7fc958f1409cdfc7fea095e
buip <prestonbui97@gmail.com>
2018-02-20 23:06:08 -0600

    Modify and update migration table

--- src/server/migrations/migrateDatabase.js
+++ src/server/migrations/migrateDatabase.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 const migrationList = require('./registerMigration');
 
 /**
@@ -27,7 +31,9 @@ function findPathToMigrate(curr, to, adjListArray) {
 	const queue = [];
 	const path = [];
 	const visited = [];
+
 	checkIfFromAndToExist(curr, to, adjListArray);
+
 	for (const vertex in adjListArray) {
 		if (Object.prototype.hasOwnProperty.call(adjListArray, vertex)) {
 			visited[vertex] = false;
---
+++ src/server/sql/migration/create_migration_table.sql
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+-- create migration table
+CREATE TABLE IF NOT EXISTS migrations (
+	id SERIAL PRIMARY KEY,
+	fromVersion VARCHAR(20) NOT NULL,
+	toVersion VARCHAR(20) NOT NULL,
+	update_timestamp TIMESTAMP NOT NULL
+);
