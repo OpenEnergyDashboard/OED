9f533e37105669e3e2e0f363023eb2fc741f1c7b
buip <prestonbui97@gmail.com>
2018-02-20 23:14:07 -0600

    create insert migration sql

--- src/server/sql/migration/insert_new_migration.sql
+++ src/server/sql/migration/insert_new_migration.sql
@@ -2,10 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
--- create migration table
-CREATE TABLE IF NOT EXISTS migrations (
-	id SERIAL PRIMARY KEY,
-	from_version VARCHAR(20) NOT NULL,
-	to_version VARCHAR(20) NOT NULL,
-	update_timestamp TIMESTAMP NOT NULL
-);
+INSERT INTO migrations(from_version, to_version, update_timestamp)
+VALUES (${from_version}, ${to_version}, ${update_timestamp});
