1a41c15ddcc98f2c970f6887d7a1d45b9d0e4327
= <fujitas@beloit.edu>
2018-09-20 22:22:48 -0500

    Revert "Revert "Fix a test error for creating btree_gist extension""
    This reverts commit ab8e87b769c9b56a5d54cc26198aec565364c7b4.

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-CREATE USER oed WITH PASSWORD 'opened';
+CREATE USER oed WITH SUPERUSER PASSWORD 'opened';
 CREATE DATABASE oed WITH OWNER oed;
 CREATE DATABASE oed_testing WITH OWNER oed;
 
