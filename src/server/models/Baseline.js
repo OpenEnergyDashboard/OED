d4f9ef6eb20e9485341bd48a41be164f63370539
James McFeeters <jameeters@gmail.com>
2018-10-04 15:04:32 -0400

    Fix baseline model to work with db wrapper

--- src/server/models/Baseline.js
+++ src/server/models/Baseline.js
@@ -4,7 +4,7 @@
  */
 const database = require('./database');
 const TimeInterval = require('../../common/TimeInterval');
-const db = database.db;
+const getDB = database.getDB;
 const sqlFile = database.sqlFile;
 class Baseline {
 	constructor(meterID, applyStart, applyEnd, calcStart, calcEnd, baselineValue = null) {
@@ -14,13 +14,13 @@ class Baseline {
 		this.baselineValue = baselineValue;
 	}
 	static createTable() {
-		return db.none(sqlFile('baseline/create_baseline_table.sql'));
+		return getDB().none(sqlFile('baseline/create_baseline_table.sql'));
 	}
 	static mapRow(row) {
 		return new Baseline(row.meter_id, row.apply_start, row.apply_end, row.calc_start, row.calc_end, row.baseline_value);
 	}
-	async insert(conn = db) {
-		const resp = await conn.one(sqlFile('baseline/new_baseline.sql'), {
+	async insert(conn = getDB) {
+		const resp = await conn().one(sqlFile('baseline/new_baseline.sql'), {
 			meter_id: this.meterID,
 			apply_start: this.applyRange.startTimestamp,
 			apply_end: this.applyRange.endTimestamp,
@@ -29,12 +29,12 @@ class Baseline {
 		});
 		this.baselineValue = resp.baseline_value;
 	}
-	static async getAllForMeterID(meterID, conn = db) {
-		const rows = await conn.any(sqlFile('baseline/get_baselines_by_meter_id.sql'), { meter_id: meterID });
+	static async getAllForMeterID(meterID, conn = getDB) {
+		const rows = await conn().any(sqlFile('baseline/get_baselines_by_meter_id.sql'), { meter_id: meterID });
 		return rows.map(row => Baseline.mapRow(row));
 	}
-	static async getAllBaselines(conn = db) {
-		const rows = await conn.any(sqlFile('baseline/get_all_baselines.sql'));
+	static async getAllBaselines(conn = getDB) {
+		const rows = await conn().any(sqlFile('baseline/get_all_baselines.sql'));
 		return rows.map(row => Baseline.mapRow(row));
 	}
 }
