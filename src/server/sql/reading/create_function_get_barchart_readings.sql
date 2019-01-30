f6f5b3fcc7682ffce70c0d795abc20497b0f9fbb
Cameron Kuchta <kuchtact@beloit.edu>
2019-01-30 12:13:29 -0600

    Remove todo.

--- src/server/sql/reading/create_function_get_barchart_readings.sql
+++ src/server/sql/reading/create_function_get_barchart_readings.sql
@@ -26,7 +26,6 @@ CREATE OR REPLACE FUNCTION barchart_readings(
 
 	BEGIN
 		-- Select the time of the earliest reading in the readings table and the latest and put them into the real_timestamps.
-		-- TODO: Should this be changed into finding the earliest and latest for just the meters that are being looked at?
 		SELECT
 			greatest(MIN(readings.start_timestamp), from_timestamp), least(MAX(readings.end_timestamp), to_timestamp)
 		INTO real_start_timestamp, real_end_timestamp
