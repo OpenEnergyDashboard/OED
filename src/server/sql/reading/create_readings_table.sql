0d10f7de1f4a13415912744f42962cc0c5c390c3
Christian <farescs@beloit.edu>
2019-01-30 14:43:58 -0600

    Added documentation to some reading sql files.

--- src/server/sql/reading/create_readings_table.sql
+++ src/server/sql/reading/create_readings_table.sql
@@ -2,7 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
--- create readings table
+-- Create a readings table that has attributes of meter_id, reading, start and end timestamps and a composite key of
+-- meter_id and start_timestamp.
 CREATE TABLE IF NOT EXISTS readings (
   meter_id INT NOT NULL REFERENCES meters(id),
   reading INT NOT NULL,
--- src/server/sql/reading/get_readings_by_meter_id_and_date_range.sql
+++ src/server/sql/reading/get_readings_by_meter_id_and_date_range.sql
@@ -2,10 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+-- Gets meter readings by id and date range.  This is then ordered by time ascending.
 SELECT
   meter_id, reading, start_timestamp, end_timestamp
 FROM readings
 WHERE meter_id = ${meterID}
   AND start_timestamp >= COALESCE(${startDate}, '-infinity'::TIMESTAMP)
-	AND end_timestamp <= COALESCE(${endDate}, 'infinity'::TIMESTAMP)
+  AND end_timestamp <= COALESCE(${endDate}, 'infinity'::TIMESTAMP)
 ORDER BY start_timestamp ASC;
