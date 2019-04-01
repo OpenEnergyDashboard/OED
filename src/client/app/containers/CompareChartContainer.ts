2fdb4c0718718ca70aeedbdadc990c8bb5d185ee
Corina <sterbetc@beloit.edu>
2019-03-31 23:54:19 -0500

    The compare graph works with plotly
    At this point, the 'Week' and '4 Week' graphs are displayed without any problems. The 'Day' graph is not loading and I suspect that that will be solved when the pull request "compare with shift" is intergated into my branch.

--- src/client/app/containers/CompareChartContainer.ts
+++ src/client/app/containers/CompareChartContainer.ts
@@ -4,35 +4,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { Bar, LinearComponentProps } from 'react-chartjs-2';
-import { ChartData, ChartDataSets, LinearTickOptions, ChartTooltipItem } from 'chart.js';
-import * as datalabels from 'chartjs-plugin-datalabels';
 import { connect } from 'react-redux';
 import { State } from '../types/redux/state';
 import { getComparePeriodLabels, getCompareChangeSummary } from '../utils/calculateCompare';
 import { CompareEntity } from './MultiCompareChartContainer';
 import translate from '../utils/translate';
 import {CompressedBarReading} from '../types/compressed-readings';
-
-if (datalabels === null || datalabels === undefined) {
-	throw new Error('Datalabels plugin was tree-shaken out.');
-}
-
-interface ChartDataSetsWithDatalabels extends ChartDataSets {
-	datalabels: {
-		anchor: string;
-		align: string;
-	};
-}
+import PlotlyChart, { IPlotlyChartProps } from 'react-plotlyjs-ts';
 
 interface CompareChartContainerProps {
 	entity: CompareEntity;
 }
 
-function mapStateToProps(state: State, ownProps: CompareChartContainerProps): LinearComponentProps {
-	const comparePeriod = state.graph.comparePeriod;
-	const datasets: ChartDataSetsWithDatalabels[] = [];
-	const labels: string[] = [];
+function mapStateToProps(state: State, ownProps: CompareChartContainerProps): IPlotlyChartProps {
+	const comparePeriod = state.graph.comparePeriod;	
+	const datasets: any[] = [];
 
 	const periodLabels = getComparePeriodLabels(comparePeriod);
 
@@ -42,132 +28,111 @@ function mapStateToProps(state: State, ownProps: CompareChartContainerProps): Li
 		}
 		return 'red';
 	};
+
 	// Compose the text to display to the user.
 	const entity = ownProps.entity;
 	const changeSummary = getCompareChangeSummary(entity.change, entity.name, periodLabels);
 
-	labels.push(periodLabels.prev);
-	labels.push(periodLabels.current);
 	const readingsAfterCurrentTimeColor = 'rgba(173, 216, 230, 1)';
 	const readingsBeforeCurrentTimeColor = 'rgba(218, 165, 32, 1)';
 	const projectedDataColor = 'rgba(173, 216, 230, 0.45)';
+
+	const previousPeriodTotal = Math.round(entity.lastPeriodTotalUsage);
+	const currentPeriodProjectedTotal = Math.round((entity.currentPeriodUsage / entity.usedToThisPointLastTimePeriod) * entity.lastPeriodTotalUsage);
+	const previousPeriodToThisPoint = Math.round(entity.usedToThisPointLastTimePeriod);
+	const currentPeriodToThisPoint = Math.round(entity.currentPeriodUsage);
+
 	datasets.push(
-		{
-			data: [entity.lastPeriodTotalUsage, Math.round((entity.currentPeriodUsage / entity.usedToThisPointLastTimePeriod) * entity.lastPeriodTotalUsage)],
-			datalabels: {
-				anchor: 'end',
-				align: 'start'
-			}
-		}, {
-			data: [entity.usedToThisPointLastTimePeriod, entity.currentPeriodUsage],
-			datalabels: {
-				anchor: 'end',
-				align: 'start'
-			}
-		}
-	);
-	// sorts the data so that one doesn't cover up the other
-	datasets.sort((a, b) => {
-		if (a.data !== undefined && b.data !== undefined) {
-			return +(a.data[0]) - +(b.data[0]);
-		} else {
-			return 0;
-		}
-	});
-
-
-	// apply info to datasets after sort
-	datasets[0].backgroundColor = [readingsBeforeCurrentTimeColor, readingsBeforeCurrentTimeColor];
-	datasets[0].hoverBackgroundColor = [readingsBeforeCurrentTimeColor, readingsBeforeCurrentTimeColor];
-	datasets[1].backgroundColor = [readingsAfterCurrentTimeColor, projectedDataColor];
-	datasets[1].hoverBackgroundColor = [readingsAfterCurrentTimeColor, projectedDataColor];
-
-
-	const data: ChartData = {datasets, labels};
-	const ticks: LinearTickOptions = {
-		beginAtZero: true
-	};
-	const options = {
-		animation: {
-			duration: 0
-		},
-		elements: {
-			point: {
-				radius: 0
+		{			
+			x: [periodLabels.prev, periodLabels.current],
+			y: [previousPeriodTotal, currentPeriodProjectedTotal],			
+			hovertext: [
+				`<b>${previousPeriodTotal} KW</b> ${translate('total')}<br>${periodLabels.prev.toLowerCase()}`,
+				`<b>${currentPeriodProjectedTotal} KW</b> ${translate('projected.to.be.used')}<br>${periodLabels.current.toLowerCase()}`
+			],
+			hoverinfo: 'text',
+			type: 'bar',
+			marker: {color: readingsAfterCurrentTimeColor},
+			text: [ `<b>${previousPeriodTotal} kW</b>`, `<b>${currentPeriodProjectedTotal} kW</b>`],
+			textposition: 'auto',
+			textfont: {    
+			  color: 'rgba(0,0,0,1)'
 			}
 		},
-		scales: {
-			xAxes: [{
-				stacked: true,
-				gridLines: {
-					display: true
-				}
-			}],
-			yAxes: [{
-				stacked: false,
-				scaleLabel: {
-					display: true,
-					labelString: 'kW'
-				},
-				ticks
-			}]
+		{			
+			x: [periodLabels.prev, periodLabels.current],
+			y: [previousPeriodToThisPoint, currentPeriodToThisPoint],			
+			hovertext: [
+				`<b>${previousPeriodToThisPoint} kW</b> ${translate('used.this.time')}<br>${periodLabels.prev.toLowerCase()}`,
+				`<b>${currentPeriodToThisPoint} KW</b> ${translate('used.so.far')}<br>${periodLabels.current.toLowerCase()}`
+			],
+			hoverinfo: 'text',
+			type: 'bar',
+			marker: {color: readingsBeforeCurrentTimeColor},			
+			text: [ `<b>${previousPeriodToThisPoint} kW</b>`, `<b>${currentPeriodToThisPoint} kW</b>`],
+			textposition: 'auto',
+			textfont: {    
+			  color: 'rgba(0,0,0,1)'
+			}
 		},
+	)
+
+	// // sorts the data so that one doesn't cover up the other
+	// datasets.sort((a, b) => {
+	// 	if (a.data !== undefined && b.data !== undefined) {
+	// 		return +(a.data[0]) - +(b.data[0]);
+	// 	} else {
+	// 		return 0;
+	// 	}
+	// });
+
+
+	// // apply info to datasets after sort
+	// datasets[0].backgroundColor = [readingsBeforeCurrentTimeColor, readingsBeforeCurrentTimeColor];
+	// datasets[0].hoverBackgroundColor = [readingsBeforeCurrentTimeColor, readingsBeforeCurrentTimeColor];
+	// datasets[1].backgroundColor = [readingsAfterCurrentTimeColor, projectedDataColor];
+	// datasets[1].hoverBackgroundColor = [readingsAfterCurrentTimeColor, projectedDataColor];
+
+	const layout: any = {
+		title: `<b>${changeSummary}</b>`,
+		titlefont: {
+			size: 10,
+			color: colorize(entity.change)
+		},
+		hovermode:'closest',
+		barmode: 'overlay',
+		autozise: true,		
+		showlegend: false,
 		legend: {
-			display: false
 		},
-		tooltips: {
-			mode: 'nearest',
-			intersect: false,
-			backgroundColor: 'rgba(0,0,0,0.6)',
-			displayColors: false,
-			callbacks: {
-				label: (tooltipItem: ChartTooltipItem, data: ChartData) => { // tslint:disable-line no-shadowed-variable
-					const usage = tooltipItem.yLabel;
-					const usedThisTime = data.datasets![0].data![0];
-					const usedSoFar = data.datasets![0].data![1];
-					const totalUsed = data.datasets![1].data![0];
-					const labelText = tooltipItem.xLabel!.toLowerCase();
-					switch (usage) {
-						case usedThisTime:
-							return `${usage} kW ${translate('used.this.time')} ${labelText}`;
-						case usedSoFar:
-							return `${usage} kW ${translate('used.so.far')} ${labelText}`;
-						case totalUsed:
-							return `${usage} kW ${translate('total')} ${labelText}`;
-						default:
-							return `${usage} kW ${translate('projected.to.be.used')} ${labelText}`;
-					}
-				},
-				title: () => ''
-			}
+		yaxis: {
+			title: 'kW',
+			showgrid: true,
+			gridcolor: '#ddd',			
 		},
-		title: {
-			display: true,
-			text: changeSummary,
-			fontColor: colorize(entity.change)
+		xaxis: {					
+			showgrid: false,
+			gridcolor: '#ddd'
 		},
-		plugins: {
-			datalabels: {
-				color: 'black',
-				font: {
-					weight: 'bold'
-				},
-				display: true,
-				formatter: (value: number) => `${value} kW`
-			}
+		margin: {
+			t: 20,
+			b: 120,
+			l: 60,
+			r: 20,
 		}
 	};
 
-
-	const props: LinearComponentProps = {
-		data,
-		options,
-		redraw: true
+	// Assign all the paramaters required to create the Plotly object (data, layout, config) to the variable props, returned by mapStateToProps
+	// The Plotly toolbar is displayed if displayModeBar is set to true
+	const props: IPlotlyChartProps = {
+		data: datasets,
+		layout,
+		config: {
+			displayModeBar: false
+		}
 	};
 
 	return props;
 }
 
-// Escape from TypeScript here. TypeScript doesn't like the fact that Bar is non typed.
-const barConstructor: any = Bar;
-export default connect(mapStateToProps)(barConstructor);
+export default connect(mapStateToProps)(PlotlyChart);
