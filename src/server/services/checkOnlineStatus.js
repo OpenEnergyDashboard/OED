e3dc8c163ccc27ebe71876206717abb5fd5676a0
Jerry <ngohongphuc2001@gmail.com>
2020-02-25 15:45:02 -0600

    Fixing trailing whitespace, indentation

--- src/server/services/checkOnlineStatus.js
+++ src/server/services/checkOnlineStatus.js
@@ -4,20 +4,20 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-//This script checks whether the server is online or not using Javascript. 
+//This script checks whether the server is online or not using Javascript.
 //This script may encounter redirecting problem due to the web policy
 
 const { log } = require('../log');
 var script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
-document.body.appendChild(script);	
+document.body.appendChild(script);
 
 $.ajax({
 	type: "HEAD",
 	url: 'YOUR SITE URL',
 	success : function() {
-	    log.info('The server is still alive');
+		log.info('The server is still alive');
 	}, error: function() {
-	    log.error('The server is down');
+		log.error('The server is down');
 	}
 });
