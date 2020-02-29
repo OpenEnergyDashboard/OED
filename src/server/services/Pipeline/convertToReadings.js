fc3a3916fa32298e1166cd30cabce7b8bac403ca
Ha Truong <hatruong@Has-MacBook.local>
2020-02-29 17:20:31 -0600

    Starting code for pipelined Reading validation

---
+++ src/server/services/Pipeline/convertToReadings.js
@@ -0,0 +1,31 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const Reading = require('./../models/Reading');
+const { log } = require('../log');
+
+/**
+ * Convert an matrix of number values to an array of Readings
+ * @param {matrix of numbers} toConvert a number array in which each row represents a Reading values
+ * @param {ipAddress} meter IP address of the meter which all the Reading values belong to
+ * @param {number} maxVal maximum acceptable reading value 
+ * @param {number} minVal minimum acceptable reading value
+ * @param {date} minDate earliest acceptable date
+ * @param {date} maxDate latest acceptable date
+ * @param {boolean} equalInterval true if expecting equal intervals, otherwise false
+ */
+
+function convertToReadings(toConvert, ipAddress, maxVal, minVal, minDate, maxDate, equalInterval) {
+	readings = toConvert.map(row => new Reading(ipAddress,row[0],row[1],row[2]));
+	if (!validateReading(arrayOfReadings, maxVal, minVal, minDate, maxDate, equalInterval)) {
+		log.error(`ERROR WHEN VALIDATING READINGS FROM METER ${meter.id}`);
+		return null;
+	}
+	return arrayOfReadings;
+ }
+
+ module.exports = convertToReadings;
+
---
+++ src/server/services/Pipeline/validateReadings.js
@@ -0,0 +1,58 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const Reading = require('./../models/Reading');
+const { log } = require('../log');
+
+/**
+ * Validate an array of Readings value according to certain criteria
+ * @param {array of Readings} arrayToValidate 
+ * @param {number} maxVal maximum acceptable reading value 
+ * @param {number} minVal minimum acceptable reading value
+ * @param {Moment} minDate earliest acceptable date
+ * @param {Moment} maxDate latest acceptable date
+ * @param {boolean} equalInterval true if expecting equal intervals, otherwise false
+ */
+
+function validateReadings(arrayToValidate, maxVal, minVal, minDate, maxDate, equalInterval) {
+	validDates = checkDate(arrayToValidate, minDate, maxDate);
+	validValues = checkValue(arayToValidate, minVal, maxVal);
+	validIntervals = checkIntervals(arrayToValidate, equalInterval);
+	return validDates && validValues && validIntervals;
+}
+
+function checkDate(arrayToValidate, minDate, maxDate) {
+	for (reading in arrayToValidate) {
+		if (reading.startTimestamp.isBefore(minDate)) {
+			log.error(`ERROR WHEN CHECKING READING TIME FROM METER ${reading.meterID}: Time ${reading.startTimestamp} is earlier than lower bound ${minDate}`);
+			return false;
+		}
+		if (reading.endTimestamp.isAfter(maxDate)) {
+			log.error(`ERROR WHEN CHECKING READING TIME FROM METER ${reading.meterID}: Time ${reading.startTimestamp} is later than upper bound ${maxDate}`);
+			return false;
+		}
+	}cocov
+	return true;
+}
+
+function checkValue(arrayToValidate, minVal, maxVal) {
+	for (reading in arrayToValidate) {
+		if (reading.reading < minVal) {
+			log.error(`ERROR WHEN CHECKING READING VALUE FROM METER ${reading.meterID}: ${reading.reading} is smaller than lower bound ${minVal}`);
+			return false;
+		} else if (reading.reading > maxDate) {
+			log.error(`ERROR WHEN CHECKING READING VALUE FROM METER ${reading.meterID}: ${reading.reading} is larger than upper bound ${maxVal}`);
+			return false;
+		}
+	}
+	return true;	
+}
+
+function checkIntervals(arrayToValidate, equalInterval) {
+	if (!equalInterval) return true;
+
+}
+module.exports = validateReadings;
