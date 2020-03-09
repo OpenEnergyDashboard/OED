d240aa765769a218c270f4e77e514d3f57392fe9
truonghh99 <truonghh@beloit.edu>
2020-03-08 19:01:14 -0700

    Fix lint errrỏ

--- src/server/test/db/convertToReadingsTests.js
+++ src/server/test/db/convertToReadingsTests.js
@@ -3,21 +3,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-const convertToReadings = require('../../services/Pipeline/convertToReadings')
+const convertToReadings = require('../../services/Pipeline/convertToReadings');
 const Reading = require('../../models/Reading');
 
 mocha.describe('Convert array of values to array of Readings', () => {
 	mocha.it('Convert array of array to array of Reading values', async () => {
-        let sampleArray = [[0, moment('1980-01-01 00:00:00'), moment('1990-01-01 00:00:00')],
-                            [1, moment('2000-01-01 00:00:00'), moment('2000-01-01 00:00:00')],
-                            [2, moment('2010-01-01 00:00:00'), moment('2020-01-01 00:00:00')]];
-        let minDate = moment('1970-01-01 00:00:00');
-        let maxDate = moment('2970-01-01 00:00:00');
-        let result = convertToReadings(sampleArray, undefined, Number.MAX_VALUE, Number.MIN_VALUE, minDate, maxDate);
-        for (let i = 0; i < result.length; ++i) {
-            expect(result[i].reading).to.equal(sampleArray[i][0]);
-            expect(result[i].startTimeStamp).to.equal(sammpleArray[i][1]);
-            expect(result[i].endTimeStamp.to.equal(sampleArray[i][2]));
-        }
+		let sampleArray = [[0, moment('1980-01-01 00:00:00'), moment('1990-01-01 00:00:00')],
+							[1, moment('2000-01-01 00:00:00'), moment('2000-01-01 00:00:00')],
+							[2, moment('2010-01-01 00:00:00'), moment('2020-01-01 00:00:00')]];
+		let minDate = moment('1970-01-01 00:00:00');
+		let maxDate = moment('2970-01-01 00:00:00');
+		let result = convertToReadings(sampleArray, undefined, Number.MAX_VALUE, Number.MIN_VALUE, minDate, maxDate);
+		for (let i = 0; i < result.length; ++i) {
+			expect(result[i].reading).to.equal(sampleArray[i][0]);
+			expect(result[i].startTimeStamp).to.equal(sammpleArray[i][1]);
+			expect(result[i].endTimeStamp.to.equal(sampleArray[i][2]));
+		}
 	});
 });
