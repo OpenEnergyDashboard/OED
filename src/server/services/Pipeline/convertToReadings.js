73f4aa3f286381be9c1d9f6fd69dd5012438bdad
truonghh99 <truonghh@beloit.edu>
2020-03-16 13:54:45 -0700

    debug convertToReadings

--- src/server/services/Pipeline/convertToReadings.js
+++ src/server/services/Pipeline/convertToReadings.js
@@ -7,6 +7,7 @@
 const Reading = require('../../models/Reading');
 const { log } = require('../../log');
 const { validateReadings } = require('./validateReadings');
+const moment = require('moment');
 
 /**
  * Convert an matrix of number values to an array of Readings
