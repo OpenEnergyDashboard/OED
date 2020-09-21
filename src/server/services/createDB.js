e05576a61ee03cd7f34e1574aa4a4cfca12e273e
Steven Huss-Lederman <huss@beloit.edu>
2020-09-21 11:51:59 -0500

    make install handle DB errors better
    The install script continued even when createdb errors occurred.
    
    createDB.js now returns an exit code so know its status.
    
    installOED.sh now considers createDB exit code. If there is an error
    that is not known and recoverable then it exits with error code 3.
    You can override with new argument of --continue_on_db_error.
    This can be useful if you want to use the web container without DB
    when install will not work.
    Script outputs more information on what it is doing.
    Script consistently uses printf instead of echo.
    Space indenting removed for tabs.
    
    Note there are debug statements present that will be removed later.
    
    Also note that createUser should probably work the same way but
    it seems to require a change in how it deals with errors so this was
    delayed. This seems less important because DB errors seem to
    always happen first on createDB and then createUser runs okay.

--- src/server/services/createDB.js
+++ src/server/services/createDB.js
@@ -6,13 +6,21 @@ const { createSchema } = require('../models/database');
 const { log } = require('../log');
 const { getConnection, dropConnection } = require('../db');
 (async function createSchemaWrapper() {
+	console.log("DEBUG: about to do getConnection");
 	const conn = getConnection();
 	try {
+		console.log("DEBUG: begin createSchema in createDB.js");
 		await createSchema(conn);
+		console.log("DEBUG: finish createSchema in createDB.js");
 		log.info('Schema created', skipMail = true);
+		process.exitCode = 0;
 	} catch (err) {
+		// Should we really catch all errors and then allow the code to continue?? TODO
+		console.log(`DEBUG: in err clause createSchema in createDB.js with err/stack: ${err.stack}: end err/stack`);
 		log.error(`Error creating schema: ${err}`, err, skipMail = true);
+		process.exitCode = 1;
 	} finally {
+		console.log("DEBUG: in finally clause createSchema in createDB.js");
 		dropConnection();
 	}
 }());
