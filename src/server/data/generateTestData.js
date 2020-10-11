7baa7b6de02d289da103f7edc6b60101ae6ffde1
Davin Lin <lindavin@grinnell.edu>
2020-10-11 14:39:09 -0400

    Remove parameters from ending } and added default parameter for isEpsilon

--- src/server/data/generateTestData.js
+++ src/server/data/generateTestData.js
@@ -11,6 +11,11 @@
  *
  */
 
+// SHL: I think this should be TypeScript with added types.
+
+// SHL: I don't see where this might allow making the value not be -1 to 1.
+// Meter data generally goes from 0 to some max value.
+
 // Imports
 const fs = require('fs');
 const stringify = require('csv-stringify');
@@ -20,10 +25,15 @@ const moment = require('moment');
 // Global Constants
 var TWO_PI = Math.PI * 2;
 
+// SHL: I'm an advocate for big picture comments on how the software features are designed to work.
+// This would be the overall design and why it works. I think that helps a lot in understanding what 
+// is being done. This is not a requirement and could be a separate document if appropriate.
+
 /**
  * 
  * @param {String} startDate String of the form 'YYYY-MM-DD HH:MM:SS'
  * @param {String} endDate String of the form 'YYYY-MM-DD HH:MM:SS'
+ * SHL: maybe make default 20 minutes.
  * @param {Object} timeStep Object with keys describe the time step, by default 
  * this is { ms: 15000 } or 15 seconds. 
  * 
@@ -32,6 +42,7 @@ var TWO_PI = Math.PI * 2;
  * the timeStep should also be at least 1 second. 
  */
 function generateDates(startDate, endDate, timeStep = { ms: 15000 }) {
+	// SHL: check timeStep is at least 1 second and maybe error if not (or return empty values?).
 	const array_of_moments = [];
 	const startMoment = moment(startDate);
 	const endMoment = moment(endDate);
@@ -40,22 +51,25 @@ function generateDates(startDate, endDate, timeStep = { ms: 15000 }) {
 		startMoment.add(timeStep);
 	}
 	return array_of_moments;
-} // generateDates(String,String,Object)
+} // generateDates
 
 /**
  * 
  * @param {moment} startTime should not be after the endTime: !startTime.isAfter(endTime) should return true 
  * @param {moment} endTime should or be before startTime: !endTime.isBefore(startTime) should return true 
- * @param {moment} moment should be in between startTime and endTime 
+ * @param {moment} currentMoment should be in between startTime and endTime 
  * 
  * Determine what percentage of elapsed time passed that is at what percentage 
  * if the moment between startTime and endTime.
  * Source: https://stackoverflow.com/questions/18960327/javascript-moment-js-calculate-percentage-between-two-dates
  */
-function moment_percentage(startTime, endTime, moment) {
+function _momentPercentage(startTime, endTime, currentMoment) {
+	// SHL: I really believe in using {} for all statements, even one line ifs.
 	if (endTime - startTime <= 0) return 1;
-	return (moment - startTime) / (endTime - startTime);
-} // moment_percentage(moment,moment,moment)
+	// SHL: if someone makes a mistake and moment < startTime then you get a negative %.
+	// Would it be better to check for this as you do endTime - startTime above?
+	return (currentMoment - startTime) / (endTime - startTime);
+} // _momentPercentage
 
 /**
  * 
@@ -74,11 +88,11 @@ function momenting(array_of_moments, period_length) {
 		while (moment.isAfter(endMoment)) {
 			startMoment.add(period_length);
 			endMoment.add(period_length);
-		}
-		return (moment_percentage(startMoment, endMoment, moment))
+		};
+		return (_momentPercentage(startMoment, endMoment, moment));
 	});
 	return result;
-} // momenting(Array[moment],Object)
+} // momenting
 
 /**
  * 
@@ -87,10 +101,12 @@ function momenting(array_of_moments, period_length) {
  * Checks if a number is really close to zero.
  * Source: https://www.quora.com/In-JavaScript-how-do-I-test-if-a-number-is-close-to-zero
  */
-function isEpsilon(number) {
-	return Math.abs(number) < 1e-10;
-} // isEpisilon(Number)
+function isEpsilon(number, epsilon = 1e-10) {
+	return Math.abs(number) < epsilon;
+} // isEpisilon
 
+// SHL: should you describe the format of the Time's? (I think they are allowed moment format?)
+// SHL: Your descriptions of methods don't include return info.
 /**
  * 
  * @param {String} startTimeStamp 
@@ -98,7 +114,7 @@ function isEpsilon(number) {
  * @param {Object} options controls the timeStep and the period_length 
  * 
  * Generates sine data over a period of time. By default the timeStep is 1 day 
- * and the peiord_length is one day.
+ * and the period_length is one day.
  */
 function _generateSineData(startTimeStamp, endTimeStamp, options = { timeStep: { hour: 12 }, period_length: { day: 1 } }) {
 	const defaultOptions = {
@@ -108,13 +124,14 @@ function _generateSineData(startTimeStamp, endTimeStamp, options = { timeStep: {
 	}
 	const dates = generateDates(startTimeStamp, endTimeStamp, defaultOptions.timeStep);
 	const dates_as_moments = dates.map(date => moment(date));
+	// SHL: I think some comments here (and maybe elsewhere) would help other more readily understand this.
 	const sineValues = momenting(dates_as_moments, defaultOptions.period_length)
 		.map(x => {
 			const result = Math.sin(Math.PI * 2 * x);
 			return (isEpsilon(result) ? '0' : `${result}`)
 		});
 	return (_.zip(dates, sineValues));
-} // _generateSineData(String,String,Object)
+} // _generateSineData
 
 /** data needs to be of form
  * 
@@ -130,7 +147,7 @@ function _generateSineData(startTimeStamp, endTimeStamp, options = { timeStep: {
  * https://csv.js.org/stringify/api/
  * https://stackoverflow.com/questions/2496710/writing-files-in-node-js
  */
-async function write_to_csv(data, filename = 'test.csv') {
+function write_to_csv(data, filename = 'test.csv') {
 	stringify(data, (err, output) => {
 		if (err) {
 			return console.log(err);
@@ -142,8 +159,9 @@ async function write_to_csv(data, filename = 'test.csv') {
 			console.log("The file was saved!");
 		});
 	});
-} // write_to_csv(Array[Array[String, String]],String)
+} // write_to_csv
 
+// SHL: should options be described?
 /**
  * 
  * @param {String} startTimeStamp 
@@ -152,10 +170,10 @@ async function write_to_csv(data, filename = 'test.csv') {
  * 
  * Creates a csv with sine data 
  */
-async function generateSine(startTimeStamp, endTimeStamp, options = { filename: 'test.csv', timeStep: { hour: 12 }, period_length: { day: 1 } }) {
+function generateSine(startTimeStamp, endTimeStamp, options = { filename: 'test.csv', timeStep: { hour: 12 }, period_length: { day: 1 } }) {
 	const chosen_data_options = { timeStep: options.timeStep, period_length: options.period_length };
 	write_to_csv(_generateSineData(startTimeStamp, endTimeStamp, chosen_data_options), options.filename);
-} // generateSine(String,String,Object)
+} // generateSine
 
 module.exports = {
 	generateDates,
