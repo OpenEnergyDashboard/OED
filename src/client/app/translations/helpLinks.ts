8b575a37a65a875091f010111cd39f49acdeb328
Davin Lin <lindavin@grinnell.edu>
2020-10-31 21:47:02 -0400

    Add links object file

---
+++ src/client/app/translations/helpLinks.ts
@@ -0,0 +1,14 @@
+const links: Record<string, string> = {
+	"help.home.chart.plotly.controls": "https://plotly.com/chart-studio-help/getting-to-know-the-plotly-modebar/",
+	"help.home.chart.redraw.restore": "https://openenergydashboard.github.io/help/graphing",
+	"help.home.chart.select": "https://openenergydashboard.github.io/help/graphs",
+	"help.home.export.graph.data": "https://openenergydashboard.github.io/help/general#export",
+	"help.home.header": "https://openenergydashboard.github.io/help/admins",
+	"help.home.hide.or.show.options": "https://openenergydashboard.github.io/help/general#hideOptions",
+	"help.home.select.groups": "https://openenergydashboard.github.io/help/metersAndGroups",
+	"help.home.select.meters": "https://openenergydashboard.github.io/help/metersAndGroups",
+	"help.home.toggle.chart.link": "https://openenergydashboard.github.io/help/general#chartlink",
+	"help": "help"
+}
+
+export default links;
