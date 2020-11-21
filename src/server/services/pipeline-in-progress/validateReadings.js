0c50cae0c81e3e22f22c3e18c4335fcb4594c1c8
truonghh99 <truonghh@beloit.edu>
2020-11-20 21:31:32 -0800

    Clarify logi in validateReadings

--- src/server/services/pipeline-in-progress/validateReadings.js
+++ src/server/services/pipeline-in-progress/validateReadings.js
@@ -22,8 +22,8 @@ function validateReadings(arrayToValidate, conditionSet) {
 /**
  * Check and report any out-of-bound date. Can be ignored by passing null minDate and maxDate
  * @param {array of Readings} arrayToValidate
- * @param {Moment} minDate earliest acceptable date
- * @param {Moment} maxDate latest acceptable date
+ * @param {Moment} minDate inclusive earliest acceptable date (won't be rejected)
+ * @param {Moment} maxDate inclusive latest acceptable date (won't be rejected)
  * @param {number} maxError maximum number of errors to be reported, ignore the rest
  */
 function checkDate(arrayToValidate, minDate, maxDate, maxError) {
@@ -35,12 +35,12 @@ function checkDate(arrayToValidate, minDate, maxDate, maxError) {
 		if (maxError <= 0) {
 			break;
 		}
-		if (reading.startTimestamp.isBefore(minDate)) {
+		if (reading.startTimestamp < minDate) {
 			log.warn(`ERROR WHEN CHECKING READING TIME FROM METER ${reading.meterID}: Time ${reading.startTimestamp} is earlier than lower bound ${minDate}`);
 			--maxError;
 			validDates = false;
 		}
-		if (reading.endTimestamp.isAfter(maxDate)) {
+		if (reading.endTimestamp > maxDate) {
 			log.warn(`ERROR WHEN CHECKING READING TIME FROM METER ${reading.meterID}: Time ${reading.startTimestamp} is later than upper bound ${maxDate}`);
 			--maxError;
 			validDates = false;
@@ -52,8 +52,8 @@ function checkDate(arrayToValidate, minDate, maxDate, maxError) {
 /**
  * Check and report any out-of-bound reading value. Can be ignored by passing MIN_VALUE & MAX_VALUE
  * @param {array of Readings} arrayToValidate
- * @param {number} minVal maximum acceptable reading value
- * @param {number} maxVal minimum acceptable reading value
+ * @param {number} minVal inclusive minimum acceptable reading value (won't be rejected)
+ * @param {number} maxVal inclusive maximum acceptable reading value (won't be rejected)
  * @param {number} maxError maximum number of errors to be reported, ignore the rest
  */
 function checkValue(arrayToValidate, minVal, maxVal, maxError) {
@@ -78,22 +78,24 @@ function checkValue(arrayToValidate, minVal, maxVal, maxError) {
 /**
  * Check and report unequal intervals. Can be ignore by passing null interval
  * @param {array of Readings} arrayToValidate
- * @param {number} interval expected gap between 2 consecutive reading times in seconds
- * @param {number} maxError maximum number of errors to be reported. Ignore the rest
+ * @param {number} threshold the maximum allowed difference between consecutive data points' intervals
  */
 function checkIntervals(arrayToValidate, threshold) {
 	if (threshold === null) {
 		return true;
 	}
-	interval = arrayToValidate[1].startTimestamp - arrayToValidate[0].endTimestamp;
+	// Set the expected interval to be the time gap between the first 2 data points
+	interval = arrayToValidate[1].startTimestamp.diff(arrayToValidate[0].endTimestamp, 'seconds');
 	lastTime = arrayToValidate[1].endTimestamp;
+	// Calculate the time gap between every pair of consecutive data points 
 	for (reading of arrayToValidate) {
 		if (reading === arrayToValidate[0]) {
 			continue;
 		}
 		currGap = reading.startTimestamp.diff(lastTime, 'seconds');
-		if (currGap < interval - threshold || currGap > interval + threshold) {
-			log.warn(`UNEQUAL INTERVAL IS DETECTED FROM METER ${reading.meterID}`);
+		// Compare the current time gap with the expected interval. Terminate if the difference is larger than accepted threshold
+		if (Math.abs(currGap - interval) > threshold) {
+			log.error(`UNEQUAL INTERVAL IS DETECTED FROM METER ${reading.meterID}: Time gap between ${reading.startTimestamp} and ${lastTime} is too big`);
 			return false;
 		}
 		lastTime = reading.endTimestamp;
