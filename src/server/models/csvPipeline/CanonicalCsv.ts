afa3d5ee0915d054788888102f59393f6addecef
lindavin <lindavin@grinnell.edu>
2020-12-12 21:44:48 -0500

    Begin csv shape

---
+++ src/server/models/csvPipeline/CanonicalCsv.ts
@@ -0,0 +1,43 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+type Mode = "meter" | "reading";
+type TimeSortDirection = "increasing" | "decreasing";
+
+type Options = {
+	createMeter?: boolean;
+	cumulative?: boolean;
+	duplications?: number;
+	timeSort?: TimeSortDirection;
+	update?: boolean;
+}
+
+class CanonicalCsv {
+	mode: Mode;
+	meter: string;
+	length: number;
+	createMeter = true;
+	cumulative = false; // This is the current default; we leave it out of the constructor/options for now.
+	duplications = 1;
+	timeSort: TimeSortDirection = 'increasing';
+	update = false;
+
+	constructor(mode: Mode, meter: string, length: number, options?: Options) {
+		// Pre-checks
+		if (mode === 'reading' && !meter) {
+			throw Error('Invalid. Cannot submit a meter reading without a meter name.');
+		}
+		if (options) {
+			const { createMeter = true, duplications = 1, timeSort = 'increasing', update = false } = options;
+			this.createMeter = createMeter;
+			this.duplications = duplications;
+			this.timeSort = timeSort;
+			this.update = update;
+		}
+
+		this.mode = mode;
+		this.meter = meter;
+		this.length = length;
+	}
+}
