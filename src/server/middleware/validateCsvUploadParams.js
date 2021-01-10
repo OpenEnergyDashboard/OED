7a3b7b1b750311d62aeba864fd9f08ebc8bf0ecb
lindavin <lindavin@grinnell.edu>
2021-01-09 23:44:56 -0500

    Set default params when not provided

--- src/server/middleware/validateCsvUploadParams.js
+++ src/server/middleware/validateCsvUploadParams.js
@@ -13,32 +13,46 @@ async function validateCsvUploadParams(req, res, next) {
 				// Fail unimplemented createmeter value.
 				if (createMeter && createMeter !== 'true' && createMeter !== 'false') { // default flag based on what exists on the meter
 					throw new CSVPipelineError(`Create meter value ${createMeter} is not implemented.`);
+				} else {
+					req.body.createmeter = 'false'; // set default createmeter param if not supplied
 				}
 				// Fail unimplemented cumulative value.
 				if (cumulative && cumulative !== 'true' && cumulative !== 'false') {
 					throw new CSVPipelineError(`Cumulative value ${cumulative} is not implemented.`);
+				} else {
+					req.body.cumulative = 'false'; // set default cumulative param if not supplied
 				} // TODO: Think about how to handle the case where the cumulative is incorrectly 'yes' when it should actually be 'no'.
+
 				// Fail on incorrect duplication value.
 				if (duplications && isNaN(duplications)) {
 					throw new CSVPipelineError(`Duplications value ${duplications} is invalid.`);
+				} else {
+					req.body.duplications = '1'; // set default duplications param if not supplied
 				}
 				// Fail if no meter name provided
 				if (!meterName) {
 					throw new CSVPipelineError(`Meter name must be provided as field meter.`);
 				}
+
 				// Fail unimplemented time sort.
 				if (timeSort && timeSort !== 'increasing' && timeSort !== 'decreasing') {
 					throw new CSVPipelineError(`Time sort '${timeSort}' is invalid. Only 'increasing' is currently implemented.`);
+				} else {
+					req.body.timesort = 'increasing'; // set default timesort param if not supplied
 				}
 				// Fail if request to update readings.
-				if (update &&  update !== 'true' && update !== 'false') {
+				if (update && update !== 'true' && update !== 'false') {
 					throw new CSVPipelineError(`Update value for readings is not implemented for update=${update}.`);
+				} else {
+					req.body.update = 'false'; // set default update param if not supplied
 				}
 				break;
 			case 'meter':
 				// Fail if request to update meters.
 				if (update && update !== 'true' && update !== 'false') {
 					throw new CSVPipelineError(`Update data for a meter is not implemented for update=${update}.`);
+				} else {
+					req.body.update = 'false'; // set default update param if not supplied
 				}
 				break;
 			default:
