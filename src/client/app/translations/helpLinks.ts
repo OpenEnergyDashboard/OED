30116e5eebe1e92dc901c0748d4ea11a6dcdb2c6
lindavin <lindavin@grinnell.edu>
2021-01-25 16:28:39 -0500

    Update base link

--- src/client/app/translations/helpLinks.ts
+++ src/client/app/translations/helpLinks.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-const BASE_URL = 'https://openenergydashboard.org';
+const BASE_URL = 'https://openenergydashboard.github.io';
 
 const links: Record<string, Record<string, string>> = {
 	'help.home.chart.plotly.controls': { link: 'https://plotly.com/chart-studio-help/getting-to-know-the-plotly-modebar/' },
