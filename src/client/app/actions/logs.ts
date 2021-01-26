82d68dba37575c7e579de9df6aedf2d92b4f9a17
ak476519 <wangx@beloit.edu>
2021-01-26 15:09:52 -0800

    add comment for client-side logging

--- src/client/app/actions/logs.ts
+++ src/client/app/actions/logs.ts
@@ -1,16 +1,22 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-import { LogData } from "../types/redux/logs";
-import { logsApi } from "../utils/api";
-const moment = require('moment');
+import { LogData } from '../types/redux/logs';
+import { logsApi } from '../utils/api';
 
+/**
+ * pass client-side information to server console using logsApi on server-side, mainly for debugging purposes
+ * @param level {string} type of message being logged
+ * @param message {String} message to log
+ * @param error {Error?} An optional error object to provide a stacktrace
+ * @param skipMail {boolean?} Don't e-mail this message even if we would normally emit an e-mail for this level.
+ */
 export function logToServer(level: string, message: string, error?: Error, skipMail?: boolean) {
-	let log: LogData = {
-		message: message,
+	const log: LogData = {
+		message
 	};
-	if (error) log.error = error;
-	if (skipMail) log.skipMail = skipMail;
+	if (error) { log.error = error; }
+	if (skipMail) { log.skipMail = skipMail; }
 	return async () => {
 		try {
 			switch (level) {
@@ -27,7 +33,7 @@ export function logToServer(level: string, message: string, error?: Error, skipM
 					break;
 			}
 		} catch (e) {
-			console.log(e);
+			throw new Error(e);
 		}
-	}
+	};
 }
