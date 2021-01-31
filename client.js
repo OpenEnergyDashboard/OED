e047eb4dddcfc3362bd4451e2c0d4ca5cb1f284b
campionfellin <campionfellin@gmail.com>
2021-01-31 12:32:03 -0800

    feat: set up github actions for testing
    Signed-off-by: campionfellin <campionfellin@gmail.com>

---
+++ client.js
@@ -0,0 +1,28 @@
+const { Client } = require('pg');
+
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.OED_DB_TEST_DATABASE
+});
+
+pgclient.connect();
+
+const createUser = "CREATE USER " + process.env.OED_DB_USER + " WITH PASSWORD '" + process.env.POSTGRES_PASSWORD + "'"
+const alterUser = "ALTER USER " + process.env.OED_DB_USER + " WITH SUPERUSER;"
+const extension = "CREATE EXTENSION IF NOT EXISTS btree_gist;"
+
+pgclient.query(createUser, (err, res) => {
+  if (err) throw err
+});
+
+pgclient.query(alterUser, (err, res) => {
+  if (err) throw err
+});
+
+pgclient.query(extension, (err, res) => {
+  if (err) throw err
+  pgclient.end()
+});
