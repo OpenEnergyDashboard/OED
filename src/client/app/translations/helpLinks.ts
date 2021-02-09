60016f5959dbaaaf4a421f67b922e305a6f92a1b
Steven Huss-Lederman <huss@beloit.edu>
2021-02-08 20:14:02 -0600

    new links for help web pages
    - The new help web pages (currently only on @huss fork) changed links
    and these are now updated for tooltips.
    - I also added a commented out line that allows the web address to be
    someone's own GitHub web page if they have a fork and set it up
    to serve that page. Used for debugging.

--- src/client/app/translations/helpLinks.ts
+++ src/client/app/translations/helpLinks.ts
@@ -1,21 +1,29 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Normal/live URL for OED
 const BASE_URL = 'https://openenergydashboard.github.io';
+// Debug URL where need to put own GitHub ID before .github.io.
+// This works if you have a fork of the web pages and setup your GitHub
+// account to serve them up.
+//  const BASE_URL = 'https://xxx.github.io/OpenEnergyDashboard.github.io';
 
 const links: Record<string, Record<string, string>> = {
 	'help.home.chart.plotly.controls': { link: 'https://plotly.com/chart-studio-help/getting-to-know-the-plotly-modebar/' },
-	'help.home.chart.redraw.restore': { link: `${BASE_URL}/help/graphing` },
-	'help.home.chart.select': { link: `${BASE_URL}/help/graphs` },
-	'help.home.export.graph.data': { link: `${BASE_URL}/help/general#export` },
+	'help.home.chart.redraw.restore': { link: `${BASE_URL}/help/lineGraphic.html#redrawRestore` },
+	'help.home.chart.select': { link: `${BASE_URL}/help/graphType.html` },
+	'help.home.export.graph.data': { link: `${BASE_URL}/help/export.html` },
 	'help.home.header': {
-		link0: `${BASE_URL}/help/metersAndGroups`,
-		link1: `${BASE_URL}/help/admins`
+		link0: `${BASE_URL}/help/pageChoices.html`,
+		link1: `${BASE_URL}/help/meterViewing.html`,
+		link2: `${BASE_URL}/help/groupViewing.html`,
+		link3: `${BASE_URL}/help/admin.html`
 	},
-	'help.home.hide.or.show.options': { link: `${BASE_URL}/help/general#hideOptions` },
-	'help.home.select.groups': { link: `${BASE_URL}/help/metersAndGroups` },
-	'help.home.select.meters': { link: `${BASE_URL}/help/metersAndGroups` },
-	'help.home.toggle.chart.link': { link: `${BASE_URL}/help/general#chartlink` }
+	'help.home.hide.or.show.options': { link: `${BASE_URL}/help/hideOptions.html` },
+	'help.home.select.groups': { link: `${BASE_URL}/help/graphingGroups.html` },
+	'help.home.select.meters': { link: `${BASE_URL}/help/graphingMeters.html` },
+	'help.home.toggle.chart.link': { link: `${BASE_URL}/help/chartLink.html` }
 };
 
 export default links;
