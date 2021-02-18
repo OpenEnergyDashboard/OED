601f4149f47c2fa2da4587bfc5f312cbe5767340
LouisLu705 <54871600+LouisLu705@users.noreply.github.com>
2021-02-18 18:34:34 -0500

    Removed extra space on PreferencesComponent. Spaces do not affect anything

--- src/client/app/components/admin/PreferencesComponent.tsx
+++ src/client/app/components/admin/PreferencesComponent.tsx
@@ -70,9 +70,7 @@ class PreferencesComponent extends React.Component<PreferencesPropsWithIntl, {}>
 						value={this.props.displayTitle}
 						onChange={this.handleDisplayTitleChange}
 						maxLength={50}
-					/>
-
-					
+					/>	
 				</div>
 				<div>
 					<p style={labelStyle}>
