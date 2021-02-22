c5c98cb8963e08ce65072ca3c88ae2d43ef91e45
lavanl <luke.lavan01@gmail.com>
2021-02-22 15:54:26 -0500

    creation of 'locales.d.ts'
    moves the plotly localization module declarations to a new file
    with a better name
    (locales.d.ts)
    
    removes the above declarations from the former file
    (json.d.ts)

--- src/client/app/types/json.d.ts
+++ src/client/app/types/json.d.ts
@@ -6,11 +6,3 @@ declare module '*.json' {
 	const value: any;
 	export default value;
 }
-declare module 'plotly.js/lib/locales/es'{
-	const value: any;
-	export default value;
-}
-declare module 'plotly.js/lib/locales/fr'{
-	const value: any;
-	export default value;
-}
---
+++ src/client/app/types/locales.d.ts
@@ -0,0 +1,8 @@
+declare module 'plotly.js/lib/locales/es'{
+	const value: any;
+	export default value;
+}
+declare module 'plotly.js/lib/locales/fr'{
+	const value: any;
+	export default value;
+}
