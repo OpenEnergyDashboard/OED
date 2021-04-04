aed9164eb16956a25b6c435498eb679778c8f478
lindavin <lindavin@grinnell.edu>
2021-04-03 20:35:30 -0400

    Fix field ordering issue found by @LouisLu705

--- src/server/models/Meter.js
+++ src/server/models/Meter.js
@@ -20,14 +20,15 @@ class Meter {
 	 * @param identifier Another way to identify a meter
 	 */
 	constructor(id, name, ipAddress, enabled, displayable, type, meterTimezone, gps = undefined, identifier = name) {
+		// In order for the CSV pipeline to work, the order of the parameters needs to match the order that the fields are declared.
 		this.id = id;
 		this.name = name;
 		this.ipAddress = ipAddress;
 		this.enabled = enabled;
 		this.displayable = displayable;
 		this.type = type;
-		this.gps = gps;
 		this.meterTimezone = meterTimezone;
+		this.gps = gps;
 		this.identifier = identifier;
 	}
 
