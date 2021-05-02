5a1a01c4c7e29758842d9c843d14b03eb9969217
lindavin <lindavin@grinnell.edu>
2021-05-02 14:33:32 -0400

    Exclude test files from header check, thanks to help from @LouisLu705

--- src/server/test/web/csvPipelineTest.js
+++ src/server/test/web/csvPipelineTest.js
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { chai, mocha, expect, app, testDB, testUser } = require('../../common');
-const Meter = require('../../../models/Meter');
-const Reading = require('../../../models/Reading');
+const { chai, mocha, expect, app, testDB, testUser } = require('../common');
+const Meter = require('../../models/Meter');
+const Reading = require('../../models/Reading');
 const zlib = require('zlib');
 
 const fs = require('fs');
