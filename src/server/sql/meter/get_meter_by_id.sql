743097e7dee4f39396ffb358ff8409c39c02e003
Steven Huss-Lederman <huss@beloit.edu>
2021-05-04 10:58:42 -0500

    fix fomatting

--- src/server/sql/meter/get_meter_by_id.sql
+++ src/server/sql/meter/get_meter_by_id.sql
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 SELECT id, name, ipaddress, enabled, displayable, meter_type, default_timezone_meter, gps, identifier,
-note, area, cumulative, cumulative_reset, cumulative_reset_start, cumulative_reset_end,
-reading_length, reading_variation, reading, start_timestamp, end_timestamp
-FROM meters WHERE id=${id};
+    note, area, cumulative, cumulative_reset, cumulative_reset_start, cumulative_reset_end,
+    reading_length, reading_variation, reading, start_timestamp, end_timestamp
+FROM meters
+WHERE id=${id};
