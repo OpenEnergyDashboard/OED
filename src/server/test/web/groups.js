ac979c795dd37b132b9aaa8602fc33eaeec1df1e
Steven Huss-Lederman <huss@beloit.edu>
2021-05-04 12:06:16 -0500

    fix up for removing previous day

--- src/server/test/web/groups.js
+++ src/server/test/web/groups.js
@@ -35,13 +35,13 @@ mocha.describe('groups API', () => {
 		groupC = new Group(undefined, 'C', true, gps, 'notes C', 53.5);
 		await Promise.all([groupA, groupB, groupC].map(group => group.insert(conn)));
 		meterA = new Meter(undefined, 'A', null, false, true, Meter.type.MAMAC, null, gps,
-		'Identified A' ,'notes A', 35.0, true, true, '01:01:25' , '00:00:00', true, '05:00:00','00:00:00', 1.5,
+		'Identified A' ,'notes A', 35.0, true, true, '01:01:25' , '00:00:00', '05:00:00','00:00:00', 1.5,
 		'0001-01-01 23:59:59', '2020-07-02 01:00:10');
 		meterB = new Meter(undefined, 'B', null, false, true, Meter.type.MAMAC, null, gps, 
-		'IDENTIFIED B', 'notes B', 33.5, true, true, '05:05:09', '09:00:01', true, '00:00:00', 
+		'Identified B', 'notes B', 33.5, true, true, '05:05:09', '09:00:01', '00:00:00', 
 		'00:00:00', 25.5, '0001-01-01 23:59:59', '2020-07-02 01:00:10');
 		meterC = new Meter(undefined, 'C', null, false, true, Meter.type.METASYS, null, gps, 
-		'IDENTIFIED C', 'notes C', 33.5, true, true, '05:05:09', '09:00:01', true, '00:00:00', 
+		'Identified C', 'notes C', 33.5, true, true, '05:05:09', '09:00:01', '00:00:00', 
 		'00:00:00', 25.5, '0001-01-01 23:59:59', '2020-07-02 01:00:10');
 		await Promise.all([meterA, meterB, meterC].map(meter => meter.insert(conn)));
 
@@ -189,7 +189,7 @@ mocha.describe('groups API', () => {
 			mocha.it('allows adding a new child meter to a group', async () => {
 				const conn = testDB.getConnection();
 				const meterD = new Meter(undefined, 'D', null, false, true, Meter.type.MAMAC, null, gps,
-				'IDENTIFIED D', 'notes D', 33.5, true, true, '05:05:09', '09:00:01', true, '00:00:00', 
+				'Identified D', 'notes D', 33.5, true, true, '05:05:09', '09:00:01', '00:00:00', 
 				'00:00:00', 25.5, '0001-01-01 23:59:59', '2020-07-02 01:00:10');
 				await meterD.insert(conn);
 				let res = await chai.request(app).put('/api/groups/edit').set('token', token).type('json').send({
