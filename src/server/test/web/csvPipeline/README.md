955c5f28656e823dc0b661d26f4ef7a69cf10cb6
Steven Huss-Lederman <huss@beloit.edu>
2021-06-18 11:26:34 -0500

    information on pipeline testing
    The files & script were added and README was updated to describe the
    pipeline tests run to check the PR. These are not everything desired but
    a good start and plan to convert to automated testing.
    
    An old, unneeded README was removed.

--- src/server/test/web/csvPipeline/README.md
+++ src/server/test/web/csvPipeline/README.md
@@ -1,4 +1,4 @@
-# Csv web testing tools
+# CSV web testing tools
 
 Here are some sample files for testing the csv pipeline route.
 
@@ -18,3 +18,540 @@ curl localhost:3000/api/csv/readings -X POST \
 	-F 'password=password' \
 	-F 'csvfile=@src/server/test/web/csvPipeline/sampleReadings.csv'
 ```
+
+## Testing the new pipeline and CSV uploads
+
+The following is a description of how the new pipeline was tested during evaluation of the PR. It is planned to convert these tests to automated tests in the future.
+
+The CSV test files are described in the following table. The script pipelineCurl.sh runs the commands for each case and assumes OED is up and running. It creates a new meter for each file. Reading normally go from 6/1/21-6/5/21 (5 points) where get straight line from 1 to 5 so values are 1, 2, 3, 4 & 5 when plotted. In the readings table the values will be 24, 48, 72, 96 & 120 because over 24 hours. With cumulative you lose the first value. With the noon reset/start time you get Get 2.0, 2.5, 3.5, 4.5, 5 as readings since the days other than the first/last average between two readings.
+
+Note get two warning messages if load more than once on many cases because the time of the first reading is before the last time loaded before.
+
+Looked at readings with: select * from readings where meter_id in (select id from meters where name='pipe#');
+
+The script also describes how to delete all the meters/readings if you want to rerun from scratch.
+
+<table>
+  <tr>
+   <td>File name (.csv)
+   </td>
+   <td>Nominal meter name
+   </td>
+   <td>Descending time
+   </td>
+   <td>Cumulative
+   </td>
+   <td>Cumulative reset
+   </td>
+   <td>Reading length variation
+   </td>
+   <td>Reading gap
+   </td>
+   <td>Description
+   </td>
+  </tr>
+  <tr>
+   <td>regAsc
+   </td>
+   <td>pipe1
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Normal 1-5 values
+   </td>
+  </tr>
+  <tr>
+   <td>regDsc
+   </td>
+   <td>pipe2
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+  </tr>
+  <tr>
+   <td>cumAsc
+   </td>
+   <td>pipe3
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Drops 1<sup>st</sup> reading as expected
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetMidnight
+   </td>
+   <td>pipe4
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>Default
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Drops 1<sup>st</sup> reading as expected
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetMidnight
+   </td>
+   <td>pipe5
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>23:45-00:15
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Same as above but explicit reset times and backward for start/end
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetMidnight
+   </td>
+   <td>pipe6
+   </td>
+   <td>
+   </td>
+   <td>x
+   </td>
+   <td>11:45-12:15
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Similar to above but reset times wrong so drops all readings; forward for start/end reset
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetMidnight
+   </td>
+   <td>pipe7
+   </td>
+   <td>
+   </td>
+   <td>x
+   </td>
+   <td>00:00-00:00.001
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Same as second midnight above above but reset times tight so see all but first value
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetMidnight
+   </td>
+   <td>pipe8
+   </td>
+   <td>
+   </td>
+   <td>x
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Make sure rejects all readings if reset not indicated
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetNoon
+   </td>
+   <td>pipe9
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>Default
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Allows reset at any time so all but first value.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetNoon
+   </td>
+   <td>pipe10
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Make sure rejects all readings if reset not indicated
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetNoon
+   </td>
+   <td>pipe11
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>11:45-12:15
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>explicit reset times and forward for start/end so same as 2 above where all but first.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetNoon
+   </td>
+   <td>pipe12
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>23:45-00:15
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>reset times wrong and backward for start/end so should have no values
+   </td>
+  </tr>
+  <tr>
+   <td>regAscLen
+   </td>
+   <td>pipe13
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Default
+   </td>
+   <td>
+   </td>
+   <td>Time varies by 1 & 2 min; All but 1<sup>st</sup> point vary slightly in reading due to time variation  – get low, high, high, low as expected; should get 4 warnings for line 2-5
+   </td>
+  </tr>
+  <tr>
+   <td>regAscLen
+   </td>
+   <td>pipe14
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>60
+   </td>
+   <td>
+   </td>
+   <td>Time varies by 1 & 2 min and allow 1 min (60 sec); Should warn about lines 3, 5.
+   </td>
+  </tr>
+  <tr>
+   <td>regAscLen
+   </td>
+   <td>pipe15
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>120
+   </td>
+   <td>
+   </td>
+   <td>Time varies by 1 & 2 min; Should warn about line 5.
+   </td>
+  </tr>
+  <tr>
+   <td>regAscGap
+   </td>
+   <td>pipe16
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>121
+   </td>
+   <td>Default
+   </td>
+   <td>Should get warning for lines 2 & 4. Length variation so no warnings about that. Point 2 & 4 should be slightly high since shorter length.
+   </td>
+  </tr>
+  <tr>
+   <td>regAscGap
+   </td>
+   <td>pipe17
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>121
+   </td>
+   <td>60
+   </td>
+   <td>Similar to above but now only warning for line 4.
+   </td>
+  </tr>
+  <tr>
+   <td>regAscGap
+   </td>
+   <td>pipe18
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>121
+   </td>
+   <td>120
+   </td>
+   <td>Similar to above but no warnings.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscHeader
+   </td>
+   <td>pipe19
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Check header. Get standard cum values.
+   </td>
+  </tr>
+  <tr>
+   <td>CumAscHeader.csv.gz
+   </td>
+   <td>pipe20
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Check gzip (with header). Get standard cum values.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscDuplication3
+   </td>
+   <td>pipe21
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Repeat every reading 3x but should only see once.  Get standard cum values.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscNegative
+   </td>
+   <td>pipe22
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>Default
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Negative reading on #4 with cumulative so should get no reading.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAsc & cumAscAdd1 & cumAscAdd2
+   </td>
+   <td>pipe23
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Add in cumulative readings via two more uploads of a single value. Expect normal cum readings plus 6 & 7.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAsc & cumAscAddReset
+   </td>
+   <td>pipe24
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>Add two more values via second curl but this time the first one involves a reset. Expect normal cum readings plus 6 & 7.
+   </td>
+  </tr>
+  <tr>
+   <td>CumAsc & meterPipe25 (to create meter)
+   </td>
+   <td>pipe25
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>The meter in DB is set to cumulative but not sent via curl. Expect usual cumulative values.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetMidnight & meterPipe26 (to create meter)
+   </td>
+   <td>pipe26
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>The meter in DB is set to cumulative & cumulative_reset but not sent via curl. Expect usual cumulative values.
+   </td>
+  </tr>
+  <tr>
+   <td>cumAscResetMidnight & meterPipe27 (to create meter)
+   </td>
+   <td>pipe27
+   </td>
+   <td>
+   </td>
+   <td>X
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>The meter in DB is set to cumulative, cumulative_reset to true & reset range is 11:45-12:15 but not sent via curl. The reset value should be reject row 4 since negative and no readings for meter.
+   </td>
+  </tr>
+  <tr>
+   <td>RegAscGapLength & meterPipe28 (to create meter)
+   </td>
+   <td>pipe28
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>
+   </td>
+   <td>The meter in DB is set to reading_gap=60 and reading_variation=120. Should get warning for gap for line 4 and Length variation for line 5. Expect 1, 2+, 3, 4+, 5-, 5- (same as previous point and extra one since last reading goes into next day)
+   </td>
+  </tr>
+</table>
---
+++ src/server/test/web/csvPipeline/pipelineCurl.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# The following lines can be used to remove all the readings and meter associated with
+# the pipeline test data. This is valuable if you want to run the process again and the
+# meters and readings already exist.
+# NOTE this removes the meters and readings. You may need to remove dependent groups
+# before doing this in the web groups page in OED.
+# This assumes you do not have any meters named pipe* of they will also be removed.
+# Get into postgres in the terminal.
+# docker-compose exec database psql -U oed
+# Remove all the readings.
+# > delete from readings where meter_id in (select id from meters where name like 'pipe%');
+# Remove all the meters. Normally gives "DELETE 28"
+# > delete from meters where name like 'pipe%';
+# Quit postgres.
+# > \q
+
+# You need to set this to your actual OED home directory.
+oedHome=~/OED/OED
+
+echo -e "\nstarting pipe1"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe1' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regAsc.csv'
+# allow when descending working and have parameter for it.
+# echo -e "\nstarting pipe2"
+# curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe2' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regDsc.csv'
+echo -e "\nstarting pipe3"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'meterName=pipe3' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAsc.csv'
+echo -e "\nstarting pipe4"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F 'meterName=pipe4' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetMidnight.csv'
+echo -e "\nstarting pipe5"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F'cumulativeResetStart=23:45' -F'cumulativeResetEnd=00:15' -F 'meterName=pipe5' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetMidnight.csv'
+echo -e "\nstarting pipe6"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F'cumulativeResetStart=11:45' -F'cumulativeResetEnd=12:15' -F 'meterName=pipe6' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetMidnight.csv'
+echo -e "\nstarting pipe7"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F'cumulativeResetStart=00:00' -F'cumulativeResetEnd=00:00.001' -F 'meterName=pipe7' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetMidnight.csv'
+echo -e "\nstarting pipe8"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'meterName=pipe8' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetMidnight.csv'
+echo -e "\nstarting pipe9"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F 'meterName=pipe9' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetNoon.csv'
+echo -e "\nstarting pipe10"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'meterName=pipe10' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetNoon.csv'
+echo -e "\nstarting pipe11"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F'cumulativeResetStart=11:45' -F'cumulativeResetEnd=12:15' -F 'meterName=pipe11' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetNoon.csv'
+echo -e "\nstarting pipe12"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F'cumulativeResetStart=23:45' -F'cumulativeResetEnd=00:15' -F 'meterName=pipe12' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetNoon.csv'
+echo -e "\nstarting pipe13"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe13' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regAscLen.csv'
+echo -e "\nstarting pipe14"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe14' -F 'lengthVariation=60' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regAscLen.csv'
+echo -e "\nstarting pipe15"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe15' -F 'lengthVariation=120' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regAscLen.csv'
+echo -e "\nstarting pipe16"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe16' -F 'lengthVariation=121' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regAscGap.csv'
+echo -e "\nstarting pipe17"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe17' -F 'length=60' -F 'lengthVariation=121' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regAscGap.csv'
+echo -e "\nstarting pipe18"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe18' -F 'length=120' -F 'lengthVariation=121' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regAscGap.csv'
+echo -e "\nstarting pipe19"
+curl localhost:3000/api/csv/readings -X POST -F 'headerRow=true' -F 'cumulative=true' -F 'meterName=pipe19' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscHeader.csv'
+echo -e "\nstarting pipe20"
+curl localhost:3000/api/csv/readings -X POST -F 'headerRow=true' -F 'cumulative=true' -F 'meterName=pipe20' -F 'createMeter=true' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscHeader.csv.gz'
+echo -e "\nstarting pipe21"
+curl localhost:3000/api/csv/readings -X POST -F 'duplications=3' -F 'cumulative=true' -F 'meterName=pipe21' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscDuplication3.csv'
+echo -e "\nstarting pipe22"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F 'meterName=pipe22' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscNegative.csv'
+echo -e "\nstarting pipe23"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'meterName=pipe23' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAsc.csv'
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'meterName=pipe23' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscAdd1.csv'
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'meterName=pipe23' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscAdd2.csv'
+echo -e "\nstarting pipe24"
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'meterName=pipe24' -F 'createMeter=true' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAsc.csv'
+curl localhost:3000/api/csv/readings -X POST -F 'cumulative=true' -F 'cumulativeReset=true' -F 'meterName=pipe24' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscAddReset.csv'
+echo -e "\nstarting pipe25"
+curl localhost:3000/api/csv/meters -X POST -F 'headerRow=false' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@meterPipe25.csv'
+# edit meter so reset is true
+docker compose -f $oedHome/docker-compose.yml exec database psql -U oed -c "update meters set cumulative=true where name='pipe25'"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe25' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAsc.csv'
+echo -e "\nstarting pipe26"
+curl localhost:3000/api/csv/meters -X POST -F 'headerRow=false' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@meterPipe26.csv'
+# edit meter so reset is true
+docker compose -f $oedHome/docker-compose.yml exec database psql -U oed -c "update meters set cumulative=true where name='pipe26'; update meters set cumulative_reset=true where name='pipe26'"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe26' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetMidnight.csv'
+echo -e "\nstarting pipe27"
+curl localhost:3000/api/csv/meters -X POST -F 'headerRow=false' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@meterPipe27.csv'
+# edit meter so reset is true
+docker compose -f $oedHome/docker-compose.yml exec database psql -U oed -c "update meters set cumulative=true where name='pipe27'; update meters set cumulative_reset=true where name='pipe27'; update meters set cumulative_reset_start='11:45' where name='pipe27'; update meters set cumulative_reset_end='12:15' where name='pipe27'"
+curl localhost:3000/api/csv/readings -X POST -F 'meterName=pipe27' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@cumAscResetMidnight.csv'
+echo -e "\nstarting pipe28"
+curl localhost:3000/api/csv/meters -X POST -F 'headerRow=false' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@meterPipe28.csv'
+# edit meter so reset is true
+docker compose -f $oedHome/docker-compose.yml exec database psql -U oed -c "update meters set reading_gap=60 where name='pipe28'; update meters set reading_variation=120 where name='pipe28'"
+# refresh on last upload of readings and all will be available for graphing
+curl localhost:3000/api/csv/readings -X POST -F 'refreshReadings=true' -F 'meterName=pipe28' -F 'gzip=false' -F 'email=test@example.com' -F 'password=password' -F 'csvfile=@regAscGapLength.csv'
