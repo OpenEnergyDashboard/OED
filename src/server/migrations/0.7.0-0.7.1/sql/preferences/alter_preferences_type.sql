000076c9ce235d5d447da45d440d587632612156
jina2k <jina26p@gmail.com>
2021-11-08 17:59:36 -0500

    changed sql query to separate altering and create graph_type, now in migrations instead

--- src/server/migrations/0.7.0-0.7.1/sql/preferences/alter_preferences_type.sql
+++ src/server/migrations/0.7.0-0.7.1/sql/preferences/alter_preferences_type.sql
@@ -2,10 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
--- This should avoid an error when the type already exists. This is an issue since
--- the OED install stops the creation of database items after this.
 DO $$ BEGIN
-    CREATE TYPE graph_type AS ENUM('line', 'bar', 'compare', 'map');
+    ALTER TYPE graph_type ADD VALUE 'map' AFTER 'compare';
 EXCEPTION
     WHEN duplicate_object THEN null;
 END $$;
