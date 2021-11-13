531930baec66e605fcf5c67b61badf8c38f06814
jina2k <jina26p@gmail.com>
2021-11-13 18:41:33 -0500

    preferences

--- src/server/migrations/0.7.0-0.7.1/sql/preferences/alter_preferences_type.sql
+++ src/server/migrations/0.7.0-0.7.1/sql/preferences/alter_preferences_type.sql
@@ -2,8 +2,4 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-DO $$ BEGIN
-    ALTER TYPE graph_type ADD VALUE 'map' AFTER 'compare';
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
+ALTER TYPE graph_type ADD VALUE 'map' AFTER 'compare';
