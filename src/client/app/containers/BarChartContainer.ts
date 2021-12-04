a0a00050c3e93da79e7fc60783aab3a654bf227b
Ngan <hieungan5b@gmail.com>
2021-12-04 10:33:42 -0600

    fix chart link and bar chart duration

--- src/client/app/containers/BarChartContainer.ts
+++ src/client/app/containers/BarChartContainer.ts
@@ -39,8 +39,9 @@ function mapStateToProps(state: State) {
 				const hoverText: string[] = [];
 				const readings = _.orderBy(readingsData.readings, ['startTimestamp'], ['asc']);
 				readings.forEach(barReading => {
+					// subtracting one extra day caused by day ending at midnight of the next day
 					const timeReading: string =
-						`${moment(barReading.startTimestamp).utc().format('LL')} - ${moment(barReading.endTimestamp).utc().format('LL')}`;
+						`${moment(barReading.startTimestamp).utc().format('LL')} - ${moment(barReading.endTimestamp).subtract(1, 'days').utc().format('LL')}`;
 					xData.push(timeReading);
 					yData.push(barReading.reading);
 					hoverText.push(`<b> ${timeReading} </b> <br> ${label}: ${barReading.reading.toPrecision(6)} kWh`);
@@ -78,9 +79,9 @@ function mapStateToProps(state: State) {
 				const hoverText: string[] = [];
 				const readings = _.orderBy(readingsData.readings, ['startTimestamp'], ['asc']);
 				readings.forEach(barReading => {
-					// calculate more precise bar duration 
+					// subtracting one extra day caused by day ending at midnight of the next day
 					const timeReading: string =
-						`${moment(barReading.startTimestamp).utc().format('LL')} - ${moment(barReading.endTimestamp).utc().format('LL')}`;
+						`${moment(barReading.startTimestamp).utc().format('LL')} - ${moment(barReading.endTimestamp).subtract(1, 'days').utc().format('LL')}`;
 					xData.push(timeReading);
 					yData.push(barReading.reading);
 					hoverText.push(`<b> ${timeReading} </b> <br> ${label}: ${barReading.reading.toPrecision(6)} kWh`);
