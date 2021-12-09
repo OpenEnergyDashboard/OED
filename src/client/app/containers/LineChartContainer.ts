bda070a3d4bcabacb66443a438b1d96f771e3b57
Ngan <hieungan5b@gmail.com>
2021-12-09 09:22:38 -0600

    update reading time for line chart

--- src/client/app/containers/LineChartContainer.ts
+++ src/client/app/containers/LineChartContainer.ts
@@ -2,15 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import * as _ from 'lodash';
-import * as moment from 'moment-timezone';
-import { connect } from 'react-redux';
-import getGraphColor from '../utils/getGraphColor';
-import { State } from '../types/redux/state';
-import PlotlyChart, { IPlotlyChartProps } from 'react-plotlyjs-ts';
-import {TimeInterval} from '../../../common/TimeInterval';
-import Locales from '../types/locales';
-import { DataType } from '../types/Datasources';
+import * as _ from "lodash";
+import * as moment from "moment-timezone";
+import { connect } from "react-redux";
+import getGraphColor from "../utils/getGraphColor";
+import { State } from "../types/redux/state";
+import PlotlyChart, { IPlotlyChartProps } from "react-plotlyjs-ts";
+import { TimeInterval } from "../../../common/TimeInterval";
+import Locales from "../types/locales";
+import { DataType } from "../types/Datasources";
 
 function mapStateToProps(state: State) {
 	const timeInterval = state.graph.timeInterval;
@@ -25,7 +25,9 @@ function mapStateToProps(state: State) {
 				const label = state.meters.byMeterID[meterID].name;
 				const colorID = meterID;
 				if (readingsData.readings === undefined) {
-					throw new Error('Unacceptable condition: readingsData.readings is undefined.');
+					throw new Error(
+						"Unacceptable condition: readingsData.readings is undefined."
+					);
 				}
 
 				// Create two arrays for the x and y values. Fill the array with the data from the compressed readings
@@ -33,27 +35,32 @@ function mapStateToProps(state: State) {
 				const yData: number[] = [];
 				const hoverText: string[] = [];
 				const readings = _.values(readingsData.readings);
-				readings.forEach(reading => {
-					const st = moment(reading.startTimestamp)
+				readings.forEach((reading) => {
+					const st = moment(reading.startTimestamp);
 					// Time reading is in the middle of the start and end timestamp
-					const timeReading = st.add(moment(reading.endTimestamp).diff(st)/2);
-					xData.push(timeReading.utc().format('YYYY-MM-DD HH:mm:ss'));
+					const timeReading = st.add(moment(reading.endTimestamp).diff(st) / 2);
+					xData.push(timeReading.utc().format("YYYY-MM-DD HH:mm:ss"));
 					yData.push(reading.reading);
-					hoverText.push(`<b> ${timeReading.format('dddd, LL LTS')} </b> <br> ${label}: ${reading.reading.toPrecision(6)} kW`);
+					hoverText.push(
+						`<b> ${timeReading.format(
+							"dddd, LL LTS"
+						)} </b> <br> ${label}: ${reading.reading.toPrecision(6)} kW`
+					);
 				});
 
 				// Save the timestamp range of the plot
-				let minTimestamp: string = '';
-				let maxTimestamp: string = '';
+				let minTimestamp: string = "";
+				let maxTimestamp: string = "";
 				if (readings.length > 0) {
 					/* tslint:disable:no-string-literal */
-					minTimestamp = readings[0]['startTimestamp'].toString();
-					maxTimestamp = readings[readings.length - 1]['startTimestamp'].toString();
+					minTimestamp = readings[0]["startTimestamp"].toString();
+					maxTimestamp =
+						readings[readings.length - 1]["startTimestamp"].toString();
 					/* tslint:enable:no-string-literal */
 				}
-				const root: any = document.getElementById('root');
-				root.setAttribute('min-timestamp', minTimestamp);
-				root.setAttribute('max-timestamp', maxTimestamp);
+				const root: any = document.getElementById("root");
+				root.setAttribute("min-timestamp", minTimestamp);
+				root.setAttribute("max-timestamp", maxTimestamp);
 
 				// This variable contains all the elements (x and y values, line type, etc.) assigned to the data parameter of the Plotly object
 				datasets.push({
@@ -61,21 +68,21 @@ function mapStateToProps(state: State) {
 					x: xData,
 					y: yData,
 					text: hoverText,
-					hoverinfo: 'text',
-					type: 'scatter',
-					mode: 'lines',
+					hoverinfo: "text",
+					type: "scatter",
+					mode: "lines",
 					line: {
-						shape: 'spline',
+						shape: "spline",
 						width: 2,
-						color: getGraphColor(colorID, DataType.Meter)
-					}
+						color: getGraphColor(colorID, DataType.Meter),
+					},
 				});
 			}
 		}
 	}
 
-// TODO The meters and groups code is very similar and maybe it should be refactored out to create a function to do
-// both. This would mean future changes would automatically happen to both.
+	// TODO The meters and groups code is very similar and maybe it should be refactored out to create a function to do
+	// both. This would mean future changes would automatically happen to both.
 	// Add all valid data from existing groups to the line plot
 	for (const groupID of state.graph.selectedGroups) {
 		const byGroupID = state.readings.line.byGroupID[groupID];
@@ -85,7 +92,9 @@ function mapStateToProps(state: State) {
 				const label = state.groups.byGroupID[groupID].name;
 				const colorID = groupID;
 				if (readingsData.readings === undefined) {
-					throw new Error('Unacceptable condition: readingsData.readings is undefined.');
+					throw new Error(
+						"Unacceptable condition: readingsData.readings is undefined."
+					);
 				}
 
 				// Create two arrays for the x and y values. Fill the array with the data from the compressed readings
@@ -93,11 +102,17 @@ function mapStateToProps(state: State) {
 				const yData: number[] = [];
 				const hoverText: string[] = [];
 				const readings = _.values(readingsData.readings);
-				readings.forEach(reading => {
-					const timeReading = moment(reading.startTimestamp);
-					xData.push(timeReading.utc().format('YYYY-MM-DD HH:mm:ss'));
+				readings.forEach((reading) => {
+					const st = moment(reading.startTimestamp);
+					// Time reading is in the middle of the start and end timestamp
+					const timeReading = st.add(moment(reading.endTimestamp).diff(st) / 2);
+					xData.push(timeReading.utc().format("YYYY-MM-DD HH:mm:ss"));
 					yData.push(reading.reading);
-					hoverText.push(`<b> ${timeReading.format('dddd, LL LTS')} </b> <br> ${label}: ${reading.reading.toPrecision(6)} kW`);
+					hoverText.push(
+						`<b> ${timeReading.format(
+							"dddd, LL LTS"
+						)} </b> <br> ${label}: ${reading.reading.toPrecision(6)} kW`
+					);
 				});
 
 				// This variable contains all the elements (x and y values, line type, etc.) assigned to the data parameter of the Plotly object
@@ -106,23 +121,31 @@ function mapStateToProps(state: State) {
 					x: xData,
 					y: yData,
 					text: hoverText,
-					hoverinfo: 'text',
-					type: 'scatter',
-					mode: 'lines',
+					hoverinfo: "text",
+					type: "scatter",
+					mode: "lines",
 					line: {
-						shape: 'spline',
+						shape: "spline",
 						width: 2,
-						color: getGraphColor(colorID, DataType.Group)
-					}
+						color: getGraphColor(colorID, DataType.Group),
+					},
 				});
 			}
 		}
 	}
 
 	// Calculate slider interval if rangeSliderInterval is specified;
-	const sliderInterval = (state.graph.rangeSliderInterval.equals(TimeInterval.unbounded())) ? timeInterval : state.graph.rangeSliderInterval;
-	const start = Date.parse(moment(sliderInterval.getStartTimestamp()).toISOString());
-	const end = Date.parse(moment(sliderInterval.getEndTimestamp()).toISOString());
+	const sliderInterval = state.graph.rangeSliderInterval.equals(
+		TimeInterval.unbounded()
+	)
+		? timeInterval
+		: state.graph.rangeSliderInterval;
+	const start = Date.parse(
+		moment(sliderInterval.getStartTimestamp()).toISOString()
+	);
+	const end = Date.parse(
+		moment(sliderInterval.getEndTimestamp()).toISOString()
+	);
 
 	// Customize the layout of the plot
 	const layout: any = {
@@ -131,25 +154,25 @@ function mapStateToProps(state: State) {
 		legend: {
 			x: 0,
 			y: 1.1,
-			orientation: 'h'
+			orientation: "h",
 		},
 		yaxis: {
-			title: 'kW',
-			gridcolor: '#ddd'
+			title: "kW",
+			gridcolor: "#ddd",
 		},
 
 		xaxis: {
 			range: [start, end], // Specifies the start and end points of visible part of graph(unshaded region on slider);
 			rangeslider: {
-				thickness: 0.1
+				thickness: 0.1,
 			},
 			showgrid: true,
-			gridcolor: '#ddd'
+			gridcolor: "#ddd",
 		},
 		margin: {
 			t: 10,
-			b: 10
-		}
+			b: 10,
+		},
 	};
 
 	// Assign all the parameters required to create the Plotly object (data, layout, config) to the variable props, returned by mapStateToProps
@@ -159,8 +182,8 @@ function mapStateToProps(state: State) {
 		layout,
 		config: {
 			displayModeBar: true,
-			locales: Locales // makes locales available for use
-		}
+			locales: Locales, // makes locales available for use
+		},
 	};
 	props.config.locale = state.admin.defaultLanguage;
 	return props;
