85f1961be261ef9d813533ce551667ce8aa277dd
lindavin <lindavin@grinnell.edu>
2022-01-09 19:41:09 -0500

    Fix shadowed name

--- src/server/test/db/newCompressedReadingsTests.js
+++ src/server/test/db/newCompressedReadingsTests.js
@@ -11,7 +11,6 @@ const Reading = require('../../models/Reading');
 const Group = require('../../models/Group');
 const Point = require('../../models/Point');
 const { generateSineData } = require('../../data/generateTestingData');
-const { DEFAULT_OPTIONS } = require('../../data/automatedTestingData');
 const gps = new Point(90, 45);
 
 mocha.describe('Compressed Readings 2', () => {
@@ -238,15 +237,15 @@ mocha.describe('Compressed Readings 2', () => {
 		});
 
 		mocha.describe('Compression for underlying 15-minute data:', () => {
-			let meter;
+			let meter1;
 			// Uploading data that is over one months at 15-minute intervals causes Mocha to timeout.
 			const startDate = '2020-01-01 00:00:00';
 			const endDate = '2020-02-01 00:00:00';
 			mocha.beforeEach(async () => {
 				const conn = testDB.getConnection();
 				await new Meter(undefined, 'Meter1', null, false, true, Meter.type.MAMAC, null, gps).insert(conn);
-				meter = await Meter.getByName('Meter1', conn);
-				const data = generateSineData(startDate, endDate, { timeStep: { minute: 15 }}).map(row => new Reading(meter.id, row[0], row[1], row[2]));
+				meter1 = await Meter.getByName('Meter1', conn);
+				const data = generateSineData(startDate, endDate, { timeStep: { minute: 15 }}).map(row => new Reading(meter1.id, row[0], row[1], row[2]));
 				await Reading.insertAll(data, conn);
 				await Reading.refreshCompressedReadings(conn);
 
@@ -258,7 +257,7 @@ mocha.describe('Compressed Readings 2', () => {
 				const start = moment(startDate);
 				const end = moment(startDate).clone();
 				end.add(61, 'days');
-				const allReadings = await Reading.getNewCompressedReadings([meter.id], start, end, conn);
+				const allReadings = await Reading.getNewCompressedReadings([meter1.id], start, end, conn);
 				expect(allReadings['2'].length).to.greaterThan(61); // more data will be returned because of shift down to the hour view.
 			});
 
@@ -266,7 +265,7 @@ mocha.describe('Compressed Readings 2', () => {
 				const start = moment(startDate);
 				const end = moment(startDate).clone();
 				end.add(61, 'days');
-				const allReadings = await Reading.getNewCompressedReadings([meter.id], start, end, conn);
+				const allReadings = await Reading.getNewCompressedReadings([meter1.id], start, end, conn);
 				expect(allReadings['2'].length).to.equal(24 * 31);
 			});
 
@@ -274,21 +273,21 @@ mocha.describe('Compressed Readings 2', () => {
 				const start = moment(startDate);
 				const end = moment(startDate).clone();
 				end.add(14, 'days');
-				const allReadings = await Reading.getNewCompressedReadings([meter.id], start, end, conn);
+				const allReadings = await Reading.getNewCompressedReadings([meter1.id], start, end, conn);
 				const hourToRawScaleFactor = 4; // This is four since there are four 15-minute intervals in one hour.
 				expect(allReadings['2'].length).to.equal(14 * 24 * hourToRawScaleFactor);
 			});
 		});
 
 		mocha.describe('Compression for underlying 23-minute data:', () => {
-			let meter;
+			let meter1;
 			const startDate = '2020-01-01 00:00:00';
 			const endDate = '2020-02-01 00:00:00';
 			mocha.beforeEach(async () => {
 				const conn = testDB.getConnection();
 				await new Meter(undefined, 'Meter1', null, false, true, Meter.type.MAMAC, null, gps).insert(conn);
-				meter = await Meter.getByName('Meter1', conn);
-				const data = generateSineData(startDate, endDate, { timeStep: { minute: 23 }}).map(row => new Reading(meter.id, row[0], row[1], row[2]));
+				meter1 = await Meter.getByName('Meter1', conn);
+				const data = generateSineData(startDate, endDate, { timeStep: { minute: 23 }}).map(row => new Reading(meter1.id, row[0], row[1], row[2]));
 				await Reading.insertAll(data, conn);
 				await Reading.refreshCompressedReadings(conn);
 
@@ -300,7 +299,7 @@ mocha.describe('Compressed Readings 2', () => {
 				const start = moment(startDate);
 				const end = moment(startDate).clone();
 				end.add(61, 'days');
-				const allReadings = await Reading.getNewCompressedReadings([meter.id], start, end, conn);
+				const allReadings = await Reading.getNewCompressedReadings([meter1.id], start, end, conn);
 				expect(allReadings['2'].length).to.greaterThan(61); // more data will be returned because of shift down to the hour view.
 			});
 
@@ -308,7 +307,7 @@ mocha.describe('Compressed Readings 2', () => {
 				const start = moment(startDate);
 				const end = moment(startDate).clone();
 				end.add(61, 'days');
-				const allReadings = await Reading.getNewCompressedReadings([meter.id], start, end, conn);
+				const allReadings = await Reading.getNewCompressedReadings([meter1.id], start, end, conn);
 				expect(allReadings['2'].length).to.equal(24 * 31);
 			});
 
@@ -316,7 +315,7 @@ mocha.describe('Compressed Readings 2', () => {
 				const start = moment(startDate);
 				const end = moment(startDate).clone();
 				end.add(14, 'days');
-				const allReadings = await Reading.getNewCompressedReadings([meter.id], start, end, conn);
+				const allReadings = await Reading.getNewCompressedReadings([meter1.id], start, end, conn);
 				const hourToRawScaleFactor = 1 / (moment.duration('00:23:00').asHours()); // The number of 23-minute intervals in one hour.
 				expect(allReadings['2'].length).to.be.closeTo(14 * 24 * hourToRawScaleFactor, 1);
 			});
