65fcb9851c857659c6a8a6482afde42b0da24b3b
Sagar Prasad <saprasad@csumb.edu>
2022-03-12 19:48:56 -0800

    Added the button to identifier. Need to fix the logic

--- src/client/app/actions/unit.ts
+++ src/client/app/actions/unit.ts
@@ -1,7 +1,9 @@
-import {ActionType, Dispatch, Thunk} from '../types/redux/actions';
+import {ActionType, Dispatch, Thunk, GetState} from '../types/redux/actions';
 import * as t from '../types/redux/unit'
 import {unitsApi} from '../utils/api';
 import { NamedIDItem } from '../types/items';
+import { showErrorNotification } from '../utils/notifications';
+import translate from '../utils/translate';
 
 function requestUnitsDetails(): t.RequestUnitsDetailsAction {
     return { type: ActionType.RequestUnitsDetails}
@@ -10,6 +12,47 @@ function requestUnitsDetails(): t.RequestUnitsDetailsAction {
 function receiveUnitsDetails(data: NamedIDItem[]): t.ReceiveUnitsDetailsAction{
     return {type: ActionType.ReceiveUnitsDetails, data}
 } 
+
+export function confirmUnitEdits(unit: number): t.ConfirmEditedUnitAction {
+	return { type: ActionType.ConfirmEditedUnit, unit};
+}
+
+export function submitUnitEdits(unit: number): t.SubmitEditedUnitAction {
+	return { type: ActionType.SubmitEditedUnit, unit };
+}
+
+export function confirmEditedUnits(): Thunk {
+	return async (dispatch: Dispatch, getState: GetState) => {
+		Object.keys(getState().units.editedUnits).forEach(unitIdS => {
+			const unitId = parseInt(unitIdS);
+			dispatch(confirmUnitEdits(unitId));
+		});
+	}
+}
+
+export function submitEditedUnits(): Thunk {
+	return async (dispatch: Dispatch, getState: GetState) => {
+		Object.keys(getState().units.editedUnits).forEach(unitIdS => {
+			const unitId = parseInt(unitIdS);
+			if (getState().units.submitting.indexOf(unitId) === -1) {
+				dispatch(submitEditedUnit(unitId));
+			}
+		});
+	};
+}
+
+export function submitEditedUnit(unitId: number): Thunk {
+	return async (dispatch: Dispatch, getState: GetState) => {
+		const submittingUnit = getState().units.editedUnits[unitId];
+		dispatch(submitUnitEdits(unitId));
+		try {
+			await unitsApi.edit(submittingUnit);
+			dispatch(confirmUnitEdits(unitId));
+		} catch (err) {
+			showErrorNotification(translate('failed.to.edit.unit'));
+		}
+	};
+}
 
 export function fetchUnitsDetails(): Thunk{
     return async (dispatch: Dispatch) => {
