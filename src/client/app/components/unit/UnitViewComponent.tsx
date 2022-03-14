78679a9d2083b9cce3e89d3a3b1581a4726b873e
Andrew <ancline@csumb.edu>
2022-03-13 19:54:36 -0700

    added button to unitRepresent column in UnitViewComponent

--- src/client/app/components/unit/UnitViewComponent.tsx
+++ src/client/app/components/unit/UnitViewComponent.tsx
@@ -17,9 +17,20 @@ interface UnitViewProps {
 
 type UnitViewPropsWithIntl = UnitViewProps & WrappedComponentProps;
 
-class UnitViewComponent extends React.Component<UnitViewPropsWithIntl, {}> {
+interface UnitViewState {
+    unitRepresentFocus: boolean;
+    unitRepresentInput: string;
+}
+
+class UnitViewComponent extends React.Component<UnitViewPropsWithIntl, UnitViewState> {
     constructor(props: UnitViewPropsWithIntl){
         super(props); 
+        this.state = {
+            unitRepresentFocus: false,
+            unitRepresentInput: this.props.unit.unitRepresent
+        }
+        this.handleUnitRepresentChange = this.handleUnitRepresentChange.bind(this);
+        this.toggleUnitRepresentInput = this.toggleUnitRepresentInput.bind(this);
     }
     public render() {
         const loggedInAsAdmin = this.props.loggedInAsAdmin;
@@ -28,7 +39,7 @@ class UnitViewComponent extends React.Component<UnitViewPropsWithIntl, {}> {
                 {loggedInAsAdmin && <td> {this.props.unit.id} {this.formatStatus()} </td>}
                 {loggedInAsAdmin && <td> {this.props.unit.name} {this.formatStatus()} </td>}
                 {loggedInAsAdmin && <td> {this.props.unit.identifier} {this.formatStatus()} </td>}
-                {loggedInAsAdmin && <td> {this.props.unit.unitRepresent} {this.formatStatus()} </td>}
+                {loggedInAsAdmin && <td> {this.formatUnitRepresentInput()} </td>}
                 {loggedInAsAdmin && <td> {this.props.unit.secInRate} {this.formatStatus()} </td>}
                 {loggedInAsAdmin && <td> {this.props.unit.typeOfUnit} {this.formatStatus()} </td>}
                 {loggedInAsAdmin && <td> {this.props.unit.suffix} {this.formatStatus()} </td>}
@@ -37,6 +48,57 @@ class UnitViewComponent extends React.Component<UnitViewPropsWithIntl, {}> {
                 {loggedInAsAdmin && <td> {this.props.unit.note} {this.formatStatus()} </td>}
             </tr>
         );
+    }
+
+    private styleToggleBtn(): React.CSSProperties {
+		return { float: 'right' };
+	}
+
+    private handleUnitRepresentChange(event: React.ChangeEvent<HTMLSelectElement>){
+        this.setState({unitRepresentInput: event.target.value});
+    }
+
+    private toggleUnitRepresentInput(){
+        if(this.state.unitRepresentFocus){
+            const unitRepresent = this.state.unitRepresentInput;
+
+            const editedUnit = {
+                ...this.props.unit,
+                unitRepresent
+            };
+            // this.props.editUnitDetails(editedUnit);
+        }
+        this.setState({unitRepresentFocus: !this.state.unitRepresentFocus});
+    }
+    
+
+    private formatUnitRepresentInput() {
+        let formattedUnitRepresent;
+        let buttonMessageId;
+        if(this.state.unitRepresentFocus){
+            formattedUnitRepresent = <select 
+            id={'unitRepresent'} 
+            value={this.state.unitRepresentInput}
+            onChange={event => this.handleUnitRepresentChange(event)}>
+            <option value="quantity">Quantity</option>
+            <option value="flow">Flow</option>
+            <option value="raw">Raw</option>
+            <option value="unused">Unused</option>
+            </select>
+            buttonMessageId = 'update';
+        }else{
+            formattedUnitRepresent = <div>{this.state.unitRepresentInput}</div>
+            buttonMessageId = 'edit';
+        }
+
+        let toggleButton;
+        toggleButton = <Button style={this.styleToggleBtn()} color='primary' onClick={this.toggleUnitRepresentInput}>
+            <FormattedMessage id={buttonMessageId} />
+        </Button>
+        return(<div>
+            {formattedUnitRepresent}
+            {toggleButton}
+        </div>)
     }
 
     private formatStatus(): string {
