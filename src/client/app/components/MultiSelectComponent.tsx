94183abd59c5e64b761b9d1e1ceffd09e2c39b8f
jina2k <jina26p@gmail.com>
2022-03-20 01:31:16 -0400

    lint

--- src/client/app/components/MultiSelectComponent.tsx
+++ src/client/app/components/MultiSelectComponent.tsx
@@ -4,16 +4,13 @@
 
 import * as React from 'react';
 import Select from 'react-select';
-//import 'react-select/dist/react-select.css';
 import { SelectOption } from '../types/items';
-//import '../styles/react-select-css.css';
 
 interface MultiSelectProps<I> {
 	placeholder: string;
 	/* tslint:disable:array-type */
 	options: Array<SelectOption & I>;
 	selectedOptions: Array<SelectOption & I> | undefined;
-	//style?: React.CSSProperties;
 	singleSelect?: boolean;
 	onValuesChange(values: Array<SelectOption & I>): void;
 	/* tslint:enable:array-type */
@@ -42,11 +39,10 @@ export default class MultiSelectComponent<I> extends React.Component<MultiSelect
 	public render() {
 		return (
 			<Select
-				isMulti={true}
+				isMulti
 				options={this.props.options}
 				value={this.state.selectedOptions}
 				placeholder={this.props.placeholder}
-				//style={this.props.style}
 				onChange={this.onValuesChangeInternal}
 				isClearable={false}
 				closeMenuOnSelect={false}
