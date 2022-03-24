1156a1ea62163fad7620fb82ceb31861c38ae71c
Joseph Newman <josephnew900@gmail.com>
2022-03-24 15:17:34 -0700

    Full Edit Meter Modal without logic
    All input value spots added to meter page. Needs Cssing to make it look more friendly. Currently just a list

--- src/client/app/components/meters/MeterModalEditComponent.tsx
+++ src/client/app/components/meters/MeterModalEditComponent.tsx
@@ -1,6 +1,6 @@
 import  { useState } from "react";
 import * as React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Dropdown } from "react-bootstrap";
 
 function ModalCard() {
 const [showModal, setShow] = useState(false);
@@ -10,17 +10,187 @@ const [showModal, setShow] = useState(false);
 
   return (
     <>
-        <Button variant="primary" onClick={handleShow}>
-          Modal Launch for Meters
+        <Button variant="Secondary" onClick={handleShow}>
+          Edit Meter
         </Button>
       
       <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Meter Information</Modal.Title>
+          <Modal.Title> Edit Meter Information</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
+          <div>
+           Identifier: <span><br/><input type="text" value="filled in info" autoFocus /></span>
+          </div>
+
+          <div>
+            Units: <span> 
+              <Dropdown>
+              <Dropdown.Toggle id="dropdown-button-dark-example1" size="sm" variant="secondary">
+               Dropdown Button
+            </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                  <Dropdown.Item active>
+                    Action
+                  </Dropdown.Item>
+                  <Dropdown.Item >option1</Dropdown.Item>
+                  <Dropdown.Item >Something else</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item >other option </Dropdown.Item>
+              </Dropdown.Menu>
+              </Dropdown>
+              </span>
+          </div>
+
+          <div>
+            Meter-Type: <span>
+              <Dropdown>
+              <Dropdown.Toggle id="dropdown-button-dark-example1" size="sm" variant="secondary">
+                Dropdown Button
+              </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                  <Dropdown.Item active>
+                    Action
+                  </Dropdown.Item>
+                  <Dropdown.Item >option1</Dropdown.Item>
+                  <Dropdown.Item >Something else</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item >other option </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              </span>
+          </div>
+
+          <div>
+          Graphic Units: 
+              <Dropdown>
+              <Dropdown.Toggle id="dropdown-button-dark-example1" size="sm"variant="secondary">
+                Dropdown Button
+              </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                  <Dropdown.Item active>
+                    Action
+                  </Dropdown.Item>
+                  <Dropdown.Item >option1</Dropdown.Item>
+                  <Dropdown.Item >Something else</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item >other option </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+          </div>
+
+          <div>
+            GPS: <span><br/><input type="text" value="Filler Information" autoFocus/></span>
+          </div>
+
+          <div>
+            Area: <span><br/><input type="text" value="Filler Information" autoFocus/></span>
+          </div>
+
+          <div>
+            Displayable: <span><input type="checkbox"/></span>
+          </div>
+
+          <div>
+            Enabled: <span><input type="checkbox"/></span>
+          </div>
+
+          <div>
+            Meter Address: <span><br/><input type="text" value="Filler Information" autoFocus/></span>
+          </div>
+
+          <div>
+            Timezones:
+            <span> 
+              <Dropdown>
+              <Dropdown.Toggle id="dropdown-button-dark-example1" size="sm" variant="secondary">
+               EST/PST/CST
+            </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                  <Dropdown.Item active>
+                    Action
+                  </Dropdown.Item>
+                  <Dropdown.Item >option1</Dropdown.Item>
+                  <Dropdown.Item >Something else</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item >other option </Dropdown.Item>
+              </Dropdown.Menu>
+              </Dropdown>
+              </span>
+          </div>
+
+          <div>
+            Notes:<br/> <input type="text" value="Filler Information" autoFocus/>
+          </div>
+
+          <div>
+            Cumulative: <input type="checkbox"/>
+          </div>
+
+          <div>
+            Cumulative Reset: <input type="checkbox"/>
+          </div>
+
+          <div>
+            Resrt Start-Time: <br/> <input type="text" value="00:00:00" autoFocus/>
+          </div>
+        
+          <div id="Split it here">
+            Time Sort: <input type="checkbox"/>
+          </div>
           
+          <div>
+            End Only: <input type="checkbox"/>
+          </div>
+
+          <div>
+            Read Gap: <br/> <input type="text" value="Filler Information"/>
+          </div>
+
+          <div>
+            Reading Variation: <br/> <input type="text" value="Filler Information"/>
+          </div>
+          
+          <div>
+            Reading Duplication: <br/> 
+            <span> 
+              <Dropdown>
+              <Dropdown.Toggle id="dropdown-button-dark-example1" size="sm" variant="secondary">
+               Filler Information
+            </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                  <Dropdown.Item active>
+                    Action
+                  </Dropdown.Item>
+                  <Dropdown.Item >option1</Dropdown.Item>
+                  <Dropdown.Item >Something else</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item >other option </Dropdown.Item>
+              </Dropdown.Menu>
+              </Dropdown>
+              </span>
+          </div>
+
+          <div>
+            Reading:
+          </div>
+
+          <div>
+            Start Date/Time:<br/> <input type="text" value="Filler Information"/>
+          </div>
+
+          <div>
+            End Date/Time:<br/> <input type="text" value="Filler Information"/>
+          </div>
+          <br/>
+          <div>
+            ID: <input type="text" readOnly value="00"/>
+          </div>
+
+          <div>
+            Name of Meter: <br/> <input type="text" value="Filler Meter Name"/> 
+          </div>
         </Modal.Body>
 
         <Modal.Footer>
