c8ff4480abfbfbae7ee2dca78ad865b1ac2c9baa
Guruprem Rajpal <57023709+Gurupremrajpal@users.noreply.github.com>
2022-04-22 11:11:27 -0700

    Update test.js

--- src/server/test/web/individualTests/test.js
+++ src/server/test/web/individualTests/test.js
@@ -148,7 +148,88 @@ const { assert } = require('console');
             mocha.describe('testing ...', () => {
                 mocha.it('should be able to ...', async () => {
                     // Here is where we run our logic for that specifc loop
-			
+			const res = await chai.request(app).post(UPLOAD_METERS_ROUTE)
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('gzip', 'false')
+				.attach('csvfile', metersBuffer, `${cumAscNegative}`)
+
+				expect(res).to.have.status(200);
+				const csvMeters = (await parseCsv(metersBuffer)).map(row =>
+					(new Meter(undefined, row[1], row[2] === 'TRUE', row[3] === 'TRUE', row[4], row[5]))
+				);
+
+				const con = testDB.getConnection();
+				const dbMeters = await Promise.all(csvMeters.map(async (meter, idx) => {
+					const dbMeter = await Meter.getByName(meter.name, con);
+					csvMeters[idx].id = dbMeter.id;
+					return meter;
+				}));
+				expect(dbMeters.length).to.equal(csvMeters.length);
+				expect(dbMeters).to.deep.equal(csvMeters);
+				expect((await Meter.getAll(con)).length).to.equal(3);
+			});
+		mocha.it('should be able to accept a post request to upload unzipped meter data with header row.', async () => {
+			const res = await chai.request(app).post(UPLOAD_METERS_ROUTE)
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('headerRow', 'true')
+				.field('gzip', 'false')
+				.attach('csvfile', metersWithHeaderBuffer, `${metersPathWithHeader}`)
+
+			expect(res).to.have.status(200);
+			const csvMeters = (await parseCsv(metersWithHeaderBuffer)).map(row =>
+				(new Meter(undefined, row[0], row[1], row[2] === 'TRUE', row[3] === 'TRUE', row[4], row[5]))
+			).slice(1);
+
+			const conn = testDB.getConnection();
+			const dbMeters = await Promise.all(csvMeters.map(async (meter, idx) => {
+				const dbMeter = await Meter.getByName(meter.name, conn);
+				csvMeters[idx].id = dbMeter.id;
+				return meter;
+			}));
+			expect(dbMeters).to.deep.equal(csvMeters);
+			expect((await Meter.getAll(conn)).length).to.equal(3);
+			});
+		mocha.it('should be able to load unzipped readings data for an existing meter.', async () => {
+			const conn = testDB.getConnection();
+			const meter = new Meter(undefined, 'XXX', undefined, false, false, Meter.type.MAMAC, 'XXX')
+			await meter.insert(conn); // insert meter
+			const res = await chai.request(app).post(UPLOAD_READINGS_ROUTE) // make request to api to upload readings data for this meter
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('meterName', 'XXX')
+				.field('timeSort', 'increasing')
+				.field('gzip', 'false')
+				.attach('csvfile', readingsBuff, `${cumAscNegative}`)
+
+			expect(res).to.have.status(200);
+			const readings = await Reading.getAllByMeterID(meter.id, conn);
+			const extractedReadings = readings.map(reading => {
+				return [`${reading.reading}`, reading.startTimestamp._i, reading.endTimestamp._i];
+			});
+			const fileReadings = await parseCsv(readingsBuffer);
+			expect(extractedReadings).to.deep.equals(fileReadings);
+			});
+		mocha.it('should be able to load unzipped readings data for a non existing meter.', async () => {
+			const conn = testDB.getConnection();
+			const res = await chai.request(app).post(UPLOAD_READINGS_ROUTE) // make request to api to upload readings data for this meter
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('createMeter', 'true')
+				.field('meterName', 'ABG')
+				.field('timeSort', 'increasing')
+				.field('gzip', 'false')
+				.attach('csvfile', readingsBuffer, `${cumAscNegative}`)
+
+			const meter = await Meter.getByName('ABG', conn);
+			const readings = await Reading.getAllByMeterID(meter.id, conn);
+			const extractedReadings = readings.map(reading => {
+				return [`${reading.reading}`, reading.startTimestamp._i, reading.endTimestamp._i];
+			});
+			const fileReadings = await parseCsv(readingsBuffer);
+			expect(extractedReadings).to.deep.equals(fileReadings);
+
 		
                 })
             })
@@ -158,6 +239,88 @@ const { assert } = require('console');
             mocha.describe('testing ...', () => {
                 mocha.it('should be able to ...', async () => {
                     // Here is where we run our logic for that specifc loop
+			const res = await chai.request(app).post(UPLOAD_METERS_ROUTE)
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('gzip', 'false')
+				.attach('csvfile', metersBuffer, `${meterpipe40}`)
+
+				expect(res).to.have.status(200);
+				const csvMeters = (await parseCsv(metersBuffer)).map(row =>
+					(new Meter(undefined, row[1], row[2] === 'TRUE', row[3] === 'TRUE', row[4], row[5]))
+				);
+
+				const con = testDB.getConnection();
+				const dbMeters = await Promise.all(csvMeters.map(async (meter, idx) => {
+					const dbMeter = await Meter.getByName(meter.name, con);
+					csvMeters[idx].id = dbMeter.id;
+					return meter;
+				}));
+				expect(dbMeters.length).to.equal(csvMeters.length);
+				expect(dbMeters).to.deep.equal(csvMeters);
+				expect((await Meter.getAll(con)).length).to.equal(3);
+			});
+		mocha.it('should be able to accept a post request to upload unzipped meter data with header row.', async () => {
+			const res = await chai.request(app).post(UPLOAD_METERS_ROUTE)
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('headerRow', 'true')
+				.field('gzip', 'false')
+				.attach('csvfile', metersWithHeaderBuffer, `${metersPathWithHeader}`)
+
+			expect(res).to.have.status(200);
+			const csvMeters = (await parseCsv(metersWithHeaderBuffer)).map(row =>
+				(new Meter(undefined, row[0], row[1], row[2] === 'TRUE', row[3] === 'TRUE', row[4], row[5]))
+			).slice(1);
+
+			const conn = testDB.getConnection();
+			const dbMeters = await Promise.all(csvMeters.map(async (meter, idx) => {
+				const dbMeter = await Meter.getByName(meter.name, conn);
+				csvMeters[idx].id = dbMeter.id;
+				return meter;
+			}));
+			expect(dbMeters).to.deep.equal(csvMeters);
+			expect((await Meter.getAll(conn)).length).to.equal(3);
+			});
+		mocha.it('should be able to load unzipped readings data for an existing meter.', async () => {
+			const conn = testDB.getConnection();
+			const meter = new Meter(undefined, 'XXX', undefined, false, false, Meter.type.MAMAC, 'XXX')
+			await meter.insert(conn); // insert meter
+			const res = await chai.request(app).post(UPLOAD_READINGS_ROUTE) // make request to api to upload readings data for this meter
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('meterName', 'XXX')
+				.field('timeSort', 'increasing')
+				.field('gzip', 'false')
+				.attach('csvfile', readingsBuff, `${meterPipe40}`)
+
+			expect(res).to.have.status(200);
+			const readings = await Reading.getAllByMeterID(meter.id, conn);
+			const extractedReadings = readings.map(reading => {
+				return [`${reading.reading}`, reading.startTimestamp._i, reading.endTimestamp._i];
+			});
+			const fileReadings = await parseCsv(readingsBuffer);
+			expect(extractedReadings).to.deep.equals(fileReadings);
+			});
+		mocha.it('should be able to load unzipped readings data for a non existing meter.', async () => {
+			const conn = testDB.getConnection();
+			const res = await chai.request(app).post(UPLOAD_READINGS_ROUTE) // make request to api to upload readings data for this meter
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('createMeter', 'true')
+				.field('meterName', 'ABG')
+				.field('timeSort', 'increasing')
+				.field('gzip', 'false')
+				.attach('csvfile', readingsBuffer, `${meterPipe40}`)
+
+			const meter = await Meter.getByName('ABG', conn);
+			const readings = await Reading.getAllByMeterID(meter.id, conn);
+			const extractedReadings = readings.map(reading => {
+				return [`${reading.reading}`, reading.startTimestamp._i, reading.endTimestamp._i];
+			});
+			const fileReadings = await parseCsv(readingsBuffer);
+			expect(extractedReadings).to.deep.equals(fileReadings);
+
                 })
             })
         }
@@ -166,6 +329,88 @@ const { assert } = require('console');
             mocha.describe('testing ...', () => {
                 mocha.it('should be able to ...', async () => {
                     // Here is where we run our logic for that specifc loop
+			const res = await chai.request(app).post(UPLOAD_METERS_ROUTE)
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('gzip', 'false')
+				.attach('csvfile', metersBuffer, `${regAscEndonlyEndSamePreviousEnd}`)
+
+				expect(res).to.have.status(200);
+				const csvMeters = (await parseCsv(metersBuffer)).map(row =>
+					(new Meter(undefined, row[1], row[2] === 'TRUE', row[3] === 'TRUE', row[4], row[5]))
+				);
+
+				const con = testDB.getConnection();
+				const dbMeters = await Promise.all(csvMeters.map(async (meter, idx) => {
+					const dbMeter = await Meter.getByName(meter.name, con);
+					csvMeters[idx].id = dbMeter.id;
+					return meter;
+				}));
+				expect(dbMeters.length).to.equal(csvMeters.length);
+				expect(dbMeters).to.deep.equal(csvMeters);
+				expect((await Meter.getAll(con)).length).to.equal(3);
+			});
+		mocha.it('should be able to accept a post request to upload unzipped meter data with header row.', async () => {
+			const res = await chai.request(app).post(UPLOAD_METERS_ROUTE)
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('headerRow', 'true')
+				.field('gzip', 'false')
+				.attach('csvfile', metersWithHeaderBuffer, `${metersPathWithHeader}`)
+
+			expect(res).to.have.status(200);
+			const csvMeters = (await parseCsv(metersWithHeaderBuffer)).map(row =>
+				(new Meter(undefined, row[0], row[1], row[2] === 'TRUE', row[3] === 'TRUE', row[4], row[5]))
+			).slice(1);
+
+			const conn = testDB.getConnection();
+			const dbMeters = await Promise.all(csvMeters.map(async (meter, idx) => {
+				const dbMeter = await Meter.getByName(meter.name, conn);
+				csvMeters[idx].id = dbMeter.id;
+				return meter;
+			}));
+			expect(dbMeters).to.deep.equal(csvMeters);
+			expect((await Meter.getAll(conn)).length).to.equal(3);
+			});
+		mocha.it('should be able to load unzipped readings data for an existing meter.', async () => {
+			const conn = testDB.getConnection();
+			const meter = new Meter(undefined, 'XXX', undefined, false, false, Meter.type.MAMAC, 'XXX')
+			await meter.insert(conn); // insert meter
+			const res = await chai.request(app).post(UPLOAD_READINGS_ROUTE) // make request to api to upload readings data for this meter
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('meterName', 'XXX')
+				.field('timeSort', 'increasing')
+				.field('gzip', 'false')
+				.attach('csvfile', readingsBuff, `${regAscEndonlyEndSamePreviousEnd}`)
+
+			expect(res).to.have.status(200);
+			const readings = await Reading.getAllByMeterID(meter.id, conn);
+			const extractedReadings = readings.map(reading => {
+				return [`${reading.reading}`, reading.startTimestamp._i, reading.endTimestamp._i];
+			});
+			const fileReadings = await parseCsv(readingsBuffer);
+			expect(extractedReadings).to.deep.equals(fileReadings);
+			});
+		mocha.it('should be able to load unzipped readings data for a non existing meter.', async () => {
+			const conn = testDB.getConnection();
+			const res = await chai.request(app).post(UPLOAD_READINGS_ROUTE) // make request to api to upload readings data for this meter
+				.field('email', testUser.email)
+				.field('password', testUser.password)
+				.field('createMeter', 'true')
+				.field('meterName', 'ABG')
+				.field('timeSort', 'increasing')
+				.field('gzip', 'false')
+				.attach('csvfile', readingsBuffer, `${regAscEndonlyEndSamePreviousEnd}`)
+
+			const meter = await Meter.getByName('ABG', conn);
+			const readings = await Reading.getAllByMeterID(meter.id, conn);
+			const extractedReadings = readings.map(reading => {
+				return [`${reading.reading}`, reading.startTimestamp._i, reading.endTimestamp._i];
+			});
+			const fileReadings = await parseCsv(readingsBuffer);
+			expect(extractedReadings).to.deep.equals(fileReadings);
+
                 })
             })
         }
