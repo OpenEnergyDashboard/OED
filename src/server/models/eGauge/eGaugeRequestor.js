b0a4fb05d6c4ea3f35b0d50d14ad766cdf7447ed
Steven Huss-Lederman <huss@beloit.edu>
2022-05-03 09:55:45 -0500

    modify comments

--- src/server/models/eGauge/eGaugeRequestor.js
+++ src/server/models/eGauge/eGaugeRequestor.js
@@ -12,9 +12,11 @@ const { meterTimezone } = require('../../services/meterTimezone');
 class EgaugeRequestor {
 
 	/**
-	 * @param {Meter} meter 
+	 * Create object for reading an eGauge meter.
+	 * @param {Meter} meter associated with the eGauge readings.
 	 */
 	constructor(meter) {
+		// Information expected by the eGauge API and needed by OED.
 		const url = new URL(`https://${meter.url}`);
 		this.apiUrl = `${url.origin}/api`; // can we add an ending slash??
 		this.username = url.searchParams.get('username');
@@ -47,9 +49,9 @@ class EgaugeRequestor {
 		const USERNAME = this.username;
 		const PASSWORD = this.password;
 
+		// This use of MD5 is specified in the eGauge api.
 		const CNONCE = crypto.randomBytes(64).toString('hex');
 		const HA1 = md5(`${USERNAME}:${rlm}:${PASSWORD}`);
-
 		const HA2 = md5(`${HA1}:${nnc}:${CNONCE}`);
 
 		const res = await axios.post(loginUrl, {
@@ -70,53 +72,60 @@ class EgaugeRequestor {
 	 * Fetches eGauge data within a time interval. If the reading unit is detected to be Power (recorded as W*s), we convert it kWh.
 	 * @param {number | string} startTimestamp unix start timestamp or 'epoch' which is an eGauge keyword for when the device began gathering data.
 	 * @param {number | string} endTimestamp unix end timestamp or 'soh' which is an eGauge keyword for the start of the current hour.
-	 * @param {number} timeStep time step in seconds.
+	 * @param {number} timeStep time step between readings in seconds.
 	 * @param {string} timezoneUse timezone to interpret the readings in since eGauge uses Unix timestamps that depend on the timezone.
 	 * @returns {Promise<[[number, moment, moment]]>} a matrix whose rows consists of the reading value and the end timestamp as a moment.
 	 */
 	async getMeterReadingsBetweenInterval(startTimestamp, endTimestamp, timeStep, timezoneUse) {
+		// Get data from the eGauge device.
 		const res = await this.get(`${this.apiUrl}/register/?rate&format=json&time=${startTimestamp}:${timeStep}:${endTimestamp}&reg=${this.registerId}`);
 		const data = res.data;
-		if(data.error !== undefined){
+		if (data.error !== undefined) {
 			// An error can occur if the time range is improper. For example, if the endTimestamp is
-			// after the startTimestamp.
-			log.error(data.error);
+			// after the startTimestamp. In this case no readings are recorded.
+			log.error("No data received/stored in getting eGauge readings due to error: " + data.error);
 			return [];
 		}
 		const meterReadings = [];
+		// What type of reading data was sent.
 		const readingType = data.registers[0].type;
-		
+
+		// Loop over all data received.
 		for (const dataRange of data.ranges) {
+			// The readings sent.
 			const readingValues = dataRange.rows;
 			// TODO what is the 's' parameter for? I removed it and it still works.
 			const endTs = moment.unix(dataRange.ts).tz(timezoneUse);
-
-			const timeDelta = dataRange.delta; // timeStep is in seconds 
+			// timeDelta is in seconds which is time between readings.
+			const timeDelta = dataRange.delta;
+			// Loop over all readings.
 			for (let i = 0; i < readingValues.length; i++) {
 				// eGauge sends data that is relative to the dataRange.ts where each
 				// point is one more timeDelta back in time.
 				// Need to create a new moment since these function mutate.
 				const startTime = endTs.clone().subtract(timeDelta * (i + 1), 's');
+				// The end time is just a shift of the time of each reading in seconds.
 				const endTime = startTime.clone().add(timeDelta, 's');
 				// This is the reading value which is an integer.
 				let val = parseInt(readingValues[i][0]);
 				// if unit is P then we know that the upload data is in W*s so we convert to kWh; otherwise, we don't convert.
-				// TODO in the future with units we should use the raw meter value and have it converted when graphing.
+				// TODO in the future with units we should use the raw meter value and have it converted when graphing. We also need to deal with other unit types.
 				if (readingType === 'P') {
 					val = val / (1000 * 3600);
 				}
 				meterReadings.push([val, startTime, endTime]);
 			}
 		}
-
 		return meterReadings;
 	}
 
 	/**
-	 * Fetches the next hour of eGauge data for this.meter.
+	 * Fetches the next hour of eGauge data for this.meter if fetched the last hour. Otherwise, it fetches
+	 * data back to the last time fetched or all time if never fetched before.
 	 * @returns {Promise<[[number, moment]]>} a matrix whose rows consists of the reading value and the end timestamp as a moment.
 	 */
 	async getMeterReadings() {
+		// The meter stores the last time fetched.
 		const lastTimestamp = this.meter.endTimestamp;
 		let startTimestamp;
 		// The timezone we interpret the readings in.
@@ -126,23 +135,27 @@ class EgaugeRequestor {
 		// 'soh' will instruct eGauge to respond with all the data from the startTimestamp
 		// to the start of the current hour.
 		const endTimestamp = 'soh';
+		// This is the time stored when a meter is created and means no readings have yet been acquired.
 		const E0 = moment(0).utc();
+		// 900 seconds is 15 minutes, which is the default granularity that we will store for eGauge registers.
+		// TODO We should allow the admin to decide the frequency of readings acquired.
 		const timeStep = 900;
-		// 900 seconds is 15 minutes; which is the default granularity that we will store for eGauge registers.
 		// The eGauge meters stored on OED should be also have cumulative set to true.
-		// We would have preferred gzip on the pull system, but we will continue with the assumption that transmissions are clean;
+		// We would have preferred gzip on the pull system, but we will continue with the assumption that transmissions are clean.
 		if (E0.format('YYYY-MM-DD HH:mm:ss') === lastTimestamp.format('YYYY-MM-DD HH:mm:ss')) {
-			startTimestamp = 'epoch'; // We assume that there is not so much data that crashes OED.
+			// We assume that there is not so much data that crashes OED. Thus, we get all the data that the meter has.
+			startTimestamp = 'epoch';
 		} else {
-			// This takes the date/time in the database, sets its timezone to be the one desired with
-			// the same date/time and finally converts it to a unit timestamp.
+			// This takes the date/time in the database on the meter, sets its timezone to be the one desired with
+			// the same date/time and finally converts it to a unit timestamp. This will be the time to start
+			// getting the meter readings.
 			startTimestamp = moment(lastTimestamp).tz(timezoneUse, true).unix();
 		}
 		return this.getMeterReadingsBetweenInterval(startTimestamp, endTimestamp, timeStep, timezoneUse);
 	}
 
 	/**
-	 * Sets the register id based on the register name.
+	 * Sets the register id on this instance based on the register name.
 	 * The register id cannot be found on the eGauge UI side and so
 	 * must be found programmatically.
 	 * @post this.registerId is set to the correct register id on the eGauge device.
@@ -156,8 +169,8 @@ class EgaugeRequestor {
 	}
 
 	/**
-	 * Logouts the jwt.
-	 * @post this.jwt and this.request headers are set to undefined.
+	 * Logout the jwt.
+	 * this.jwt and this.request headers are set to undefined.
 	 */
 	async logout() {
 		if (this.jwt !== undefined) {
@@ -169,7 +182,7 @@ class EgaugeRequestor {
 
 	/**
 	 * A wrapper for axios.get; includes headers.
-	 * @param {string} url 
+	 * @param {string} url for the request.
 	 * @returns 
 	 */
 	async get(url) {
--- src/server/services/eGauge/updateEgaugeMeters.js
+++ src/server/services/eGauge/updateEgaugeMeters.js
@@ -11,22 +11,31 @@ const { getConnection } = require('../../db');
 const readEgaugeData = require('./readEgaugeData');
 const { refreshCompressedReadings, refreshCompressedHourlyReadings } = require('../../models/Reading');
 
+/**
+ * For every enabled eGauge meter, update the readings in the database.
+ */
 async function updateEgaugeMeters() {
 	const conn = getConnection();
 	log.info('Fetching new eGauge meter data');
 	try {
+		// Only work with meters that are enabled to get readings.
 		const allMeters = await Meter.getEnabled(conn);
-		// console.log(allMeters);
+		// We only want the eGauge meters.
 		const metersToUpdate = allMeters.filter(m => m.type === Meter.type.EGAUGE);
 		// Ignoring that loadArrayInput is called in this sequence and returns values
 		// since this is only called by an automated process at this time.
 		// Issues from the pipeline will be logged by called functions.
 		await updateMeters(readEgaugeData, metersToUpdate, conn);
+		// We refresh the readings so they can be graphed to see the new ones.
+		// TODO If the system is getting other types of meters this may cause the refresh
+		// to happen multiple times. Might want to work on this in the future.
 		await refreshCompressedReadings(conn);
 		await refreshCompressedHourlyReadings(conn);
 	} catch (err) {
 		log.error(`Error fetching eGauge meter data: ${err}`, err);
 	}
+	log.info('Completed fetching new eGauge meter data');
 }
 
+// run the update.
 updateEgaugeMeters();
