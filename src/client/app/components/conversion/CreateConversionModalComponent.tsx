2337ce081e63c66fbec3a84593a28098929d0894
Steven Huss-Lederman <huss@beloit.edu>
2022-08-03 16:25:38 -0500

    TODO for no default unit on conversion create.

--- src/client/app/components/conversion/CreateConversionModalComponent.tsx
+++ src/client/app/components/conversion/CreateConversionModalComponent.tsx
@@ -30,6 +30,7 @@ export default function CreateConversionModalComponent(props: CreateConversionMo
 		// The first unit in the unit state will be the default value in the dropdown menu for sourceId and destinationId.
 		// Since not changing the value doesn't call event handler, we must set the default ID values to this first unit
 		// in order to ensure that we are not setting invalid or incorrect unit IDs for source and destination.
+		// TODO We really want to not select a value for units when creating. Try to figure out way to do this.
 		sourceId: Object.values(props.unitsState)[0],       // Set default to ID of first unit in state.
 		destinationId: Object.values(props.unitsState)[0],  // Set default to ID of first unit in state.
 		bidirectional: true,
