c5b9b044d33084cd347b1b977b40bf5c8a04e61b
tqleemail <t.q.le.email@gmail.com>
2022-10-15 19:25:19 +0000

    WIP - Disabled some buttons instead of hiding them

--- src/client/app/components/HeaderButtonsComponent.tsx
+++ src/client/app/components/HeaderButtonsComponent.tsx
@@ -39,10 +39,10 @@ export default class HeaderButtonsComponent extends React.Component<HeaderButton
 		const loggedInAsAdmin = this.props.loggedInAsAdmin;
 		const showOptions = getPage() === '';
 		const renderLoginButton = !hasToken();
-		const renderHomeButton = getPage() !== '';
+		const shouldHomeButtonDisabled = getPage() === '';
 		const renderAdminButton = loggedInAsAdmin && getPage() !== 'admin';
-		const renderGroupsButton = getPage() !== 'groups';
-		const renderMetersButton = getPage() !== 'meters';
+		const shouldGroupsButtonDisabled = getPage() === 'groups';
+		const shouldMetersButtonDisabled = getPage() === 'meters';
 		const renderMapsButton = loggedInAsAdmin && getPage() !== 'maps';
 		const renderCSVButton = role && hasPermissions(role, UserRole.CSV) && getPage() !== 'csv';
 		const renderUnitsButton = loggedInAsAdmin && getPage() !== 'units';
@@ -54,7 +54,7 @@ export default class HeaderButtonsComponent extends React.Component<HeaderButton
 			paddingLeft: '5px'
 		};
 		const homeLinkStyle: React.CSSProperties = {
-			display: renderHomeButton ? 'inline' : 'none',
+			display: 'inline',
 			paddingLeft: '5px'
 		};
 		const adminLinkStyle: React.CSSProperties = {
@@ -65,11 +65,11 @@ export default class HeaderButtonsComponent extends React.Component<HeaderButton
 			paddingLeft: '5px'
 		};
 		const groupsLinkStyle: React.CSSProperties = {
-			display: renderGroupsButton ? 'inline' : 'none',
+			display: 'inline',
 			paddingLeft: '5px'
 		};
 		const metersLinkStyle: React.CSSProperties = {
-			display: renderMetersButton ? 'inline' : 'none',
+			display: 'inline',
 			paddingLeft: '5px'
 		};
 		const mapsLinkStyle: React.CSSProperties = {
@@ -103,11 +103,11 @@ export default class HeaderButtonsComponent extends React.Component<HeaderButton
 					<TooltipHelpContainer page='all' />
 					<TooltipMarkerComponent page='all' helpTextId='help.home.header' />
 					<Link style={adminLinkStyle} to='/admin'><Button outline><FormattedMessage id='admin.panel' /></Button></Link>
-					<Link style={groupsLinkStyle} to='/groups'><Button outline><FormattedMessage id='groups' /></Button></Link>
-					<Link style={metersLinkStyle} to='/meters'><Button outline><FormattedMessage id='meters' /></Button></Link>
+					<Link style={groupsLinkStyle} to='/groups'><Button disabled={shouldGroupsButtonDisabled} outline><FormattedMessage id='groups' /></Button></Link>
+					<Link style={metersLinkStyle} to='/meters'><Button disabled={shouldMetersButtonDisabled} outline><FormattedMessage id='meters' /></Button></Link>
 					<Link style={mapsLinkStyle} to='/maps'><Button outline><FormattedMessage id='maps' /></Button></Link>
 					<Link style={csvLinkStyle} to='/csv'><Button outline><FormattedMessage id='csv' /></Button></Link>
-					<Link style={homeLinkStyle} to='/'><Button outline><FormattedMessage id='home' /></Button></Link>
+					<Link style={homeLinkStyle} to='/'><Button disabled={shouldHomeButtonDisabled} outline><FormattedMessage id='home' /></Button></Link>
 					<Link style={loginLinkStyle} to='/login'><Button outline><FormattedMessage id='log.in' /></Button></Link>
 					<Link style={unitsLinkStyle} to='/units'><Button outline><FormattedMessage id='units' /></Button></Link>
 					<Link style={conversionsLinkStyle} to='/conversions'><Button outline><FormattedMessage id='conversions' /></Button></Link>
