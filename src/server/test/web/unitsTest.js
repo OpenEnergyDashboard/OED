c70199ef882e388ad62b284391865b507e6a184c
Tam Nguyen <minhtam@pgvn.org>
2022-10-29 18:57:15 +0000

    Added unit route test
    Created compareUnits.js to store unit create functions

--- src/server/test/web/unitsTest.js
+++ src/server/test/web/unitsTest.js
@@ -7,10 +7,56 @@
 
 const { chai, mocha, expect, app, testDB, testUser } = require('../common');
 const Unit = require('../../models/Unit');
+const { expectUnitToBeEquivalent, expectArrayOfUnitsToBeEquivalent } = require('../../util/compareUnits');
 
-mocha.it('returns nothing with no units present', async () => {
-    const res = await chai.request(app).get('/api/units');
-    expect(res).to.have.status(200);
-    expect(res).to.be.json;
-    expect(res.body).to.have.lengthOf(0);
+mocha.describe("Units routes", () => {
+    mocha.it('returns nothing with no units present', async () => {
+        const res = await chai.request(app).get('/api/units');
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.lengthOf(0);
+    });
+
+    mocha.it('returns one visible unit', async () => {
+        const conn = testDB.getConnection();
+        const expected = new Unit(undefined, 'kwh', 'kWh', Unit.unitRepresentType.QUANTITY, undefined, Unit.unitType.UNIT, null, '', Unit.displayableType.ALL, true, "standard unit");
+        await expected.insert(conn);
+        const res = await chai.request(app).get('/api/units');
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.lengthOf(1);
+        expectUnitToBeEquivalent(expected, res.body[0]);
+    });
+
+    mocha.it('returns all visible units', async () => {
+        const conn = testDB.getConnection();
+        // Code similar to src/server/data/automatedTestingData.js
+        // Define the values of the units
+        const units =
+            [
+                ['kWh', 'kWh', Unit.unitRepresentType.QUANTITY, Unit.unitType.UNIT, '', Unit.displayableType.ALL, true],
+                ['MJ', 'MegaJoules', Unit.unitRepresentType.QUANTITY, Unit.unitType.UNIT, '', Unit.displayableType.ADMIN, false],
+                ['BTU', 'BTU', Unit.unitRepresentType.QUANTITY, Unit.unitType.UNIT, '', Unit.displayableType.ALL, true],
+                ['M3_gas', 'cubic meters of gas', Unit.unitRepresentType.QUANTITY, Unit.unitType.UNIT, '', Unit.displayableType.ALL, false],
+                ['kg', 'kg', Unit.unitRepresentType.QUANTITY, Unit.unitType.METER, '', Unit.displayableType.NONE, false],
+                ['Metric_ton', 'Metric ton', Unit.unitRepresentType.FLOW, Unit.unitType.UNIT, '', Unit.displayableType.ALL, false],
+                ['Fahrenheit', 'Fahrenheit', Unit.unitRepresentType.RAW, Unit.unitType.UNIT, '', Unit.displayableType.ALL, false],
+                ['Celsius', 'Celsius', Unit.unitRepresentType.RAW, Unit.unitType.UNIT, '', Unit.displayableType.ALL, false]
+            ];
+        // Create and insert units
+        const expectedUnits = [];
+        for (let i = 0; i < units.length; ++i) {
+            const unitData = units[i];
+            const aUnit = new Unit(undefined, unitData[0], unitData[1], unitData[2], undefined,
+                unitData[3], null, unitData[4], unitData[5], unitData[6], 'test unit' + i);
+            expectedUnits.push(aUnit);
+            await aUnit.insert(conn);
+        }
+
+        const res = await chai.request(app).get('/api/units');
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.lengthOf(8);
+        expectArrayOfUnitsToBeEquivalent(expectedUnits, res.body);
+    });
 });
