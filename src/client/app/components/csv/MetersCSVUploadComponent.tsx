5a7d3c058be5e55b3868ad02f932d28bff457359
Don Truong <dong23102003@gmail.com>
2022-11-13 22:35:02 -0500

    refetch meters details if successfully uploaded

--- src/client/app/components/csv/MetersCSVUploadComponent.tsx
+++ src/client/app/components/csv/MetersCSVUploadComponent.tsx
@@ -8,6 +8,8 @@ import { MetersCSVUploadProps } from '../../types/csvUploadForm';
 import FormFileUploaderComponent from '../FormFileUploaderComponent';
 import { FormattedMessage } from 'react-intl';
 import { MODE } from '../../containers/csv/UploadCSVContainer';
+import { fetchMetersDetails } from '../../actions/meters';
+import store from '../../index';
 
 export default class MetersCSVUploadComponent extends React.Component<MetersCSVUploadProps> {
 	private fileInput: React.RefObject<HTMLInputElement>;
@@ -29,8 +31,12 @@ export default class MetersCSVUploadComponent extends React.Component<MetersCSVU
 				// TODO Using an alert is not the best. At some point this should be integrated
 				// with react.
 				window.alert('<h1>SUCCESS</h1>The meter upload was a success.');
+				// Refetch meters details.
+				store.getState().meters.hasBeenFetchedOnce = false;
+				fetchMetersDetails();
 			}
 		} catch (error) {
+			console.log(error);
 			// A failed axios request should result in an error.
 			window.alert(error.response.data as string);
 		}
