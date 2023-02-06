3c643e744d87e145665595709a835c0fab43cc6d
Steven Huss-Lederman <huss@beloit.edu>
2023-02-06 16:35:11 -0600

    fix bug if some items don't have readings

--- src/client/app/utils/exportData.ts
+++ src/client/app/utils/exportData.ts
@@ -61,44 +61,50 @@ function downloadCSV(inputCSV: string, fileName: string) {
  */
 export default function graphExport(readings: LineReading[], meter: string, unitLabel: string, unitIdentifier: string,
 	chartName: ChartTypes, scaling: number) {
-	const dataToExport = convertToCSV(readings, meter, unitLabel, scaling);
+	// It is possible that some meters have not readings so skip if do. This can happen if resize the range of dates (or no data).
+	if (readings.length !== 0) {
+		const dataToExport = convertToCSV(readings, meter, unitLabel, scaling);
 
-	// Determine and format the first time in the dataset which is first one in array since just sorted and the start time.
-	// As usual, maintain UTC.
-	const startTime = moment.utc(readings[0].startTimestamp);
-	// Determine and format the last time in the dataset which is the end time.
-	const endTime = moment.utc(readings[readings.length - 1].endTimestamp);
-	// Use regex to remove commas and replace spaces/colons/hyphens with underscores in timestamps
-	const startTimeString = startTime.format('LL_LTS').replace(/,/g, '').replace(/[\s:-]/g, '_');
-	const endTimeString = endTime.format('LL_LTS').replace(/,/g, '').replace(/[\s:-]/g, '_');
+		// Determine and format the first time in the dataset which is first one in array since just sorted and the start time.
+		// As usual, maintain UTC.
+		const startTime = moment.utc(readings[0].startTimestamp);
+		// Determine and format the last time in the dataset which is the end time.
+		const endTime = moment.utc(readings[readings.length - 1].endTimestamp);
+		// Use regex to remove commas and replace spaces/colons/hyphens with underscores in timestamps
+		const startTimeString = startTime.format('LL_LTS').replace(/,/g, '').replace(/[\s:-]/g, '_');
+		const endTimeString = endTime.format('LL_LTS').replace(/,/g, '').replace(/[\s:-]/g, '_');
 
-	// This is the file name with all the above info so unique.
-	// Note it only uses the unit identifier not with the rate because that has funny characters.
-	const filename = `oedExport_${chartName}_${startTimeString}_to_${endTimeString}_${meter}_${unitIdentifier}.csv`;
-	downloadCSV(dataToExport, filename);
+		// This is the file name with all the above info so unique.
+		// Note it only uses the unit identifier not with the rate because that has funny characters.
+		const filename = `oedExport_${chartName}_${startTimeString}_to_${endTimeString}_${meter}_${unitIdentifier}.csv`;
+		downloadCSV(dataToExport, filename);
+	}
 }
 
 /**
  * Function to export raw data that we request on button click
  *
- * @param {RawReadings[]} items list of readings directly from the database
+ * @param {RawReadings[]} readings list of readings directly from the database
  * @param {string} meter the meter identifier for data being exported
  * @param {string} unit the unit identifier for data being exported
  */
-export function downloadRawCSV(items: RawReadings[], meter: string, unit: string) {
-	let csvOutput = `Readings, Start Timestamp, End Timestamp, Meter, ${meter}, Unit, ${unit} \n`;
-	items.forEach(ele => {
-		// As elsewhere, preserve the UTC time that comes from the DB.
-		// See above for why formatted this way.
-		const startTimestamp = moment.utc(ele.s).format('YYYY-MM-DD HH:mm:ss');
-		const endTimestamp = moment.utc(ele.e).format('YYYY-MM-DD HH:mm:ss');
-		csvOutput += `${ele.r},${startTimestamp},${endTimestamp}\n`;
-	})
-	// Use regex to remove commas and replace spaces/colons/hyphens with underscores.
-	// These are time times for the file name which go from the first reading start time to the last reading end time.
-	// Easy to get since the data is sorted.
-	const startTime = moment.utc(items[0].s).format('LL_LTS').replace(/,/g, '').replace(/[\s:-]/g, '_');
-	const endTime = moment.utc(items[items.length - 1].e).format('LL_LTS').replace(/,/g, '').replace(/[\s:-]/g, '_');
-	const filename = `oedRawExport_line_${startTime}_to_${endTime}_for_${meter}.csv`;
-	downloadCSV(csvOutput, filename);
+export function downloadRawCSV(readings: RawReadings[], meter: string, unit: string) {
+	// It is possible that some meters have not readings so skip if do. This can happen if resize the range of dates (or no data).
+	if (readings.length !== 0) {
+		let csvOutput = `Readings, Start Timestamp, End Timestamp, Meter, ${meter}, Unit, ${unit} \n`;
+		readings.forEach(ele => {
+			// As elsewhere, preserve the UTC time that comes from the DB.
+			// See above for why formatted this way.
+			const startTimestamp = moment.utc(ele.s).format('YYYY-MM-DD HH:mm:ss');
+			const endTimestamp = moment.utc(ele.e).format('YYYY-MM-DD HH:mm:ss');
+			csvOutput += `${ele.r},${startTimestamp},${endTimestamp}\n`;
+		})
+		// Use regex to remove commas and replace spaces/colons/hyphens with underscores.
+		// These are time times for the file name which go from the first reading start time to the last reading end time.
+		// Easy to get since the data is sorted.
+		const startTime = moment.utc(readings[0].s).format('LL_LTS').replace(/,/g, '').replace(/[\s:-]/g, '_');
+		const endTime = moment.utc(readings[readings.length - 1].e).format('LL_LTS').replace(/,/g, '').replace(/[\s:-]/g, '_');
+		const filename = `oedRawExport_line_${startTime}_to_${endTime}_for_${meter}.csv`;
+		downloadCSV(csvOutput, filename);
+	}
 }
