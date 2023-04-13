2ce06029d541678b8cf374c215f2e2f1e4a3f1a8
Nicolax0 <108036935+Nicolax0@users.noreply.github.com>
2023-04-13 17:42:44 -0400

    Create temp dropdown menu

--- src/client/app/components/HeaderButtonsComponent.tsx
+++ src/client/app/components/HeaderButtonsComponent.tsx
@@ -3,8 +3,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import * as React from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
 import { Link } from "react-router-dom";
-import { Button, Dropdown } from "reactstrap";
+import { Button } from "reactstrap";
 import { FormattedMessage } from "react-intl";
 import MenuModalComponent from "./MenuModalComponent";
 import getPage from "../utils/getPage";
@@ -41,6 +42,7 @@ export default function HeaderButtonsComponent(args: {showCollapsedMenuButton: b
 	const shouldConversionsButtonDisabled = getPage() === "conversions";
 	const dataFor = args.isModal ? "all-modal" : "all";
 
+
 	const linkStyle: React.CSSProperties = {
 		display: "inline",
 		paddingLeft: "5px",
@@ -53,22 +55,13 @@ export default function HeaderButtonsComponent(args: {showCollapsedMenuButton: b
 		display: renderLogoutButton ? "inline" : "none",
 		paddingLeft: "5px",
 	};
-	const adminViewableLinkStyle: React.CSSProperties = {
-		display: loggedInAsAdmin ? "inline" : "none",
-		paddingLeft: "5px",
+	/*const adminViewableLinkStyle: React.CSSProperties = {
+		display: loggedInAsAdmin ? "block" : "none",
 	};
 	const csvLinkStyle: React.CSSProperties = {
-		display: renderCSVButton ? "inline" : "none",
-		paddingLeft: "5px",
-	};
-	const dropdownContainer: React.CSSProperties = {
-		position: "relative",
-		display: "inline-block"
-	};
-	const dropdownContent: React.CSSProperties = {
-		position: "absolute",
-		display: "none"
-	};
+		display: renderCSVButton ? "block" : "none",
+	};*/
+
 
 	const handleLogOut = () => {
 		if (useSelector((state: State) => state.unsavedWarning.hasUnsavedChanges))
@@ -93,55 +86,55 @@ export default function HeaderButtonsComponent(args: {showCollapsedMenuButton: b
 				<TooltipHelpContainer page={dataFor} />
 				<TooltipMarkerComponent page={dataFor} helpTextId="help.home.header" />
 				<Dropdown style={linkStyle}>
-					<Button style={linkStyle}>Menu</Button>
-					<div>
-						<Link style={linkStyle} to="/meters">
-							<Button disabled={shouldMetersButtonDisabled} outline>
-								<FormattedMessage id="meters" />
-							</Button>
-						</Link>
-					</div>
+					<Dropdown.Toggle variant="outline-dark">Menu</Dropdown.Toggle>
+					<Dropdown.Menu>
+						<Dropdown.Item 
+							disabled = {shouldHomeButtonDisabled} 
+							onClick={() => {window.location.href = "/";}}>
+							<FormattedMessage id="home" />
+						</Dropdown.Item>
+						<Dropdown.Item 
+							//style={adminViewableLinkStyle} 
+							disabled = {shouldAdminButtonDisabled}
+							onClick={() => {window.location.href = "/admin";}}>
+							<FormattedMessage id="admin.panel" />
+						</Dropdown.Item>
+						<Dropdown.Item
+							//style={adminViewableLinkStyle}
+							disabled = {shouldConversionsButtonDisabled}
+							onClick={() => {window.location.href = "/conversions";}}>
+							<FormattedMessage id="conversions" />
+						</Dropdown.Item>
+						<Dropdown.Item
+							//style={csvLinkStyle}
+							disabled={shouldCSVButtonDisabled}
+							onClick={() => {window.location.href = "/csv";}}>
+							<FormattedMessage id="csv" />
+						</Dropdown.Item>
+						<Dropdown.Item
+							disabled={shouldGroupsButtonDisabled}
+							onClick={() => {window.location.href = "/groups";}}>
+							<FormattedMessage id="groups" />
+						</Dropdown.Item>
+						<Dropdown.Item
+							//style={adminViewableLinkStyle}
+							disabled={shouldMapsButtonDisabled}
+							onClick={() => {window.location.href = "/maps";}}>
+							<FormattedMessage id="maps" />
+						</Dropdown.Item>
+						<Dropdown.Item
+							disabled={shouldMetersButtonDisabled}
+							onClick={() => {window.location.href = "/meters";}}>
+							<FormattedMessage id="meters" />
+						</Dropdown.Item>
+						<Dropdown.Item
+							//style={adminViewableLinkStyle}
+							disabled={shouldUnitsButtonDisabled}
+							onClick={() => {window.location.href = "/units";}}>
+							<FormattedMessage id="units" />
+						</Dropdown.Item>
+					</Dropdown.Menu>
 				</Dropdown>
-				<Link style={adminViewableLinkStyle} to="/admin">
-					<Button disabled={shouldAdminButtonDisabled} outline>
-						<FormattedMessage id="admin.panel" />
-					</Button>
-				</Link>
-				<Link style={adminViewableLinkStyle} to="/conversions">
-					<Button disabled={shouldConversionsButtonDisabled} outline>
-						<FormattedMessage id="conversions" />
-					</Button>
-				</Link>
-				<Link style={csvLinkStyle} to="/csv">
-					<Button disabled={shouldCSVButtonDisabled} outline>
-						<FormattedMessage id="csv" />
-					</Button>
-				</Link>
-				<Link style={linkStyle} to="/groups">
-					<Button disabled={shouldGroupsButtonDisabled} outline>
-						<FormattedMessage id="groups" />
-					</Button>
-				</Link>
-				<Link style={linkStyle} to="/">
-					<Button disabled={shouldHomeButtonDisabled} outline>
-						<FormattedMessage id="home" />
-					</Button>
-				</Link>
-				<Link style={adminViewableLinkStyle} to="/maps">
-					<Button disabled={shouldMapsButtonDisabled} outline>
-						<FormattedMessage id="maps" />
-					</Button>
-				</Link>
-				<Link style={linkStyle} to="/meters">
-					<Button disabled={shouldMetersButtonDisabled} outline>
-						<FormattedMessage id="meters" />
-					</Button>
-				</Link>
-				<Link style={adminViewableLinkStyle} to="/units">
-					<Button disabled={shouldUnitsButtonDisabled} outline>
-						<FormattedMessage id="units" />
-					</Button>
-				</Link>
 				<Link style={loginLinkStyle} to="/login">
 					<Button outline>
 						<FormattedMessage id="log.in" />
