1b3752dd85b3a4301f84f511f39eedbedd8cd138
Steven Huss-Lederman <huss@beloit.edu>
2023-04-26 10:41:05 -0500

    resolve migration comments on meter

--- src/server/migrations/0.8.0-1.0.0/sql/meter/add_meter_reading_frequency.sql
+++ src/server/migrations/0.8.0-1.0.0/sql/meter/add_meter_reading_frequency.sql
@@ -2,6 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
--- Add unit_id and default_graphic_unit to the meters table.
+-- Add meter reading frequency to the meters table.
 ALTER TABLE meters
-    ADD COLUMN IF NOT EXISTS meter_reading_frequency INTERVAL;
+    ADD COLUMN IF NOT EXISTS meter_reading_frequency INTERVAL DEFAULT '00:15:00';
