3f15a8da7da2ec8368acefb224074225eee1afb9
Steven Huss-Lederman <huss@beloit.edu>
2023-04-27 18:53:14 -0500

    update SQL, not full check yet
    - This changes how the checks are done so they should work with
    any Postgres interval.
    - Changed so now calculates the range of readings for
    each meter. This means you get the optimal for each meter
    but it may be slower. Need to test for speed.
    - Some cleanup of variable names, etc.

--- src/server/sql/reading/create_reading_views.sql
+++ src/server/sql/reading/create_reading_views.sql
@@ -24,17 +24,18 @@ $$;
 
 /*
 This takes tsrange_to_shrink which is the requested time range to plot and makes sure it does
-not exceed the start/end times for all the readings. This can be an issue, in particular,
+not exceed the start/end times for the meter's readings. This can be an issue, in particular,
 because infinity is used to indicate to graph all readings.
  */
-CREATE OR REPLACE FUNCTION shrink_tsrange_to_real_readings(tsrange_to_shrink TSRANGE)
+CREATE OR REPLACE FUNCTION shrink_tsrange_to_real_readings(tsrange_to_shrink TSRANGE, meter INTEGER)
 	RETURNS TSRANGE
 AS $$
 DECLARE
 	readings_max_tsrange TSRANGE;
 BEGIN
 	SELECT tsrange(min(start_timestamp), max(end_timestamp)) INTO readings_max_tsrange
-	FROM readings;
+	FROM readings
+	WHERE meter_id = meter;
 	RETURN tsrange_to_shrink * readings_max_tsrange;
 END;
 $$ LANGUAGE 'plpgsql';
@@ -219,7 +220,6 @@ min_data_points: The minimum number of data points to return if using the day vi
 min_hour_points: The minimum number of data points to return if using the hour view.
 Details on how this function works can be found in the devDocs in the resource generalization document.
  */
-
 CREATE OR REPLACE FUNCTION meter_line_readings_unit (
 	meter_ids INTEGER[],
 	graphic_unit_id INTEGER,
@@ -231,31 +231,42 @@ CREATE OR REPLACE FUNCTION meter_line_readings_unit (
 	RETURNS TABLE(meter_id INTEGER, reading_rate FLOAT, start_timestamp TIMESTAMP, end_timestamp TIMESTAMP)
 AS $$
 DECLARE
-	requested_interval INTERVAL;
 	requested_range TSRANGE;
+	requested_interval INTERVAL;
+	requested_interval_seconds INTEGER;
 	unit_column INTEGER;
 	frequency INTERVAL;
-	current_meter_id INTEGER := 1;
+	frequency_seconds INTEGER;
+	-- Which index of the meter_id array you are currently working on.
+	current_meter_index INTEGER := 1;
+	-- The id of the meter index working on
+	current_meter_id INTEGER;
 	BEGIN
-	-- Make sure the time range is withing the reading values.
-	requested_range := shrink_tsrange_to_real_readings(tsrange(start_stamp, end_stamp, '[]'));
-	requested_interval := upper(requested_range) - lower(requested_range);
 	-- unit_column holds the column index into the cik table. This is the unit that was requested for graphing.
 	SELECT unit_index INTO unit_column FROM units WHERE id = graphic_unit_id;
-
-	-- For each frequency of points, verify that you will get the minimum graphing points to use.
+	-- For each frequency of points, verify that you will get the minimum graphing points to use for each meter.
 	-- Start with the raw, then hourly and then daily if others will not work.
 	-- Loop over all meters.
-	while current_meter_id <= cardinality(meter_ids) loop
+	while current_meter_index <= cardinality(meter_ids) loop
+		current_meter_id := meter_ids[current_meter_index];
+		-- Make sure the time range is within the reading values for this meter.
+		requested_range := shrink_tsrange_to_real_readings(tsrange(start_stamp, end_stamp, '[]'), current_meter_id);
+		-- The interval of time.
+		requested_interval := upper(requested_range) - lower(requested_range);
+		-- Get the seconds in the interval.
+		-- Wanted to use the INTO syntax used above but could not get it to work so using the set syntax.
+		requested_interval_seconds := (SELECT * from EXTRACT(EPOCH FROM requested_interval));
 		-- Get the frequency that this meter reads at.
-		select meter_reading_frequency into frequency from meters where id = meter_ids[current_meter_id];
-		-- There's no quick way to get the number of hours in an interval. extract(HOURS FROM '1 day, 3 hours') gives 3.
-		-- Divide the time being graphed in minutes by the frequency of reading for this meter in minutes.
-		-- This should give the number of raw readings. If this is less than or equal to 1440 then use raw readings.
-		IF (((EXTRACT (DAY FROM requested_interval) * 1440) / (EXTRACT (HOUR FROM frequency) * 60 + EXTRACT (MINUTE FROM frequency))) <= 1440) 
-		-- Also, if the frequency is at least 1 day (1440 minutes) then use raw readings since even daily would
-		-- interpolate points. This can lead to too many points but do this for now since that is unlikely.
-		OR (EXTRACT (HOUR FROM frequency) * 60 + EXTRACT (MINUTE FROM frequency) >= 1440) THEN
+		select meter_reading_frequency into frequency from meters where id = current_meter_id;
+		-- Get the seconds in the frequency.
+		frequency_seconds := (SELECT * from EXTRACT(EPOCH FROM frequency));
+		-- The first part is making sure that there are no more than 1440 readings to graph if use raw readings.
+		-- Divide the time being graphed by the frequency of reading for this meter to get the number of raw readings.
+		-- The second part checks if the frequency of raw readings is more than a day and use raw if this is the case
+		-- because even daily would
+		-- interpolate points. 1 day is 24 hours * 60 minute/hour * 60 seconds/minute = 86400 seconds.
+		-- This can lead to too many points but do this for now since that is unlikely as you would need around 4+ years of data.
+		IF ((requested_interval_seconds / frequency_seconds <= 1440) OR (frequency_seconds >= 86400)) THEN
 			RETURN QUERY
 				SELECT r.meter_id as meter_id,
 				CASE WHEN u.unit_represent = 'quantity'::unit_represent_type THEN
@@ -270,15 +281,18 @@ DECLARE
 				r.start_timestamp,
 				r.end_timestamp
 				FROM (((readings r
-				INNER JOIN meters m ON m.id = meter_ids[current_meter_id])
+				INNER JOIN meters m ON m.id = current_meter_id)
 				INNER JOIN units u ON m.unit_id = u.id)
 				INNER JOIN cik c on c.row_index = u.unit_index AND c.column_index = unit_column)
-				WHERE lower(requested_range) <= r.start_timestamp AND r.end_timestamp <= upper(requested_range) AND r.meter_id = meter_ids[current_meter_id]
+				WHERE lower(requested_range) <= r.start_timestamp AND r.end_timestamp <= upper(requested_range) AND r.meter_id = current_meter_id
 				-- This ensures the data is sorted
 				ORDER BY r.start_timestamp ASC;
-		-- If hours in the interval is less than or equal to 1440 and frequency of readings is an hour or less then use hourly readings.
-		-- This means the number of hourly points is not too large and  
-		ELSIF (((EXTRACT (DAY FROM requested_interval) * 1440) / 60) <= 1440) AND (EXTRACT (HOUR FROM frequency) * 60 + EXTRACT (MINUTE FROM frequency) <= 60) THEN 
+		-- The first part is making sure that the number of hour points is 1440 or less.
+		-- Thus, check if no more than 1440 hours * 60 minutes/hour * 60 seconds/hour = 5184000 seconds.
+		-- The second part is making sure that the frequency of reading is an hour or less (3600 seconds)
+		-- so you don't interpolate points by using the hourly data.
+		-- TODO Not sure the second part is needed since if the raw readings are more than once an hour then you should have done raw above.
+		ELSIF ((requested_interval_seconds <= 5184000) AND (frequency_seconds <= 3600)) THEN
 			-- Get hourly points to graph. See daily for more comments.
 			RETURN QUERY
 				SELECT hourly.meter_id AS meter_id,
@@ -288,10 +302,10 @@ DECLARE
 					lower(hourly.time_interval) AS start_timestamp,
 					upper(hourly.time_interval) AS end_timestamp
 				FROM (((hourly_readings_unit hourly
-				INNER JOIN meters m ON m.id = meters_ids[current_meter_id])
+				INNER JOIN meters m ON m.id = current_meter_id)
 				INNER JOIN units u ON m.unit_id = u.id)
 				INNER JOIN cik c on c.row_index = u.unit_index AND c.column_index = unit_column)
-				WHERE requested_range @> time_interval AND hourly.meter_id = meter_ids[current_meter_id]
+				WHERE requested_range @> time_interval AND hourly.meter_id = current_meter_id
 				-- This ensures the data is sorted
 				ORDER BY start_timestamp ASC;
 		ELSE 
@@ -310,16 +324,16 @@ DECLARE
 				-- Get all the meter_ids in the passed array of meters.
 				-- This sequence of joins takes the meter id to its unit and in the final join
 				-- it then uses the unit_index for this unit.
-				INNER JOIN meters m ON m.id = meter_ids[current_meter_id])
+				INNER JOIN meters m ON m.id = current_meter_id)
 				INNER JOIN units u ON m.unit_id = u.id)
 				-- This is getting the conversion for the meter (row_index) and unit to graph (column_index).
 				-- The slope and intercept are used above the transform the reading to the desired unit.
 				INNER JOIN cik c on c.row_index = u.unit_index AND c.column_index = unit_column)
-				WHERE requested_range @> time_interval AND daily.meter_id = meter_ids[current_meter_id]
+				WHERE requested_range @> time_interval AND daily.meter_id = current_meter_id
 				-- This ensures the data is sorted
 				ORDER BY start_timestamp ASC;
 		END IF;
-		current_meter_id := current_meter_id + 1;
+		current_meter_index := current_meter_index + 1;
 	end loop;
 END;
 $$ LANGUAGE 'plpgsql';
