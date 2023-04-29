043ecb4d2b38ded86d824e752b79e424c8093991
Steven Huss-Lederman <huss@beloit.edu>
2023-04-29 11:03:18 -0500

    integrate groups
    Groups now work where do all meters as either hourly or daily.

--- src/server/sql/reading/create_reading_views.sql
+++ src/server/sql/reading/create_reading_views.sql
@@ -24,18 +24,18 @@ $$;
 
 /*
 This takes tsrange_to_shrink which is the requested time range to plot and makes sure it does
-not exceed the start/end times for the meter's readings. This can be an issue, in particular,
+not exceed the start/end times for the readings for the supplied meters. This can be an issue, in particular,
 because infinity is used to indicate to graph all readings.
  */
-CREATE OR REPLACE FUNCTION shrink_tsrange_to_real_readings(tsrange_to_shrink TSRANGE, meter INTEGER)
+CREATE OR REPLACE FUNCTION shrink_tsrange_to_real_readings(tsrange_to_shrink TSRANGE, meter_ids INTEGER[])
 	RETURNS TSRANGE
 AS $$
 DECLARE
 	readings_max_tsrange TSRANGE;
 BEGIN
 	SELECT tsrange(min(start_timestamp), max(end_timestamp)) INTO readings_max_tsrange
-	FROM readings
-	WHERE meter_id = meter;
+	FROM (readings r
+		INNER JOIN unnest(meter_ids) meters(id) ON r.meter_id = meters.id);
 	RETURN tsrange_to_shrink * readings_max_tsrange;
 END;
 $$ LANGUAGE 'plpgsql';
@@ -216,6 +216,7 @@ meter_ids: A array of meter ids to query.
 graphic_unit_id: The unit id of the unit to use for the graphic.
 start_timestamp: The start timestamp of the data to return.
 end_timestamp: The end timestamp of the data to return.
+point_accuracy: Tells how decisions should be made on which types of points to return. 'auto' if automatic.
 min_data_points: The minimum number of data points to return if using the day view.
 min_hour_points: The minimum number of data points to return if using the hour view.
 Details on how this function works can be found in the devDocs in the resource generalization document.
@@ -255,7 +256,8 @@ DECLARE
 		current_point_accuracy := point_accuracy;
 		current_meter_id := meter_ids[current_meter_index];
 		-- Make sure the time range is within the reading values for this meter.
-		requested_range := shrink_tsrange_to_real_readings(tsrange(start_stamp, end_stamp, '[]'), current_meter_id);
+		-- There may be a better way to create the array with one element as last argument.
+		requested_range := shrink_tsrange_to_real_readings(tsrange(start_stamp, end_stamp, '[]'), array_append(ARRAY[]::INTEGER[], current_meter_id));
 		if (current_point_accuracy = 'auto'::reading_line_accuracy) THEN
 			-- The request wants automatic calculation of the points returned.
 
@@ -368,13 +370,14 @@ $$ LANGUAGE 'plpgsql';
 
 
 /*
-The following function determines the correct duration view to query from, and returns averaged or raw readings from it.
+The following function determines the correct duration view to query from, and returns averaged readings from it.
 It is designed to return data for plotting line graphs. It works on groups.
 It is the new version of compressed_group_readings_2 that works with units. It takes these parameters:
 group_ids: A array of group ids to query.
 graphic_unit_id: The unit id of the unit to use for the graph.
 start_timestamp: The start timestamp of the data to return.
 end_timestamp: The end timestamp of the data to return.
+point_accuracy: Tells how decisions should be made on which types of points to return. 'auto' if automatic.
 min_data_points: The minimum number of data points to return if using the day view.
 min_hour_points: The minimum number of data points to return if using the hour view.
 Details on how this function works can be found in the devDocs in the resource generalization document and above
@@ -385,6 +388,7 @@ CREATE OR REPLACE FUNCTION group_line_readings_unit (
 	graphic_unit_id INTEGER,
 	start_stamp TIMESTAMP,
 	end_stamp TIMESTAMP,
+	point_accuracy reading_line_accuracy,
 	min_day_points INTEGER,
 	min_hour_points INTEGER
 )
@@ -392,11 +396,56 @@ CREATE OR REPLACE FUNCTION group_line_readings_unit (
 AS $$
 DECLARE
 	meter_ids INTEGER[];
+	requested_range TSRANGE;
+	requested_interval INTERVAL;
+	requested_interval_seconds INTEGER;
+	meters_min_frequency INTERVAL;
+
 BEGIN
 	-- First get all the meter ids that will be included in one or more groups being queried.
+	-- In case meter is repeated, make this distinct.
 	SELECT array_agg(DISTINCT gdm.meter_id) INTO meter_ids
 	FROM groups_deep_meters gdm
 	INNER JOIN unnest(group_ids) gids(id) ON gdm.group_id = gids.id;
+
+	-- Calculate point accuracy if request (auto) or if raw since that is not allowed for groups.
+	IF (point_accuracy = 'auto'::reading_line_accuracy OR point_accuracy = 'raw'::reading_line_accuracy) THEN
+		-- The request needs automatic calculation of the points returned.
+
+		-- Make sure the time range is within the reading values for meters in this group.
+		requested_range := shrink_tsrange_to_real_readings(tsrange(start_stamp, end_stamp, '[]'), meter_ids);
+		-- The interval of time for the requested_range.
+		requested_interval := upper(requested_range) - lower(requested_range);
+		-- Get the seconds in the interval.
+		-- Wanted to use the INTO syntax used above but could not get it to work so using the set syntax.
+		requested_interval_seconds := (SELECT * FROM EXTRACT(EPOCH FROM requested_interval));
+		-- Make sure that the number of hour points is 1440 or less.
+		-- Thus, check if no more than 1440 hours * 60 minutes/hour * 60 seconds/hour = 5184000 seconds.
+		IF ((requested_interval_seconds <= 5184000)) THEN
+			-- Return hourly reading data.
+			point_accuracy := 'hourly'::reading_line_accuracy;
+		ELSE
+			-- Return daily reading data.
+			point_accuracy := 'daily'::reading_line_accuracy;
+		END IF;
+
+		-- Groups can require reading interpolation because of multiple meters. For example, if one meter
+		-- is 30 day reading frequency then it will interpolate to hourly or daily depending other
+		-- meters (if exist). However, to limit this effect, if hourly has been selected automatically,
+		-- check if shortest meter reading frequency for this group is more than an hour and then
+		-- choose daily instead.
+		IF (point_accuracy = 'hourly'::reading_line_accuracy) THEN
+			-- Find the min reading frequency for all meters in the group.
+			SELECT min(reading_frequency) INTO meters_min_frequency
+			FROM (meters m
+			INNER JOIN unnest(meter_ids) meters(id) ON m.id = meters.id);
+			IF (EXTRACT(EPOCH FROM meters_min_frequency) > 3600) THEN
+				-- The smallest meter frequency is greater than 1 hour (3600 seconds) so use daily instead.
+				point_accuracy = 'daily'::reading_line_accuracy;
+			END IF;
+		END IF;
+	END IF;
+	-- point_accuracy should either be daily or hourly at this point.
 
 	RETURN QUERY
 		SELECT
@@ -404,7 +453,7 @@ BEGIN
 			SUM(readings.reading_rate) AS reading_rate,
 			readings.start_timestamp,
 			readings.end_timestamp
-		FROM meter_line_readings_unit(meter_ids, graphic_unit_id, start_stamp, end_stamp, min_day_points, min_hour_points) readings
+		FROM meter_line_readings_unit(meter_ids, graphic_unit_id, start_stamp, end_stamp, point_accuracy, min_day_points, min_hour_points) readings
 		INNER JOIN groups_deep_meters gdm ON readings.meter_id = gdm.meter_id
 		INNER JOIN unnest(group_ids) gids(id) ON gdm.group_id = gids.id
 		GROUP BY gdm.group_id, readings.start_timestamp, readings.end_timestamp
