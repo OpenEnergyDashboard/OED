2753a3f4119740f5da6ca02bd5f2f81102844647
Edwin Kofler <edwin@kofler.dev>
2023-05-18 18:15:19 -0700

    fix: Use static plotly.js imports to make Vite happy
    Vite didn't play nice with these dynamic requires for some reason. To make Vite
    happy, convert the imports to static imports. Also change them to
    ESM-imports for consistency.

--- src/client/app/types/locales.ts
+++ src/client/app/types/locales.ts
@@ -1,8 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// @ts-expect-error Suppress TypeScript declaration file not found error
+import esLocale from 'plotly.js/lib/locales/es'
+// @ts-expect-error Suppress TypeScript declaration file not found error
+import frLocale from 'plotly.js/lib/locales/fr'
 
-export default class Locales{
-	static es = require('plotly.js/lib/locales/es');
-	static fr = require('plotly.js/lib/locales/fr');
+export default class Locales {
+	static es = esLocale;
+	static fr = frLocale;
 }
