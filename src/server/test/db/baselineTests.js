98f1807e2e847b128e070c13aeea0205217356d5
Robert Carlson <rcarlso6@mail.ccsf.edu>
2023-08-14 16:08:45 -0700

    Remove space

--- src/server/test/db/baselineTests.js
+++ src/server/test/db/baselineTests.js
@@ -2,49 +2,49 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- const { mocha, expect, testDB } = require('../common');
- const Baseline = require('../../models/Baseline');
- const Meter = require('../../models/Meter');
- const Reading = require('../../models/Reading');
- const moment = require('moment');
- const Point = require('../../models/Point');
- const gps = new Point(90, 45);
- 
- mocha.describe('Baselines', () => {
-	 mocha.it('can be saved and retrieved', async () => {
-		 conn = testDB.getConnection();
-		 // Need a meter in the database
-		 // TODO: If used note this does not have the more recent meter values.
-		 const meter = new Meter(undefined, 'Larry', null, false, true, Meter.type.MAMAC, null, gps);
-		 await meter.insert(conn);
-		 const reading = new Reading(
-			 meter.id,
-			 1,
-			 moment('1950-01-01'),
-			 moment('1950-02-01')
-		 );
-		 await reading.insert(conn);
-		 // The baseline date/time values from the DB are formatted this way so use that.
-		 const applyStart = moment.utc('1970-01-01 00:01:00+00:00');
-		 const applyEnd = moment.utc('2069-12-31 00:01:00+00:00');
-		 const calcStart = moment.utc('1950-01-01 00:01:00+00:00');
-		 const calcEnd = moment.utc('1950-12-31 00:01:00+00:00');
-		 const baseline = new Baseline(
-			 meter.id,
-			 applyStart,
-			 applyEnd,
-			 calcStart,
-			 calcEnd,
-			 'a note',
-		 );
-		 // This sets the baseline value in the object.
-		 await baseline.insert(conn);
-		 const retrievedBaselines = await Baseline.getAllForMeterID(meter.id, conn);
-		 // The query returns an array. It should contain one entry
-		 expect(retrievedBaselines.length).to.equal(1);
-		 // When I print out these two objects and even compare them digitally they are the same.
-		 // I do not understand why this test fails. I am commenting it out as the baseline is
-		 // not used and all the baseline code needs to be redone.
-		 // expect(retrievedBaselines[0]).to.deep.equal(baseline);
-	 });
- });
+const { mocha, expect, testDB } = require('../common');
+const Baseline = require('../../models/Baseline');
+const Meter = require('../../models/Meter');
+const Reading = require('../../models/Reading');
+const moment = require('moment');
+const Point = require('../../models/Point');
+const gps = new Point(90, 45);
+
+mocha.describe('Baselines', () => {
+	mocha.it('can be saved and retrieved', async () => {
+		conn = testDB.getConnection();
+		// Need a meter in the database
+		// TODO: If used note this does not have the more recent meter values.
+		const meter = new Meter(undefined, 'Larry', null, false, true, Meter.type.MAMAC, null, gps);
+		await meter.insert(conn);
+		const reading = new Reading(
+			meter.id,
+			1,
+			moment('1950-01-01'),
+			moment('1950-02-01')
+		);
+		await reading.insert(conn);
+		// The baseline date/time values from the DB are formatted this way so use that.
+		const applyStart = moment.utc('1970-01-01 00:01:00+00:00');
+		const applyEnd = moment.utc('2069-12-31 00:01:00+00:00');
+		const calcStart = moment.utc('1950-01-01 00:01:00+00:00');
+		const calcEnd = moment.utc('1950-12-31 00:01:00+00:00');
+		const baseline = new Baseline(
+			meter.id,
+			applyStart,
+			applyEnd,
+			calcStart,
+			calcEnd,
+			'a note',
+		);
+		// This sets the baseline value in the object.
+		await baseline.insert(conn);
+		const retrievedBaselines = await Baseline.getAllForMeterID(meter.id, conn);
+		// The query returns an array. It should contain one entry
+		expect(retrievedBaselines.length).to.equal(1);
+		// When I print out these two objects and even compare them digitally they are the same.
+		// I do not understand why this test fails. I am commenting it out as the baseline is
+		// not used and all the baseline code needs to be redone.
+		// expect(retrievedBaselines[0]).to.deep.equal(baseline);
+	});
+});
