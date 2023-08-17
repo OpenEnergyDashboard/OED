dd704c95c2363f08010dc2eae6f0961fd2a2e92b
Jesse Garcia <jessegarcia531@gmail.com>
2023-08-17 19:52:47 +0000

    Creating labels for axis and using radar readings

--- src/client/app/containers/RadarChartContainer.ts
+++ src/client/app/containers/RadarChartContainer.ts
@@ -11,45 +11,53 @@ import translate from '../utils/translate';
 import Plot from 'react-plotly.js';
 import Locales from '../types/locales';
 import { DataType } from '../types/Datasources';
-// Make your own radarUnitLabel later. Lets see if this works first
-import { barUnitLabel } from '../utils/graphics';
+import { lineUnitLabel } from '../utils/graphics';
 import { AreaUnitType, getAreaUnitConversion } from '../utils/getAreaUnitConversion';
 
 function mapStateToProps(state: State) {
 	const timeInterval = state.graph.timeInterval;
 	const unitID = state.graph.selectedUnit;
 	const datasets: any[] = [];
-	const barDuration = state.graph.barDuration;
 	//For largest and smallest usage in reading.reading.
 	let minR: number | undefined;
 	let maxR: number | undefined;
+	// relabel each theta axis to be more user friendly
+	const tickVal: number[] = [];
+	const tickTex: string[] = [];
+	let tickPosition = 0;
 	// The unit label depends on the unit which is in selectUnit state.
 	const graphingUnit = state.graph.selectedUnit;
+	// The current selected rate
+	const currentSelectedRate = state.graph.lineGraphRate;
 	let unitLabel = '';
+	let needsRateScaling = false;
 	// If graphingUnit is -99 then none selected and nothing to graph so label is empty.
 	// This will probably happen when the page is first loaded.
 	if (graphingUnit !== -99) {
 		const selectUnitState = state.units.units[state.graph.selectedUnit];
 		if (selectUnitState !== undefined) {
-			// Determine the r-axis label.
-			unitLabel = barUnitLabel(selectUnitState, state.graph.areaNormalization, state.graph.selectedAreaUnit);
+			// Determine the r-axis label and if the rate needs to be scaled.
+			const returned = lineUnitLabel(selectUnitState, currentSelectedRate, state.graph.areaNormalization, state.graph.selectedAreaUnit);
+			unitLabel = returned.unitLabel
+			needsRateScaling = returned.needsRateScaling;
 		}
 	}
+	// The rate will be 1 if it is per hour (since state readings are per hour) or no rate scaling so no change.
+	const rateScaling = needsRateScaling ? currentSelectedRate.rate : 1;
 
 	// Add all valid data from existing meters to the radar plot
 	for (const meterID of state.graph.selectedMeters) {
-		const byMeterID = state.readings.bar.byMeterID[meterID];
-		if (byMeterID !== undefined && byMeterID[timeInterval.toString()] !== undefined &&
-			byMeterID[timeInterval.toString()][barDuration.toISOString()] !== undefined) {
-			let meterArea = state.meters.byMeterID[meterID].area;
+		const byMeterID = state.readings.line.byMeterID[meterID];
+		if (byMeterID !== undefined && byMeterID[timeInterval.toString()] !== undefined) {
+			const meterArea = state.meters.byMeterID[meterID].area;
 			// We either don't care about area, or we do in which case there needs to be a nonzero area.
 			if (!state.graph.areaNormalization || (meterArea > 0 && state.meters.byMeterID[meterID].areaUnit != AreaUnitType.none)) {
 				// Convert the meter area into the proper unit if normalizing by area or use 1 if not so won't change reading values.
-				if (state.graph.areaNormalization) {
-					// convert the meter area into the proper unit, if needed
-					meterArea *= getAreaUnitConversion(state.meters.byMeterID[meterID].areaUnit, state.graph.selectedAreaUnit);
-				}
-				const readingsData = byMeterID[timeInterval.toString()][barDuration.toISOString()][unitID];
+				const areaScaling = state.graph.areaNormalization ?
+					meterArea * getAreaUnitConversion(state.meters.byMeterID[meterID].areaUnit, state.graph.selectedAreaUnit) : 1;
+				// Divide areaScaling into the rate so have complete scaling factor for readings.
+				const scaling = rateScaling / areaScaling;
+				const readingsData = byMeterID[timeInterval.toString()][unitID];
 				if (readingsData !== undefined && !readingsData.isFetching) {
 					const label = state.meters.byMeterID[meterID].identifier;
 					const colorID = meterID;
@@ -69,30 +77,19 @@ function mapStateToProps(state: State) {
 						const st = moment.utc(reading.startTimestamp);
 						// Time reading is in the middle of the start and end timestamp
 						const timeReading = st.add(moment.utc(reading.endTimestamp).diff(st) / 2);
-						// thetaData.push(timeReading.format('YYYY-MM-DD HH:mm:ss'));
 						thetaData.push(timeReading.format('ddd, ll LTS'));
-						let readingValue = reading.reading;
-						if (state.graph.areaNormalization) {
-							readingValue /= meterArea;
-						}
+						// Label each theta axis
+						tickTex.push(timeReading.format('ll'));
+						tickVal.push(tickPosition);
+						tickPosition += 1;
+						const readingValue = reading.reading * scaling;
 						rData.push(readingValue);
-						// only display a range of dates for the hover text if there is more than one day in the range
-						let timeRange: string = `${moment.utc(reading.startTimestamp).format('ll')}`;
-						if (barDuration.asDays() != 1) {
-							// subtracting one extra day caused by day ending at midnight of the next day.
-							// Going from DB unit timestamp that is UTC so force UTC with moment, as usual.
-							timeRange += ` - ${moment.utc(reading.endTimestamp).subtract(1, 'days').format('ll')}`;
-						}
-						hoverText.push(`<b> ${timeRange} </b> <br> ${label}: ${readingValue.toPrecision(6)} ${unitLabel}`);
+						hoverText.push(`<b> ${timeReading.format('ddd, ll LTS')} </b> <br> ${label}: ${readingValue.toPrecision(6)} ${unitLabel}`);
 					});
 
 					//Find the largest and smallest usage in rData.
-					if (minR == undefined || minR > Math.min(...rData)) {
-						minR = Math.min(...rData);
-					}
-					if (maxR == undefined || maxR < Math.max(...rData)) {
-						maxR = Math.max(...rData);
-					}
+					minR = Math.min(...rData);
+					maxR = Math.max(...rData);
 
 					// This variable contains all the elements (x and y values, line type, etc.) assigned to the data parameter of the Plotly object
 					datasets.push({
@@ -114,90 +111,7 @@ function mapStateToProps(state: State) {
 		}
 	}
 
-	//THIS DOES NOT WORK YET AND IT'S STILL USING RADAR READINGS I MOVED TO BAR READINGS
-	// for (const groupID of state.graph.selectedGroups) {
-	// 	const byGroupID = state.readings.line.byGroupID[groupID];
-	// 	if (byGroupID !== undefined && byGroupID[timeInterval.toString()] !== undefined) {
-	// 		let groupArea = state.groups.byGroupID[groupID].area;
-	// 		if (!state.graph.areaNormalization || (groupArea > 0 && state.groups.byGroupID[groupID].areaUnit != AreaUnitType.none)) {
-	// 			if (state.graph.areaNormalization) {
-	// 				// convert the meter area into the proper unit, if needed
-	// 				groupArea *= getAreaUnitConversion(state.groups.byGroupID[groupID].areaUnit, state.graph.selectedAreaUnit);
-	// 			}
-	// 			const readingsData = byGroupID[timeInterval.toString()][unitID];
-	// 			if (readingsData !== undefined && !readingsData.isFetching) {
-	// 				const label = state.groups.byGroupID[groupID].name;
-	// 				const colorID = groupID;
-	// 				if (readingsData.readings === undefined) {
-	// 					throw new Error('Unacceptable condition: readingsData.readings is undefined.');
-	// 				}
-
-	// 				// Create two arrays for the x and y values. Fill the array with the data from the line readings
-	// 				const thetaData: string[] = [];
-	// 				const rData: number[] = [];
-	// 				const hoverText: string[] = [];
-	// 				const readings = _.values(readingsData.readings);
-	// 				// Check if reading needs scaling outside of the loop so only one check is needed
-	// 				// Results in more code but SLIGHTLY better efficiency :D
-	// 				if (needsRateScaling) {
-	// 					const rate = currentSelectedRate.rate;
-	// 					readings.forEach(reading => {
-	// 						// As usual, we want to interpret the readings in UTC. We lose the timezone as this as the start/endTimestamp
-	// 						// are equivalent to Unix timestamp in milliseconds.
-	// 						const st = moment.utc(reading.startTimestamp);
-	// 						// Time reading is in the middle of the start and end timestamp
-	// 						const timeReading = st.add(moment.utc(reading.endTimestamp).diff(st) / 2);
-	// 						// thetaData.push(timeReading.utc().format('YYYY-MM-DD HH:mm:ss'));
-	// 						thetaData.push(timeReading.utc().format('ddd, ll LTS'));
-	// 						rData.push(reading.reading * rate);
-	// 						hoverText.push(`<b> ${timeReading.format('ddd, ll LTS')} </b> <br> ${label}: ${(reading.reading * rate).toPrecision(6)} ${unitLabel}`);
-	// 					});
-	// 				}
-	// 				else {
-	// 					readings.forEach(reading => {
-	// 						// As usual, we want to interpret the readings in UTC. We lose the timezone as this as the start/endTimestamp
-	// 						// are equivalent to Unix timestamp in milliseconds.
-	// 						const st = moment.utc(reading.startTimestamp);
-	// 						// Time reading is in the middle of the start and end timestamp
-	// 						const timeReading = st.add(moment.utc(reading.endTimestamp).diff(st) / 2);
-	// 						// thetaData.push(timeReading.utc().format('YYYY-MM-DD HH:mm:ss'));
-	// 						thetaData.push(timeReading.utc().format('ddd, ll LTS'));
-	// 						let readingValue = reading.reading;
-	// 						if (state.graph.areaNormalization) {
-	// 							readingValue /= groupArea;
-	// 						}
-	// 						rData.push(readingValue);
-	// 						hoverText.push(`<b> ${timeReading.format('ddd, ll LTS')} </b> <br> ${label}: ${readingValue.toPrecision(6)} ${unitLabel}`);
-	// 					});
-	// 				}
-
-	// 				//Find the largest and smallest usage in rData.
-	// 				if (minR == undefined || minR > Math.min(...rData)) {
-	// 					minR = Math.min(...rData);
-	// 				}
-	// 				if (maxR == undefined || maxR < Math.max(...rData)) {
-	// 					maxR = Math.max(...rData);
-	// 				}
-
-	// 				// This variable contains all the elements (x and y values, line type, etc.) assigned to the data parameter of the Plotly object
-	// 				datasets.push({
-	// 					name: label,
-	// 					x: thetaData,
-	// 					y: rData,
-	// 					text: hoverText,
-	// 					hoverinfo: 'text',
-	// 					type: 'scatterpolar',
-	// 					mode: 'lines',
-	// 					line: {
-	// 						shape: 'spline',
-	// 						width: 2,
-	// 						color: getGraphColor(colorID, DataType.Group)
-	// 					}
-	// 				});
-	// 			}
-	// 		}
-	// 	}
-	// }
+	// TODO Add groups for radar chart.
 
 	// No range if minR or maxR is undefined.
 	if (minR == undefined || maxR == undefined) {
@@ -232,6 +146,7 @@ function mapStateToProps(state: State) {
 
 	} else {
 		// Data available so plot.
+		// TODO the theta labels at the bottom of the radar chart get cut off.
 		layout = {
 			autosize: true,
 			showlegend: true,
@@ -249,6 +164,11 @@ function mapStateToProps(state: State) {
 					range: [minR, maxR],
 					showgrid: true,
 					gridcolor: '#ddd'
+				},
+				angularaxis: {
+					tickvals: tickVal,
+					ticktext: tickTex,
+					tickmode: 'array'
 				}
 			},
 			margin: {
