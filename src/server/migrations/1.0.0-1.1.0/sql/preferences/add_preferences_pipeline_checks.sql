df9cfc8e8207cf23ac3b10e3803a920d1d566ced
Steven Huss-Lederman <huss@beloit.edu>
2023-08-20 08:09:04 -0500

    add migration

---
+++ src/server/migrations/1.0.0-1.1.0/sql/preferences/add_preferences_pipeline_checks.sql
@@ -0,0 +1,24 @@
+* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+-- Add PREFERENCES pipeline check columns to the meters table.
+
+-- You need to set default so any existing rows get that value since NOT NULL.
+ALTER TABLE preferences
+    ADD COLUMN IF NOT EXISTS default_meter_minimum_value DOUBLE PRECISION NOT NULL DEFAULT -9007199254740991,
+	ADD COLUMN IF NOT EXISTS default_meter_maximum_value DOUBLE PRECISION NOT NULL DEFAULT 9007199254740991,
+	ADD COLUMN IF NOT EXISTS default_meter_minimum_date TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:00+00:00',
+    ADD COLUMN IF NOT EXISTS default_meter_maximum_date TIMESTAMP NOT NULL DEFAULT '6970-01-01 00:00:00+00:00',
+	ADD COLUMN IF NOT EXISTS default_meter_reading_gap REAL NOT NULL DEFAULT 0,
+    ADD COLUMN IF NOT EXISTS default_meter_maximum_errors INTEGER NOT NULL DEFAULT 75
+;
+-- Now remove default since not desired.
+ALTER TABLE preferences
+    ALTER COLUMN default_meter_minimum_value DROP DEFAULT,
+	ALTER COLUMN default_meter_maximum_value DROP DEFAULT,
+	ALTER COLUMN default_meter_minimum_date DROP DEFAULT,
+    ALTER COLUMN default_meter_maximum_date DROP DEFAULT,
+	ALTER COLUMN default_meter_reading_gap DROP DEFAULT,
+    ALTER COLUMN default_meter_maximum_errors DROP DEFAULT
+;
