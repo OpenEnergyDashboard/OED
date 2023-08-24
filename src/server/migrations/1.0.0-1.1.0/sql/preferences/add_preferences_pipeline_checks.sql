a59ab933a79c1ac0c259c08f9b7d2b67cd5e66f6
Steven Huss-Lederman <huss@beloit.edu>
2023-08-24 11:57:05 -0500

    update migrations

--- src/server/migrations/1.0.0-1.1.0/sql/preferences/add_preferences_pipeline_checks.sql
+++ src/server/migrations/1.0.0-1.1.0/sql/preferences/add_preferences_pipeline_checks.sql
@@ -6,12 +6,13 @@
 
 -- You need to set default so any existing rows get that value since NOT NULL.
 ALTER TABLE preferences
-    ADD COLUMN IF NOT EXISTS default_meter_minimum_value DOUBLE PRECISION NOT NULL DEFAULT -9007199254740991,
-	ADD COLUMN IF NOT EXISTS default_meter_maximum_value DOUBLE PRECISION NOT NULL DEFAULT 9007199254740991,
+    ADD COLUMN IF NOT EXISTS default_meter_minimum_value FLOAT NOT NULL DEFAULT -9007199254740991,
+	ADD COLUMN IF NOT EXISTS default_meter_maximum_value FLOAT NOT NULL DEFAULT 9007199254740991,
 	ADD COLUMN IF NOT EXISTS default_meter_minimum_date TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:00+00:00',
     ADD COLUMN IF NOT EXISTS default_meter_maximum_date TIMESTAMP NOT NULL DEFAULT '6970-01-01 00:00:00+00:00',
 	ADD COLUMN IF NOT EXISTS default_meter_reading_gap REAL NOT NULL DEFAULT 0,
-    ADD COLUMN IF NOT EXISTS default_meter_maximum_errors INTEGER NOT NULL DEFAULT 75
+    ADD COLUMN IF NOT EXISTS default_meter_maximum_errors INTEGER NOT NULL DEFAULT 75,
+	ADD COLUMN IF NOT EXISTS default_meter_disable_checks BOOLEAN NOT NULL DEFAULT false
 ;
 -- Now remove default since not desired.
 ALTER TABLE preferences
@@ -20,5 +21,6 @@ ALTER TABLE preferences
 	ALTER COLUMN default_meter_minimum_date DROP DEFAULT,
     ALTER COLUMN default_meter_maximum_date DROP DEFAULT,
 	ALTER COLUMN default_meter_reading_gap DROP DEFAULT,
-    ALTER COLUMN default_meter_maximum_errors DROP DEFAULT
+    ALTER COLUMN default_meter_maximum_errors DROP DEFAULT,
+    ALTER COLUMN default_meter_disable_checks DROP DEFAULT
 ;
