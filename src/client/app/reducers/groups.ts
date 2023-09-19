988b2eaa46cdb4dc43abe0b083d4b4c2e33f677a
ChrisMart21 <ChrisMart21@github.com>
2023-09-19 21:53:17 +0000

    Fix Groups Query for deep meters

--- src/client/app/reducers/groups.ts
+++ src/client/app/reducers/groups.ts
@@ -49,7 +49,7 @@ export const groupsSlice = createSlice({
 				childMeters: (state.byGroupID[group.id] && state.byGroupID[group.id].childMeters) ? state.byGroupID[group.id].childMeters : [],
 				selectedGroups: [],
 				selectedMeters: [],
-				deepMeters: state.byGroupID[group.id]?.deepMeters
+				deepMeters: group.deepMeters ? group.deepMeters : []
 			}));
 			// newGroups is an array: this converts it into a nested object where the key to each group is its ID.
 			// Without this, byGroupID will not be keyed by group ID.
@@ -96,29 +96,30 @@ export const groupsSlice = createSlice({
 				...action.payload
 			};
 		}
+	},
+	extraReducers: builder => {
+		builder.addMatcher(groupsApi.endpoints.getGroups.matchFulfilled,
+			(state, { payload }) => {
+				const newGroups = payload.map(group => ({
+					...group,
+					isFetching: false,
+					// Sometimes OED fetches both the details and the child meters/groups as separate actions. Since the order they will happen is
+					// uncertain, we need to preserve the child meters/groups if they exist. If not, put empty so no issues when accessing in other
+					// places. Note this may be the wrong values but they should refresh quickly once all actions are done.
+					childGroups: (state.byGroupID[group.id] && state.byGroupID[group.id].childGroups) ? state.byGroupID[group.id].childGroups : [],
+					childMeters: (state.byGroupID[group.id] && state.byGroupID[group.id].childMeters) ? state.byGroupID[group.id].childMeters : [],
+					selectedGroups: [],
+					selectedMeters: [],
+
+					// line added due to conflicting typing. TS Warns about potential undefined deepMeters
+					deepMeters: group.deepMeters ? group.deepMeters : []
+				}));
+				// newGroups is an array: this converts it into a nested object where the key to each group is its ID.
+				// Without this, byGroupID will not be keyed by group ID.
+				state.isFetching = false;
+				// TODO FIX TYPES HERE Weird interaction here
+				state.byGroupID = _.keyBy(newGroups, 'id');
+			}
+		)
 	}
-	// ,
-	// extraReducers: builder => {
-	// 	builder.addMatcher(groupsApi.endpoints.getGroups.matchFulfilled,
-	// 		(state, { payload }) => {
-	// 			const newGroups = payload.map(group => ({
-	// 				...group,
-	// 				isFetching: false,
-	// 				// Sometimes OED fetches both the details and the child meters/groups as separate actions. Since the order they will happen is
-	// 				// uncertain, we need to preserve the child meters/groups if they exist. If not, put empty so no issues when accessing in other
-	// 				// places. Note this may be the wrong values but they should refresh quickly once all actions are done.
-	// 				childGroups: (state.byGroupID[group.id] && state.byGroupID[group.id].childGroups) ? state.byGroupID[group.id].childGroups : [],
-	// 				childMeters: (state.byGroupID[group.id] && state.byGroupID[group.id].childMeters) ? state.byGroupID[group.id].childMeters : [],
-	// 				selectedGroups: [],
-	// 				selectedMeters: [],
-	// 				deepMeters: state.byGroupID[group.id]?.deepMeters ? state.byGroupID[group.id].deepMeters : []
-	// 			}));
-	// 			// newGroups is an array: this converts it into a nested object where the key to each group is its ID.
-	// 			// Without this, byGroupID will not be keyed by group ID.
-	// 			state.isFetching = false;
-	// 			// TODO FIX TYPES HERE Weird interaction here
-	// 			state.byGroupID = _.keyBy(newGroups, 'id');
-	// 		}
-	// 	)
-	// }
 });
