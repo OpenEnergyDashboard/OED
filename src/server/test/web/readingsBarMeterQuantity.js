bffcefa431d46f4bd48e4f2f591333a69007b089
Tang Liang <524018786@qq.com>
2023-11-14 23:41:03 +0000

    'add test B8 and and related expected csv'

--- src/server/test/web/readingsBarMeterQuantity.js
+++ src/server/test/web/readingsBarMeterQuantity.js
@@ -117,6 +117,7 @@ mocha.describe('readings API', () => {
                     // Check that the API reading is equal to what it is expected to equal
                     expectReadingToEqualExpected(res, expected);
                 });
+                
                 // Add B9 here
 
                 // Add B10 here
