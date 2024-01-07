ef099d017bcab32fd6e1da5cba7174bde1e7c9a4
qnhn2 <nhatquan234@gmail.com>
2024-01-07 00:35:36 -0500

    add header

--- src/server/sql/cik/get_cik.sql
+++ src/server/sql/cik/get_cik.sql
@@ -1,3 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+-- Get all ciks through joining cik and units tables.
 SELECT U.id AS meter_unit_id, U2.id AS non_meter_unit_id, C.slope, C.intercept
 FROM cik AS C
 JOIN units AS U ON C.row_index = U.unit_index
