af0a1e99102e1da147cc796561263cfe3bef0725
Andrey Butenko <andreyxbutenko@gmail.com>
2024-02-23 14:51:25 -0800

    Implement test LG12
    Co-authored-by: Ema Ikeda <e.mc2ikeda@gmail.com>
    Co-authored-by: Emily Su <emilysu36@gmail.com>
    Co-authored-by: Sean Fite <sean.fite@bellevuecollege.edu>

--- src/server/test/web/readingsLineGroupQuantity.js
+++ src/server/test/web/readingsLineGroupQuantity.js
@@ -57,7 +57,62 @@ mocha.describe('readings API', () => {
 
                 // Add LG11 here
 
-                // Add LG12 here
+                mocha.it('LG12: should have daily points for 15 + 20 minute reading intervals and quantity units with +-inf start/end time & kWh as BTU chained', async () => {
+                    const unitData = unitDatakWh.concat([
+                        {
+                            name: 'MJ',
+                            identifier: 'megaJoules',
+                            unitRepresent: Unit.unitRepresentType.QUANTITY,
+                            secInRate: 3600,
+                            typeOfUnit: Unit.unitType.UNIT,
+                            suffix: '',
+                            displayable:
+                            Unit.displayableType.ALL,
+                            preferredDisplay: false,
+                            note: 'MJ'
+                        },
+                        {
+                            name: 'BTU',
+                            identifier: '',
+                            unitRepresent: Unit.unitRepresentType.QUANTITY,
+                            secInRate: 3600,
+                            typeOfUnit: Unit.unitType.UNIT,
+                            suffix: '',
+                            displayable: Unit.displayableType.ALL,
+                            preferredDisplay: true,
+                            note: 'OED created standard unit'
+                        }
+                    ]);
+                    const conversionData = conversionDatakWh.concat([
+                        {
+                            sourceName: 'kWh',
+                            destinationName: 'MJ',
+                            bidirectional: true,
+                            slope: 3.6,
+                            intercept: 0,
+                            note: 'kWh → MJ'
+                        },
+                        {
+                            sourceName: 'MJ',
+                            destinationName: 'BTU',
+                            bidirectional: true,
+                            slope: 947.8,
+                            intercept: 0,
+                            note: 'MJ → BTU'
+                        }
+                    ]);
+                    // Load the data into the database
+                    await prepareTest(unitData, conversionData, meterDatakWhGroups, groupDatakWh);
+                    // Get the unit ID since the DB could use any value.
+                    const unitId = await getUnitId('BTU');
+                    // Load the expected response data from the corresponding csv file
+                    const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_line_group_ri_15-20_mu_kWh_gu_BTU_st_-inf_et_inf.csv');
+                    // Create a request to the API for unbounded reading times and save the response
+                    const res = await chai.request(app).get(`/api/unitReadings/line/groups/${GROUP_ID}`)
+                        .query({ timeInterval: ETERNITY.toString(), graphicUnitId: unitId });
+                    // Check that the API reading is equal to what it is expected to equal
+                    expectReadingToEqualExpected(res, expected, GROUP_ID);
+                });
 
                 // Add LG13 here
 
