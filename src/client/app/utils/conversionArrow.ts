8f3cf8890ff57b715c146dffa0ac97730cd22844
Steven Huss-Lederman <huss@beloit.edu>
2024-03-08 17:12:25 -0600

    fix formatting

--- src/client/app/utils/conversionArrow.ts
+++ src/client/app/utils/conversionArrow.ts
@@ -2,11 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- /**
-  * Returns proper string to use for conversion representation of direction
-  * @param bidirectional if the conversion is bidirectional (true) or not (false)
-  * @returns double-ended arrow if bidirectional or single if not
-  */
+/**
+ * Returns proper string to use for conversion representation of direction
+ * @param bidirectional if the conversion is bidirectional (true) or not (false)
+ * @returns double-ended arrow if bidirectional or single if not
+ */
 export function conversionArrow(bidirectional: boolean) {
 	// Arrow is bidirectional if conversion is bidirectional and one way if not.
 	let arrowShown: string;
