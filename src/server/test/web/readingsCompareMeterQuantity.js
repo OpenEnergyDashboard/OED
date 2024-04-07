6a85a56c6cfb7360fc6aff558c47f453c20c6124
Steven Huss-Lederman <huss@beloit.edu>
2024-04-07 16:58:07 -0500

    update compare testing
    - spaces to tabs
    - Make start date standard one.

--- src/server/test/web/readingsCompareMeterQuantity.js
+++ src/server/test/web/readingsCompareMeterQuantity.js
@@ -3,39 +3,39 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /*
-    This file tests the readings retrieval API compare chart meters.
-    See: https://github.com/OpenEnergyDashboard/DesignDocs/blob/main/testing/testing.md for information.
+	This file tests the readings retrieval API compare chart meters.
+	See: https://github.com/OpenEnergyDashboard/DesignDocs/blob/main/testing/testing.md for information.
 */
 const { chai, mocha, app } = require('../common');
 const { prepareTest,
-    expectCompareToEqualExpected,
-    getUnitId,
-    METER_ID,
-    unitDatakWh,
-    conversionDatakWh,
-    meterDatakWh } = require('../../util/readingsUtils');
+	expectCompareToEqualExpected,
+	getUnitId,
+	METER_ID,
+	unitDatakWh,
+	conversionDatakWh,
+	meterDatakWh } = require('../../util/readingsUtils');
 
 mocha.describe('readings API', () => {
-    mocha.describe('readings test, test if data returned by API is as expected', () => {
-        mocha.describe('for compare charts', () => {
-            mocha.describe('for meters', () => {
-                // Test 15 minutes over all time for flow unit.
-                mocha.it('Expected and actual data should line up', async () => {
-                    await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-                    // Get the unit ID since the DB could use any value.
-                    const unitId = await getUnitId('kWh');
-                    const expected = [ '7962.23097109771', '8764.06090894387' ];
-                    // for compare, need the unitID, currentStart, currentEnd, shift
-                    const res = await chai.request(app).get(`/api/compareReadings/meters/${METER_ID}`)
-                        .query({
-                            curr_start: '2022-10-30 00:00:00',
-                            curr_end: '2022-10-31 17:00:00',
-                            shift: 'P1D',
-                            graphicUnitId: unitId
-                        });
-                    expectCompareToEqualExpected(res, expected);
-                });
-            });
-        });
-    });
+	mocha.describe('readings test, test if data returned by API is as expected', () => {
+		mocha.describe('for compare charts', () => {
+			mocha.describe('for meters', () => {
+				// Test 15 minutes over all time for flow unit.
+				mocha.it('Expected and actual data should line up', async () => {
+					await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+					// Get the unit ID since the DB could use any value.
+					const unitId = await getUnitId('kWh');
+					const expected = [3120.01835362067, 3367.50141893133];
+					// for compare, need the unitID, currentStart, currentEnd, shift
+					const res = await chai.request(app).get(`/api/compareReadings/meters/${METER_ID}`)
+						.query({
+							curr_start: '2022-10-31 00:00:00',
+							curr_end: '2022-10-31 17:00:00',
+							shift: 'P1D',
+							graphicUnitId: unitId
+						});
+					expectCompareToEqualExpected(res, expected);
+				});
+			});
+		});
+	});
 });
