26e81d07a9015d23288035abb0ddbcd0ac35cdaf
Steven Huss-Lederman <huss@beloit.edu>
2024-05-30 16:31:45 -0500

    bar interval via text box
    The user enters the desired number of days in a text box and then
    hits enter. The value is validated by the form. Once input, the bar
    graphic is updated with the entered value (if valid). The buttons
    with set days are kept in sync with user input.

--- src/client/app/components/BarControlsComponent.tsx
+++ src/client/app/components/BarControlsComponent.tsx
@@ -4,9 +4,10 @@
 
 import * as moment from 'moment';
 import * as React from 'react';
-import { Button, ButtonGroup } from 'reactstrap';
-import { graphSlice, selectBarStacking, selectBarWidthDays } from '../redux/slices/graphSlice';
+import { FormattedMessage } from 'react-intl';
+import { Button, ButtonGroup, FormFeedback, FormGroup, Input, Label } from 'reactstrap';
 import { useAppDispatch, useAppSelector } from '../redux/reduxHooks';
+import { graphSlice, selectBarStacking, selectBarWidthDays } from '../redux/slices/graphSlice';
 import translate from '../utils/translate';
 import TooltipMarkerComponent from './TooltipMarkerComponent';
 
@@ -14,26 +15,46 @@ import TooltipMarkerComponent from './TooltipMarkerComponent';
  * @returns controls for the Options Ui page.
  */
 export default function BarControlsComponent() {
+	// The min/max days allowed for user selection
+	const minBarDays = 1;
+	const maxBarDays = 366;
+
 	const dispatch = useAppDispatch();
+	// This is the current bar interval for graphic.
 	const barDuration = useAppSelector(selectBarWidthDays);
 	const barStacking = useAppSelector(selectBarStacking);
-	const [showSlider, setShowSlider] = React.useState<boolean>(false);
-	const [sliderVal, setSliderVal] = React.useState<number>(barDuration.asDays());
+	// Holds the value during input so only update graphic when done entering.
+	const [barDays, setBarDays] = React.useState<number>(barDuration.asDays());
 
-	const toggleSlider = () => {
-		setShowSlider(showSlider => !showSlider);
+	const barDaysValid = (barDays: number) => {
+		return Number.isInteger(barDays) && barDays >= minBarDays && barDays <= maxBarDays;
 	};
 
 	const handleChangeBarStacking = () => {
 		dispatch(graphSlice.actions.changeBarStacking());
 	};
 
-	const handleSliderChange = (value: number) => {
-		setSliderVal(value);
+	const handleBarDaysChange = (value: number) => {
+		setBarDays(value);
 	};
+
+	const handleEnter = (key: string) => {
+		// This detects the enter key and then uses the previously entered bar days
+		// to set the bar duration for the graphic.
+		if (key == 'Enter') {
+			updateBarDurationChange(barDays);
+		}
+	}
+
 	const updateBarDurationChange = (value: number) => {
-		dispatch(graphSlice.actions.updateBarDuration(moment.duration(value, 'days')));
+		// Update if okay value. May not be okay if this came from user entry in form.
+		if (barDaysValid(value)) {
+			dispatch(graphSlice.actions.updateBarDuration(moment.duration(value, 'days')));
+			// Set user entry field to same value so if change came from button choices then same.
+			setBarDays(value);
+		}
 	};
+
 	const barDurationDays = barDuration.asDays();
 
 	return (
@@ -43,30 +64,52 @@ export default function BarControlsComponent() {
 				<label htmlFor='barStacking'>{translate('bar.stacking')}</label>
 				<TooltipMarkerComponent page='home' helpTextId='help.home.bar.stacking.tip' />
 			</div>
-			<div>
-				<p style={labelStyle}>{translate('bar.interval')}:</p>
-				<ButtonGroup style={zIndexFix}>
-					<Button outline={barDurationDays !== 1} onClick={() => updateBarDurationChange(1)}> {translate('day')} </Button>
-					<Button outline={barDurationDays !== 7} onClick={() => updateBarDurationChange(7)}> {translate('week')} </Button>
-					<Button outline={barDurationDays !== 28} onClick={() => updateBarDurationChange(28)}> {translate('4.weeks')} </Button>
-				</ButtonGroup>
-				<TooltipMarkerComponent page='home' helpTextId='help.home.bar.interval.tip' />
-			</div>
-			<div>
-				<Button outline={!showSlider} onClick={toggleSlider}>{translate('toggle.custom.slider')}</Button>
-				<TooltipMarkerComponent page='home' helpTextId='help.home.bar.custom.slider.tip' />
-			</div>
-			{showSlider &&
-				<div style={divTopPadding}>
-					<p>Add days input here</p>
+			<div style={divTopPadding}>
+				<p style={labelStyle}>
+					{translate('bar.interval')}:
+					<TooltipMarkerComponent page='home' helpTextId='help.home.bar.days.tip' />
+				</p>
+				{/* Indent to left so all items stand out under label. */}
+				<div style={divLeftBottomPadding}>
+					<ButtonGroup style={zIndexFix}>
+						{/* If the user enter a different value then 1, 7 0r 28 then no button highlighted. */}
+						<Button outline={barDurationDays !== 1} onClick={() => { updateBarDurationChange(1); }}> {translate('day')} </Button>
+						<Button outline={barDurationDays !== 7} onClick={() => { updateBarDurationChange(7); }}> {translate('week')} </Button>
+						<Button outline={barDurationDays !== 28} onClick={() => { updateBarDurationChange(28); }}> {translate('4.weeks')} </Button>
+					</ButtonGroup>
+					<div>
+						{translate('or')}
+					</div>
+					<div>
+						<FormGroup>
+							<Label for='barDays'>{translate('bar.days.enter')}:</Label>
+							<Input id='barDays' name='barDays' type='number'
+								onChange={e => handleBarDaysChange(Number(e.target.value))}
+								// This grabs each key hit and then finishes input when hit enter.
+								onKeyDown={e => {handleEnter(e.key);}}
+								step='1'
+								min={minBarDays}
+								max={maxBarDays}
+								value={barDays}
+								invalid={!barDaysValid(barDays)} />
+							<FormFeedback>
+								<FormattedMessage id="error.bounds" values={{ min: minBarDays, max: maxBarDays }} />
+							</FormFeedback>
+						</FormGroup>
+					</div>
 				</div>
-			}
+			</div>
 		</div>
 	);
 }
 
 const divTopPadding: React.CSSProperties = {
 	paddingTop: '15px'
+};
+
+const divLeftBottomPadding: React.CSSProperties = {
+	paddingLeft: 15,
+	paddingBottom: 20
 };
 
 const labelStyle: React.CSSProperties = {
