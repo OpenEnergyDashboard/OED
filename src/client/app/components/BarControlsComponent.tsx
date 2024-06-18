a1e01fa5a6b025aa6e4f952b388942f444927b65
Steven Huss-Lederman <huss@beloit.edu>
2024-06-18 18:16:31 -0500

    modify bar duration input
    - Dropdown menu rather than buttons.
    - Separate choice for custom input whre custom input only shown
    if this is selected.
    - Make const names follow conventions.
    - Added more comments.
    - Few other minor changes in style and page layout.
    - Removed unused translations.

--- src/client/app/components/BarControlsComponent.tsx
+++ src/client/app/components/BarControlsComponent.tsx
@@ -5,7 +5,7 @@
 import * as moment from 'moment';
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Button, ButtonGroup, FormFeedback, FormGroup, Input, Label } from 'reactstrap';
+import { FormFeedback, FormGroup, Input, Label } from 'reactstrap';
 import { useAppDispatch, useAppSelector } from '../redux/reduxHooks';
 import { graphSlice, selectBarStacking, selectBarWidthDays } from '../redux/slices/graphSlice';
 import translate from '../utils/translate';
@@ -15,47 +15,82 @@ import TooltipMarkerComponent from './TooltipMarkerComponent';
  * @returns controls for the Options Ui page.
  */
 export default function BarControlsComponent() {
-	// The min/max days allowed for user selection
-	const minBarDays = 1;
-	const maxBarDays = 366;
-
 	const dispatch = useAppDispatch();
+
+	// The min/max days allowed for user selection
+	const MIN_BAR_DAYS = 1;
+	const MAX_BAR_DAYS = 366;
+	// Special value if custom input for standard menu.
+	const CUSTOM_INPUT = '-99';
+
 	// This is the current bar interval for graphic.
 	const barDuration = useAppSelector(selectBarWidthDays);
 	const barStacking = useAppSelector(selectBarStacking);
-	// Holds the value during input so only update graphic when done entering.
-	const [barDays, setBarDays] = React.useState<number>(barDuration.asDays());
-
-	const barDaysValid = (barDays: number) => {
-		return Number.isInteger(barDays) && barDays >= minBarDays && barDays <= maxBarDays;
-	};
+	// Holds the value of standard bar duration choices used so decoupled from custom and
+	// also to allow special value for custom choice.
+	const [barDays, setBarDays] = React.useState<string>(barDuration.asDays().toString());
+	// Holds the value during custom bar duration input so only update graphic when done entering and
+	// separate from standard choices.
+	const [barDaysCustom, setBarDaysCustom] = React.useState<number>(barDuration.asDays());
+	// True if custom bar duration input is active.
+	const [showCustomBarDuration, setShowCustomBarDuration] = React.useState<boolean>(false);
 
 	const handleChangeBarStacking = () => {
 		dispatch(graphSlice.actions.changeBarStacking());
 	};
 
-	const handleBarDaysChange = (value: number) => {
-		setBarDays(value);
+	// Update when the custom input bar duration is shown/hidden.
+	React.useEffect(() => {
+		if (showCustomBarDuration) {
+			// It changed to true so set the input value as the current barDuration
+			// so shows in custom field at start of input.
+			setBarDaysCustom(barDuration.asDays());
+		}
+		// If false then hiding custom so value does not matter.
+	}, [showCustomBarDuration]);
+
+	// Returns true if this is a valid bar duration.
+	const barDaysValid = (barDays: number) => {
+		return Number.isInteger(barDays) && barDays >= MIN_BAR_DAYS && barDays <= MAX_BAR_DAYS;
+	};
+
+	// Updates values when the standard bar duration menu is used.
+	const handleBarDaysChange = (value: string) => {
+		if (value === CUSTOM_INPUT) {
+			// Set menu value for standard bar to special value to show custom
+			// and show the custom input area.
+			setBarDays(CUSTOM_INPUT);
+			setShowCustomBarDuration(true);
+		} else {
+			// Set bar duration for graphing, the standard menu value
+			// and hide the custom bar duration input.
+			// Since controlled values know it is a valid integer.
+			console.log('setting barDuration to ', Number(value));
+			updateBarDurationChange(Number(value));
+			setBarDays(value);
+			setShowCustomBarDuration(false);
+		}
+	};
+
+	// Updates value when the custom bar duration input is used.
+	const handleCustomBarDaysChange = (value: number) => {
+		setBarDaysCustom(value);
 	};
 
 	const handleEnter = (key: string) => {
-		// This detects the enter key and then uses the previously entered bar days
-		// to set the bar duration for the graphic.
+		// This detects the enter key and then uses the previously entered custom
+		// bar duration to set the bar duration for the graphic.
 		if (key == 'Enter') {
-			updateBarDurationChange(barDays);
+			updateBarDurationChange(barDaysCustom);
 		}
 	};
 
 	const updateBarDurationChange = (value: number) => {
-		// Update if okay value. May not be okay if this came from user entry in form.
+		// Update if okay value. May not be okay if this came from user entry in custom form.
 		if (barDaysValid(value)) {
 			dispatch(graphSlice.actions.updateBarDuration(moment.duration(value, 'days')));
-			// Set user entry field to same value so if change came from button choices then same.
-			setBarDays(value);
 		}
 	};
-
-	const barDurationDays = barDuration.asDays();
 
 	return (
 		<div>
@@ -64,59 +99,52 @@ export default function BarControlsComponent() {
 				<label htmlFor='barStacking'>{translate('bar.stacking')}</label>
 				<TooltipMarkerComponent page='home' helpTextId='help.home.bar.stacking.tip' />
 			</div>
-			<div style={divTopPadding}>
+			<div style={divTopBottomPadding}>
 				<p style={labelStyle}>
 					{translate('bar.interval')}:
 					<TooltipMarkerComponent page='home' helpTextId='help.home.bar.days.tip' />
 				</p>
-				{/* Indent to left so all items stand out under label. */}
-				<div style={divLeftBottomPadding}>
-					<ButtonGroup style={zIndexFix}>
-						{/* If the user enter a different value then 1, 7 0r 28 then no button highlighted. */}
-						<Button outline={barDurationDays !== 1} onClick={() => { updateBarDurationChange(1); }}> {translate('day')} </Button>
-						<Button outline={barDurationDays !== 7} onClick={() => { updateBarDurationChange(7); }}> {translate('week')} </Button>
-						<Button outline={barDurationDays !== 28} onClick={() => { updateBarDurationChange(28); }}> {translate('4.weeks')} </Button>
-					</ButtonGroup>
-					<div>
-						{translate('or')}
-					</div>
-					<div>
-						<FormGroup>
-							<Label for='barDays'>{translate('bar.days.enter')}:</Label>
-							<Input id='barDays' name='barDays' type='number'
-								onChange={e => handleBarDaysChange(Number(e.target.value))}
-								// This grabs each key hit and then finishes input when hit enter.
-								onKeyDown={e => {handleEnter(e.key);}}
-								step='1'
-								min={minBarDays}
-								max={maxBarDays}
-								value={barDays}
-								invalid={!barDaysValid(barDays)} />
-							<FormFeedback>
-								<FormattedMessage id="error.bounds" values={{ min: minBarDays, max: maxBarDays }} />
-							</FormFeedback>
-						</FormGroup>
-					</div>
-				</div>
-			</div>
-		</div>
+				<Input
+					id='barDurationDays'
+					name='barDurationDays'
+					type='select'
+					value={barDays}
+					onChange={e => handleBarDaysChange(e.target.value)}
+				>
+					<option value='1'>{translate('day')}</option>
+					<option value='7'>{translate('week')}</option>
+					<option value='28'>{translate('4.weeks')}</option>
+					<option value={CUSTOM_INPUT}>{translate('custom.value')}</option>
+				</Input>
+				{/* This has a little more spacing at bottom than optimal. */}
+				{showCustomBarDuration &&
+					<FormGroup>
+						<Label for='barDays'>{translate('bar.days.enter')}:</Label>
+						<Input id='barDays' name='barDays' type='number'
+							onChange={e => handleCustomBarDaysChange(Number(e.target.value))}
+							// This grabs each key hit and then finishes input when hit enter.
+							onKeyDown={e => { handleEnter(e.key); }}
+							step='1'
+							min={MIN_BAR_DAYS}
+							max={MAX_BAR_DAYS}
+							value={barDaysCustom}
+							invalid={!barDaysValid(barDaysCustom)} />
+						<FormFeedback>
+							<FormattedMessage id="error.bounds" values={{ min: MIN_BAR_DAYS, max: MAX_BAR_DAYS }} />
+						</FormFeedback>
+					</FormGroup>
+				}
+			</div >
+		</div >
 	);
 }
 
-const divTopPadding: React.CSSProperties = {
-	paddingTop: '15px'
-};
-
-const divLeftBottomPadding: React.CSSProperties = {
-	paddingLeft: 15,
-	paddingBottom: 20
+const divTopBottomPadding: React.CSSProperties = {
+	paddingTop: '15px',
+	paddingBottom: '15px'
 };
 
 const labelStyle: React.CSSProperties = {
 	fontWeight: 'bold',
 	margin: 0
-};
-
-const zIndexFix: React.CSSProperties = {
-	zIndex: 0
 };
