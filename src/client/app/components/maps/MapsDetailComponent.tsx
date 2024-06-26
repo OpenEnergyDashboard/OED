c95817a3f52debc95ec0424ebce95905dd94903a
juangutierrez <juanjoseguva@gmail.com>
2024-06-25 23:02:07 -0700

    First commit for maps update. Changed to card view and took a stab at implementing the redux framework in MapViewComponent.

--- src/client/app/components/maps/MapsDetailComponent.tsx
+++ src/client/app/components/maps/MapsDetailComponent.tsx
@@ -5,11 +5,12 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
-import { Button, Table } from 'reactstrap';
+import { Button } from 'reactstrap';
 import TooltipHelpComponent from '../../components/TooltipHelpComponent';
 import MapViewContainer from '../../containers/maps/MapViewContainer';
 import { hasToken } from '../../utils/token';
 import TooltipMarkerComponent from '../TooltipMarkerComponent';
+import '../../styles/card-page.css';
 
 interface MapsDetailProps {
 	maps: number[];
@@ -30,31 +31,8 @@ export default class MapsDetailComponent extends React.Component<MapsDetailProps
 	}
 
 	public render() {
-
-		const titleStyle: React.CSSProperties = {
-			textAlign: 'center'
-		};
-
-		const tableStyle: React.CSSProperties = {
-			marginLeft: '5%',
-			marginRight: '5%'
-		};
-
-		const buttonContainerStyle: React.CSSProperties = {
-			minWidth: '150px',
-			width: '10%',
-			marginLeft: '40%',
-			marginRight: '40%'
-		};
-
-		const tooltipStyle = {
-			display: 'inline-block',
-			fontSize: '50%'
-		};
-
 		return (
 			<div className='flexGrowOne'>
-				{/* <UnsavedWarningContainer /> */}
 				<TooltipHelpComponent page='maps' />
 				<div className='container-fluid'>
 					<h2 style={titleStyle}>
@@ -63,56 +41,46 @@ export default class MapsDetailComponent extends React.Component<MapsDetailProps
 							<TooltipMarkerComponent page='maps' helpTextId='help.admin.mapview' />
 						</div>
 					</h2>
-					<div style={tableStyle}>
-						<Table striped bordered hover>
-							<thead>
-								<tr>
-									<th> <FormattedMessage id='map.id' /> </th>
-									<th> <FormattedMessage id='map.name' /> </th>
-									{hasToken() && <th> <FormattedMessage id='map.displayable' /> </th>}
-									{hasToken() && <th> <FormattedMessage id='map.circle.size' /> </th>}
-									{hasToken() && <th> <FormattedMessage id='map.modified.date' /> </th>}
-									{hasToken() && <th> <FormattedMessage id='map.filename' /> </th>}
-									{hasToken() && <th> <FormattedMessage id='note' /> </th>}
-									{hasToken() && <th> <FormattedMessage id='map.calibration' /> </th>}
-									{hasToken() && <th> <FormattedMessage id='remove' /> </th>}
-								</tr>
-							</thead>
-							<tbody>
-								{this.props.maps.map(mapID =>
-									(<MapViewContainer key={mapID} id={mapID} />))}
-								<tr>
-									<td colSpan={8}>
-										<Link to='/calibration' onClick={() => this.props.createNewMap()}>
-											<Button style={buttonContainerStyle} color='primary'>
-												<FormattedMessage id='create.map' />
-											</Button>
-										</Link>
-									</td>
-								</tr>
-							</tbody>
-						</Table>
+					<div className="edit-btn">
+						<Link to='/calibration' onClick={() => this.props.createNewMap()}>
+							<Button color='primary'>
+								<FormattedMessage id='create.map' />
+							</Button>
+						</Link>
 					</div>
-					{hasToken() && <Button
-						color='success'
-						style={buttonContainerStyle}
-						disabled={!this.props.unsavedChanges}
-						onClick={this.handleSubmitClicked}
-					>
-						<FormattedMessage id='save.map.edits' />
-					</Button>}
+					<div className="card-container">
+						{this.props.maps.map(mapID => (
+							<MapViewContainer key={mapID} id={mapID} />
+						))}
+					</div>
+					{hasToken() && (
+						<div className="edit-btn">
+							<Button
+								color='success'
+								disabled={!this.props.unsavedChanges}
+								onClick={this.handleSubmitClicked}
+							>
+								<FormattedMessage id='save.map.edits' />
+							</Button>
+						</div>
+					)}
 				</div>
 			</div>
 		);
 	}
 
-	private removeUnsavedChanges() {
-		// store.dispatch(unsavedWarningSlice.actions.removeUnsavedChanges());
-	}
-
 	private handleSubmitClicked() {
 		this.props.submitEditedMaps();
 		// Notify that the unsaved changes have been submitted
-		this.removeUnsavedChanges();
+		// this.removeUnsavedChanges();
 	}
 }
+
+const titleStyle: React.CSSProperties = {
+	textAlign: 'center'
+};
+
+const tooltipStyle = {
+	display: 'inline-block',
+	fontSize: '50%'
+};
