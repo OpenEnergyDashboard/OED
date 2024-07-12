f84880921e55d60b9ab9f96ee84cae7c2c8576c3
Nicky Lin <nickylin.328@gmail.com>
2024-07-12 19:50:15 +0000

    Setup for use with MoreOptionsComponent

---
+++ src/client/app/components/admin/MoreOptionsComponent.tsx
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// import * as React from 'react';
+// import ReactTooltip from 'react-tooltip';
+// import { ChartTypes } from '../types/redux/graph';
+// import { useAppSelector } from '../redux/reduxHooks';
+// import { selectChartToRender } from '../redux/slices/graphSlice';
+
+export default function MoreOptionsComponent() {
+    
+}
