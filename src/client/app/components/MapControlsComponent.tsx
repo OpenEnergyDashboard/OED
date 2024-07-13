0f05b009ffbbf17662b7627bf76f833dd037eb3f
danielshid <danielshi74@gmail.com>
2024-07-12 22:26:24 -0400

    added drop-down menu for map

--- src/client/app/components/MapControlsComponent.tsx
+++ src/client/app/components/MapControlsComponent.tsx
@@ -136,4 +136,50 @@ export default function MapControlsComponent() {
 		}
 	};
 
+	return (
+		<div>
+			<div key='side-options'>
+				<p style={labelStyle}>
+					{translate('map.interval')}:
+					<TooltipMarkerComponent page='home' helpTextId='help.home.map.interval.tip' />
+				</p>
+				<Input
+					id='mapDurationDays'
+					name='mapDurationDays'
+					type='select'
+					value={mapDays}
+					onChange={e => handleMapDaysChange(e.target.value)}
+				>
+					<option value='1'>{translate('day')}</option>
+					<option value='7'>{translate('week')}</option>
+					<option value='28'>{translate('4.weeks')}</option>
+					<option value={CUSTOM_INPUT}>{translate('custom.value')}</option>
+				</Input>
+				{/* This has a little more spacing at bottom than optimal. */}
+				{showCustomMapDuration &&
+					<FormGroup>
+						<Label for='mapDays'>{translate('map.days.enter')}:</Label>
+						<Input id='mapDays' name='mapDays' type='number'
+							onChange={e => handleCustomMapDaysChange(Number(e.target.value))}
+							// This grabs each key hit and then finishes input when hit enter.
+							onKeyDown={e => { handleEnter(e.key); }}
+							step='1'
+							min={MIN_MAP_DAYS}
+							max={MAX_MAP_DAYS}
+							value={mapDaysCustom}
+							invalid={!mapDaysValid(mapDaysCustom)} />
+						<FormFeedback>
+							<FormattedMessage id="error.bounds" values={{ min: MIN_MAP_DAYS, max: MAX_MAP_DAYS }} />
+						</FormFeedback>
+					</FormGroup>
+				}
+			</div >
+			<MapChartSelectComponent key='chart' />
+		</div >
+	);
+}
 	
+const labelStyle: React.CSSProperties = {
+	fontWeight: 'bold',
+	margin: 0
+};
