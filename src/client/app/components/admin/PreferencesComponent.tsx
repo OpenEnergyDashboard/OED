ac72e286c44e96a9feae55766e69d84d66192907
hazeltonbw <hazeltonbw@gmail.com>
2024-08-01 01:20:21 -0700

    Organize settings by category and give each a header, make CSS changes

--- src/client/app/components/admin/PreferencesComponent.tsx
+++ src/client/app/components/admin/PreferencesComponent.tsx
@@ -47,18 +47,8 @@ export default function PreferencesComponent() {
 				successMessage='updated.preferences'
 				failureMessage='failed.to.submit.changes'
 			/>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.site.title')}:`}
-				</p>
-				<Input
-					type='text'
-					placeholder={translate('name')}
-					value={localAdminPref.displayTitle}
-					onChange={e => makeLocalChanges('displayTitle', e.target.value)}
-					maxLength={50}
-				/>
-			</div>
+			
+			<h3 className='border-bottom'>Graph Settings</h3>
 			<div>
 				<p style={labelStyle}>
 					<FormattedMessage id='default.graph.type' />:
@@ -139,7 +129,110 @@ export default function PreferencesComponent() {
 					</label>
 				</div>
 			</div>
+			<h3 className='border-bottom mt-3'>Meter Settings</h3>
 			<div>
+				<p style={titleStyle}>
+					{`${translate('default.meter.reading.frequency')}:`}
+				</p>
+				<Input
+					type='text'
+					value={localAdminPref.defaultMeterReadingFrequency}
+					onChange={e => makeLocalChanges('defaultMeterReadingFrequency', e.target.value)}
+				/>
+			</div>
+			<div>
+				<p style={titleStyle}>
+					{`${translate('default.meter.minimum.value')}:`}
+				</p>
+				<Input
+					type='number'
+					value={localAdminPref.defaultMeterMinimumValue}
+					onChange={e => makeLocalChanges('defaultMeterMinimumValue', e.target.value)}
+					maxLength={50}
+				/>
+			</div>
+			<div>
+				<p style={titleStyle}>
+					{`${translate('default.meter.maximum.value')}:`}
+				</p>
+				<Input
+					type='number'
+					value={localAdminPref.defaultMeterMaximumValue}
+					onChange={e => makeLocalChanges('defaultMeterMaximumValue', e.target.value)}
+					maxLength={50}
+				/>
+			</div>
+			<div>
+				<p style={titleStyle}>
+					{`${translate('default.meter.minimum.date')}:`}
+				</p>
+				<Input
+					type='text'
+					value={localAdminPref.defaultMeterMinimumDate}
+					onChange={e => makeLocalChanges('defaultMeterMinimumDate', e.target.value)}
+					placeholder='YYYY-MM-DD HH:MM:SS'
+				/>
+			</div>
+			<div>
+				<p style={titleStyle}>
+					{`${translate('default.meter.maximum.date')}:`}
+				</p>
+				<Input
+					type='text'
+					value={localAdminPref.defaultMeterMaximumDate}
+					onChange={e => makeLocalChanges('defaultMeterMaximumDate', e.target.value)}
+					placeholder='YYYY-MM-DD HH:MM:SS'
+				/>
+			</div>
+			<div>
+				<p style={titleStyle}>
+					{`${translate('default.meter.reading.gap')}:`}
+				</p>
+				<Input
+					type='number'
+					value={localAdminPref.defaultMeterReadingGap}
+					onChange={e => makeLocalChanges('defaultMeterReadingGap', e.target.value)}
+					maxLength={50}
+				/>
+			</div>
+			<div>
+				<p style={titleStyle}>
+					{`${translate('default.meter.maximum.errors')}:`}
+				</p>
+				<Input
+					type='number'
+					value={localAdminPref.defaultMeterMaximumErrors}
+					onChange={e => makeLocalChanges('defaultMeterMaximumErrors', e.target.value)}
+					maxLength={50}
+				/>
+			</div>
+			<div>
+				<p style={titleStyle}>
+					{`${translate('default.meter.disable.checks')}:`}
+				</p>
+				<Input
+					type='select'
+					value={localAdminPref.defaultMeterDisableChecks?.toString()}
+					onChange={e => makeLocalChanges('defaultMeterDisableChecks', e.target.value)}>
+					{Object.keys(TrueFalseType).map(key => {
+						return (<option value={key} key={key}>{translate(`TrueFalseType.${key}`)}</option>);
+					})}
+				</Input>
+			</div>
+			<div>
+				<h3 className='border-bottom mt-3'>Site Settings</h3>
+			<div>
+				<p style={titleStyle}>
+					{`${translate('site.title')}:`}
+				</p>
+				<Input
+					type='text'
+					placeholder={translate('name')}
+					value={localAdminPref.displayTitle}
+					onChange={e => makeLocalChanges('displayTitle', e.target.value)}
+					maxLength={50}
+				/>
+			</div>
 				<p style={labelStyle}>
 					{translate('default.language')}
 				</p>
@@ -183,7 +276,7 @@ export default function PreferencesComponent() {
 					</label>
 				</div>
 			</div>
-			<div style={bottomPaddingStyle}>
+			<div>
 				<p style={titleStyle}>
 					{`${translate('default.time.zone')}:`}
 				</p>
@@ -191,7 +284,7 @@ export default function PreferencesComponent() {
 					current={localAdminPref.defaultTimezone}
 					handleClick={e => makeLocalChanges('defaultTimezone', e)} />
 			</div>
-			<div style={bottomPaddingStyle}>
+			<div>
 				<p style={titleStyle}>
 					{`${translate('default.warning.file.size')}:`}
 				</p>
@@ -202,7 +295,7 @@ export default function PreferencesComponent() {
 					maxLength={50}
 				/>
 			</div>
-			<div style={bottomPaddingStyle}>
+			<div>
 				<p style={titleStyle}>
 					{`${translate('default.file.size.limit')}:`}
 				</p>
@@ -213,96 +306,7 @@ export default function PreferencesComponent() {
 					maxLength={50}
 				/>
 			</div>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.meter.reading.frequency')}:`}
-				</p>
-				<Input
-					type='text'
-					value={localAdminPref.defaultMeterReadingFrequency}
-					onChange={e => makeLocalChanges('defaultMeterReadingFrequency', e.target.value)}
-				/>
-			</div>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.meter.minimum.value')}:`}
-				</p>
-				<Input
-					type='number'
-					value={localAdminPref.defaultMeterMinimumValue}
-					onChange={e => makeLocalChanges('defaultMeterMinimumValue', e.target.value)}
-					maxLength={50}
-				/>
-			</div>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.meter.maximum.value')}:`}
-				</p>
-				<Input
-					type='number'
-					value={localAdminPref.defaultMeterMaximumValue}
-					onChange={e => makeLocalChanges('defaultMeterMaximumValue', e.target.value)}
-					maxLength={50}
-				/>
-			</div>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.meter.minimum.date')}:`}
-				</p>
-				<Input
-					type='text'
-					value={localAdminPref.defaultMeterMinimumDate}
-					onChange={e => makeLocalChanges('defaultMeterMinimumDate', e.target.value)}
-					placeholder='YYYY-MM-DD HH:MM:SS'
-				/>
-			</div>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.meter.maximum.date')}:`}
-				</p>
-				<Input
-					type='text'
-					value={localAdminPref.defaultMeterMaximumDate}
-					onChange={e => makeLocalChanges('defaultMeterMaximumDate', e.target.value)}
-					placeholder='YYYY-MM-DD HH:MM:SS'
-				/>
-			</div>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.meter.reading.gap')}:`}
-				</p>
-				<Input
-					type='number'
-					value={localAdminPref.defaultMeterReadingGap}
-					onChange={e => makeLocalChanges('defaultMeterReadingGap', e.target.value)}
-					maxLength={50}
-				/>
-			</div>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.meter.maximum.errors')}:`}
-				</p>
-				<Input
-					type='number'
-					value={localAdminPref.defaultMeterMaximumErrors}
-					onChange={e => makeLocalChanges('defaultMeterMaximumErrors', e.target.value)}
-					maxLength={50}
-				/>
-			</div>
-			<div style={bottomPaddingStyle}>
-				<p style={titleStyle}>
-					{`${translate('default.meter.disable.checks')}:`}
-				</p>
-				<Input
-					type='select'
-					value={localAdminPref.defaultMeterDisableChecks?.toString()}
-					onChange={e => makeLocalChanges('defaultMeterDisableChecks', e.target.value)}>
-					{Object.keys(TrueFalseType).map(key => {
-						return (<option value={key} key={key}>{translate(`TrueFalseType.${key}`)}</option>);
-					})}
-				</Input>
-			</div>
-			<div style={bottomPaddingStyle}>
+			<div>
 				<p style={titleStyle}>
 					<FormattedMessage id='default.help.url' />:
 				</p>
@@ -312,6 +316,8 @@ export default function PreferencesComponent() {
 					onChange={e => makeLocalChanges('defaultHelpUrl', e.target.value)}
 				/>
 			</div>
+			
+			
 			<Button
 				type='submit'
 				onClick={() =>
@@ -335,9 +341,6 @@ export default function PreferencesComponent() {
 const labelStyle: React.CSSProperties = {
 	fontWeight: 'bold',
 	margin: 0
-};
-const bottomPaddingStyle: React.CSSProperties = {
-	paddingBottom: '15px'
 };
 
 const titleStyle: React.CSSProperties = {
