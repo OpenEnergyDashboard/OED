18fa27558489e3bcb1a8526480e7c7fba98d67ed
hazeltonbw <hazeltonbw@gmail.com>
2024-08-03 20:17:08 -0700

    Fix linting errors

--- src/client/app/redux/selectors/maps.ts
+++ src/client/app/redux/selectors/maps.ts
@@ -2,15 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { RootState } from "store";
-import { MapState } from "types/redux/map";
-import { createAppSelector } from "./selectors";
+import { RootState } from 'store';
+import { MapState } from 'types/redux/map';
+import { createAppSelector } from './selectors';
 
 export const selectMapState = (state: RootState) => state.maps;
-export const selectMaps = createAppSelector([selectMapState], (maps) =>
+export const selectMaps = createAppSelector([selectMapState], maps =>
 	Object.keys(maps.byMapID)
-		.map((key) => parseInt(key))
-		.filter((key) => !isNaN(key))
+		.map(key => parseInt(key))
+		.filter(key => !isNaN(key))
 );
 
 export const selectMapById = (id: number) =>
