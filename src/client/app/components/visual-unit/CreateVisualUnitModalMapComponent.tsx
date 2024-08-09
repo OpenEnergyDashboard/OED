27b55bfd09364a10bc5e241f467be32f79b434c7
Nicky Lin <nickylin.328@gmail.com>
2024-08-09 18:20:54 +0000

    Add comments

--- src/client/app/components/visual-unit/CreateVisualUnitModalMapComponent.tsx
+++ src/client/app/components/visual-unit/CreateVisualUnitModalMapComponent.tsx
@@ -5,11 +5,17 @@ import { useAppSelector } from '../../redux/reduxHooks';
 import { selectAllUnits } from '../../redux/api/unitsApi';
 import { selectConversionsDetails } from '../../redux/api/conversionsApi';
 
+/**
+ * Visual unit-conversion graph component
+ * @returns D3 force graph visual
+ */
 export default function CreateVisualUnitMapModalComponent() {
 
+	/* Get unit and conversion data from redux */
 	const unitData = useAppSelector(selectAllUnits);
 	const conversionData = useAppSelector(selectConversionsDetails);
 
+	/* Create data container to pass to D3 force graph */
 	const data: { nodes: any[], links: any[] } = {
 		nodes: [],
 		links: []
@@ -23,24 +29,27 @@ export default function CreateVisualUnitMapModalComponent() {
 		data.links.push({
 			'source': value.sourceId,
 			'target': value.destinationId,
-			'bidirectional': value.bidirectional
+			'bidirectional': value.bidirectional		/* boolean value */
 		});
 	});
 
+	/* Visuals start here */
 	useEffect(() => {
+		/* View-box dimensions */
 		const width = window.innerWidth;
 		const height = 750;
 
+		/* Grab data */
 		const nodes = data.nodes.map(d => ({...d}));
 		const links = data.links.map(d => ({...d}));
 
 		const simulation = d3.forceSimulation(nodes)
 			.force('link', d3.forceLink(links)
-				.id((d: any) => d.id)
-				.distance(60)
+				.id((d: any) => d.id)					/* Set all link ids (from data.links) */
+				.distance(60)							/* This controls how long each link is */
 			)
-			.force('charge', d3.forceManyBody()
-				.strength(-500)
+			.force('charge', d3.forceManyBody()			/* Create new many-body force */
+				.strength(-500)							/* This controls the 'repelling' force on each node */
 			)
 			.force('x', d3.forceX())
 			.force('y', d3.forceY());
@@ -53,6 +62,7 @@ export default function CreateVisualUnitMapModalComponent() {
 			.attr('style', 'max-width: 100%; height: auto;')
 			.append('g');
 
+		/* End arrow head */
 		svg.append('defs').append('marker')
 			.attr('id', 'arrow-end')
 			.attr('viewBox', '0 -5 10 10')
@@ -60,10 +70,11 @@ export default function CreateVisualUnitMapModalComponent() {
 			.attr('refY', 0)
 			.attr('markerWidth', 4)
 			.attr('markerHeight', 4)
-			.attr('orient', 'auto')
+			.attr('orient', 'auto')			/* auto: point towards dest. node */
 			.append('svg:path')
 			.attr('d', 'M0,-5L10,0L0,5');
 
+		/* Start arrow head (for bidirectional edges) */
 		svg.append('defs').append('marker')
 			.attr('id', 'arrow-start')
 			.attr('viewBox', '0 -5 10 10')
@@ -71,29 +82,33 @@ export default function CreateVisualUnitMapModalComponent() {
 			.attr('refY', 0)
 			.attr('markerWidth', 4)
 			.attr('markerHeight', 4)
-			.attr('orient', 'auto-start-reverse')
+			.attr('orient', 'auto-start-reverse')		/* auto-start-reverse: point towards src. node */
 			.append('svg:path')
 			.attr('d', 'M0,-5L10,0L0,5');
 
+		/* Link style */
 		const link = svg.selectAll('line')
 			.data(links)
 			.enter().append('line')
 			.style('stroke', '#aaa')
 			.attr('stroke-width', 3)
 			.attr('marker-end', 'url(#arrow-end)')
-			.attr('marker-start', d => d.bidirectional === true ? 'url(#arrow-start)' : '');
+			.attr('marker-start', d => d.bidirectional === true ? 'url(#arrow-start)' : '');	/* Only draw start arrow head if bidirectional */
 
+		/* Node style */
 		const node = svg.selectAll('.node')
 			.data(nodes)
 			.enter().append('circle')
-			.attr('r', 16)
+			.attr('r', 16)					/* Node radius */
 			.style('fill', '#69b3a2');
 
+		/* Drag behavior */
 		node.call(d3.drag()
 			.on('start', dragstart)
 			.on('drag', dragged)
 			.on('end', dragend));
 
+		/* Node label style */
 		const label = svg.selectAll('.label')
 			.data(nodes)
 			.enter()
@@ -104,6 +119,7 @@ export default function CreateVisualUnitMapModalComponent() {
 			.style('font-family', 'Arial')
 			.style('font-size', 14);
 
+		/* Update element positions when moved */
 		simulation.on('tick', () => {
 			link
 				.attr('x1', d => d.source.x)
