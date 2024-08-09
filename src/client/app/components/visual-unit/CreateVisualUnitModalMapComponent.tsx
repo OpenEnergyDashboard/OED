338cb02c09589617fbda803db6158c826eeec5c0
Nicky Lin <nickylin.328@gmail.com>
2024-08-09 18:20:04 +0000

    Add arrows to links and removed unused code

--- src/client/app/components/visual-unit/CreateVisualUnitModalMapComponent.tsx
+++ src/client/app/components/visual-unit/CreateVisualUnitModalMapComponent.tsx
@@ -4,124 +4,105 @@ import { useEffect } from 'react';
 import { useAppSelector } from '../../redux/reduxHooks';
 import { selectAllUnits } from '../../redux/api/unitsApi';
 import { selectConversionsDetails } from '../../redux/api/conversionsApi';
-// import { useSelector } from 'react-redux';
-// import { State } from 'types/redux/state';
-// import { noUnitTranslated } from 'utils/input';
-// import { MeterData } from 'types/redux/meters';
-// import { CurrentUserState } from 'types/redux/currentUser';
-// import { UnitData } from 'types/redux/units';
-// import { Dispatch } from 'types/redux/actions';
-// import { useDispatch } from 'react-redux';
-// import { fetchMetersDetailsIfNeeded } from 'actions/meters';
-
-// interface MeterViewComponentProps {
-// 	meter: MeterData;
-// 	currentUser: CurrentUserState;
-// 	// These two aren't used in this component but are passed to the edit component
-// 	// This is done to avoid having to recalculate the possible units sets in each view component
-// 	possibleMeterUnits: Set<UnitData>;
-// 	possibleGraphicUnits: Set<UnitData>;
-// }
 
 export default function CreateVisualUnitMapModalComponent() {
-	// const dispatch: Dispatch = useDispatch();
 
-	// useEffect(() => {
-	// 	// Makes async call to Meters API for Meters details if one has not already been made somewhere else, stores Meter ids in state
-	// 	dispatch(fetchMetersDetailsIfNeeded());
-	// }, []);
-
-	// // Access Redux state directly
-	// const currentUnitState = useSelector((state: State) => state.units.units);
-	// const meterDetails = useSelector((state: State) => state.meters); // Replace with your actual slice name
-
-	// useEffect(() => {
-	//   // Check if meterDetails are available
-	//   if (meterDetails && meterDetails.length > 0) {
-	//     meterDetails.forEach((meter: MeterData) => {
-	//       const unitName = (Object.keys(currentUnitState).length === 0 || meter.unitId === -99) ?
-	//         noUnitTranslated().identifier : currentUnitState[meter.unitId].identifier;
-
-	//       const graphicName = (Object.keys(currentUnitState).length === 0 || meter.defaultGraphicUnit === -99) ?
-	//         noUnitTranslated().identifier : currentUnitState[meter.defaultGraphicUnit].identifier;
-
-	//       console.log('Meter Unit Name:', unitName);
-	//       console.log('Meter Graphic Name:', graphicName);
-	//       console.log('Meter Details:', meter);
-	//     });
-	//   }
-	// }, [currentUnitState, meterDetails]);
+	const unitData = useAppSelector(selectAllUnits);
+	const conversionData = useAppSelector(selectConversionsDetails);
 
 	const data: { nodes: any[], links: any[] } = {
 		nodes: [],
 		links: []
 	};
-
-	const unitData = useAppSelector(selectAllUnits);
 	unitData.map(function (value) {
-		data.nodes.push({
-			'name': value.name,
+		data.nodes.push({'name': value.name,
 			'id': value.id
 		});
 	});
-
-	const conversionData = useAppSelector(selectConversionsDetails);
 	conversionData.map(function (value) {
 		data.links.push({
 			'source': value.sourceId,
-			'target': value.destinationId
+			'target': value.destinationId,
+			'bidirectional': value.bidirectional
 		});
 	});
 
 	useEffect(() => {
-		const margin = { top: 10, right: 30, bottom: 30, left: 40 };
-		const width = 600 - margin.left - margin.right;
-		const height = 600 - margin.top - margin.bottom;
+		const width = window.innerWidth;
+		const height = 750;
 
 		const nodes = data.nodes.map(d => ({...d}));
 		const links = data.links.map(d => ({...d}));
 
 		const simulation = d3.forceSimulation(nodes)
-			.force('link', d3.forceLink(links).id((d: any) => d.id))
-			.force('charge', d3.forceManyBody().strength(-200))
+			.force('link', d3.forceLink(links)
+				.id((d: any) => d.id)
+				.distance(60)
+			)
+			.force('charge', d3.forceManyBody()
+				.strength(-500)
+			)
 			.force('x', d3.forceX())
 			.force('y', d3.forceY());
 
 		const svg = d3.select('#sample')
 			.append('svg')
-			.attr('width', width + margin.left + margin.right)
-			.attr('height', height + margin.top + margin.bottom)
-			.attr('viewBox',
-				[-(width + margin.left + margin.right) / 2, -(height + margin.top + margin.bottom) / 2,
-					width + margin.left + margin.right, height + margin.top + margin.bottom]
-			)
-			.append('g')
-			.attr('transform', `translate(${margin.left}, ${margin.top})`);
+			.attr('width', width)
+			.attr('height', height)
+			.attr('viewBox', [-width / 2, -height / 2, width, height])
+			.attr('style', 'max-width: 100%; height: auto;')
+			.append('g');
 
-		// connection link style
+		svg.append('defs').append('marker')
+			.attr('id', 'arrow-end')
+			.attr('viewBox', '0 -5 10 10')
+			.attr('refX', 20)
+			.attr('refY', 0)
+			.attr('markerWidth', 4)
+			.attr('markerHeight', 4)
+			.attr('orient', 'auto')
+			.append('svg:path')
+			.attr('d', 'M0,-5L10,0L0,5');
+
+		svg.append('defs').append('marker')
+			.attr('id', 'arrow-start')
+			.attr('viewBox', '0 -5 10 10')
+			.attr('refX', 20)
+			.attr('refY', 0)
+			.attr('markerWidth', 4)
+			.attr('markerHeight', 4)
+			.attr('orient', 'auto-start-reverse')
+			.append('svg:path')
+			.attr('d', 'M0,-5L10,0L0,5');
+
 		const link = svg.selectAll('line')
 			.data(links)
 			.enter().append('line')
 			.style('stroke', '#aaa')
-			.attr('stroke-width', 3);
+			.attr('stroke-width', 3)
+			.attr('marker-end', 'url(#arrow-end)')
+			.attr('marker-start', d => d.bidirectional === true ? 'url(#arrow-start)' : '');
 
-		// node style
 		const node = svg.selectAll('.node')
 			.data(nodes)
 			.enter().append('circle')
-			.attr('r', 12)
+			.attr('r', 16)
 			.style('fill', '#69b3a2');
 
-		// label style
+		node.call(d3.drag()
+			.on('start', dragstart)
+			.on('drag', dragged)
+			.on('end', dragend));
+
 		const label = svg.selectAll('.label')
 			.data(nodes)
 			.enter()
 			.append('text')
 			.text(function (d) { return d.name; })
 			.style('text-anchor', 'middle')
-			.style('fill', '#555')
+			.style('fill', '#000')
 			.style('font-family', 'Arial')
-			.style('font-size', 12);
+			.style('font-size', 14);
 
 		simulation.on('tick', () => {
 			link
@@ -136,8 +117,28 @@ export default function CreateVisualUnitMapModalComponent() {
 
 			label
 				.attr('x', function(d){ return d.x; })
-				.attr('y', function (d) {return d.y - 10; });
+				.attr('y', function (d) {return d.y + 15; });
 		});
+
+		// eslint-disable-next-line jsdoc/require-jsdoc
+		function dragstart(event: any) {
+			if (!event.active) simulation.alphaTarget(0.3).restart();
+			event.subject.fx = event.subject.x;
+			event.subject.fy = event.subject.y;
+		}
+
+		// eslint-disable-next-line jsdoc/require-jsdoc
+		function dragged(event: any) {
+			event.subject.fx = event.x;
+			event.subject.fy = event.y;
+		}
+
+		// eslint-disable-next-line jsdoc/require-jsdoc
+		function dragend(event: any) {
+			if (!event.active) simulation.alphaTarget(0);
+			event.subject.fx = null;
+			event.subject.fy = null;
+		}
 
 	}, []); // Empty dependency array to run the effect only once
 
@@ -146,4 +147,5 @@ export default function CreateVisualUnitMapModalComponent() {
 			<div id="sample"></div>
 		</div>
 	);
+
 }
