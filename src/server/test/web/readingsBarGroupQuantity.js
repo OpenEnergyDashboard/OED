3117b16d0768d19c8455c83ea3a73e2857f2728f
Gowrisankar Palanickal <156338043+shankarp05@users.noreply.github.com>
2024-10-01 11:27:51 -0400

    Adding code for Test BG10

--- src/server/test/web/readingsBarGroupQuantity.js
+++ src/server/test/web/readingsBarGroupQuantity.js
@@ -129,6 +129,7 @@ mocha.describe('readings API', () => {
                             graphicUnitId: unitId });
                     // Check that the API reading is equal to what it is expected to equal
                     expectReadingToEqualExpected(res, expected, GROUP_ID);
+                            
                 });
                 // Add BG7 here
 
@@ -137,6 +138,24 @@ mocha.describe('readings API', () => {
                 // Add BG9 here
 
                 // Add BG10 here
+
+                mocha.it('BG10: 1 day bars for 15 + 20 minute reading intervals and quantity units with +-inf start/end time & kWh as BTU', async () =>{
+                    //load data into database
+                    await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWhGroups, groupDatakWh);
+                    //get unit ID since the DB could use any value.
+                    const unitId = await getUnitId('BTU');
+                    // Load the expected response data from the corresponding csv file
+                    const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_bar_group_ri_15-20_mu_kWh_gu_BTU_st_-inf_et_inf_bd_1.csv');
+                    // Create a request to the API for unbounded reading times and save the response
+                    const res = await chai.request(app).get(`/api/unitReadings/bar/groups/${GROUP_ID}`)
+                        .query({ 
+                            timeInterval: ETERNITY.toString(), 
+                            barWidthDays: '1',
+                            graphicUnitId: unitId });
+                    // Check that the API reading is equal to what it is expected to equal
+                    expectReadingToEqualExpected(res, expected, GROUP_ID);
+
+                });
 
                 // Add BG11 here
 
