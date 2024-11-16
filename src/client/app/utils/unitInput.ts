4b6d8db5515103ef5bb62530b2258fbcd70e25ec
gyordong <duongryan139@gmail.com>
2024-11-16 03:55:03 +0000

    added mozilla header

--- src/client/app/utils/unitInput.ts
+++ src/client/app/utils/unitInput.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // Checks if custom rate is valid by verifying that it is a positive integer.
 export const customRateValid = (customRate: number) => {
 	return Number.isInteger(customRate) && customRate >= 1;
