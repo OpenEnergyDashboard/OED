9241b997f16479e563405319e9d076ef36e932e2
Bao Mai <bgm47@drexel.edu>
2025-04-09 17:11:40 -0400

    Update LR25

--- src/server/test/web/readingsLineMeterRangeFlow.js
+++ src/server/test/web/readingsLineMeterRangeFlow.js
@@ -14,7 +14,7 @@ const { prepareTest,
     expectRangeToEqualExpected,
     getUnitId,
     ETERNITY,
-    METER_ID } = require('../../util/readingsUtils');
+    METER_ID} = require('../../util/readingsUtils');
 
 mocha.describe('readings API', () => {
     mocha.describe('readings test, test if data returned by API is as expected', () => {
@@ -84,7 +84,70 @@ mocha.describe('readings API', () => {
 						expectRangeToEqualExpected(res, expected);
 					});
 
-                // Add LR25 here
+                mocha.it('LR25: should have daily points for 15 minute reading intervals and flow units with +-inf start/end time & thing as thing where rate is 36', async () => {
+                    const unitData = [
+                        {   
+                            // u14
+                            name: 'Thing_36',
+                            identifier: '',
+                            unitRepresent: Unit.unitRepresentType.FLOW,
+                            secInRate: 36,
+                            typeOfUnit: Unit.unitType.METER,
+                            suffix: '',
+                            displayable: Unit.displayableType.NONE,
+                            preferredDisplay: false,
+                            note: 'special unit'
+                        },
+                        {
+                            // u15
+                            name: 'thing unit',
+                            identifier: '',
+                            unitRepresent: Unit.unitRepresentType.FLOW,
+                            secInRate: 3600,
+                            typeOfUnit: Unit.unitType.UNIT,
+                            suffix: '',
+                            displayable: Unit.displayableType.ALL,
+                            preferredDisplay: false,
+                            note: 'special unit'
+                        }
+                    ];
+
+                    const conversionData = [
+                        {
+                            // c15
+                            sourceName: 'Thing_36',
+                            destinationName: 'thing unit',
+                            bidirectional: false,
+                            slope: 1,
+                            intercept: 0,
+                            note: 'Thing_36 → thing unit'
+                        }
+                    ];
+
+                    const meterData = [
+                        {
+                            name: 'Thing_36 thing unit',
+                            unit: 'Thing_36',
+                            defaultGraphicUnit: 'thing unit',
+                            displayable: true,
+                            gps: undefined,
+                            note: 'special meter',
+                            file: 'test/web/readingsData/readings_ri_15_days_75.csv',
+                            deleteFile: false,
+                            readingFrequency: '15 minutes',
+                            id: METER_ID
+                        }
+                    ];
+                    await prepareTest(unitData, conversionData, meterData);
+
+                    const unitId = await getUnitId('thing unit');
+
+                    const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_line_range_ri_15_mu_Thing36_gu_thing_st_-inf_et_inf.csv');
+
+                    const res = await chai.request(app).get(`/api/unitReadings/line/meters/${METER_ID}`)
+                        .query({ timeInterval: ETERNITY.toString(), graphicUnitId: unitId });
+                    expectRangeToEqualExpected(res, expected)
+                });
 
                 // Add LR26 here
             	});
