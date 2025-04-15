9d8d24a37dd59bbe9d655d86914c7c0fdb02e35b
theREALNguyenAnh <ndna9902@gmail.com>
2025-04-15 18:24:16 -0400

    clean up

--- src/client/app/components/conversion/ConversionsDetailComponent.tsx
+++ src/client/app/components/conversion/ConversionsDetailComponent.tsx
@@ -13,7 +13,7 @@ import TooltipMarkerComponent from '../TooltipMarkerComponent';
 import ConversionViewComponent from './ConversionViewComponent';
 import CreateConversionModalComponent from './CreateConversionModalComponent';
 import { useAppSelector } from '../../redux/reduxHooks';
-import { selectRefreshingReadings, selectSelectedLanguage } from '../../redux/slices/appStateSlice';
+import { selectSelectedLanguage } from '../../redux/slices/appStateSlice';
 import { titleStyle, tooltipBaseStyle } from '../../styles/modalStyle';
 
 /**
@@ -23,8 +23,6 @@ import { titleStyle, tooltipBaseStyle } from '../../styles/modalStyle';
 export default function ConversionsDetailComponent() {
 	// The route stops you from getting to this page if not an admin.
 	const locale = useAppSelector(selectSelectedLanguage);
-	// Track when the readings are done being refreshed
-	const isRefreshingReadings = useAppSelector(selectRefreshingReadings);
 	// Conversions state
 	const { data: conversionsState = stableEmptyConversions, isFetching: conversionsFetching } = conversionsApi.useGetConversionsDetailsQuery();
 	// Units DataById
@@ -43,7 +41,7 @@ export default function ConversionsDetailComponent() {
 
 	return (
 		<div className='flexGrowOne'>
-			{(conversionsFetching || unitsFetching || isRefreshingReadings) ? (
+			{(conversionsFetching || unitsFetching) ? (
 				<div className='text-center'>
 					<SpinnerComponent loading width={50} height={50} />
 					<FormattedMessage id='redo.cik.and.refresh.db.views'></FormattedMessage>
@@ -70,7 +68,7 @@ export default function ConversionsDetailComponent() {
 									.sort((conversionA: ConversionData, conversionB: ConversionData) =>
 										((unitDataById[conversionA.sourceId]?.identifier + unitDataById[conversionA.destinationId]?.identifier).toLowerCase().localeCompare((
 											unitDataById[conversionB.sourceId]?.identifier + unitDataById[conversionB.destinationId]?.identifier).toLowerCase(), locale,
-										{ sensitivity: 'accent'})))
+										{ sensitivity: 'accent' })))
 									.map(conversionData => (
 										<ConversionViewComponent
 											conversion={conversionData}
