657b3bd8d7a016cea7eb286868ff96c1308c70b7
KaylaCrush <kayla.e.rieck@gmail.com>
2025-08-11 11:36:05 -0700

    added login/logout and dropdown menu tests to the general ui tests

--- src/cypress/e2e/general_ui.cy.ts
+++ src/cypress/e2e/general_ui.cy.ts
@@ -2,40 +2,42 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { get } from "lodash";
+
 // TODO: The commented out tests were attempts to test the UI that are
 // not yet working. They need to be fixed.
 
-describe('UI Functionality Tests for Open Energy Dashboard', () => {
-	beforeEach(() => {
-		// Visit the page before each test
-		cy.visit('/');
-	});
+// describe('UI Functionality Tests for Open Energy Dashboard', () => {
+// 	beforeEach(() => {
+// 		// Visit the page before each test
+// 		cy.visit('/');
+// 	});
 
-	const viewports = [
-		{ name: 'desktop', width: 1280, height: 800 },
-		{ name: 'mobile',  width: 375,  height: 667 }
-	];
+// 	const viewports = [
+// 		{ name: 'desktop', width: 1280, height: 800 },
+// 		{ name: 'mobile',  width: 375,  height: 667 }
+// 	];
 
-	viewports.forEach(({ name, width, height }) => {
-		context(`@${name} viewport`, () => {
-			before(() => {
-				cy.viewport(width, height);
-			});
+// 	viewports.forEach(({ name, width, height }) => {
+// 		context(`@${name} viewport`, () => {
+// 			before(() => {
+// 				cy.viewport(width, height);
+// 			});
 
-			it(`should click all visible buttons on ${name}`, () => {
-				cy.get('button:visible').then(($buttons) => {
-					const count = $buttons.length;
-					cy.log(`Found ${count} visible buttons on ${name}`)
-				});
-				cy.get('button:visible').each(($btn) => {
-					cy.wrap($btn)
-						.scrollIntoView()
-						.click();
-				});
+// 			it(`should click all visible buttons on ${name}`, () => {
+// 				cy.get('button:visible').then(($buttons) => {
+// 					const count = $buttons.length;
+// 					cy.log(`Found ${count} visible buttons on ${name}`)
+// 				});
+// 				cy.get('button:visible').each(($btn) => {
+// 					cy.wrap($btn)
+// 						.scrollIntoView()
+// 						.click();
+// 				});
 
-			});
-		});
-	});
+// 			});
+// 		});
+// 	});
 
 	// it('Tests all buttons functionality', () => {
 	// 	// Ensure buttons are visible and clickable
@@ -84,12 +86,12 @@ describe('UI Functionality Tests for Open Energy Dashboard', () => {
 	// 	});
 	// });
 
-	it('Tests links for navigation', () => {
-		// Ensure links have valid href attributes
-		cy.get('a[href]').each((link) => {
-			cy.wrap(link).should('have.attr', 'href').and('not.be.empty'); // Check href exists
-		});
-	});
+	// it('Tests links for navigation', () => {
+	// 	// Ensure links have valid href attributes
+	// 	cy.get('a[href]').each((link) => {
+	// 		cy.wrap(link).should('have.attr', 'href').and('not.be.empty'); // Check href exists
+	// 	});
+	// });
 
 	// it('Tests modals for correct behavior', () => {
 	// 	// Ensure modals can be triggered and closed
@@ -121,4 +123,130 @@ describe('UI Functionality Tests for Open Energy Dashboard', () => {
 	// 	// Ensure dynamically loaded elements exist and are visible
 	// 	cy.get('[data-dynamic]').should('exist').and('be.visible');
 	// });
+// });
+
+describe('Test dropdown functionality', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
+
+	function testDropdown(labelText: string){
+		cy.contains('a.dropdown-toggle',labelText)
+			.as('toggleLink')
+			.should('have.attr','aria-expanded','false');
+
+		cy.get('@toggleLink')
+			.closest('li.dropdown')
+			.find('.dropdown-menu')
+			.as('dropdownMenu')
+			.should('not.be.visible');
+
+		cy.get('@toggleLink').click();
+
+		cy.get('@toggleLink').should('have.attr','aria-expanded','true');
+
+		cy.get('@dropdownMenu').should('be.visible');
+	}
+	it('should open the Pages dropdown correctly', () => {
+		testDropdown('Pages');
+	})
+	it('should open the Options dropdown correctly', () => {
+		testDropdown('Options');
+	})
+});
+
+describe('Test login/logout via options menu', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
+
+	function testLogIn(){
+		cy.contains('a.dropdown-toggle', 'Options')
+			.click();
+
+		cy.contains('.dropdown-menu button','Log in')
+			.should('be.visible')
+			.click();
+
+		cy.get('.modal.show')
+			.within(() => {
+				cy.get('#username').type('test');
+				cy.get('#password').type('password');
+				cy.contains('button','Submit')
+					.should('not.be.disabled')
+					.click();
+			});
+
+		cy.get('.Toastify__toast--success')
+			.should('contain','Login Successful')
+			.find('.Toastify__close-button')
+			.click();
+	};
+
+	it('should open the login modal when clicking Log in',()=>{
+		cy.contains('a.dropdown-toggle', 'Options')
+			.click()
+
+		cy.contains('.dropdown-menu button','Log in')
+			.should('be.visible')
+			.click();
+
+		cy.get('.modal.show')
+			.should('be.visible')
+			.within(()=>{
+				cy.contains('h5.modal-title','Log in')
+					.should('exist');
+
+				cy.get('input#username')
+					.should('be.visible')
+					.and('have.attr','type','text');
+
+				cy.get('input#password')
+					.should('be.visible')
+					.and('have.attr', 'type', 'password');
+
+				// Submit button (disabled initially)
+				cy.contains('button', 'Submit')
+					.should('exist')
+					.and('be.disabled');
+
+				// Close button
+				cy.contains('button', 'Close')
+					.should('be.visible');
+			});
+	});
+
+	it('should log in successfully with test credentials',()=>{
+
+		testLogIn();
+
+		cy.contains('a.dropdown-toggle', 'Options')
+			.click();
+
+		cy.contains('.dropdown-menu button','Log out')
+			.should('be.visible');
+
+		cy.contains('.dropdown-menu button','Log in')
+			.should('not.be.visible');
+
+	});
+
+	it('should log out successfully', () => {
+		testLogIn();
+		cy.contains('a.dropdown-toggle', 'Options')
+			.click();
+
+		cy.contains('.dropdown-menu button','Log out')
+			.should('be.visible')
+			.click();
+
+		cy.contains('a.dropdown-toggle', 'Options')
+			.click();
+
+		cy.contains('.dropdown-menu button','Log out')
+			.should('not.be.visible');
+		cy.contains('.dropdown-menu button','Log in')
+			.should('be.visible');
+	});
+
 });
