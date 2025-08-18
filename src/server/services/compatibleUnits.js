046f9bbb5a54d788579450d23b32a2dd0e737481
theREALNguyenAnh <ndna9902@gmail.com>
2025-08-18 10:21:52 +0700

    headers

--- src/server/services/compatibleUnits.js
+++ src/server/services/compatibleUnits.js
@@ -1,4 +1,7 @@
-// Utility functions for determining compatible units for meters and groups
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 /**
  * Returns the set of unit IDs compatible with a given meter's unit.
