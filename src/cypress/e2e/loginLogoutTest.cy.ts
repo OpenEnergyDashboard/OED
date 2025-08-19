6f7391b7f8b0a1f32983a711f6451c3c9dc1835b
KaylaCrush <kayla.e.rieck@gmail.com>
2025-08-19 13:22:09 -0700

    Got desktop version of login/logout working properly

--- src/cypress/e2e/loginLogoutTest.cy.ts
+++ src/cypress/e2e/loginLogoutTest.cy.ts
@@ -3,34 +3,56 @@ const viewports = [
 	{ name: 'mobile',  width: 375,  height: 667 }
 ];
 
-before(() => {
-	cy.visit('/');
-});
+// before(() => {
+// 	cy.visit('/');
+// });
+
+
 
 describe('Login/Logout UI Tests for Open Energy Dashboard', () => {
 	viewports.forEach(({ name, width, height }) => {
 		context(`${name} viewport`, () => {
 
-			beforeEach(()=> {
+			before(()=> {
+				cy.visit('/');
 				cy.viewport(width, height);
 			});
 
-			if(name == 'mobile'){
-				it('should open the menu (mobile viewport only)',()=>{
+			// beforeEach(()=>{
+			// 	cy.visit('/');
+			//	cy.viewport(width, height);
+			// });
+
+			function openOptions(){
+				if(name == 'mobile'){
 					cy.contains('button', 'Menu')
 						.click();
-				})
-			};
+					cy.get('.modal.show')
+						.should('be.visible')
+						.find('a.dropdown-toggle')
+						.contains('Options')
+						.click();
 
-			it('should open the login modal when clicking Log in',()=>{
-				cy.contains('a.dropdown-toggle', 'Options')
-					.click();
+					return cy.get('.modal.show .dropdown-menu').should('be.visible');
+				} else {
+					cy.contains('a.dropdown-toggle', 'Options')
+						.scrollIntoView()
+						.click();
+					return cy.get('.dropdown-menu').should('be.visible');
+				}
+			}
 
-				cy.contains('.dropdown-menu button','Log in')
+			it('should allow a user to login and logout successfully',()=>{
+
+				//login test
+				openOptions().contains('button','Log in')
 					.should('be.visible')
 					.click();
 
-				cy.get('.modal.show')
+					//div class="modal fade show" style="display: block;" aria-modal="true" role="dialog" tabindex="-1"
+
+				cy.get('.modal.fade.show')
+					.last()
 					.should('be.visible')
 					.within(()=>{
 						cy.contains('h5.modal-title','Log in')
@@ -53,10 +75,9 @@ describe('Login/Logout UI Tests for Open Energy Dashboard', () => {
 						cy.contains('button', 'Close')
 							.should('be.visible');
 					});
-			});
 
-			it('should login successfully with test credentials', ()=>{
 				cy.get('.modal.show')
+					.last()
 					.within(() => {
 						cy.get('#username').type('test');
 						cy.get('#password').type('password');
@@ -64,48 +85,36 @@ describe('Login/Logout UI Tests for Open Energy Dashboard', () => {
 							.should('not.be.disabled')
 							.click();
 					});
-			});
 
-			it('should find and close the login success popup', ()=>{
 				cy.get('.Toastify__toast--success')
 					.should('contain','Login Successful')
 					.find('.Toastify__close-button')
 					.click();
+
+
+
+				// logout test
+				cy.contains('a.dropdown-toggle', 'Options')
+					.should('be.visible')
+					.click();
+
+				cy.contains('button','Log in')
+					.should('not.be.visible');
+
+				cy.contains('button','Log out')
+					.should('be.visible')
+					.click();
+
+				cy.contains('a.dropdown-toggle', 'Options')
+					.should('be.visible')
+					.click();
+
+				cy.contains('button','Log in')
+					.should('be.visible');
+
+				cy.contains('button','Log out')
+					.should('not.be.visible');
 			});
-
-
-			// it('should log in successfully with test credentials',()=>{
-
-			// 	testLogIn();
-
-			// 	cy.contains('a.dropdown-toggle', 'Options')
-			// 		.click();
-
-			// 	cy.contains('.dropdown-menu button','Log out')
-			// 		.should('be.visible');
-
-			// 	cy.contains('.dropdown-menu button','Log in')
-			// 		.should('not.be.visible');
-
-			// });
-
-			// it('should log out successfully', () => {
-			// 	testLogIn();
-			// 	cy.contains('a.dropdown-toggle', 'Options')
-			// 		.click();
-
-			// 	cy.contains('.dropdown-menu button','Log out')
-			// 		.should('be.visible')
-			// 		.click();
-
-			// 	cy.contains('a.dropdown-toggle', 'Options')
-			// 		.click();
-
-			// 	cy.contains('.dropdown-menu button','Log out')
-			// 		.should('not.be.visible');
-			// 	cy.contains('.dropdown-menu button','Log in')
-			// 		.should('be.visible');
-			// });
 		});
 	});
 });
