9df5f5e39a50f5090bf26d6d253e39e366a790be
Kayla <107811078+KaylaCrush@users.noreply.github.com>
2025-09-03 10:29:39 -0700

    fix general_ui.cy.ts
    re-commented out broken tests (hadn't meant to include this file in the commit)

--- src/cypress/e2e/general_ui.cy.ts
+++ src/cypress/e2e/general_ui.cy.ts
@@ -12,147 +12,147 @@ beforeEach(() => {
 	cy.visit('/');
 });
 
-describe('UI Functionality Tests for Open Energy Dashboard', () => {
-	viewports.forEach(({ name, width, height }) => {
-		context(`${name} viewport`, () => {
+// describe('UI Functionality Tests for Open Energy Dashboard', () => {
+// 	viewports.forEach(({ name, width, height }) => {
+// 		context(`${name} viewport`, () => {
 
-			beforeEach(() => {
-				cy.viewport(width, height);
-			});
+// 			beforeEach(() => {
+// 				cy.viewport(width, height);
+// 			});
 
-			// it('should find and click all buttons', () => {
-			// 	cy.get('button').each((button) => {
-			// 		cy.wrap(button).should('have.length.greaterThan', 0);
-			// 		cy.wrap(button).click({ force: true }); // Test click
-			// 	});
-			// });
+// 			// it('should find and click all buttons', () => {
+// 			// 	cy.get('button').each((button) => {
+// 			// 		cy.wrap(button).should('have.length.greaterThan', 0);
+// 			// 		cy.wrap(button).click({ force: true }); // Test click
+// 			// 	});
+// 			// });
 
-			it(`should click all visible buttons`, () => {
-				cy.get('button:visible').then(($buttons) => {
-					const count = $buttons.length;
-					cy.log(`Found ${count} visible buttons on ${name}`)
-				});
-				cy.get('button:visible').each(($btn) => {
-					cy.wrap($btn)
-						.scrollIntoView()
-						.click();
-				});
-			});
+// 			it(`should click all visible buttons`, () => {
+// 				cy.get('button:visible').then(($buttons) => {
+// 					const count = $buttons.length;
+// 					cy.log(`Found ${count} visible buttons on ${name}`)
+// 				});
+// 				cy.get('button:visible').each(($btn) => {
+// 					cy.wrap($btn)
+// 						.scrollIntoView()
+// 						.click();
+// 				});
+// 			});
 
-			function testDropdown(labelText: string){
-				cy.contains('a.dropdown-toggle',labelText)
-					.as('toggleLink')
-					.should('have.attr','aria-expanded','false');
+// 			function testDropdown(labelText: string){
+// 				cy.contains('a.dropdown-toggle',labelText)
+// 					.as('toggleLink')
+// 					.should('have.attr','aria-expanded','false');
 
-				cy.get('@toggleLink')
-					.closest('li.dropdown')
-					.find('.dropdown-menu')
-					.as('dropdownMenu')
-					.should('not.be.visible');
+// 				cy.get('@toggleLink')
+// 					.closest('li.dropdown')
+// 					.find('.dropdown-menu')
+// 					.as('dropdownMenu')
+// 					.should('not.be.visible');
 
-				cy.get('@toggleLink').click();
+// 				cy.get('@toggleLink').click();
 
-				cy.get('@toggleLink').should('have.attr','aria-expanded','true');
+// 				cy.get('@toggleLink').should('have.attr','aria-expanded','true');
 
-				cy.get('@dropdownMenu').should('be.visible');
-			};
+// 				cy.get('@dropdownMenu').should('be.visible');
+// 			};
 
-			it('should open the Pages dropdown correctly', () => {
-				testDropdown('Pages');
-			});
+// 			it('should open the Pages dropdown correctly', () => {
+// 				testDropdown('Pages');
+// 			});
 
-			it('should open the Options dropdown correctly', () => {
-				testDropdown('Options');
-			});
+// 			it('should open the Options dropdown correctly', () => {
+// 				testDropdown('Options');
+// 			});
 
-			function testLogIn(){
-				cy.contains('a.dropdown-toggle', 'Options')
-					.click();
+// 			function testLogIn(){
+// 				cy.contains('a.dropdown-toggle', 'Options')
+// 					.click();
 
-				cy.contains('.dropdown-menu button','Log in')
-					.should('be.visible')
-					.click();
+// 				cy.contains('.dropdown-menu button','Log in')
+// 					.should('be.visible')
+// 					.click();
 
-				cy.get('.modal.show')
-					.within(() => {
-						cy.get('#username').type('test');
-						cy.get('#password').type('password');
-						cy.contains('button','Submit')
-							.should('not.be.disabled')
-							.click();
-					});
+// 				cy.get('.modal.show')
+// 					.within(() => {
+// 						cy.get('#username').type('test');
+// 						cy.get('#password').type('password');
+// 						cy.contains('button','Submit')
+// 							.should('not.be.disabled')
+// 							.click();
+// 					});
 
-				cy.get('.Toastify__toast--success')
-					.should('contain','Login Successful')
-					.find('.Toastify__close-button')
-					.click();
-			};
+// 				cy.get('.Toastify__toast--success')
+// 					.should('contain','Login Successful')
+// 					.find('.Toastify__close-button')
+// 					.click();
+// 			};
 
-			it('should open the login modal when clicking Log in',()=>{
-				cy.contains('a.dropdown-toggle', 'Options')
-					.click()
+// 			it('should open the login modal when clicking Log in',()=>{
+// 				cy.contains('a.dropdown-toggle', 'Options')
+// 					.click()
 
-				cy.contains('.dropdown-menu button','Log in')
-					.should('be.visible')
-					.click();
+// 				cy.contains('.dropdown-menu button','Log in')
+// 					.should('be.visible')
+// 					.click();
 
-				cy.get('.modal.show')
-					.should('be.visible')
-					.within(()=>{
-						cy.contains('h5.modal-title','Log in')
-							.should('exist');
+// 				cy.get('.modal.show')
+// 					.should('be.visible')
+// 					.within(()=>{
+// 						cy.contains('h5.modal-title','Log in')
+// 							.should('exist');
 
-						cy.get('input#username')
-							.should('be.visible')
-							.and('have.attr','type','text');
+// 						cy.get('input#username')
+// 							.should('be.visible')
+// 							.and('have.attr','type','text');
 
-						cy.get('input#password')
-							.should('be.visible')
-							.and('have.attr', 'type', 'password');
+// 						cy.get('input#password')
+// 							.should('be.visible')
+// 							.and('have.attr', 'type', 'password');
 
-						// Submit button (disabled initially)
-						cy.contains('button', 'Submit')
-							.should('exist')
-							.and('be.disabled');
+// 						// Submit button (disabled initially)
+// 						cy.contains('button', 'Submit')
+// 							.should('exist')
+// 							.and('be.disabled');
 
-						// Close button
-						cy.contains('button', 'Close')
-							.should('be.visible');
-					});
-			});
+// 						// Close button
+// 						cy.contains('button', 'Close')
+// 							.should('be.visible');
+// 					});
+// 			});
 
-			it('should log in successfully with test credentials',()=>{
+// 			it('should log in successfully with test credentials',()=>{
 
-				testLogIn();
+// 				testLogIn();
 
-				cy.contains('a.dropdown-toggle', 'Options')
-					.click();
+// 				cy.contains('a.dropdown-toggle', 'Options')
+// 					.click();
 
-				cy.contains('.dropdown-menu button','Log out')
-					.should('be.visible');
+// 				cy.contains('.dropdown-menu button','Log out')
+// 					.should('be.visible');
 
-				cy.contains('.dropdown-menu button','Log in')
-					.should('not.be.visible');
+// 				cy.contains('.dropdown-menu button','Log in')
+// 					.should('not.be.visible');
 
-			});
+// 			});
 
-			it('should log out successfully', () => {
-				testLogIn();
-				cy.contains('a.dropdown-toggle', 'Options')
-					.click();
+// 			it('should log out successfully', () => {
+// 				testLogIn();
+// 				cy.contains('a.dropdown-toggle', 'Options')
+// 					.click();
 
-				cy.contains('.dropdown-menu button','Log out')
-					.should('be.visible')
-					.click();
+// 				cy.contains('.dropdown-menu button','Log out')
+// 					.should('be.visible')
+// 					.click();
 
-				cy.contains('a.dropdown-toggle', 'Options')
-					.click();
+// 				cy.contains('a.dropdown-toggle', 'Options')
+// 					.click();
 
-				cy.contains('.dropdown-menu button','Log out')
-					.should('not.be.visible');
-				cy.contains('.dropdown-menu button','Log in')
-					.should('be.visible');
-			});
-		});
-	});
-});
+// 				cy.contains('.dropdown-menu button','Log out')
+// 					.should('not.be.visible');
+// 				cy.contains('.dropdown-menu button','Log in')
+// 					.should('be.visible');
+// 			});
+// 		});
+// 	});
+// });
