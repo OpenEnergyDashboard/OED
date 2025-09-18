a4873c434723b253e48c1ea6cc504fe42811a789
Steven Huss-Lederman <huss@beloit.edu>
2025-09-18 11:39:28 -0500

    hack to remove TZ in CSV

--- src/server/test/web/csvPipelineTest.js
+++ src/server/test/web/csvPipelineTest.js
@@ -672,8 +672,8 @@ const testMeters = {
 				true, // enabled
 				true, //displayable
 				'obvius', //type
-				'US/Central', // timezone
-				// null, // timezone
+				// 'US/Central', // timezone
+				null, // timezone
 				gps, // gps
 				'pipe100b id', // identifier
 				'my Note', // note
@@ -789,8 +789,8 @@ const testMeters = {
 				true, // enabled
 				true, //displayable
 				'obvius', //type
-				'US/Central', // timezone
-				// null, // timezone
+				// 'US/Central', // timezone
+				null, // timezone
 				gps, // gps
 				'pipe100b id', // identifier
 				'my Note', // note
@@ -879,7 +879,7 @@ for (let fileKey in testMeters) {
 				// eval the string to perform the upload. res is what is returned from the request.
 				// TODO It would be nice if this was not an eval. Tried a function with closure but could not get it to work as did not find chai.
 				const res = await eval(evalString);
-				console.log('csvPipelineTest: index, res: ', index, res);
+				// console.log('csvPipelineTest: index, res: ', index, res);
 				// Verify the request response code is what was expected.
 				expect(res).to.have.status(testMeters[fileKey]['responseCode'][index]);
 				// This is a web request that should return html.
