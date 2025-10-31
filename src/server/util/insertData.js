56d571246256da1f3c069ea3db54c96cd97bd9c7
RussianGravy <valentinejones1215@gmail.com>
2025-10-31 12:43:46 -0700

    removed print statements, reverted changes

--- src/server/util/insertData.js
+++ src/server/util/insertData.js
@@ -353,9 +353,7 @@ async function insertMeters(metersToInsert, conn) {
 		if (ok) {
 			console.log(`            loading meter ${meterData.name}`);
 			// Get the unit by name if provided or -99 if not
-            console.log('Meter data unit object: ' + await Unit.getByName('Electric_Utility', conn));
 			meterData.unit = meterData.unit ? (await Unit.getByName(meterData.unit, conn)).id : -99;
-
 			meterData.defaultGraphicUnit = meterData.defaultGraphicUnit ? (await Unit.getByName(meterData.defaultGraphicUnit, conn)).id : -99;
 			// enabled and displayable are false by default.
 			meterData.enabled = meterData.enabled ? meterData.enabled : false;
