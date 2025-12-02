09d54e63432476f62799eeb3851affbab7b50677
hmhngx <nhminhhung05@gmail.com>
2025-12-02 01:18:19 -0500

    Add dataRange API endpoint for 3D graphics
    - Add dataRange query endpoint to readingsApi
    - Fix meterOrGroup type to use MeterOrGroup enum instead of string literal
    - Endpoint fetches min/max timestamps for meter or group data

--- src/client/app/redux/api/readingsApi.ts
+++ src/client/app/redux/api/readingsApi.ts
@@ -12,6 +12,7 @@ import {
 } from '../selectors/chartQuerySelectors';
 import { RootState } from '../../store';
 import { BarReadings, CompareReadings, LineReadings, ThreeDReading } from '../../types/readings';
+import { MeterOrGroup } from '../../types/redux/graph';
 import { baseApi } from './baseApi';
 
 export const readingsApi = baseApi.injectEndpoints({
@@ -151,7 +152,7 @@ export const readingsApi = baseApi.injectEndpoints({
 			},
 			providesTags: ['Readings']
 		}),
-		dataRange: builder.query<{ minDate: string | null, maxDate: string | null }, { id: number, meterOrGroup: 'meters' | 'groups' }>({
+		dataRange: builder.query<{ minDate: string | null, maxDate: string | null }, { id: number, meterOrGroup: MeterOrGroup }>({
 			query: ({ id, meterOrGroup }) => `api/unitReadings/dataRange/${meterOrGroup}/${id}`,
 			providesTags: ['Readings']
 		})
