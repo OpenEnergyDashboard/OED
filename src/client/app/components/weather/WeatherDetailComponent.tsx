e0bd5134e976117fcbb8b253de5ff9aa3fe353e7
danielshid <danielshi74@gmail.com>
2026-02-20 17:13:30 -0500

    remove spinner and cik message

--- src/client/app/components/weather/WeatherDetailComponent.tsx
+++ src/client/app/components/weather/WeatherDetailComponent.tsx
@@ -2,12 +2,10 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { QueryStatus } from '@reduxjs/toolkit/query';
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
-import SpinnerComponent from '../../components/SpinnerComponent';
 import TooltipHelpComponent from '../../components/TooltipHelpComponent';
-import { selectAllWeatherLocations, selectWeatherLocationDataResult } from '../../redux/api/weatherLocationApi';
+import { selectAllWeatherLocations } from '../../redux/api/weatherLocationApi';
 import { useAppSelector } from '../../redux/reduxHooks';
 import TooltipMarkerComponent from '../TooltipMarkerComponent';
 import CreateWeatherModalComponent from './CreateWeatherModalComponent';
@@ -18,43 +16,35 @@ import WeatherViewComponent from './WeatherViewComponent';
  * @returns Weather page element
  */
 export default function WeatherDetailComponent() {
-	const { status } = useAppSelector(selectWeatherLocationDataResult);
 	const weatherData = useAppSelector(selectAllWeatherLocations);
 	return (
 		<div className='flexGrowOne'>
-			{status === QueryStatus.pending ? (
-				<div className='text-center'>
-					<SpinnerComponent loading width={50} height={50} />
-					<FormattedMessage id='redo.cik.and.refresh.db.views'></FormattedMessage>
-				</div>
-			) : (
-				<div>
-					<TooltipHelpComponent page='weather' />
+			<div>
+				<TooltipHelpComponent page='weather' />
 
-					<div className='container-fluid'>
-						<h2 style={titleStyle}>
-							<FormattedMessage id='weather' />
-							<div style={tooltipStyle}>
-								<TooltipMarkerComponent page='weather' helpTextId={tooltipStyle.tooltipWeatherView} />
-							</div>
-						</h2>
-						<div className="edit-btn">
-							{/* The actual button for create is inside this component. */}
-							< CreateWeatherModalComponent />
+				<div className='container-fluid'>
+					<h2 style={titleStyle}>
+						<FormattedMessage id='weather' />
+						<div style={tooltipStyle}>
+							<TooltipMarkerComponent page='weather' helpTextId={tooltipStyle.tooltipWeatherView} />
 						</div>
-						<div className="card-container">
-							{/* Create a WeatherViewComponent for each WeatherLocationData in Weather State after sorting by identifier */}
-							{
-								weatherData.map(weatherData => (
-									<WeatherViewComponent
-										key={weatherData.id}
-										weather={weatherData}
-									/>
-								))}
-						</div>
+					</h2>
+					<div className="edit-btn">
+						{/* The actual button for create is inside this component. */}
+						< CreateWeatherModalComponent />
+					</div>
+					<div className="card-container">
+						{/* Create a WeatherViewComponent for each WeatherLocationData in Weather State after sorting by identifier */}
+						{
+							weatherData.map(weatherData => (
+								<WeatherViewComponent
+									key={weatherData.id}
+									weather={weatherData}
+								/>
+							))}
 					</div>
 				</div>
-			)}
+			</div>
 		</div>
 	);
 }
