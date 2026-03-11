e9122daeda3e2415475a3e3bbe17bfba3fcd851d
danielshid <danielshi74@gmail.com>
2026-03-11 14:28:07 -0400

    replace supertest in weatherLocationRouteTests.js

--- src/server/test/routes/weatherLocationRouteTests.js
+++ src/server/test/routes/weatherLocationRouteTests.js
@@ -4,17 +4,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-const { mocha } = require('../common');
-const request = require('supertest');
-const app = require('../../app');
 const { expect } = require('chai');
+const { chai, mocha, app } = require('../common');
 
 mocha.describe('Weather Location Routes', () => {
 	mocha.it('GET all weather locations', async () => {
-		const response = await request(app).get('/api/weatherLocation');
+		const res = await chai.request(app)
 		// Check for successful response
-		expect(response.status).to.equal(200);
-		expect(response.body).to.be.an('array');
+		expect(res.status).to.equal(200);
+		expect(res.body).to.be.an('array');
 	});
 
 	mocha.it('POST for adding a weather location', async () => {
@@ -25,10 +23,10 @@ mocha.describe('Weather Location Routes', () => {
 			note: 'test note'
 		};
 		// Check for successful response
-		const response = await request(app)
+		const res = await chai.request(app)
 			.post('/api/weatherLocation/addWeatherLocation')
 			.send(newWeatherLocation);
-		expect(response.status).to.equal(200);
+		expect(res.status).to.equal(200);
 	});
 	// TODO: more test cases with invalid data
 
@@ -36,10 +34,10 @@ mocha.describe('Weather Location Routes', () => {
 		// Assuming there is an existing weather location with ID 1
 		const locationId = { id: 1 };
 		// Check for successful response
-		const response = await request(app)
+		const res = await chai.request(app)
 			.post('/api/weatherLocation/delete')
 			.send(locationId);
-		expect(response.status).to.equal(200);
+		expect(res.status).to.equal(200);
 	});
 	// TODO: test case for deleting a non-existing weather location
 
