11cbbf7d3eeee66538fc8493092457d374f15ba2
Andrew-Bonner <andrew.bonner@bsu.edu>
2026-03-31 19:16:05 -0400

    updated the login route to make sure the bcrypt.copare function is called on a null users to eliminate time difference

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -49,6 +49,7 @@ router.post('/', credentialsRequestValidationMiddleware, async (req, res) => {
 			let isValid;
 			if (user === null) {
 				// User did not exist so return false.
+				isValid = await bcrypt.compare(req.body.password, user.passwordHash);
 				isValid = false;
 			} else {
 				isValid = await bcrypt.compare(req.body.password, user.passwordHash);
