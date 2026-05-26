4fa3aef5bd812963c7da04fe05884b6f56b5464d
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2026-05-26 13:04:44 -0700

    Changes for setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,21 +2,28 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-	e2e: {
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-			on("task", {
-				log(args) {
-					console.log(...args);
-					return null;
-				}
-			});
-		},
-		specPattern: 'src/cypress/e2e/*.cy.ts',
-		supportFile: 'src/cypress/support/e2e.ts',
-		screenshotsFolder: 'src/cypress/screenshots/e2e'
-	}
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on("task", {
+        log(args) {
+          console.log(...args);
+          return null;
+        },
+      });
+    },
+    specPattern: "src/cypress/e2e/*.cy.ts",
+    supportFile: "src/cypress/support/e2e.ts",
+    screenshotsFolder: "src/cypress/screenshots/e2e",
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
 });
