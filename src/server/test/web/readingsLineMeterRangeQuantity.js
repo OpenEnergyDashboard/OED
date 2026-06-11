126a4d6df9eda995405cebd6089464b53bfb6f61
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-06-11 10:21:02 -0700

    fix formatting, removed prettier formatting

--- src/server/test/web/readingsLineMeterRangeQuantity.js
+++ src/server/test/web/readingsLineMeterRangeQuantity.js
@@ -7,8 +7,8 @@
   See: https://github.com/OpenEnergyDashboard/DesignDocs/blob/main/testing/testing.md for information.
 */
 
-const { chai, mocha, app } = require("../common");
-const Unit = require("../../models/Unit");
+const { chai, mocha, app } = require('../common');
+const Unit = require('../../models/Unit');
 const {
 	prepareTest,
 	parseExpectedCsv,
@@ -19,229 +19,160 @@ const {
 	METER_ID,
 	unitDatakWh,
 	conversionDatakWh,
-	meterDatakWh,
-} = require("../../util/readingsUtils");
+	meterDatakWh } = require('../../util/readingsUtils');
 
-mocha.describe("readings API", () => {
-	mocha.describe(
-		"readings test, test if data returned by API is as expected",
-		() => {
-			mocha.describe("for line charts", () => {
-				mocha.describe("for range (min/max)", () => {
-					mocha.describe("for quantity meters", () => {
-						// Test using a date range of infinity, which should return as days
-						mocha.it(
-							"LR1: range should have daily points for 15 minute reading intervals and quantity units with +-inf start/end time & kWh as kWh",
-							async () => {
-								// Load the data into the database
-								await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-								// Get the unit ID since the DB could use any value.
-								const unitId = await getUnitId("kWh");
-								// Load the expected response data from the corresponding csv file
-								const expected = await parseExpectedCsv(
-									"src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_-inf_et_inf.csv",
-								);
-								// Create a request to the API for unbounded reading times and save the response
-								const res = await chai
-									.request(app)
-									.get(`/api/unitReadings/line/meters/${METER_ID}`)
-									.query({
-										timeInterval: ETERNITY.toString(),
-										graphicUnitId: unitId,
-									});
-								// Check that the API reading is equal to what it is expected to equal
-								expectRangeToEqualExpected(res, expected);
-							},
+mocha.describe('readings API', () => {
+	mocha.describe('readings test, test if data returned by API is as expected', () => {
+		mocha.describe('for line charts', () => {
+			mocha.describe('for range (min/max)', () => {
+				mocha.describe('for quantity meters', () => {
+					// Test using a date range of infinity, which should return as days
+					mocha.it('LR1: range should have daily points for 15 minute reading intervals and quantity units with +-inf start/end time & kWh as kWh', async () => {
+						// Load the data into the database
+						await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+						// Get the unit ID since the DB could use any value.
+						const unitId = await getUnitId('kWh');
+						// Load the expected response data from the corresponding csv file
+						const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_-inf_et_inf.csv');
+						// Create a request to the API for unbounded reading times and save the response
+						const res = await chai.request(app).get(`/api/unitReadings/line/meters/${METER_ID}`)
+							.query({ timeInterval: ETERNITY.toString(), graphicUnitId: unitId });
+						// Check that the API reading is equal to what it is expected to equal
+						expectRangeToEqualExpected(res, expected);
+					});
+
+					mocha.it('LR2: range should have daily points for 15 minute reading intervals and quantity units with explicit start/end time & kWh as kWh', async () => {
+						// Load the data into the database
+						await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+						// Get the unit ID since the DB could use any value.
+						const unitId = await getUnitId('kWh');
+						// Load the expected response data from the corresponding csv file
+						const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-08-18%00#00#00_et_2022-11-01%00#00#00.csv');
+						// Create a request to the API for unbounded reading times and save the response
+						const res = await chai.request(app).get(`/api/unitReadings/line/meters/${METER_ID}`)
+							.query({ timeInterval: createTimeString('2022-08-18', '00:00:00', '2022-11-01', '00:00:00'), graphicUnitId: unitId });
+						// Check that the API reading is equal to what it is expected to equal
+						expectRangeToEqualExpected(res, expected);
+					});
+
+					// Add LR3 here
+					mocha.it('LR3: range should have daily points for middle readings of 15 minute for a 61 day period and quantity units with kWh as kWh', async () => {
+						// 1) Seed DB with the standard unit/meter data for kWh
+						await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+
+						// 2) Resolve the kWh graphic unit id (DB can assign any id)
+						const unitId = await getUnitId('kWh');
+
+						// 3) Load the expected series for a 61-day window (daily points expected)
+						const expected = await parseExpectedCsv(
+							'src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-08-25%00#00#00_et_2022-10-25%00#00#00.csv',
 						);
 
-						mocha.it(
-							"LR2: range should have daily points for 15 minute reading intervals and quantity units with explicit start/end time & kWh as kWh",
-							async () => {
-								// Load the data into the database
-								await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-								// Get the unit ID since the DB could use any value.
-								const unitId = await getUnitId("kWh");
-								// Load the expected response data from the corresponding csv file
-								const expected = await parseExpectedCsv(
-									"src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-08-18%00#00#00_et_2022-11-01%00#00#00.csv",
-								);
-								// Create a request to the API for unbounded reading times and save the response
-								const res = await chai
-									.request(app)
-									.get(`/api/unitReadings/line/meters/${METER_ID}`)
-									.query({
-										timeInterval: createTimeString(
-											"2022-08-18",
-											"00:00:00",
-											"2022-11-01",
-											"00:00:00",
-										),
-										graphicUnitId: unitId,
-									});
-								// Check that the API reading is equal to what it is expected to equal
-								expectRangeToEqualExpected(res, expected);
-							},
-						);
+						// 4) Hit the API for the same window & unit
+						const res = await chai
+							.request(app)
+							.get(`/api/unitReadings/line/meters/${METER_ID}`)
+							.query({
+								timeInterval: createTimeString('2022-08-25', '00:00:00', '2022-10-25', '00:00:00'),
+								graphicUnitId: unitId,
+						});
 
-						// Add LR3 here
-						mocha.it(
-							"LR3: range should have daily points for middle readings of 15 minute for a 61 day period and quantity units with kWh as kWh",
-							async () => {
-								// 1) Seed DB with the standard unit/meter data for kWh
-								await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+						// 5) Assert the API response matches the expected CSV (shape + values)
+						expectRangeToEqualExpected(res, expected);
+					});
 
-								// 2) Resolve the kWh graphic unit id (DB can assign any id)
-								const unitId = await getUnitId("kWh");
 
-								// 3) Load the expected series for a 61-day window (daily points expected)
-								const expected = await parseExpectedCsv(
-									"src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-08-25%00#00#00_et_2022-10-25%00#00#00.csv",
-								);
+					mocha.it('LR4: range should have hourly points for middle readings of 15 minute for a 60 day period and quantity units with kWh as kWh', async () => {
+						await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+						// Get the unit ID since the DB could use any value.
+						const unitId = await getUnitId('kWh');
+						// Load expected response data from the corresponding csv file
+						const expected = await parseExpectedCsv('src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-08-25%00#00#00_et_2022-10-24%00#00#00.csv');
+						// Create a request to the API for unbounded reading times and save the response
+						const res = await chai.request(app).get(`/api/unitReadings/line/meters/${METER_ID}`)
+							.query({ timeInterval: createTimeString('2022-08-25', '00:00:00', '2022-10-24', '00:00:00'), graphicUnitId: unitId });
+						expectRangeToEqualExpected(res, expected);
+					});
 
-								// 4) Hit the API for the same window & unit
-								const res = await chai
-									.request(app)
-									.get(`/api/unitReadings/line/meters/${METER_ID}`)
-									.query({
-										timeInterval: createTimeString(
-											"2022-08-25",
-											"00:00:00",
-											"2022-10-25",
-											"00:00:00",
-										),
-										graphicUnitId: unitId,
-									});
+					mocha.it(
+						'LR5: range should barely have hourly points for middle readings of 15 minute for a 15 day + 15 min period and quantity units with kWh as kWh',
+						async () => {
+							// Prepare test data using existing utility
+							await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+							//Get unit ID for kWh
+							const unitId = await getUnitId('kWh');
+							//Load the expected data for the LR5 date range and unit configuration
+							const expected = await parseExpectedCsv(
+								'src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-09-21%00#00#00_et_2022-10-06%00#15#00.csv'
+							);
+							//Send API request using time range and graphic unit
+							const res = await chai.request(app).get(`/api/unitReadings/line/meters/${METER_ID}`)
+								.query({
+									timeInterval: createTimeString('2022-09-21', '00:00:00', '2022-10-06', '00:15:00'),
+									graphicUnitId: unitId
+								});
+							//Assert the response only includes data within that range and format
+							expectRangeToEqualExpected(res, expected);
+						}
+					);
 
-								// 5) Assert the API response matches the expected CSV (shape + values)
-								expectRangeToEqualExpected(res, expected);
-							},
-						);
+					mocha.it(
+						'LR6: Range with 14 days barely gives raw points and middle readings.',
+						async () => {
+							// Load data into database
+							await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+							// Get unit ID for kWh
+							const unitId = await getUnitId('kWh');
+							// Load expected values for LR6 date range and unit configuration
+							const expected = await parseExpectedCsv(
+								'src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-09-21%00#00#00_et_2022-10-05%00#00#00.csv'
+							);
 
-						mocha.it(
-							"LR4: range should have hourly points for middle readings of 15 minute for a 60 day period and quantity units with kWh as kWh",
-							async () => {
-								await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-								// Get the unit ID since the DB could use any value.
-								const unitId = await getUnitId("kWh");
-								// Load expected response data from the corresponding csv file
-								const expected = await parseExpectedCsv(
-									"src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-08-25%00#00#00_et_2022-10-24%00#00#00.csv",
-								);
-								// Create a request to the API for unbounded reading times and save the response
-								const res = await chai
-									.request(app)
-									.get(`/api/unitReadings/line/meters/${METER_ID}`)
-									.query({
-										timeInterval: createTimeString(
-											"2022-08-25",
-											"00:00:00",
-											"2022-10-24",
-											"00:00:00",
-										),
-										graphicUnitId: unitId,
-									});
-								expectRangeToEqualExpected(res, expected);
-							},
-						);
+							// Send API request with LR6 date range and graphic unit
+							const res = await chai
+								.request(app)
+								.get(`/api/unitReadings/line/meters/${METER_ID}`)
+								.query({
+									timeInterval: createTimeString('2022-09-21', '00:00:00', '2022-10-05', '00:00:00'),
+									graphicUnitId: unitId
+								});
+							expectRangeToEqualExpected(res, expected);
+						}
+					);
 
-						mocha.it(
-							"LR5: range should barely have hourly points for middle readings of 15 minute for a 15 day + 15 min period and quantity units with kWh as kWh",
-							async () => {
-								// Prepare test data using existing utility
-								await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-								//Get unit ID for kWh
-								const unitId = await getUnitId("kWh");
-								//Load the expected data for the LR5 date range and unit configuration
-								const expected = await parseExpectedCsv(
-									"src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-09-21%00#00#00_et_2022-10-06%00#15#00.csv",
-								);
-								//Send API request using time range and graphic unit
-								const res = await chai
-									.request(app)
-									.get(`/api/unitReadings/line/meters/${METER_ID}`)
-									.query({
-										timeInterval: createTimeString(
-											"2022-09-21",
-											"00:00:00",
-											"2022-10-06",
-											"00:15:00",
-										),
-										graphicUnitId: unitId,
-									});
-								//Assert the response only includes data within that range and format
-								expectRangeToEqualExpected(res, expected);
-							},
-						);
+					mocha.it(
+						'LR7: range with partial days/hours for daily gives only full days',
+						async () => {
+							// Prepare test data using existing utility
+							await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+							//Get unit ID for kWh
+							const unitId = await getUnitId('kWh');
+							//Load the expected data for the LR7 date range and unit configuration
+							const expected = await parseExpectedCsv(
+								'src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-08-20%07#25#35_et_2022-10-28%13#18#28.csv',
+							);
+							//Send API request using time range and graphic unit
+							const res = await chai.request(app).get(`/api/unitReadings/line/meters/${METER_ID}`)
+								.query({
+									timeInterval: createTimeString('2022-08-20', '07:25:35', '2022-10-28', '13:18:28'),
+									graphicUnitId: unitId
+								});
+							//Assert the response only includes data within that range and format
+							expectRangeToEqualExpected(res, expected);
+						},
+					);
 
-						mocha.it(
-							"LR6: Range with 14 days barely gives raw points and middle readings.",
-							async () => {
-								// Load data into database
-								await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-								// Get unit ID for kWh
-								const unitId = await getUnitId("kWh");
-								// Load expected values for LR6 date range and unit configuration
-								const expected = await parseExpectedCsv(
-									"src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-09-21%00#00#00_et_2022-10-05%00#00#00.csv",
-								);
+					// Add LR10 here
 
-								// Send API request with LR6 date range and graphic unit
-								const res = await chai
-									.request(app)
-									.get(`/api/unitReadings/line/meters/${METER_ID}`)
-									.query({
-										timeInterval: createTimeString(
-											"2022-09-21",
-											"00:00:00",
-											"2022-10-05",
-											"00:00:00",
-										),
-										graphicUnitId: unitId,
-									});
-								expectRangeToEqualExpected(res, expected);
-							},
-						);
+					// Add LR11 here
 
-						mocha.it(
-							"LR7: range with partial days/hours for daily gives only full days",
-							async () => {
-								// Prepare test data using existing utility
-								await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-								//Get unit ID for kWh
-								const unitId = await getUnitId("kWh");
-								//Load the expected data for the LR7 date range and unit configuration
-								const expected = await parseExpectedCsv(
-									"src/server/test/web/readingsData/expected_line_range_ri_15_mu_kWh_gu_kWh_st_2022-08-20%07#25#35_et_2022-10-28%13#18#28.csv",
-								);
-								//Send API request using time range and graphic unit
-								const res = await chai
-									.request(app)
-									.get(`/api/unitReadings/line/meters/${METER_ID}`)
-									.query({
-										timeInterval: createTimeString(
-											"2022-08-20",
-											"07:25:35",
-											"2022-10-28",
-											"13:18:28",
-										),
-										graphicUnitId: unitId,
-									});
-								//Assert the response only includes data within that range and format
-								expectRangeToEqualExpected(res, expected);
-							},
-						);
+					// Add LR12 here
 
-						// Add LR10 here
+					// Add LR13 here
 
-						// Add LR11 here
+					// Add LR18 here
 
-						// Add LR12 here
-
-						// Add LR13 here
-
-						// Add LR18 here
-
-						mocha.it(
+					mocha.it(
 							"LR19: range should have daily points for 15 minute reading intervals and quantity units with +-inf start/end time & kWh as metric ton of CO2 & chained",
 							async () => {
 								// u10: kg, u11: metric ton, u12: kg CO₂
@@ -332,12 +263,12 @@ mocha.describe("readings API", () => {
 								expectRangeToEqualExpected(res, expected);
 							},
 						);
-						// Add LR20 here
 
-						// Add LR21 here
-					});
+					// Add LR20 here
+
+					// Add LR21 here
 				});
 			});
-		},
-	);
+		});
+	});
 });
