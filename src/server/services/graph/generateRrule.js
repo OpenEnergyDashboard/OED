77364bb6147ca7f706890b40ed456a3f246abd68
Steven Huss-Lederman <huss@beloit.edu>
2026-07-01 11:12:21 -0500

    Fix up some issues/items
    This appears to be a basic working version for both
    slope/intercept and patterns. I wanted to save the
    work to be safe. Here is what was done:
    
    - Fixed addition of segments to match pre-pattern
    method and get code to work properly.
    - Changed how -infinity & infinity are dealt with. They are
    converted to fixed dates so the code works.
    This isn't the ideal/final solution but works for now.
    - Fixed the exact dates included in a pattern.
    - Move conn to last argument since most functions do that.
    - Clarified some commenting.
    - Minor formatting.
    - There are a lot of debug console statements due to
    the need to test more. They will be removed.

--- src/server/services/graph/generateRrule.js
+++ src/server/services/graph/generateRrule.js
@@ -2,12 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { RRule } = require("rrule");
+const { datetime, RRule } = require('rrule');
 const Week = require('../../models/Week');
 const DaySegment = require('../../models/DaySegment');
 
-async function generateRrule(weekId, conn, startDate, endDate) {
-
+/**
+ * Uses the provided week & start/end dates to return the RRule for this pattern.
+ * @param {*} weekId The id of the week pattern to be used.
+ * @param {*} startDate The start date of the pattern which should have time as empty or 00:00:00. Note the RRule will include the start time for the needed segments.
+ * @param {*} endDate The end date of the pattern which should have time as empty or 00:00:00.
+ * @param {*} conn The DB connection to use.
+ * @returns 
+ */
+async function generateRrule(weekId, startDate, endDate, conn) {
 	// get Week from weekId
 	const week = await Week.getById(weekId, conn);
 
@@ -23,9 +30,9 @@ async function generateRrule(weekId, conn, startDate, endDate) {
 	];
 
 	// get only unique dayIds (convert to Set then Array again)
-	const uniqueWeekDayIds = Array.from(new Set(weekDayIds.map(obj => 
-												JSON.stringify(obj))))
-														.map(e => JSON.parse(e));
+	const uniqueWeekDayIds = Array.from(new Set(weekDayIds.map(obj =>
+		JSON.stringify(obj))))
+		.map(e => JSON.parse(e));
 
 	// Fetch all daySegments for each day in the week (parallel requests)
 	const segmentPromises = uniqueWeekDayIds.map((dayId) =>
@@ -39,6 +46,7 @@ async function generateRrule(weekId, conn, startDate, endDate) {
 		dayId,
 		segments: daySegmentResponses[i],
 	}));
+	console.log('daySegments: ', daySegments);
 
 	const occurrences = [];
 
@@ -70,6 +78,12 @@ async function generateRrule(weekId, conn, startDate, endDate) {
 		}
 		// Add the rruleDay from the current pair to this dayId's rruleDays array
 		existing.rruleDays.push(pair.rruleDay);
+		// console.log('existing: ', existing);
+	});
+	// console.log('dayIdMappings: ', dayIdMappings);
+	dayIdMappings.forEach((day) => {
+		// console.log('dayId, dayId.rruleDays: ', dayId, ' ', dayId.rruleDays);
+		console.log('day.dayId, day.rruleDays: ', day.dayId, day.rruleDays);
 	});
 
 	daySegments.forEach((day) => {
@@ -79,18 +93,38 @@ async function generateRrule(weekId, conn, startDate, endDate) {
 			const { startHour, endHour, slope, intercept } = segment;
 			const weekDayArray = dayIdMappings.find(d => d.dayId === dayId).rruleDays;
 			const duration = endHour - startHour;
+			console.log('startHour, endHour, slope, intercept : ', startHour, endHour, slope, intercept);
 
 			// start = startDate +/& startHour
-			// rrule dstart must be both the first day of
-			// the event and the time of the event
-			const start = new Date(startDate);
+			// rrule dtstart must be both the first day of the event done via getRruleDate and
+			// the time of the event done with setHours where it is only a whole hour.
+			console.log('startDate: ', startDate);
+			// const start = new Date(startDate);
+			const start = getRruleDate(startDate);
+			// minutes, seconds and ms should default to 0 but passed to be sure.
 			start.setHours(startHour, 0, 0, 0);
+			console.log('start: ', start);
+			console.log('endDate: ', endDate);
+			const end = getRruleDate(endDate);
+			// There is an issue with including or excluding the start/end date. The usage in the calling
+			// function does inclusive so the end date is also included. Thus, remove the last day to
+			// avoid this. It is safe to subtract a whole day since this system assumes pattern segments
+			// start/end on the day because patterns are for whole days and RRule does each day.
+			// The inverse issue would occur if the calling function did not use inclusive and the start
+			// time would need to be fixed up.
+			// I tried the RRule exdate but it did not easily work for me so did it this way. Will probably
+			// need something like exdate to get holiday exclusions to work so this could be revised once
+			// that is done.
+			end.setDate(end.getDate() - 1);
+			console.log('end: ', end);
+			console.log('weekDayArray: ', weekDayArray);
 
 			const rule = new RRule({
 				freq: RRule.WEEKLY,
 				byweekday: weekDayArray,
 				dtstart: start,
-				until: new Date(endDate)
+				// until: new Date(endDate)
+				until: end
 			});
 
 			occurrences.push({
@@ -99,19 +133,53 @@ async function generateRrule(weekId, conn, startDate, endDate) {
 				slope,
 				intercept
 			});
-
 		});
-
 	});
 
-	// console.log("Generated rules:", occurrences.map((r) => ({
-	//   rrule: r.rule.toString(),
-	//   duration: r.duration,
-	//   slope: r.slope,
-	//   intercept: r.intercept,
-	// })));
+	console.log("Generated rules:", occurrences.map((r) => ({
+		rrule: r.rule.toString(),
+		duration: r.duration,
+		slope: r.slope,
+		intercept: r.intercept,
+	})));
 
 	return occurrences;
 }
 
-module.exports = { generateRrule };
+/**
+ * Returns the date object needed by RRule. It fixes up -infinity & infinity to fixed dates.
+ * It is currently using JS Date.
+ * @param {*} dateString A string representing the date desired including -infinity & infinity.
+ * @returns Date object needed for RRule.
+ */
+function getRruleDate(dateString) {
+	// OED currently uses -infinity, infinity for the upper and lower bound on the conversion segments.
+	// moment, Date & datetime (RRule) cannot handle this date. Thus, it must be converted into a valid
+	// date for use. For now, it will use 1970-01-01 for -infinity since that is used in other places as
+	// the min date. It will use 2050-01-01 for infinity.
+	// TODO A better scheme should be used. To get this right would mean tracking the min & max date for all
+	// readings and redo cik whenever it goes out of that range due to new data. OED could hack this to be some
+	// time before the current min reading and some time after the current time. A decision needs to be made
+	// and implemented. OED should consider if the current -infinity, infinity should somehow show the
+	// actual value used but needs the special values so the algorithm works (or must be changed somehow).
+	// No matter what is done (or not), this needs to be documented on the OED help pages.
+
+	// This is to document previous testing. Date and moment return similar values in this code:
+	// 	let xDate = new Date('1960-01-01');
+	// console.log('xDate: ', xDate);
+	// let xdt = new datetime(1960, 1, 1);
+	// console.log('xdt: ', xdt);
+	// TODO However, see https://github.com/jkbrzt/rrule#important-use-utc-dates for timezone considerations.
+	// TODO https://github.com/jkbrzt/rrule#timezone-support uses datetime from RRULE and not Date.
+
+	let dateStringUse = dateString;
+	// Fix up -infinity & infinity to be real dates. See above.
+	if (dateStringUse === '-infinity') {
+		dateStringUse = '1970-01-01';
+	} else if (dateStringUse === 'infinity') {
+		dateStringUse = '2050-01-01';
+	}
+	return new Date(dateStringUse);
+}
+
+module.exports = { generateRrule, getRruleDate };
--- src/server/services/graph/timeVaryingPathConversion.js
+++ src/server/services/graph/timeVaryingPathConversion.js
@@ -6,7 +6,7 @@ const ConversionSegment = require('../../models/ConversionSegment');
 const Conversion = require('../../models/Conversion');
 const invertConversion = require('./pathConversion').invertConversion;
 const updatedConversion = require('./pathConversion').updatedConversion;
-const generateRrule = require('./generateRrule').generateRrule;
+const { generateRrule, getRruleDate } = require('./generateRrule');
 
 /**
  * Chains time-varying conversions along a path, producing combined segments for cik_vary.
@@ -16,15 +16,21 @@ const generateRrule = require('./generateRrule').generateRrule;
  * @param {*} conn Database connection.
  * @returns Array of {source, destination, startTime, endTime, slope, intercept}
  */
-// async function timeVaryingPathConversion(path, conn) {
 async function timeVaryingPathConversion(path, conn) {
 	// 1. Fetch and sort segments for each edge
+	// This is an array where each entry is an array that contains information on each
+	// conversion segment for current path vertices/edge.
 	const edgeSegments = [];
+	console.log('path.length: ', path.length);
 	for (let i = 0; i < path.length - 1; ++i) {
+		// Create new entry to hold the conversion segments for this edge in the path.
+		edgeSegments.push([]);
 		const sourceId = path[i].id;
 		const destinationId = path[i + 1].id;
+		console.log('sourceId, destinationId: ', sourceId, destinationId);
 		// segments are sorted by start_time in getBySourceDestination
 		let segments = await ConversionSegment.getBySourceDestination(sourceId, destinationId, conn);
+		console.log('segments: ', segments);
 		// Tell if the conversion direction is okay (false) or must be reversed (true). Assumed false unless found otherwise in next step.
 		let reversed = false;
 		// Did not find the conversion segments. Since conversion should exist, it must be the other way around and bidirectional.
@@ -45,11 +51,14 @@ async function timeVaryingPathConversion(path, conn) {
 		}
 
 		// loop through segments, ..., and push to edgeSegments
+		// console.log('segments.length: ', segments.length);
 		for (let segmentIndex = 0; segmentIndex < segments.length; segmentIndex++) {
+			console.log('segmentIndex: ', segmentIndex)
 			const curSegment = segments[segmentIndex];
 
 			// deal with conversionSegments that have a SLOPE & INTERCEPT
 			if (curSegment.weekPatternsId == null) {
+				console.log('slope/intercept');
 				// The segment does not have a pattern so can use the segments found above for slope/intercept.
 				if (reversed) {
 					// Reversed so invert segment found.
@@ -60,25 +69,39 @@ async function timeVaryingPathConversion(path, conn) {
 						intercept: convertedIntercept
 					};
 				}
+				// console.log('curSegment: ', curSegment);
 				// It may be possible to avoid this push by directly putting segments into edgeSegments. Given there should not be
 				// too many without a pattern it probably is not too important.
-				edgeSegments.push(curSegment);
-			
+				edgeSegments[i].push(curSegment);
+				// console.log('edgeSegments: ', edgeSegments);
 			} else {
+				console.log('pattern');
 				// Deal with conversionSegments that have a WEEK_PATTERN_ID
 
 				const weekId = curSegment.weekPatternsId;
-				
+
 				// 1. Use the pattern for this segment to create an RRULE.
-				const ruleInfo = await generateRrule(weekId, conn, curSegment.startTime, curSegment.endTime);
+				const ruleInfo = await generateRrule(weekId, curSegment.startTime, curSegment.endTime, conn);
+				// TODO DEBUG
+				// console.log("ruleInfo:", ruleInfo.map((r) => ({
+				// 	rrule: r.rule.toString(),
+				// 	duration: r.duration,
+				// 	slope: r.slope,
+				// 	intercept: r.intercept,
+				// })));
 
 				// 2. Use an RRULE generator to create all the needed conversions from segments.start_time to segments.end_time
 				const occurrences = [];
 				// occurrences is 2D array, each array the occurrences for each generated rrule 
-				const start = new Date(curSegment.startTime);
-				const end   = new Date(curSegment.endTime);
+				// console.log('curSegment.startTime, curSegment.endTime: ', curSegment.startTime, curSegment.endTime);
+				const start = getRruleDate(curSegment.startTime);
+				const end = getRruleDate(curSegment.endTime);
+				console.log('start, end: ', start, end);
 				ruleInfo.forEach((info) => {
-					occurrences.push(info.rule.between(start, end));
+					// The third parameter of true means start and end are included. See generateRrule where
+					// the end date is adjusted so it is correct.
+					console.log('info.rule.between(start, end, true): ', info.rule.between(start, end));
+					occurrences.push(info.rule.between(start, end, true));
 
 					// 2.b. If reversed is true then invert the slope/intercept for each conversion using invertConversion().
 					if (reversed) {
@@ -94,11 +117,13 @@ async function timeVaryingPathConversion(path, conn) {
 
 				// 3. Each segment is added to edgeSegments with the start_time, end_time, slope & intercept.
 				var r = 0;
+				console.log('occurrences: ', occurrences);
 				occurrences.forEach((patternOccurrences) => {
 					patternOccurrences.forEach((occur) => {
-
+						console.log('occur: ', occur);
 						const end = new Date(occur);
 						end.setHours(ruleInfo[r].duration, 0, 0, 0);
+						console.log('end: ', end);
 
 						const newSegment = new ConversionSegment(
 							curSegment.sourceId, 					// sourceId
@@ -110,7 +135,7 @@ async function timeVaryingPathConversion(path, conn) {
 							end,													// endTime
 							''														// note
 						);
-						edgeSegments.push(newSegment);
+						edgeSegments[i].push(newSegment);
 					});
 
 					r++;
@@ -126,17 +151,25 @@ async function timeVaryingPathConversion(path, conn) {
 	}
 
 	// 2. Initialize pointers for each edge
+	console.log('2.');
 	const pointers = Array(path.length - 1).fill(0);
 
 	// 3. Main loop
+	console.log('3.');
+	// console.log('edgeSegments.length: ', edgeSegments.length);
+	// console.log('edgeSegments: ', edgeSegments);
+	// process.exit(99); // DEBUG!!!!!!!!
+	// console.log('edgeSegments[0]: ', edgeSegments[0]);
+	// console.log('edgeSegments[1]: ', edgeSegments[1]);
 	let currentStart = Number.NEGATIVE_INFINITY;
 	const results = [];
 	let done = false;
 	while (!done) {
 		// Find current segments for each edge
-		const currentSegments = edgeSegments;
-		// const currentSegments = edgeSegments.map((segments, idx) => segments[pointers[idx]]);
-		
+		// console.log('pointers: ', pointers);
+		const currentSegments = edgeSegments.map((segments, idx) => segments[pointers[idx]]);
+		// console.log('currentSegments: ', currentSegments);
+
 		// Find minimum end time among current segments
 		let currentEnd = Math.min(...currentSegments.map(seg => parsePostgresDate(seg.endTime)));
 
@@ -145,6 +178,7 @@ async function timeVaryingPathConversion(path, conn) {
 		for (const seg of currentSegments) {
 			[slope, intercept] = updatedConversion(slope, intercept, seg.slope, seg.intercept);
 		}
+		// console.log('before push');
 		results.push({
 			source: path[0].id,
 			destination: path[path.length - 1].id,
@@ -153,20 +187,26 @@ async function timeVaryingPathConversion(path, conn) {
 			slope,
 			intercept
 		});
+		// console.log('results: ', results);
 
 		if (currentEnd === Number.POSITIVE_INFINITY) {
+			// console.log('done');
 			done = true;
 		} else {
 			// Advance pointers for segments ending at currentEnd
+			// console.log('pointers.length: ', pointers.length)
 			for (let i = 0; i < pointers.length; ++i) {
+				// console.log('i, pointers: ', i, pointers);
 				if (parsePostgresDate(currentSegments[i].endTime) === currentEnd) {
 					pointers[i]++;
+					// console.log('advancing i: pointers[i]', i, pointers[i])
 				}
 			}
 		}
 		currentStart = currentEnd;
 	}
 
+	console.log('done');
 	return results;
 }
 
