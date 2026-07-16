6b1792c06ab88bd0a06820f96a7604578887009c
stockingstocker <stockingstocker@protonmail.com>
2026-07-16 10:24:51 -0700

    remove debug prints and comments from readings.js

--- src/server/routes/readings.js
+++ src/server/routes/readings.js
@@ -12,7 +12,7 @@ const { getConnection } = require('../db');
 const { STRING_GENERAL_MAX_LENGTH: GENERAL_STRING_MAX_LENGTH, NUMERIC_ID_MAX_LENGTH } = require('../util/validationConstants');
 const { HTTP_CODES } = require('../util/httpCodes');
 const { isValidTimeInterval } = require('../util/timeValidation');
-
+const { isTokenAuthorized } = require('../util/userRoles');
 const Preferences = require('../models/Preferences');
 const User = require('../models/User');
 const { success, failure } = require('./response');
@@ -100,22 +100,9 @@ router.get('/line/raw/meter/:meter_id', optionalAuthMiddleware, async (req, res)
 		}
 	};
 
-	//if (!validate(req.params, validParams).valid || !validate(req.query, validQueries).valid) {
-	//	res.sendStatus(HTTP_CODES.BAD_REQUEST);
-
-	const paramValidation = validate(req.params, validParams);
-	const queryValidation = validate(req.query, validQueries);
-
-	console.log('params:', req.params);
-	console.log('typeof meter_id:', typeof req.params.meter_id);
-	console.log('param validation:', paramValidation);
-	console.log('query validation:', queryValidation);
-
-	if (!validate(req.params, validParams).valid || !validate(req.query, validQueries).valid || !isValidTimeInterval(req.query.timeInterval, true)) {
-		//res.sendStatus(HTTP_CODES.BAD_REQUEST);
+	if (!validate(req.params, validParams).valid || !validate(req.query, validQueries).valid) {
 		failure(res, HTTP_CODES.BAD_REQUEST);
 	} else {
-		
 		const conn = getConnection();
 		// Get the routed meter id and time for the desired readings.
 		const meterID = req.params.meter_id;
@@ -142,19 +129,17 @@ router.get('/line/raw/meter/:meter_id', optionalAuthMiddleware, async (req, res)
 		});
 
 		if (fileSize <= preferences.defaultFileSizeLimit) {
-			//} else if (fileSize <= 0.01) {
 			//file size within limit, anyone can download
 			shouldDownload = true;
 		} else if (req.hasValidAuthToken) {
 			//file size above limit, only users with the role EXPORT or ADMIN can download
 			const token = req.headers.token || req.body.token || req.query.token;
-			if ((await isTokenAuthorized(token, User.role.EXPORT)) || (await isTokenAuthorized(token, User.role.ADMIN))) {
+			if (await isTokenAuthorized(token, User.role.EXPORT)) {
 				shouldDownload = true;
 			}
 		}
 
 		if (shouldDownload == false) {
-			//res.sendStatus(HTTP_CODES.FORBIDDEN);
 			failure(res, HTTP_CODES.FORBIDDEN);
 			return;
 		}
@@ -165,11 +150,9 @@ router.get('/line/raw/meter/:meter_id', optionalAuthMiddleware, async (req, res)
 			const rawReadings = await Reading.getReadingsByMeterIDAndDateRange(meterID, timeInterval.startTimestamp, timeInterval.endTimestamp, conn);
 			// They are ready to go back.
 			// nosemgrep: javascript.express.security.audit.xss.direct-response-write.direct-response-write
-			//res.send(rawReadings);
 			success(res, rawReadings);
 		} catch (err) {
 			log.error(`Error while performing GET raw readings for line with meter ${meterID} with time interval ${timeInterval}: ${err}`, err);
-			//res.sendStatus(HTTP_CODES.INTERNAL_SERVER_ERROR);
 			failure(res, HTTP_CODES.INTERNAL_SERVER_ERROR);
 		}
 	}
