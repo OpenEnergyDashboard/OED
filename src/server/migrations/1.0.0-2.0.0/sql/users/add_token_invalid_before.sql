f3ee421ef6019e17e366294176d95c145d923d70
Saul Enriquez <saul.enriquez2005@gmail.com>
2026-07-21 16:10:00 -0500

    add missing MPL 2.0 license header

--- src/server/migrations/1.0.0-2.0.0/sql/users/add_token_invalid_before.sql
+++ src/server/migrations/1.0.0-2.0.0/sql/users/add_token_invalid_before.sql
@@ -1,2 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
 ALTER TABLE users
 ADD COLUMN IF NOT EXISTS token_invalid_before TIMESTAMP NOT NULL DEFAULT TIMESTAMP 'epoch';
