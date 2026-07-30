be43d7eb167b27e2059bc6008a95f6bbbe3f6d20
GoodKimchi <224943594+GoodKimchi@users.noreply.github.com>
2026-07-30 14:21:00 -0700

    fix: share preferences file size validation limit

--- src/client/app/components/admin/PreferencesComponent.tsx
+++ src/client/app/components/admin/PreferencesComponent.tsx
@@ -21,6 +21,7 @@ import { useTranslate } from '../../redux/componentHooks';
 import TimeZoneSelect from '../TimeZoneSelect';
 import { defaultAdminState } from '../../redux/slices/adminSlice';
 import { checkboxStyle, labelStyle } from '../../styles/modalStyle';
+import { MAX_FILE_SIZE_LIMIT } from '../../../../common/preferencesValidationConstants';
 
 /**
  * @returns Preferences Component for Administrative use
@@ -72,11 +73,13 @@ export default function PreferencesComponent() {
 
 		warningFileSize: (): boolean => {
 			return Number(localAdminPref.defaultWarningFileSize) < 0
+				|| Number(localAdminPref.defaultWarningFileSize) > MAX_FILE_SIZE_LIMIT
 				|| Number(localAdminPref.defaultWarningFileSize) > Number(localAdminPref.defaultFileSizeLimit);
 		},
 
 		fileSizeLimit: (): boolean => {
 			return Number(localAdminPref.defaultFileSizeLimit) < 0
+				|| Number(localAdminPref.defaultFileSizeLimit) > MAX_FILE_SIZE_LIMIT
 				|| Number(localAdminPref.defaultWarningFileSize) > Number(localAdminPref.defaultFileSizeLimit);
 		}
 	};
@@ -324,7 +327,10 @@ export default function PreferencesComponent() {
 					value={localAdminPref.defaultWarningFileSize}
 					onChange={e => makeLocalChanges('defaultWarningFileSize', Number(e.target.value))}
 					min='0'
-					max={Number(localAdminPref.defaultFileSizeLimit)}
+					max={Math.min(
+						Number(localAdminPref.defaultFileSizeLimit),
+						MAX_FILE_SIZE_LIMIT
+					)}
 					maxLength={50}
 					invalid={invalidFuncs.warningFileSize()}
 				/>
@@ -354,6 +360,7 @@ export default function PreferencesComponent() {
 					value={localAdminPref.defaultFileSizeLimit}
 					onChange={e => makeLocalChanges('defaultFileSizeLimit', Number(e.target.value))}
 					min={Number(localAdminPref.defaultWarningFileSize)}
+					max={MAX_FILE_SIZE_LIMIT}
 					maxLength={50}
 					invalid={invalidFuncs.fileSizeLimit()}
 				/>
@@ -362,7 +369,7 @@ export default function PreferencesComponent() {
 						id="error.bounds"
 						values={{
 							min: Number(localAdminPref.defaultWarningFileSize),
-							max: Infinity
+							max: MAX_FILE_SIZE_LIMIT
 						}}
 					/>
 				</FormFeedback>
