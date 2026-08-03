3ccef871ce21223f436b5bd8c68f35ebbaa8cdbe
Joon <Joon.Kwon01@student.csulb.edu>
2026-08-03 10:21:05 -0700

    Clarify comments for parent group validation

--- src/client/app/components/groups/EditGroupModalComponent.tsx
+++ src/client/app/components/groups/EditGroupModalComponent.tsx
@@ -991,7 +991,14 @@ export default function EditGroupModalComponent(props: EditGroupModalComponentPr
 	 * If not, then continue delete process.
 	 */
 	async function validateDelete() {
-		// Get all parent groups of this group.
+		/**
+		 * Gets all parent groups of this group since it is easy and this is not done often.
+		 * In principle, the Redux state for groups has this information. However, due to the
+		 * recursive nature of groups, it is a harder to get it from that state so the
+		 * code simply uses the existing server/DB functions to do this.
+		 * Redux does not always think the state has changed so it may use stale state, esp. after a delete.
+		 * Thus, the fetch is forced to avoid this.
+		 */
 		const { data: parentGroupIDs = [] } = await store.dispatch(
 			groupsApi.endpoints.getParentIDs.initiate(groupState.id, {
 				subscribe: false,
