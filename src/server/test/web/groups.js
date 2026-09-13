191bab3534325f591c87ffbe0bbeff74c89c60a2
Nespina24 <nathane3504@gmail.com>
2026-09-12 22:34:47 -0700

    Separate admin and non-admin roles in create/edit endpoints

--- src/server/test/web/groups.js
+++ src/server/test/web/groups.js
@@ -95,168 +95,199 @@ mocha.describe('groups API', () => {
 	});
 
 	mocha.describe('modification API', async () => {
-		let token;
-		// Since this .before is in the middle of tests, it should not have issues as
-		// documented in usersTest.js.
-		mocha.before(async () => {
-			// login
-			let res = await chai.request(app).post('/api/loginLogout/login')
-				.send({ username: testUser.username, password: testUser.password });
-			token = res.body.token;
-		});
-		mocha.after(async () => {
-			// logout
-			if (token) {
-				await chai.request(app).post('/api/loginLogout/logout')
-					.set('token', token);
-			}
-		});
-		
 		mocha.describe('create endpoint', () => {
-
-			mocha.it('rejects all requests with an invalid token with 401', async () => {
-				const res = await chai.request(app).post('/api/groups/create').set('token', token + 'nope').type('json').send({});
-				expect(res).to.have.status(HTTP_CODES.UNAUTHORIZED);
-			});
-			mocha.describe('properly process roles:', () => {
-				for (const role of Object.keys(User.role)) {
-					const isAdmin = User.role[role] === User.role.ADMIN;
-					const message = `should ${isAdmin ? 'accept' : 'reject'} requests from ${role}`;
-					// Response status code should be 403 if improper role, but 400 if proper role, but improper user input.
-					const expectedResponseStatus = isAdmin ? HTTP_CODES.BAD_REQUEST : HTTP_CODES.FORBIDDEN;
-					mocha.it(message, async () => {
-						let currentToken;
-						let res;
-						// insert test user
-						const conn = testDB.getConnection();
-						const password = 'password';
-						const hashedPassword = await bcrypt.hash(password, 10);
-						const unauthorizedUser = new User(undefined, `${role}@example.com`, hashedPassword, User.role[role]);
-						await unauthorizedUser.insert(conn);
-						unauthorizedUser.password = password;
-
-						// login
-						res = await chai.request(app).post('/api/loginLogout/login')
-							.send({ username: unauthorizedUser.username, password: unauthorizedUser.password });
-						currentToken = res.body.token;
-						// create
-						res = await chai.request(app).post('/api/groups/create').set('token', currentToken);
-						expect(res).to.have.status(expectedResponseStatus);
-						// logout
-						if (currentToken) {
-							await chai.request(app).post('/api/loginLogout/logout')
-								.set('token', currentToken);
-						}
-					});
-
-				}
-			});
-			mocha.it('creates new groups when given valid parameters', async () => {
-				// Create a duplicate of Group C with a different name.
-				const res = await chai.request(app).post('/api/groups/create').type('json').set('token', token).send({
-					name: 'D',
-					displayable: false,
-					childGroups: [groupB.id],
-					childMeters: [meterA.id]
+			mocha.describe('Admin role: ', () => {
+				let token;
+				mocha.beforeEach(async () => {
+					// login
+					let res = await chai.request(app).post('/api/loginLogout/login')
+						.send({ username: testUser.username, password: testUser.password });
+					token = res.body.token;
 				});
-				expect(res).to.have.status(HTTP_CODES.OK);
+				mocha.afterEach(async () => {
+					// logout
+					if (token) {
+						await chai.request(app).post('/api/loginLogout/logout')
+							.set('token', token);
+					}
+				});
 
-				// Get the results of the API call.
-				const res2 = await chai.request(app).get('/api/groups').set('token', token);
-				expect(res2).to.have.status(HTTP_CODES.OK);
-				expect(res2).to.be.json;
-				expect(res2.body).to.have.lengthOf(4);
+				mocha.it('rejects all requests with an invalid token with 401', async () => {
+					const res = await chai.request(app).post('/api/groups/create').set('token', token + 'nope').type('json').send({});
+					expect(res).to.have.status(HTTP_CODES.UNAUTHORIZED);
+				});
 
-				// These are the meters the beforeEach creates.
-				let priorMeters = new Set([groupA.id, groupB.id, groupC.id]);
-				// These are the IDs of the groups created by the API call.
-				let newGroups = res2.body.map(group => group.id).filter(x => !priorMeters.has(x));
-				expect(newGroups).to.have.lengthOf(1);
+				mocha.it('should accept requests from Admin role', async () => {
+					// Response status code should be 400 if proper role, but improper user input.
+					let res = await chai.request(app).post('/api/groups/create').set('token', token);
+					expect(res).to.have.status(HTTP_CODES.BAD_REQUEST);
+				});
+
+				mocha.it('creates new groups when given valid parameters', async () => {
+					// Create a duplicate of Group C with a different name.
+					const res = await chai.request(app).post('/api/groups/create').type('json').set('token', token).send({
+						name: 'D',
+						displayable: false,
+						childGroups: [groupB.id],
+						childMeters: [meterA.id]
+					});
+					expect(res).to.have.status(HTTP_CODES.OK);
+
+					// Get the results of the API call.
+					const res2 = await chai.request(app).get('/api/groups').set('token', token);
+					expect(res2).to.have.status(HTTP_CODES.OK);
+					expect(res2).to.be.json;
+					expect(res2.body).to.have.lengthOf(4);
+
+					// These are the meters the beforeEach creates.
+					let priorMeters = new Set([groupA.id, groupB.id, groupC.id]);
+					// These are the IDs of the groups created by the API call.
+					let newGroups = res2.body.map(group => group.id).filter(x => !priorMeters.has(x));
+					expect(newGroups).to.have.lengthOf(1);
+				});
+			});
+			mocha.describe('Non-Admin roles: ', () => {
+				for (const role of Object.keys(User.role)) {
+					// Response status code should be 403 if improper role.
+					if (User.role[role] !== User.role.ADMIN) {
+						let token;
+						mocha.beforeEach(async () => {
+							// insert test user
+							const conn = testDB.getConnection();
+							const password = 'password';
+							const hashedPassword = await bcrypt.hash(password, 10);
+							const unauthorizedUser = new User(undefined, `${role}@example.com`, hashedPassword, User.role[role]);
+							await unauthorizedUser.insert(conn);
+							unauthorizedUser.password = password;
+
+							// login
+							let res = await chai.request(app).post('/api/loginLogout/login')
+								.send({ username: unauthorizedUser.username, password: unauthorizedUser.password });
+							token = res.body.token;
+						});
+						mocha.afterEach(async () => {
+							// logout
+							if (token) {
+								await chai.request(app).post('/api/loginLogout/logout')
+									.set('token', token);
+							}
+						});
+
+						mocha.it(`should reject requests from ${role}`, async () => {
+							// create
+							res = await chai.request(app).post('/api/groups/create').set('token', token);
+							expect(res).to.have.status(HTTP_CODES.FORBIDDEN);
+						});
+					}
+				}
 			});
 		});
 		mocha.describe('edit endpoint', () => {
+			mocha.describe('Admin role: ', () => {
+				let token;
+				mocha.beforeEach(async () => {
+					// login
+					let res = await chai.request(app).post('/api/loginLogout/login')
+						.send({ username: testUser.username, password: testUser.password });
+					token = res.body.token;
+				});
+				mocha.afterEach(async () => {
+					// logout
+					if (token) {
+						await chai.request(app).post('/api/loginLogout/logout')
+							.set('token', token);
+					}
+				});
 
-			mocha.it('rejects all requests with an invalid token with 401', async () => {
-				const res = await chai.request(app).put('/api/groups/edit').set('token', token + 'nope').type('json').send({});
-				expect(res).to.have.status(HTTP_CODES.UNAUTHORIZED);
-			});
-			mocha.describe('properly process roles', () => {
-				for (const role of Object.keys(User.role)) {
-					const isAdmin = User.role[role] === User.role.ADMIN;
-					const message = `should ${isAdmin ? 'accept' : 'reject'} requests from ${role}`;
-					// Response status code should be 403 if improper role, but 400 if proper role, but improper user input.
-					const expectedResponseStatus = isAdmin ? HTTP_CODES.BAD_REQUEST : HTTP_CODES.FORBIDDEN;
-					mocha.it(message, async () => {
-						let currentToken;
-						let res;
-						// insert test user
-						const conn = testDB.getConnection();
-						const password = 'password';
-						const hashedPassword = await bcrypt.hash(password, 10);
-						const unauthorizedUser = new User(undefined, `${role}@example.com`, hashedPassword, User.role[role]);
-						await unauthorizedUser.insert(conn);
-						unauthorizedUser.password = password;
-
-						// login
-						res = await chai.request(app).post('/api/loginLogout/login')
-							.send({ username: unauthorizedUser.username, password: unauthorizedUser.password });
-						currentToken = res.body.token;
-						// edit
-						res = await chai.request(app).put('/api/groups/edit').set('token', currentToken);
-						expect(res).to.have.status(expectedResponseStatus);
-						// logout
-						if (currentToken) {
-							await chai.request(app).post('/api/loginLogout/logout')
-								.set('token', currentToken);
-						}
+				mocha.it('rejects all requests with an invalid token with 401', async () => {
+					const res = await chai.request(app).put('/api/groups/edit').set('token', token + 'nope').type('json').send({});
+					expect(res).to.have.status(HTTP_CODES.UNAUTHORIZED);
+				});
+				mocha.it('should accept requests from Admin role', async () => {
+					// Response status code should be 400 if proper role, but improper user input.
+					let res = await chai.request(app).put('/api/groups/edit').set('token', token);
+					expect(res).to.have.status(HTTP_CODES.BAD_REQUEST);
+				});
+				mocha.it('allows adding a new child meter to a group', async () => {
+					const conn = testDB.getConnection();
+					// Several meter values not set but not needed so defaults fine.
+					const meterD = new Meter(undefined, 'D', null, false, true, Meter.type.MAMAC, null, gpsPoint,
+						'Identified D', 'notes D', 33.5, true, true, '05:05:09', '09:00:01', '0',
+						0, 1, 'increasing', false, 25.5, '0001-01-01 23:59:59', '2020-07-02 01:00:10');
+					await meterD.insert(conn);
+					let res = await chai.request(app).put('/api/groups/edit').set('token', token).type('json').send({
+						id: groupC.id,
+						name: groupC.name,
+						displayable: groupC.displayable,
+						childGroups: [],
+						childMeters: [meterC.id, meterD.id]
 					});
+					expect(res).to.have.status(HTTP_CODES.OK);
 
+					res = await chai.request(app).get(`/api/groups/children/${groupC.id}`);
+					expect(res).to.have.status(HTTP_CODES.OK);
+					expect(res).to.be.json;
+					expect(res.body).to.have.property('meters');
+					expect(res.body).to.have.property('groups');
+					expect(res.body.meters).to.have.a.lengthOf(2).and.include.members([meterC.id, meterD.id]);
+					expect(res.body.groups).to.have.a.lengthOf(0);
+				});
+				mocha.it('allows adding a new child group to a group', async () => {
+					const conn = testDB.getConnection();
+					const groupD = new Group(undefined, 'D', true, gpsPoint, 'notes 2', 43.5);
+					await groupD.insert(conn);
+					let res = await chai.request(app).put('/api/groups/edit').set('token', token).type('json').send({
+						id: groupC.id,
+						name: groupC.name,
+						displayable: groupC.displayable,
+						childGroups: [groupD.id],
+						childMeters: [meterC.id]
+					});
+					expect(res).to.have.status(HTTP_CODES.OK);
+
+					res = await chai.request(app).get(`/api/groups/children/${groupC.id}`);
+					expect(res).to.have.status(HTTP_CODES.OK);
+					expect(res).to.be.json;
+					expect(res.body).to.have.property('meters');
+					expect(res.body).to.have.property('groups');
+					expect(res.body.meters).to.have.a.lengthOf(1).and.include.members([meterC.id]);
+					expect(res.body.groups).to.have.a.lengthOf(1).and.include(groupD.id);
+				});
+			});
+			mocha.describe('Non-Admin roles', () => {
+				for (const role of Object.keys(User.role)) {
+					// Response status code should be 403 if improper role.
+					if (User.role[role] !== User.role.ADMIN) {
+						let token;
+						mocha.beforeEach(async () => {
+							// insert test user
+							const conn = testDB.getConnection();
+							const password = 'password';
+							const hashedPassword = await bcrypt.hash(password, 10);
+							const unauthorizedUser = new User(undefined, `${role}@example.com`, hashedPassword, User.role[role]);
+							await unauthorizedUser.insert(conn);
+							unauthorizedUser.password = password;
+
+							// login
+							let res = await chai.request(app).post('/api/loginLogout/login')
+								.send({ username: unauthorizedUser.username, password: unauthorizedUser.password });
+							token = res.body.token;
+						});
+						mocha.afterEach(async () => {
+							// logout
+							if (token) {
+								await chai.request(app).post('/api/loginLogout/logout')
+									.set('token', token);
+							}
+						});
+
+						mocha.it(`should reject requests from ${role}`, async () => {
+							// edit
+							res = await chai.request(app).put('/api/groups/edit').set('token', token);
+							expect(res).to.have.status(HTTP_CODES.FORBIDDEN);
+						});
+					}
 				}
-			});
-			mocha.it('allows adding a new child meter to a group', async () => {
-				const conn = testDB.getConnection();
-				// Several meter values not set but not needed so defaults fine.
-				const meterD = new Meter(undefined, 'D', null, false, true, Meter.type.MAMAC, null, gpsPoint,
-					'Identified D', 'notes D', 33.5, true, true, '05:05:09', '09:00:01', '0',
-					0, 1, 'increasing', false, 25.5, '0001-01-01 23:59:59', '2020-07-02 01:00:10');
-				await meterD.insert(conn);
-				let res = await chai.request(app).put('/api/groups/edit').set('token', token).type('json').send({
-					id: groupC.id,
-					name: groupC.name,
-					displayable: groupC.displayable,
-					childGroups: [],
-					childMeters: [meterC.id, meterD.id]
-				});
-				expect(res).to.have.status(HTTP_CODES.OK);
-
-				res = await chai.request(app).get(`/api/groups/children/${groupC.id}`);
-				expect(res).to.have.status(HTTP_CODES.OK);
-				expect(res).to.be.json;
-				expect(res.body).to.have.property('meters');
-				expect(res.body).to.have.property('groups');
-				expect(res.body.meters).to.have.a.lengthOf(2).and.include.members([meterC.id, meterD.id]);
-				expect(res.body.groups).to.have.a.lengthOf(0);
-			});
-			mocha.it('allows adding a new child group to a group', async () => {
-				const conn = testDB.getConnection();
-				const groupD = new Group(undefined, 'D', true, gpsPoint, 'notes 2', 43.5);
-				await groupD.insert(conn);
-				let res = await chai.request(app).put('/api/groups/edit').set('token', token).type('json').send({
-					id: groupC.id,
-					name: groupC.name,
-					displayable: groupC.displayable,
-					childGroups: [groupD.id],
-					childMeters: [meterC.id]
-				});
-				expect(res).to.have.status(HTTP_CODES.OK);
-
-				res = await chai.request(app).get(`/api/groups/children/${groupC.id}`);
-				expect(res).to.have.status(HTTP_CODES.OK);
-				expect(res).to.be.json;
-				expect(res.body).to.have.property('meters');
-				expect(res.body).to.have.property('groups');
-				expect(res.body.meters).to.have.a.lengthOf(1).and.include.members([meterC.id]);
-				expect(res.body.groups).to.have.a.lengthOf(1).and.include(groupD.id);
 			});
 		});
 	});
